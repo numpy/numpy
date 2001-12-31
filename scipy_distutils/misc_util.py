b1263a71669aad6b9ac5db8ce961a18c204e925f
Eric Jones <eric@enthought.com>
2001-12-31 08:40:02 +0000

    fixed path issues resulting from using setup_xxx.configuration() methods to grab information about package for use in setup.py.  the configuration() methods use absolute path names to identify files.  This is bad for sdist which uses the relative path of a file to determine which directory it goes into in the source distribution.  To solve the problem, I have sdist search all absolute file paths for the "greatest common directory" among them.  This directory is removed from them to determine where the file will go in the distribution directories.  Files without absolute paths are always put in the top directory of the distribution.
    This procedure usually ends up with a setup.py module in the top directory of the source distribution and then a number of sub-directories that hold the actual package source code files.
    
    misc_util sprouted another function or two to help with the process of adding package paths to the PYTHONPATH.  There are some assumptions made about where distutils should look for these packages (first in the current directory then in the parent) that may not be generally true, but should work well for scipy.  It also results in setup.py files that work fine from with in scipy or for building stand along packages that may depend on other scipy packages.

--- scipy_distutils/misc_util.py
+++ scipy_distutils/misc_util.py
@@ -27,6 +27,34 @@ def add_grandparent_to_path(mod_name):
 def restore_path():
     del sys.path[0]
 
+def append_package_dir_to_path(package_name):           
+    """ Search for a directory with package_name and append it to PYTHONPATH
+        
+        The local directory is searched first and then the parent directory.
+    """
+    # first see if it is in the current path
+    # then try parent.  If it isn't found, fail silently
+    # and let the import error occur.
+    
+    # not an easy way to clean up after this...
+    import os,sys
+    if os.path.exists(package_name):
+        sys.path.append(package_name)
+    elif os.path.exists(os.path.join('..',package_name)):
+        sys.path.append(os.path.join('..',package_name))
+
+def get_package_config(package_name):
+    """ grab the configuration info from the setup_xxx.py file
+        in a package directory.  The package directory is searched
+        from the current directory, so setting the path to the
+        setup.py file directory of the file calling this is usually
+        needed to get search the path correct.
+    """
+    append_package_dir_to_path(package_name)
+    mod = __import__('setup_'+package_name)
+    config = mod.configuration()
+    return config
+        
 list_keys = ['packages', 'ext_modules', 'data_files',
              'include_dirs', 'libraries', 'fortran_libraries',
                  'headers']
