e8c50dc6676e380e814c77927445ab3594382bc4
nobody <nobody@localhost>
2002-01-05 17:07:10 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_2_0'.

--- scipy_distutils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""scipy_distutils
-
-   Modified version of distutils to handle fortran source code, f2py,
-   and other issues in the scipy build process.
-"""
-
-# Need to do something here to get distutils subsumed...
--- scipy_distutils/__version__.py
+++
@@ -1,4 +0,0 @@
-# This file is automatically updated with get_version
-# function from scipy_distutils.misc_utils.py
-version = '0.1.19-alpha-47'
-version_info = (0, 1, 19, 'alpha', 47)
