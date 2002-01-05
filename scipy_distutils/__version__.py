fd3855d4fecb6a962a752c8e0f5d51eb34400a5e
Pearu Peterson <pearu.peterson@gmail.com>
2002-01-05 12:03:04 +0000

    refined get_version

--- scipy_distutils/__version__.py
+++ scipy_distutils/__version__.py
@@ -1,4 +1,4 @@
 # This file is automatically updated with get_version
 # function from scipy_distutils.misc_utils.py
-version = '0.1.19-alpha-45'
-version_info = (0, 1, 19, 'alpha', 45)
+version = '0.1.19-alpha-47'
+version_info = (0, 1, 19, 'alpha', 47)
