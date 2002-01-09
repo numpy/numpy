830fc21645c00b53518609d68feec014605d3595
Pearu Peterson <pearu.peterson@gmail.com>
2002-01-09 14:21:53 +0000

    version update

--- scipy_distutils/__version__.py
+++ scipy_distutils/__version__.py
@@ -1,4 +1,4 @@
 # This file is automatically updated with get_version
 # function from scipy_distutils.misc_utils.py
-version = '0.5.21-alpha-56'
-version_info = (0, 5, 21, 'alpha', 56)
+version = '0.6.23-alpha-70'
+version_info = (0, 6, 23, 'alpha', 70)
