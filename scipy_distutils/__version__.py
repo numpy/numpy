002fe282029c495c30116026a2171343003874eb
Travis Oliphant <oliphant@enthought.com>
2002-01-10 00:34:07 +0000

    Bug in fft2 and Mplot fixed.

--- scipy_distutils/__version__.py
+++ scipy_distutils/__version__.py
@@ -1,4 +1,2 @@
-# This file is automatically updated with update_version
-# function from scipy_distutils.misc_utils.py
 version = '0.6.23-alpha-81'
 version_info = (0, 6, 23, 'alpha', 81)
--- scipy_distutils/atlas_info.py
+++ scipy_distutils/atlas_info.py
@@ -1,4 +1,5 @@
 import sys, os
+from misc_util import get_path
 
 library_path = ''
 
