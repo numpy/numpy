79501722383e037a4e4a10d009c5ed338eb91e93
Pearu Peterson <pearu.peterson@gmail.com>
2002-01-13 11:57:54 +0000

    Added err_msg keyword argument to assert_array_equal

--- scipy_test/scipy_test.py
+++ scipy_test/scipy_test.py
@@ -238,21 +238,28 @@ try:
     from Numeric import *
     from fastumath import *
     
-    def assert_array_equal(x,y):
+    def assert_array_equal(x,y,err_msg=''):
+        msg = '\nArrays are not equal'
         try:
-            assert(alltrue(equal(shape(x),shape(y))))
+            assert alltrue(equal(shape(x),shape(y))),\
+                   msg + ' (shapes mismatch):\n\t' + err_msg
             reduced = equal(x,y)
-            assert(alltrue(ravel(reduced)))
+            assert alltrue(ravel(reduced)),\
+                   msg + ':\n\t' + err_msg
         except ValueError:
             print shape(x),shape(y)
             raise ValueError, 'arrays are not equal'
     
-    def assert_array_almost_equal(x,y,decimal=6):
+    def assert_array_almost_equal(x,y,decimal=6,err_msg=''):
+        msg = '\nArrays are not almost equal'
         try:
-            assert(alltrue(equal(shape(x),shape(y))))
-            reduced = equal(around(abs(x-y),decimal))
-            assert(alltrue(ravel(reduced)))
+            assert alltrue(equal(shape(x),shape(y))),\
+                   msg + ' (shapes mismatch):\n\t' + err_msg
+            reduced = equal(around(abs(x-y),decimal),0)
+            assert alltrue(ravel(reduced)),\
+                   msg + ':\n\t' + err_msg
         except ValueError:
+            print sys.exc_value
             print shape(x),shape(y)
             print x, y
             raise ValueError, 'arrays are not almost equal'
