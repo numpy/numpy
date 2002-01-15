5472eedf02fd8647ad3dfc246c1a65388ceb907a
Eric Jones <eric@enthought.com>
2002-01-15 21:39:59 +0000

    added a couple of implementations using lists instead of Numeric arrays.  They are very slow.

--- weave/examples/ramp.py
+++ weave/examples/ramp.py
@@ -1,3 +1,20 @@
+""" Comparison of several different ways of calculating a "ramp"
+    function.
+    
+    C:\home\ej\wrk\junk\scipy\weave\examples>python ramp.py 
+    python (seconds*ratio): 128.149998188
+    arr[500]: 0.0500050005001
+    compiled numeric1 (seconds, speed up): 1.42199993134 90.1195530071
+    arr[500]: 0.0500050005001
+    compiled numeric2 (seconds, speed up): 0.950999975204 134.752893301
+    arr[500]: 0.0500050005001
+    compiled list1 (seconds, speed up): 53.100001812 2.41337088164
+    arr[500]: 0.0500050005001
+    compiled list4 (seconds, speed up): 30.5500030518 4.19476220578
+    arr[500]: 0.0500050005001     
+
+"""
+
 import time
 import weave
 from Numeric import *
@@ -27,6 +44,30 @@ def Ramp_numeric2(result,size,start,end):
            }
            """
     weave.inline(code,['result','size','start','end'],compiler='gcc')
+
+def Ramp_list1(result, size, start, end):
+    code = """
+           double step = (end-start)/(size-1);
+           int i;        
+           for (i = 0; i < size; i++) 
+           {
+               result[i] = Py::Float(start + step*i);
+           }
+           """
+    weave.inline(code, ["result", "size", "start", "end"], verbose=2)
+
+def Ramp_list2(result, size, start, end):
+    code = """
+           double step = (end-start)/(size-1);
+           int i;
+           PyObject* raw_result = result.ptr();
+           for (i = 0; i < size; i++) 
+           {
+               PyObject* val = PyFloat_FromDouble( start + step*i );
+               PySequence_SetItem(raw_result,i, val);
+           }
+           """
+    weave.inline(code, ["result", "size", "start", "end"], verbose=2)
           
 def main():
     N_array = 10000
@@ -44,29 +85,54 @@ def main():
     print 'python (seconds*ratio):', py_time
     print 'arr[500]:', arr[500]
     
-    arr = array([0]*N_array,Float64)
+    arr1 = array([0]*N_array,Float64)
     # First call compiles function or loads from cache.
     # I'm not including this in the timing.
-    Ramp_numeric1(arr, N_array, 0.0, 1.0)
+    Ramp_numeric1(arr1, N_array, 0.0, 1.0)
     t1 = time.time()
     for i in xrange(N_c):
-        Ramp_numeric1(arr, N_array, 0.0, 1.0)
+        Ramp_numeric1(arr1, N_array, 0.0, 1.0)
     t2 = time.time()
     c_time = (t2 - t1)
     print 'compiled numeric1 (seconds, speed up):', c_time, py_time/ c_time
-    print 'arr[500]:', arr[500]
+    print 'arr[500]:', arr1[500]
 
     arr2 = array([0]*N_array,Float64)
     # First call compiles function or loads from cache.
     # I'm not including this in the timing.
-    Ramp_numeric2(arr, N_array, 0.0, 1.0)
+    Ramp_numeric2(arr2, N_array, 0.0, 1.0)
     t1 = time.time()
     for i in xrange(N_c):
-        Ramp_numeric2(arr, N_array, 0.0, 1.0)
+        Ramp_numeric2(arr2, N_array, 0.0, 1.0)
     t2 = time.time()
     c_time = (t2 - t1)   
     print 'compiled numeric2 (seconds, speed up):', c_time, py_time/ c_time
-    print 'arr[500]:', arr[500]
+    print 'arr[500]:', arr2[500]
+
+    arr3 = [0]*N_array
+    # First call compiles function or loads from cache.
+    # I'm not including this in the timing.
+    Ramp_list1(arr3, N_array, 0.0, 1.0)
+    t1 = time.time()
+    for i in xrange(N_py):
+        Ramp_list1(arr3, N_array, 0.0, 1.0)
+    t2 = time.time()
+    c_time = (t2 - t1) * ratio  
+    print 'compiled list1 (seconds, speed up):', c_time, py_time/ c_time
+    print 'arr[500]:', arr3[500]
+    
+    arr4 = [0]*N_array
+    # First call compiles function or loads from cache.
+    # I'm not including this in the timing.
+    Ramp_list2(arr4, N_array, 0.0, 1.0)
+    t1 = time.time()
+    for i in xrange(N_py):
+        Ramp_list2(arr4, N_array, 0.0, 1.0)
+    t2 = time.time()
+    c_time = (t2 - t1) * ratio  
+    print 'compiled list4 (seconds, speed up):', c_time, py_time/ c_time
+    print 'arr[500]:', arr4[500]
+    
     
 if __name__ == '__main__':
     main()
