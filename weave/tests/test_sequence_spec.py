e03b4d87272bdd651612d91bd68b4d1d78b5a97e
Eric Jones <eric@enthought.com>
2002-01-22 20:37:01 +0000

    added a few tests to catch problems in sequence conversions

---
+++ weave/tests/test_sequence_spec.py
@@ -0,0 +1,36 @@
+import unittest
+
+from scipy_distutils.misc_util import add_grandparent_to_path, restore_path
+
+add_grandparent_to_path(__name__)
+import inline_tools
+restore_path()
+    
+class test_sequence_specification(unittest.TestCase):    
+    def check_convert_to_dict(self):
+        d = {}
+        inline_tools.inline("",['d']) 
+    def check_convert_to_list(self):        
+        l = []
+        inline_tools.inline("",['l']) 
+    def check_convert_to_string(self):        
+        s = 'hello'
+        inline_tools.inline("",['s']) 
+    def check_convert_to_tuple(self):        
+        t = ()
+        inline_tools.inline("",['t']) 
+        
+def test_suite():
+    suites = []   
+    suites.append( unittest.makeSuite(test_sequence_specification,'check_'))
+    total_suite = unittest.TestSuite(suites)
+    return total_suite
+
+def test():
+    all_tests = test_suite()
+    runner = unittest.TextTestRunner()
+    runner.run(all_tests)
+    return runner
+
+if __name__ == "__main__":
+    test()
