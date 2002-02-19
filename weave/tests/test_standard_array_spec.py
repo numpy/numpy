04891cf9a4a66f2c64391eda97ecf38fff9d35c9
Eric Jones <eric@enthought.com>
2002-02-19 09:01:03 +0000

    changed references to scipy.scipy_test to scipy_test.

--- weave/tests/test_standard_array_spec.py
+++ weave/tests/test_standard_array_spec.py
@@ -22,7 +22,7 @@ def remove_whitespace(in_str):
     return out
     
 def print_assert_equal(test_string,actual,desired):
-    """this should probably be in scipy.scipy_test
+    """this should probably be in scipy_test
     """
     import pprint
     try:
