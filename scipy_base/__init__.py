2a66bd782adc3d7673d9afc3d52c78ed49362c4c
Eric Jones <eric@enthought.com>
2002-04-01 15:20:21 +0000

    changed limits.py so that its contents are no longer imported into scipy_base's namespace.

--- scipy_base/__init__.py
+++ scipy_base/__init__.py
@@ -11,5 +11,18 @@ from helpmod import help, source
 from Matrix import Matrix as mat
 Mat = mat  # deprecated
 
+#---- testing ----#
+
+def test(level=10):
+    import unittest
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
+    return runner
+
+def test_suite(level=1):
+    import scipy_base.testing
+    import scipy_base
+    this_mod = scipy_base
+    return scipy_base.testing.harvest_test_suites(this_mod,level=level)
 
 
