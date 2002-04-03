53b06c38badce57edc8d23d4ba8049cc5efc55df
Eric Jones <eric@enthought.com>
2002-04-03 14:20:51 +0000

    added import of rand

--- scipy_base/tests/test_index_tricks.py
+++ scipy_base/tests/test_index_tricks.py
@@ -1,5 +1,5 @@
 import unittest
-from scipy_base.testing import assert_array_equal, assert_equal
+from scipy_base.testing import assert_array_equal, assert_equal, rand
 from scipy_base.testing import assert_almost_equal, assert_array_almost_equal
 from scipy_base import *
 
