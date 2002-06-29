f711f4f03b8935c3c1812d1534b2c1ed46831865
Pearu Peterson <pearu.peterson@gmail.com>
2002-06-29 22:48:31 +0000

    Fixed asinh bug also in fatumath_unsigned.inc. Removed all compiler warnings.

--- scipy_base/fastumathmodule.c
+++ scipy_base/fastumathmodule.c
@@ -1,4 +1,3 @@
-
 #include "Python.h"
 #include "Numeric/arrayobject.h"
 #include "Numeric/ufuncobject.h"
@@ -6,15 +5,19 @@
 #include <math.h>
 #include "mconf_lite.h"
 
-/* Fast umath module whose functions do not check for range and domain errors.
+/* Fast umath module whose functions do not check for range and domain
+   errors.
 
    Replacement for umath + additions for isnan, isfinite, and isinf
-   Also allows comparison operations on complex numbers (just compares the real part)
-   and logical operations.
+   Also allows comparison operations on complex numbers (just compares
+   the real part) and logical operations.
 
    All logical operations return UBYTE arrays.
- */
+*/
 
+#if defined _GNU_SOURCE
+#define HAVE_INVERSE_HYPERBOLIC 1
+#endif
 
 /* Wrapper to include the correct version */
 
