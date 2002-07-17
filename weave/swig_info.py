e6e01392d8b4c678c0e7165626b002055e2f987f
Eric Jones <eric@enthought.com>
2002-07-17 02:48:32 +0000

    removed a debugging change I had made that specialized things to my machine.

--- weave/swig_info.py
+++ weave/swig_info.py
@@ -2,8 +2,7 @@ import base_info
 
 import os, swig_info
 local_dir,junk = os.path.split(os.path.abspath(swig_info.__file__))   
-#f = open(os.path.join(local_dir,'swig','swigptr.c'))
-f = open(os.path.join(r'c:\home\eric\wrk\scipy\weave','swig','swigptr.c'))
+f = open(os.path.join(local_dir,'swig','swigptr.c'))
 swig_support_code = f.read()
 f.close()
 
