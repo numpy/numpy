4ce790846079e3a252fa37ecc6d789dc282b012a
Pearu Peterson <pearu.peterson@gmail.com>
2002-09-20 22:25:53 +0000

    scipy_test can be build/installed separately

--- scipy_test/setup_scipy_test.py
+++ scipy_test/setup_scipy_test.py
@@ -1,14 +1,18 @@
+#!/usr/bin/env python
+
 import os
-from scipy_distutils.misc_util import get_path, default_config_dict
+from scipy_distutils.misc_util import get_path, default_config_dict,\
+     dot_join
 
 def configuration(parent_package=''):
-    parent_path = parent_package
-    if parent_package:
-        parent_package += '.'
+    package = 'scipy_test'
     local_path = get_path(__name__)
 
-    config = default_config_dict()
-    package = 'scipy_test'
-    config['packages'].append(parent_package+package)
+    config = default_config_dict(package,parent_package)
+    config['packages'].append(dot_join(parent_package,package))
     config['package_dir'][package] = local_path 
     return config
+
+if __name__ == '__main__':    
+    from scipy_distutils.core import setup
+    setup(**configuration())
