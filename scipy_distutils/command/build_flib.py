c50021a26d88185a2ae56f395a7362e488d3e7e2
Pearu Peterson <pearu.peterson@gmail.com>
2003-02-27 08:11:50 +0000

    Fixed typo.

--- scipy_distutils/command/build_flib.py
+++ scipy_distutils/command/build_flib.py
@@ -831,7 +831,7 @@ class sun_fortran_compiler(fortran_compiler_base):
     def get_linker_so(self):
         return [self.f90_compiler,'-Bdynamic','-G']
 
-class forte_fortran_compiler(sun_compiler_compiler):
+class forte_fortran_compiler(sun_fortran_compiler):
     vendor = 'Forte'
     ver_match = r'(f90|f95): Forte Developer 7 Fortran 95 (?P<version>[^\s]+).*'
 
