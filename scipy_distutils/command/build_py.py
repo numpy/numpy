694e8f63c9eb27fcdd246e408b2acdb7234ef764
Pearu Peterson <pearu.peterson@gmail.com>
2003-03-18 09:28:21 +0000

    Introduced .build_py_ignore hooks: If a .py file directory contains .build_py_ignore and its base name is listed in .build_py_ignore file, then this .py file is removed from the modules list of build_py command.
    This generalizes "avoiding setup*.py from installation" hook to
    arbitrary files (e.g. linalg, freetype etc contain .py files
    that are not meant to be installed) that was also broken: it worked
    only when installing packages as standalone.

--- scipy_distutils/command/build_py.py
+++ scipy_distutils/command/build_py.py
@@ -1,16 +1,32 @@
-from distutils.command.build_py import *
+import os
+from glob import glob
+#from distutils.command.build_py import *
 from distutils.command.build_py import build_py as old_build_py
 from fnmatch import fnmatch
 
 def is_setup_script(file):
     file = os.path.basename(file)
     return fnmatch(file,"setup.py")
-#    return (fnmatch(file,"setup.py") or fnmatch(file,"setup_*.py"))
-    
+    #return (fnmatch(file,"setup.py") or fnmatch(file,"setup_*.py"))
+
+def in_build_py_ignore(file, _cache={}):
+    base,file = os.path.split(file)
+    ignore_list = _cache.get(base)
+    if ignore_list is None:
+        ignore_list = []
+        fn = os.path.join(base,'.build_py_ignore')
+        if os.path.isfile(fn):
+            f = open(fn,'r')
+            ignore_list = [x for x in f.read().split('\n') if x]
+            f.close()
+        _cache[base] = ignore_list
+    return file in ignore_list
+
 class build_py(old_build_py):
 
     def find_package_modules (self, package, package_dir):
-        # we filter all files that are setup.py or setup_xxx.py        
+        # we filter all files that are setup.py or setup_xxx.py
+        # or listed in .build_py_ignore file of files base directory.
         self.check_package(package, package_dir)
         module_files = glob(os.path.join(package_dir, "*.py"))
         modules = []
@@ -18,9 +34,10 @@ class build_py(old_build_py):
 
         for f in module_files:
             abs_f = os.path.abspath(f)
-            if abs_f != setup_script and not is_setup_script(f):
+            if not in_build_py_ignore(abs_f) \
+               and abs_f != setup_script and not is_setup_script(f):
                 module = os.path.splitext(os.path.basename(f))[0]
                 modules.append((package, module, f))
             else:
-                self.debug_print("excluding %s" % setup_script)
+                self.debug_print("excluding %s" % f)
         return modules
