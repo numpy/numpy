b04d9606250f384bce67acd98c9accb20dbeb7fa
Pearu Peterson <pearu.peterson@gmail.com>
2004-02-02 11:10:37 +0000

    Workaround for missing __cvs_version__ which is available only in MAIN branch

--- scipy_distutils/scipy_distutils_version.py
+++ scipy_distutils/scipy_distutils_version.py
@@ -3,9 +3,14 @@ minor = 2
 micro = 2
 release_level = 'alpha'
 
-from __cvs_version__ import cvs_version
-cvs_minor = cvs_version[-3]
-cvs_serial = cvs_version[-1]
+try:
+    from __cvs_version__ import cvs_version
+    cvs_minor = cvs_version[-3]
+    cvs_serial = cvs_version[-1]
+except ImportError,msg:
+    print msg
+    cvs_minor = 0
+    cvs_serial = 0
 
 scipy_distutils_version = '%(major)d.%(minor)d.%(micro)d_%(release_level)s'\
                           '_%(cvs_minor)d.%(cvs_serial)d' % (locals ())
