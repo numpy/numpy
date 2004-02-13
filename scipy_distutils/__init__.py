c74322b996d922f02bcc51d52a0d23083da3f072
Pearu Peterson <pearu.peterson@gmail.com>
2004-02-13 15:44:35 +0000

    Disableing scipy_distutils hooks in case distutils has been imported before, otherwise I cannot predict what will happen..

--- scipy_distutils/__init__.py
+++ scipy_distutils/__init__.py
@@ -10,13 +10,14 @@ from scipy_distutils_version import scipy_distutils_version as __version__
 
 import sys
 
-# Replace distutils.ccompiler with scipy_distutils.ccompiler
-assert not sys.modules.has_key('distutils.ccompiler'),\
-       'distutils has been imported before scipy_distutils'
-import ccompiler
-sys.modules['distutils.ccompiler'] = ccompiler
-
-assert not sys.modules.has_key('distutils.unixccompiler'),\
-       'cannot override distutils.unixccompiler'
-import unixccompiler
-sys.modules['distutils.unixccompiler'] = unixccompiler
+if not sys.modules.has_key('distutils'):
+    # Replace distutils.ccompiler with scipy_distutils.ccompiler
+    assert not sys.modules.has_key('distutils.ccompiler'),\
+           'distutils has been imported before scipy_distutils'
+    import ccompiler
+    sys.modules['distutils.ccompiler'] = ccompiler
+    
+    assert not sys.modules.has_key('distutils.unixccompiler'),\
+           'cannot override distutils.unixccompiler'
+    import unixccompiler
+    sys.modules['distutils.unixccompiler'] = unixccompiler
