9d206bd711194eb5cdc60a5acaa4017c7d67f3e3
Pearu Peterson <pearu.peterson@gmail.com>
2004-02-18 08:51:39 +0000

    Fixed linking flags for darwin

--- scipy_distutils/gnufcompiler.py
+++ scipy_distutils/gnufcompiler.py
@@ -48,7 +48,7 @@ class GnuFCompiler(FCompiler):
     def get_flags_linker_so(self):
         opt = []
         if sys.platform=='darwin':
-            opt.extend(['-framework','Python'])
+            opt.extend(["-Wl,-framework","-Wl,Python","-lcc_dynamic","-bundle"])
         else:
             opt.append("-shared")
         if sys.platform[:5]=='sunos':
