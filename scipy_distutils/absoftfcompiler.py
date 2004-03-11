4d2ead9b70e451408cb184f19c692814ec4781ca
Travis Oliphant <oliphant@enthought.com>
2004-03-11 21:54:41 +0000

    Added compiler flag for versions later than Absoft 6.2 so that compiler will free memory on encountering a dealloc.

--- scipy_distutils/absoftfcompiler.py
+++ scipy_distutils/absoftfcompiler.py
@@ -71,6 +71,9 @@ class AbsoftFCompiler(FCompiler):
         opt = FCompiler.get_flags_f90(self)
         opt.extend(["-YCFRL=1","-YCOM_NAMES=LCS","-YCOM_PFX","-YEXT_PFX",
                     "-YCOM_SFX=_","-YEXT_SFX=_","-YEXT_NAMES=LCS"])
+        if self.get_version():
+            if self.get_version()>'4.6':
+                opt.extend(["-YDEALLOC=ALL"])                
         return opt
 
     def get_flags_fix(self):
