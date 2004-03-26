3743e36458bcd3750e9edaba192e891633e2e1d6
Pearu Peterson <pearu.peterson@gmail.com>
2004-03-26 18:52:12 +0000

    Added support to Sun WorkShop 6 F95 compiler (untested though)

--- scipy_distutils/sunfcompiler.py
+++ scipy_distutils/sunfcompiler.py
@@ -7,11 +7,11 @@ from fcompiler import FCompiler
 class SunFCompiler(FCompiler):
 
     compiler_type = 'sun'
-    version_pattern = r'(f90|f95): (Sun|Forte Developer 7) Fortran 95 (?P<version>[^\s]+).*'
+    version_pattern = r'(f90|f95): (Sun|Forte Developer 7|WorkShop 6 update \d+) Fortran 95 (?P<version>[^\s]+).*'
 
     executables = {
         'version_cmd'  : ["f90", "-V"],
-        'compiler_f77' : ["f90", "-f77", "-ftrap=%none"],
+        'compiler_f77' : ["f90"],
         'compiler_fix' : ["f90", "-fixed"],
         'compiler_f90' : ["f90"],
         'linker_so'    : ["f90","-Bdynamic","-G"],
@@ -22,6 +22,13 @@ class SunFCompiler(FCompiler):
     module_include_switch = '-M'
     pic_flags = ['-xcode=pic32']
 
+    def get_flags_f77(self):
+        ret = ["-ftrap=%none"]
+        if (self.get_version() or '') >= '7':
+            ret.append("-f77")
+        else:
+            ret.append("-fixed")
+        return ret
     def get_opt(self):
         return ['-fast','-dalign']
     def get_arch(self):
