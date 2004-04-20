64f222c4b80a059ce33c371d540bda6e52aa4b98
prabhu <prabhu@localhost>
2004-04-20 09:00:55 +0000

    NEW: New modules to enable the use of SWIG2 (SWIG >= 1.3) wrapped code with weave.

---
+++ weave/swig2_spec.py
@@ -0,0 +1,181 @@
+"""
+This module allows one to use SWIG2 (SWIG version >= 1.3) wrapped
+objects from Weave.  SWIG-1.3 wraps objects differently from SWIG-1.1.
+
+The code here is based on wx_spec.py.  However, this module is more
+like a template for any SWIG2 wrapped converter.  To wrap specific
+code that uses SWIG the user simply needs to override the defaults in
+the swig2_converter class.
+
+By default this code assumes that the user will not link with the SWIG
+runtime library (libswigpy under *nix).  In this case no type checking
+will be performed by SWIG.
+
+To turn on type checking and link with the SWIG runtime library, there
+are two approaches.
+
+ 1. If you are writing a customized converter based on this code then
+    in the overloaded init_info, just call swig2_converter.init_info
+    with runtime=1 and add the swig runtime library to the libraries
+    loaded.
+
+ 2. If you are using the default swig2_converter you need to add two
+    keyword arguments to your weave.inline call:
+
+     a. Add a define_macros=[('SWIG_NOINCLUDE', None)]
+
+     b. Add the swigpy library to the libraries like so:
+        libraries=['swigpy']
+
+Prabhu Ramachandran <prabhu@aero.iitm.ernet.in>
+"""
+
+import common_info
+from c_spec import common_base_converter
+import converters
+import swigptr2
+
+#----------------------------------------------------------------------
+# This code obtains the C++ pointer given a a SWIG2 wrapped C++ object
+# in Python.
+#----------------------------------------------------------------------
+
+swig2_py_to_c_template = \
+"""
+class %(type_name)s_handler
+{
+public:    
+    %(c_type)s convert_to_%(type_name)s(PyObject* py_obj, const char* name)
+    {
+        %(c_type)s c_ptr;
+        swig_type_info *ty = SWIG_TypeQuery("%(c_type)s");
+        // work on this error reporting...
+        if (SWIG_ConvertPtr(py_obj, (void **) &c_ptr, ty,
+            SWIG_POINTER_EXCEPTION | 0) == -1) {
+            handle_conversion_error(py_obj,"%(type_name)s", name);
+        }
+        %(inc_ref_count)s
+        return c_ptr;
+    }
+    
+    %(c_type)s py_to_%(type_name)s(PyObject* py_obj,const char* name)
+    {
+        %(c_type)s c_ptr;
+        swig_type_info *ty = SWIG_TypeQuery("%(c_type)s");
+        // work on this error reporting...
+        if (SWIG_ConvertPtr(py_obj, (void **) &c_ptr, ty,
+            SWIG_POINTER_EXCEPTION | 0) == -1) {
+            handle_bad_type(py_obj,"%(type_name)s", name);
+        }
+        %(inc_ref_count)s
+        return c_ptr;
+    }
+};
+
+%(type_name)s_handler x__%(type_name)s_handler = %(type_name)s_handler();
+#define convert_to_%(type_name)s(py_obj,name) \\
+        x__%(type_name)s_handler.convert_to_%(type_name)s(py_obj,name)
+#define py_to_%(type_name)s(py_obj,name) \\
+        x__%(type_name)s_handler.py_to_%(type_name)s(py_obj,name)
+
+"""
+
+#----------------------------------------------------------------------
+# This code generates a new SWIG pointer object given a C++ pointer.
+#
+# Important note: The thisown flag of the returned object is set to 0
+# by default.
+#----------------------------------------------------------------------
+
+swig2_c_to_py_template = """
+PyObject* %(type_name)s_to_py(void *obj)
+{
+    swig_type_info *ty = SWIG_TypeQuery("%(c_type)s");
+    return SWIG_NewPointerObj(obj, ty, 0);
+}
+"""
+
+class swig2_converter(common_base_converter):
+    """ A converter for SWIG >= 1.3 wrapped objects."""
+    def __init__(self,class_name="undefined"):
+        self.class_name = class_name
+        common_base_converter.__init__(self)
+
+    def init_info(self, runtime=0):
+        """Keyword arguments:
+        
+          runtime -- If false (default), the user does not need to
+          link to the swig runtime (libswipy).  In this case no SWIG
+          type checking is performed.  If true, the user must link to
+          the swipy runtime library and in this case type checking
+          will be performed.  This option is useful when you derive a
+          subclass of this one for your object converters.          
+        """
+        common_base_converter.init_info(self)
+        # These are generated on the fly instead of defined at 
+        # the class level.
+        self.type_name = self.class_name
+        self.c_type = self.class_name + "*"
+        self.return_type = self.class_name + "*"
+        self.to_c_return = None # not used
+        self.check_func = None # not used
+
+        if runtime:
+            self.define_macros.append(("SWIG_NOINCLUDE", None))
+        self.support_code.append(swigptr2.swigptr2_code)
+    
+    def type_match(self,value):        
+        """ This is a generic type matcher for SWIG-1.3 objects.  For
+        specific instances, override this function."""
+        is_match = 0
+        try:
+            data = value.this.split('_')
+            if data[2] == 'p':
+                is_match = 1
+        except AttributeError:
+            pass
+        return is_match
+
+    def generate_build_info(self):
+        if self.class_name != "undefined":
+            res = common_base_converter.generate_build_info(self)
+        else:
+            # if there isn't a class_name, we don't want the
+            # support_code to be included
+            import base_info
+            res = base_info.base_info()
+        return res
+        
+    def py_to_c_code(self):
+        return swig2_py_to_c_template % self.template_vars()
+
+    def c_to_py_code(self):
+        return swig2_c_to_py_template % self.template_vars()
+                    
+    def type_spec(self,name,value):
+        """ This returns a generic type converter for SWIG-1.3
+        objects.  For specific instances, override this function if
+        necessary."""        
+        # factory
+        class_name = value.this.split('_')[-1]
+        new_spec = self.__class__(class_name)
+        new_spec.name = name        
+        return new_spec
+
+    def __cmp__(self,other):
+        #only works for equal
+        res = -1
+        try:
+            res = cmp(self.name,other.name) or \
+                  cmp(self.__class__, other.__class__) or \
+                  cmp(self.class_name, other.class_name) or \
+                  cmp(self.type_name,other.type_name)
+        except:
+            pass
+        return res
+
+#----------------------------------------------------------------------
+# Uncomment the next line if you want this to be a default converter
+# that is magically invoked by inline.
+#----------------------------------------------------------------------
+#converters.default.insert(0, swig2_converter())
---
+++ weave/swigptr2.py
@@ -0,0 +1,441 @@
+# This code allows one to use SWIG wrapped objects from weave.  This
+# code is specific to SWIG-1.3 and above where things are different.
+# The code is basically all copied out from the SWIG wrapper code but
+# it has been hand edited for brevity.
+#
+# Prabhu Ramachandran <prabhu@aero.iitm.ernet.in>
+
+swigptr2_code = """
+
+#include "Python.h"
+
+/*************************************************************** -*- c -*-
+ * python/precommon.swg
+ *
+ * Rename all exported symbols from common.swg, to avoid symbol
+ * clashes if multiple interpreters are included
+ *
+ ************************************************************************/
+
+#define SWIG_TypeCheck       SWIG_Python_TypeCheck
+#define SWIG_TypeCast        SWIG_Python_TypeCast
+#define SWIG_TypeName        SWIG_Python_TypeName
+#define SWIG_TypeQuery       SWIG_Python_TypeQuery
+#define SWIG_PackData        SWIG_Python_PackData 
+#define SWIG_UnpackData      SWIG_Python_UnpackData 
+
+
+/***********************************************************************
+ * common.swg
+ *
+ *     This file contains generic SWIG runtime support for pointer
+ *     type checking as well as a few commonly used macros to control
+ *     external linkage.
+ *
+ * Author : David Beazley (beazley@cs.uchicago.edu)
+ *
+ * Copyright (c) 1999-2000, The University of Chicago
+ * 
+ * This file may be freely redistributed without license or fee provided
+ * this copyright message remains intact.
+ ************************************************************************/
+
+#include <string.h>
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#  if defined(_MSC_VER) || defined(__GNUC__)
+#    if defined(STATIC_LINKED)
+#      define SWIGEXPORT(a) a
+#      define SWIGIMPORT(a) extern a
+#    else
+#      define SWIGEXPORT(a) __declspec(dllexport) a
+#      define SWIGIMPORT(a) extern a
+#    endif
+#  else
+#    if defined(__BORLANDC__)
+#      define SWIGEXPORT(a) a _export
+#      define SWIGIMPORT(a) a _export
+#    else
+#      define SWIGEXPORT(a) a
+#      define SWIGIMPORT(a) a
+#    endif
+#  endif
+#else
+#  define SWIGEXPORT(a) a
+#  define SWIGIMPORT(a) a
+#endif
+
+#ifdef SWIG_GLOBAL
+#  define SWIGRUNTIME(a) SWIGEXPORT(a)
+#else
+#  define SWIGRUNTIME(a) static a
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void *(*swig_converter_func)(void *);
+typedef struct swig_type_info *(*swig_dycast_func)(void **);
+
+typedef struct swig_type_info {
+  const char             *name;
+  swig_converter_func     converter;
+  const char             *str;
+  void                   *clientdata;
+  swig_dycast_func        dcast;
+  struct swig_type_info  *next;
+  struct swig_type_info  *prev;
+} swig_type_info;
+
+#ifdef SWIG_NOINCLUDE
+
+SWIGIMPORT(swig_type_info *) SWIG_TypeCheck(char *c, swig_type_info *);
+SWIGIMPORT(void *)           SWIG_TypeCast(swig_type_info *, void *);
+SWIGIMPORT(const char *)     SWIG_TypeName(const swig_type_info *);
+SWIGIMPORT(swig_type_info *) SWIG_TypeQuery(const char *);
+SWIGIMPORT(char *)           SWIG_PackData(char *, void *, int);
+SWIGIMPORT(char *)           SWIG_UnpackData(char *, void *, int);
+
+#else
+
+static swig_type_info *swig_type_list = 0;
+
+/* Check the typename */
+SWIGRUNTIME(swig_type_info *) 
+SWIG_TypeCheck(char *c, swig_type_info *ty) {
+  swig_type_info *s;
+  if (!ty) return 0;        /* Void pointer */
+  s = ty->next;             /* First element always just a name */
+  do {
+    if (strcmp(s->name,c) == 0) {
+      if (s == ty->next) return s;
+      /* Move s to the top of the linked list */
+      s->prev->next = s->next;
+      if (s->next) {
+        s->next->prev = s->prev;
+      }
+      /* Insert s as second element in the list */
+      s->next = ty->next;
+      if (ty->next) ty->next->prev = s;
+      ty->next = s;
+      s->prev = ty;
+      return s;
+    }
+    s = s->next;
+  } while (s && (s != ty->next));
+  return 0;
+}
+
+/* Cast a pointer up an inheritance hierarchy */
+SWIGRUNTIME(void *) 
+SWIG_TypeCast(swig_type_info *ty, void *ptr) {
+  if ((!ty) || (!ty->converter)) return ptr;
+  return (*ty->converter)(ptr);
+}
+
+/* Return the name associated with this type */
+SWIGRUNTIME(const char *)
+SWIG_TypeName(const swig_type_info *ty) {
+  return ty->name;
+}
+
+/* 
+   Compare two type names skipping the space characters, therefore
+   "char*" == "char *" and "Class<int>" == "Class<int >", etc.
+
+   Return 0 when the two name types are equivalent, as in
+   strncmp, but skipping ' '.
+*/
+static int
+SWIG_TypeNameComp(const char *f1, const char *l1,
+		  const char *f2, const char *l2) {
+  for (;(f1 != l1) && (f2 != l2); ++f1, ++f2) {
+    while ((*f1 == ' ') && (f1 != l1)) ++f1;
+    while ((*f2 == ' ') && (f2 != l2)) ++f2;
+    if (*f1 != *f2) return *f1 - *f2;
+  }
+  return (l1 - f1) - (l2 - f2);
+}
+
+/*
+  Check type equivalence in a name list like <name1>|<name2>|...
+*/
+static int
+SWIG_TypeEquiv(const char *nb, const char *tb) {
+  int equiv = 0;
+  const char* te = tb + strlen(tb);
+  const char* ne = nb;
+  while (!equiv && *ne) {
+    for (nb = ne; *ne; ++ne) {
+      if (*ne == '|') break;
+    }
+    equiv = SWIG_TypeNameComp(nb, ne, tb, te) == 0;
+    if (*ne) ++ne;
+  }
+  return equiv;
+}
+  
+
+/* Search for a swig_type_info structure */
+SWIGRUNTIME(swig_type_info *)
+SWIG_TypeQuery(const char *name) {
+  swig_type_info *ty = swig_type_list;
+  while (ty) {
+    if (ty->str && (SWIG_TypeEquiv(ty->str,name))) return ty;
+    if (ty->name && (strcmp(name,ty->name) == 0)) return ty;
+    ty = ty->prev;
+  }
+  return 0;
+}
+
+/* Pack binary data into a string */
+SWIGRUNTIME(char *)
+SWIG_PackData(char *c, void *ptr, int sz) {
+  static char hex[17] = "0123456789abcdef";
+  int i;
+  unsigned char *u = (unsigned char *) ptr;
+  register unsigned char uu;
+  for (i = 0; i < sz; i++,u++) {
+    uu = *u;
+    *(c++) = hex[(uu & 0xf0) >> 4];
+    *(c++) = hex[uu & 0xf];
+  }
+  return c;
+}
+
+/* Unpack binary data from a string */
+SWIGRUNTIME(char *)
+SWIG_UnpackData(char *c, void *ptr, int sz) {
+  register unsigned char uu = 0;
+  register int d;
+  unsigned char *u = (unsigned char *) ptr;
+  int i;
+  for (i = 0; i < sz; i++, u++) {
+    d = *(c++);
+    if ((d >= '0') && (d <= '9'))
+      uu = ((d - '0') << 4);
+    else if ((d >= 'a') && (d <= 'f'))
+      uu = ((d - ('a'-10)) << 4);
+    d = *(c++);
+    if ((d >= '0') && (d <= '9'))
+      uu |= (d - '0');
+    else if ((d >= 'a') && (d <= 'f'))
+      uu |= (d - ('a'-10));
+    *u = uu;
+  }
+  return c;
+}
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/***********************************************************************
+ * python.swg
+ *
+ *     This file contains the runtime support for Python modules
+ *     and includes code for managing global variables and pointer
+ *     type checking.
+ *
+ * Author : David Beazley (beazley@cs.uchicago.edu)
+ ************************************************************************/
+
+#include "Python.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SWIG_PY_INT     1
+#define SWIG_PY_FLOAT   2
+#define SWIG_PY_STRING  3
+#define SWIG_PY_POINTER 4
+#define SWIG_PY_BINARY  5
+
+/* Flags for pointer conversion */
+
+#define SWIG_POINTER_EXCEPTION     0x1
+#define SWIG_POINTER_DISOWN        0x2
+
+/* Exception handling in wrappers */
+#define SWIG_fail   goto fail
+
+/* Constant information structure */
+typedef struct swig_const_info {
+    int type;
+    char *name;
+    long lvalue;
+    double dvalue;
+    void   *pvalue;
+    swig_type_info **ptype;
+} swig_const_info;
+
+/* Common SWIG API */
+#define SWIG_ConvertPtr(obj, pp, type, flags) \
+  SWIG_Python_ConvertPtr(obj, pp, type, flags)
+#define SWIG_NewPointerObj(p, type, flags) \
+  SWIG_Python_NewPointerObj(p, type, flags)
+#define SWIG_MustGetPtr(p, type, argnum, flags) \
+  SWIG_Python_MustGetPtr(p, type, argnum, flags)
+ 
+
+typedef double (*py_objasdbl_conv)(PyObject *obj);
+
+#ifdef SWIG_NOINCLUDE
+
+SWIGIMPORT(int)               SWIG_Python_ConvertPtr(PyObject *, void **, swig_type_info *, int);
+SWIGIMPORT(PyObject *)        SWIG_Python_NewPointerObj(void *, swig_type_info *,int own);
+SWIGIMPORT(void *)            SWIG_Python_MustGetPtr(PyObject *, swig_type_info *, int, int);
+
+#else
+
+
+/* Convert a pointer value */
+SWIGRUNTIME(int)
+SWIG_Python_ConvertPtr(PyObject *obj, void **ptr, swig_type_info *ty, int flags) {
+  swig_type_info *tc;
+  char  *c = 0;
+  static PyObject *SWIG_this = 0;
+  int    newref = 0;
+  PyObject  *pyobj = 0;
+
+  if (!obj) return 0;
+  if (obj == Py_None) {
+    *ptr = 0;
+    return 0;
+  }
+#ifdef SWIG_COBJECT_TYPES
+  if (!(PyCObject_Check(obj))) {
+    if (!SWIG_this)
+      SWIG_this = PyString_FromString("this");
+    pyobj = obj;
+    obj = PyObject_GetAttr(obj,SWIG_this);
+    newref = 1;
+    if (!obj) goto type_error;
+    if (!PyCObject_Check(obj)) {
+      Py_DECREF(obj);
+      goto type_error;
+    }
+  }  
+  *ptr = PyCObject_AsVoidPtr(obj);
+  c = (char *) PyCObject_GetDesc(obj);
+  if (newref) Py_DECREF(obj);
+  goto cobject;
+#else
+  if (!(PyString_Check(obj))) {
+    if (!SWIG_this)
+      SWIG_this = PyString_FromString("this");
+    pyobj = obj;
+    obj = PyObject_GetAttr(obj,SWIG_this);
+    newref = 1;
+    if (!obj) goto type_error;
+    if (!PyString_Check(obj)) {
+      Py_DECREF(obj);
+      goto type_error;
+    }
+  } 
+  c = PyString_AsString(obj);
+  /* Pointer values must start with leading underscore */
+  if (*c != '_') {
+    *ptr = (void *) 0;
+    if (strcmp(c,"NULL") == 0) {
+      if (newref) { Py_DECREF(obj); }
+      return 0;
+    } else {
+      if (newref) { Py_DECREF(obj); }
+      goto type_error;
+    }
+  }
+  c++;
+  c = SWIG_UnpackData(c,ptr,sizeof(void *));
+  if (newref) { Py_DECREF(obj); }
+#endif
+
+#ifdef SWIG_COBJECT_TYPES
+cobject:
+#endif
+
+  if (ty) {
+    tc = SWIG_TypeCheck(c,ty);
+    if (!tc) goto type_error;
+    *ptr = SWIG_TypeCast(tc,(void*) *ptr);
+  }
+
+  if ((pyobj) && (flags & SWIG_POINTER_DISOWN)) {
+    PyObject *zero = PyInt_FromLong(0);
+    PyObject_SetAttrString(pyobj,(char*)"thisown",zero);
+    Py_DECREF(zero);
+  }
+  return 0;
+
+type_error:
+  PyErr_Clear();
+  if (flags & SWIG_POINTER_EXCEPTION) {
+    if (ty && c) {
+      PyErr_Format(PyExc_TypeError, 
+		   "Type error. Got %s, expected %s",
+		   c, ty->name);
+    } else {
+      PyErr_SetString(PyExc_TypeError,"Expected a pointer");
+    }
+  }
+  return -1;
+}
+
+/* Convert a pointer value, signal an exception on a type mismatch */
+SWIGRUNTIME(void *)
+SWIG_Python_MustGetPtr(PyObject *obj, swig_type_info *ty, int argnum, int flags) {
+  void *result;
+  SWIG_Python_ConvertPtr(obj, &result, ty, flags | SWIG_POINTER_EXCEPTION);
+  return result;
+}
+
+/* Create a new pointer object */
+SWIGRUNTIME(PyObject *)
+SWIG_Python_NewPointerObj(void *ptr, swig_type_info *type, int own) {
+  PyObject *robj;
+  if (!ptr) {
+    Py_INCREF(Py_None);
+    return Py_None;
+  }
+#ifdef SWIG_COBJECT_TYPES
+  robj = PyCObject_FromVoidPtrAndDesc((void *) ptr, (char *) type->name, NULL);
+#else
+  {
+    char result[1024];
+    char *r = result;
+    *(r++) = '_';
+    r = SWIG_PackData(r,&ptr,sizeof(void *));
+    strcpy(r,type->name);
+    robj = PyString_FromString(result);
+  }
+#endif
+  if (!robj || (robj == Py_None)) return robj;
+  if (type->clientdata) {
+    PyObject *inst;
+    PyObject *args = Py_BuildValue((char*)"(O)", robj);
+    Py_DECREF(robj);
+    inst = PyObject_CallObject((PyObject *) type->clientdata, args);
+    Py_DECREF(args);
+    if (inst) {
+      if (own) {
+        PyObject *n = PyInt_FromLong(1);
+        PyObject_SetAttrString(inst,(char*)"thisown",n);
+        Py_DECREF(n);
+      }
+      robj = inst;
+    }
+  }
+  return robj;
+}
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+"""
