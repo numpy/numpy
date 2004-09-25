a0b4476d08c59da74eb64cbcc92621cad160fbce
Pearu Peterson <pearu.peterson@gmail.com>
2004-09-25 09:27:13 +0000

    Clean up sys.path after scipy_distutils has been imported.

--- scipy_distutils/setup.py
+++ scipy_distutils/setup.py
@@ -3,8 +3,12 @@ sys.path.insert(0,'..')
 import os
 d = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 if d == 'scipy_distutils':
+    import scipy_distutils
+    del sys.path[0]
     execfile('setup_scipy_distutils.py')
 else:
     os.system('cd .. && ln -s %s scipy_distutils' % (d))
+    import scipy_distutils
+    del sys.path[0]
     execfile('setup_scipy_distutils.py')
     os.system('cd .. && rm -f scipy_distutils')
