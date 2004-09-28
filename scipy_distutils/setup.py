a48e9f9d44945ac3c18cc2ec34c1a3a68ef8e989
Pearu Peterson <pearu.peterson@gmail.com>
2004-09-28 11:05:52 +0000

    Backport to Python 2.2 and earlier.

--- scipy_distutils/setup.py
+++ scipy_distutils/setup.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(0,'..')
 import os
+if sys.prefix[:3]< '2.3' and __name__ == '__main__':
+    __file__ = sys.argv[0]
 d = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 if d == 'scipy_distutils':
     import scipy_distutils
