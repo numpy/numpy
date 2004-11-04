eac24cf48d7ca152e8a7fb59305e119dce778cfe
Pearu Peterson <pearu.peterson@gmail.com>
2004-11-04 23:07:20 +0000

    Fixed the order of including math.h to support freebsd.

--- scipy_base/fastumathmodule.c
+++ scipy_base/fastumathmodule.c
@@ -1,8 +1,8 @@
+#include <math.h>
 #include "Python.h"
 #include "Numeric/arrayobject.h"
 #include "Numeric/ufuncobject.h"
 #include "abstract.h"
-#include <math.h>
 #include "mconf_lite.h"
 
 /* Fast umath module whose functions do not check for range and domain
