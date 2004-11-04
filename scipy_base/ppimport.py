40b324c0cb3092eba16a641b08b5b1ba67edf4f0
Pearu Peterson <pearu.peterson@gmail.com>
2004-11-04 17:39:56 +0000

    Fixed dir().

--- scipy_base/ppimport.py
+++ scipy_base/ppimport.py
@@ -423,6 +423,14 @@ _inspect.getdoc = _ppimport_inspect_getdoc
 import __builtin__ as _builtin
 _old_builtin_dir = _builtin.dir
 def _ppimport_builtin_dir(*arg):
+    if not arg:
+        p_frame = _get_frame(1)
+        g = p_frame.f_globals
+        l = p_frame.f_locals
+        l['_ppimport_old_builtin_dir'] = _old_builtin_dir
+        r = eval('_ppimport_old_builtin_dir()',g,l)
+        del r[r.index('_ppimport_old_builtin_dir')]
+        return r
     return _old_builtin_dir(*map(_ppresolve_ignore_failure,arg))
 _ppimport_builtin_dir.__doc__ = _old_builtin_dir.__doc__
 _builtin.dir = _ppimport_builtin_dir
