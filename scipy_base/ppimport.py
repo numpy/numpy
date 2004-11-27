c1361c857a2f28db25e4af652e0fc6de3fd3ad76
Pearu Peterson <pearu.peterson@gmail.com>
2004-11-27 21:23:27 +0000

    Fixed getfile wrapper and wrapped inspect.getsource.

--- scipy_base/ppimport.py
+++ scipy_base/ppimport.py
@@ -410,7 +410,7 @@ _old_inspect_getfile = _inspect.getfile
 def _ppimport_inspect_getfile(object):
     if isinstance(object,_ModuleLoader):
         return object.__dict__['__file__']
-    return _old_inspect_getfile(object)
+    return _old_inspect_getfile(_ppresolve_ignore_failure(object))
 _ppimport_inspect_getfile.__doc__ = _old_inspect_getfile.__doc__
 _inspect.getfile = _ppimport_inspect_getfile
 
@@ -419,6 +419,12 @@ def _ppimport_inspect_getdoc(object):
     return _old_inspect_getdoc(_ppresolve_ignore_failure(object))
 _ppimport_inspect_getdoc.__doc__ = _old_inspect_getdoc.__doc__
 _inspect.getdoc = _ppimport_inspect_getdoc
+
+_old_inspect_getsource = _inspect.getsource
+def _ppimport_inspect_getsource(object):
+    return _old_inspect_getsource(_ppresolve_ignore_failure(object))
+_ppimport_inspect_getsource.__doc__ = _old_inspect_getsource.__doc__
+_inspect.getsource = _ppimport_inspect_getsource
 
 import __builtin__ as _builtin
 _old_builtin_dir = _builtin.dir
