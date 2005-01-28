94d664637f86a02327efa902bc29b82dec58679c
Pearu Peterson <pearu.peterson@gmail.com>
2005-01-28 16:32:20 +0000

    Fixed bdist_rpm command.

--- scipy_base/setup_scipy_base.py
+++ scipy_base/setup_scipy_base.py
@@ -1,50 +1,6 @@
 #!/usr/bin/env python
 import os, sys
 from glob import glob
-import shutil
-
-class _CleanUpFile:
-    """CleanUpFile deletes the specified filename when self is destroyed."""
-    def __init__(self, name):
-        self.name = name
-    def __del__(self):
-        os.remove(self.name)
-        # pass # leave source around for debugging
-
-def _temp_copy(_from, _to):
-    """temp_copy copies a named file into a named temporary file.
-    The temporary will be deleted when the setupext module is destructed.
-    """
-    # Copy the file data from _from to _to
-    s = open(_from).read()
-    open(_to,"w+").write(s)
-    # Suppress object rebuild by preserving time stamps.
-    stats = os.stat(_from)
-    os.utime(_to, (stats.st_atime, stats.st_mtime))
-    # Make an object to eliminate the temporary file at exit time.
-    globals()["_cleanup_"+_to] = _CleanUpFile(_to)
-
-def _config_compiled_base(package, local_path, numerix_prefix, macro, info):
-    """_config_compiled_base returns the Extension object for an
-    Numeric or numarray specific version of _compiled_base.
-    """
-    from scipy_distutils.system_info import dict_append
-    from scipy_distutils.core import Extension
-    from scipy_distutils.misc_util import dot_join
-    module = numerix_prefix + "_compiled_base"
-    source = module + '.c'
-    _temp_copy(os.path.join(local_path, "_compiled_base.c"),
-               os.path.join(local_path, source))
-    sources = [source]
-    sources = [os.path.join(local_path,x) for x in sources]
-    depends = sources
-    ext_args = {'name':dot_join(package, module),
-                'sources':sources,
-                'depends':depends,
-                'define_macros':[(macro,1)],
-                }
-    dict_append(ext_args,**info)
-    return Extension(**ext_args)
     
 def configuration(parent_package='',parent_path=None):
     from scipy_distutils.system_info import get_info, dict_append
@@ -52,6 +8,8 @@ def configuration(parent_package='',parent_path=None):
     from scipy_distutils.misc_util import get_path,default_config_dict,dot_join
     from scipy_distutils.misc_util import get_path,default_config_dict,\
          dot_join,SourceGenerator
+    from distutils.dep_util import newer
+    from distutils.file_util import copy_file
 
     package = 'scipy_base'
     local_path = get_path(__name__,parent_path)
@@ -99,19 +57,41 @@ def configuration(parent_package='',parent_path=None):
                  'depends': umath_c_sources}
     dict_append(ext_args,**numpy_info)
     config['ext_modules'].append(Extension(**ext_args))
- 
-    # _compiled_base module for Numeric: _nc_compiled_base
-    _nc_compiled_base_ext = _config_compiled_base(
-        package, local_path, "_nc", "NUMERIC", numpy_info)
-    config['ext_modules'].append(_nc_compiled_base_ext)
 
-    # _compiled_base module for numarray: _na_compiled_base
+
+    # _nc_compiled_base and _na_compiled_base modules
+
+    _compiled_base_c = os.path.join(local_path,'_compiled_base.c')
+    def compiled_base_c(ext,src_dir):
+        source = os.path.join(src_dir,ext.name.split('.')[-1] + '.c')
+        if newer(_compiled_base_c,source):
+            copy_file(_compiled_base_c,source)
+        return [source]
+
+    ext_args = {}
+    dict_append(ext_args,
+                name=dot_join(package,'_nc_compiled_base'),
+                sources = [compiled_base_c],
+                depends = [_compiled_base_c],
+                define_macros = [('NUMERIC',None)],
+                include_dirs = [local_path]
+                )
+    dict_append(ext_args,**numpy_info)
+    config['ext_modules'].append(Extension(**ext_args))
+
     numarray_info = get_info('numarray')
     if numarray_info:
-        _na_compiled_base_ext = _config_compiled_base(
-            package, local_path, "_na", "NUMARRAY", numarray_info)
-        config['ext_modules'].append(_na_compiled_base_ext)
-        
+        ext_args = {}
+        dict_append(ext_args,
+                    name=dot_join(package,'_na_compiled_base'),
+                    sources = [compiled_base_c],
+                    depends = [_compiled_base_c],
+                    define_macros = [('NUMARRAY',None)],
+                    include_dirs = [local_path]
+                    )
+        dict_append(ext_args,**numarray_info)
+        config['ext_modules'].append(Extension(**ext_args))
+
     # display_test module
     sources = [os.path.join(local_path,'src','display_test.c')]
     x11 = get_info('x11')
