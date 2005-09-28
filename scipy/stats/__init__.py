861df810553f700101e479ac298d0bec7ca194f1
Robert Kern <robert.kern@gmail.com>
2005-09-28 02:56:42 +0000

    r3492@803638d6:  kern | 2005-09-27 19:03:06 -0700  Update scipy.stats to use mtrand

--- scipy/stats/__init__.py
+++ scipy/stats/__init__.py
@@ -11,26 +11,60 @@
  random_sample(n) returns a vector of length n filled with random numbers
 """
 import scipy.base as Numeric
-from scipy.lib.rng import *
+from scipy.lib.mtrand import *
+import scipy.linalg as LinearAlgebra
 
+# some aliases
+ranf = random_sample
+randn = standard_normal
 
-standard_generator = CreateGenerator(-1)
+def multivariate_normal(mean, cov, shape=[]):
+       """multivariate_normal(mean, cov) or multivariate_normal(mean, cov, [m, n, ...])
+          returns an array containing multivariate normally distributed random numbers
+          with specified mean and covariance.
 
-def ranf():
-        "ranf() = a random number from the standard generator."
-        return standard_generator.ranf()
+          mean must be a 1 dimensional array. cov must be a square two dimensional
+          array with the same number of rows and columns as mean has elements.
 
-def random_sample(*n):
-        """random_sample(n) = array of n random numbers;
-        random_sample(n1, n2, ...)= random array of shape (n1, n2, ..)"""
+          The first form returns a single 1-D array containing a multivariate
+          normal.
 
-        if not n:
-                return standard_generator.sample(1)
-        m = 1
-        for i in n:
-                m = m * i
-        return Numeric.reshape (standard_generator.sample(m), n)
+          The second form returns an array of shape (m, n, ..., cov.shape[0]).
+          In this case, output[i,j,...,:] is a 1-D array containing a multivariate
+          normal."""
+       # Check preconditions on arguments
+       mean = Numeric.array(mean)
+       cov = Numeric.array(cov)
+       if len(mean.shape) != 1:
+              raise ArgumentError, "mean must be 1 dimensional."
+       if (len(cov.shape) != 2) or (cov.shape[0] != cov.shape[1]):
+              raise ArgumentError, "cov must be 2 dimensional and square."
+       if mean.shape[0] != cov.shape[0]:
+              raise ArgumentError, "mean and cov must have same length."
+       # Compute shape of output
+       if isinstance(shape, IntType):
+           shape = [shape]
+       final_shape = list(shape[:])
+       final_shape.append(mean.shape[0])
+       # Create a matrix of independent standard normally distributed random
+       # numbers. The matrix has rows with the same length as mean and as
+       # many rows are necessary to form a matrix of shape final_shape.
+       x = standard_normal(Numeric.multiply.reduce(final_shape))
+       x.shape = (Numeric.multiply.reduce(final_shape[0:len(final_shape)-1]),
+                  mean.shape[0])
+       # Transform matrix of standard normals into matrix where each row
+       # contains multivariate normals with the desired covariance.
+       # Compute A such that matrixmultiply(transpose(A),A) == cov.
+       # Then the matrix products of the rows of x and A has the desired
+       # covariance. Note that sqrt(s)*v where (u,s,v) is the singular value
+       # decomposition of cov is such an A.
+       (u,s,v) = LinearAlgebra.singular_value_decomposition(cov)
+       x = Numeric.matrixmultiply(x*Numeric.sqrt(s),v)
+       # The rows of x now have the correct covariance but mean 0. Add
+       # mean to each row. Then each row will have mean mean.
+       Numeric.add(mean,x,x)
+       x.shape = final_shape
+       return x
 
-from scipy.lib.ranlib import *
-
-from random_lite import *
+# XXX: should we also bring over mean_var_test() from random_lite.py? It seems 
+# out of place.
