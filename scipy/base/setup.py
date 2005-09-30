8087035fbe8986921b1f7d44bdfec47b541cde23
Pearu Peterson <pearu.peterson@gmail.com>
2005-09-30 17:27:11 +0000

    Define atanhf,asinhf,acoshf when missing (like in mingw32).

--- scipy/base/setup.py
+++ scipy/base/setup.py
@@ -60,6 +60,8 @@ def configuration(parent_package='',top_path=None):
                 moredefs.append('HAVE_FLOAT_FUNCS')
             if config_cmd.check_func('asinh', **kws_args):
                 moredefs.append('HAVE_INVERSE_HYPERBOLIC')
+            if config_cmd.check_func('asinhf', **kws_args):
+                moredefs.append('HAVE_INVERSE_HYPERBOLIC_FLOAT')
             if config_cmd.check_func('isnan', **kws_args):
                 moredefs.append('HAVE_ISNAN')
 
