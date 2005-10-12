004d0049fbb9e5aa4983991ac1df763622ae122e
Travis Oliphant <oliphant@enthought.com>
2005-10-12 07:14:51 +0000

    Added check for inverse hyperbolic long double functions

--- scipy/base/setup.py
+++ scipy/base/setup.py
@@ -62,6 +62,8 @@ def configuration(parent_package='',top_path=None):
                 moredefs.append('HAVE_INVERSE_HYPERBOLIC')
             if config_cmd.check_func('atanhf', **kws_args):
                 moredefs.append('HAVE_INVERSE_HYPERBOLIC_FLOAT')
+            if config_cmd.check_func('atanhl', **kw_args):
+                moredefs.append('HAVE_INVERSE_HYPERBOLIC_LONGDOUBLE')
             if config_cmd.check_func('isnan', **kws_args):
                 moredefs.append('HAVE_ISNAN')
 
