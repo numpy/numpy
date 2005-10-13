d47b79d847d963251bcaf8612a9f1f1438632104
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-13 07:45:16 +0000

    Fixed getting base svn version number - avoiding importing base while in source directory. 2nd try.

--- scipy/core_version.py
+++ scipy/core_version.py
@@ -4,5 +4,9 @@ import os
 svn_version_file = os.path.join(os.path.dirname(__file__),
                                 'base','__svn_version__.py')
 if os.path.isfile(svn_version_file):
-    import base.__svn_version as svn
+    import imp
+    svn = imp.load_module('scipy.base.__svn_version__',
+                          open(svn_version_file),
+                          svn_version_file,
+                          ('.py','U',1))
     version += '.'+svn.version
