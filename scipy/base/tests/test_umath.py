704e5a103b9fb5944f41b81bd8e7217e1157b033
Pearu Peterson <pearu.peterson@gmail.com>
2005-10-13 10:51:09 +0000

    Renamed test_basic to test_umath.

--- scipy/base/tests/test_umath.py
+++ scipy/base/tests/test_umath.py
@@ -1,21 +1,18 @@
-import unittest
 import sys
 from scipy.test.testing import *
 set_package_path()
-import scipy.base;reload(scipy.base)
-#from scipy.base import fastumath;reload(fastumath)
+from scipy.base.umath import minimum, maximum
 del sys.path[0]
 
 
 class test_maximum(ScipyTestCase):
     def check_reduce_complex(self):
-        x = [1,2]
         assert_equal(maximum.reduce([1,2j]),1)
+        assert_equal(maximum.reduce([1+3j,2j]),1+3j)
 
 class test_minimum(ScipyTestCase):
     def check_reduce_complex(self):
-        x = [1,2]
         assert_equal(minimum.reduce([1,2j]),2j)
 
 if __name__ == "__main__":
-    ScipyTest('scipy.base').run()
+    ScipyTest().run()
