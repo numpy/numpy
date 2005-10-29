fb209515eba92549f7806ac6127c9eb5710bdd63
cookedm <cookedm@localhost>
2005-10-29 21:45:38 +0000

    Give ufunclike some TLC. Cleanup, add docstrings and unit tests. The unit tests are as doctests, and currently use the deprecated test_suite method of running.

--- scipy/base/ufunclike.py
+++ scipy/base/ufunclike.py
@@ -1,18 +1,21 @@
+"""
+Module of functions that are like ufuncs in acting on arrays and optionally
+storing results in an output array.
+"""
+__all__ = ['fix', 'isneginf', 'isposinf', 'sign', 'log2']
 
-import numeric as _nx
+import numeric as nx
 from numeric import asarray, empty, empty_like, isinf, signbit, zeros
 import umath
-
-__all__ = ['fix','isneginf','isposinf','sign','log2']
 
 def fix(x, y=None):
     """ Round x to nearest integer towards zero.
     """
     x = asarray(x)
     if y is None:
-        y = _nx.floor(x)
+        y = nx.floor(x)
     else:
-        _nx.floor(x,y)
+        nx.floor(x, y)
     if x.ndim == 0:
         if (x<0):
             y += 1
@@ -21,14 +24,22 @@ def fix(x, y=None):
     return y
 
 def isposinf(x, y=None):
+    """Return a boolean array y with y[i] True for x[i] = +Inf.
+
+    If y is an array, the result replaces the contents of y.
+    """
     if y is None:
-        y = empty(x.shape, dtype='?')
+        y = empty(x.shape, dtype=nx.bool_)
     umath.logical_and(isinf(x), ~signbit(x), y)
     return y
-    
+
 def isneginf(x, y=None):
+    """Return a boolean array y with y[i] True for x[i] = -Inf.
+
+    If y is an array, the result replaces the contents of y.
+    """
     if y is None:
-        y = empty(x.shape, dtype='?')
+        y = empty(x.shape, dtype=nx.bool_)
     umath.logical_and(isinf(x), signbit(x), y)
     return y
 
@@ -39,11 +50,11 @@ def sign(x, y=None):
     """
     x = asarray(x)
     if y is None:
-        y = zeros(x.shape, dtype=_nx.int_)
+        y = zeros(x.shape, dtype=nx.int_)
     if x.ndim == 0:
-        if x<0:
+        if x < 0:
             y -= 1
-        elif x>0:
+        elif x > 0:
             y += 1
     else:
         y[x<0] = -1
@@ -52,11 +63,15 @@ def sign(x, y=None):
 
 _log2 = umath.log(2)
 def log2(x, y=None):
+    """Returns the base 2 logarithm of x
+
+    If y is an array, the result replaces the contents of y.
+    """
     x = asarray(x)
     if y is None:
         y = umath.log(x)
     else:
-        res = umath.log(x,y)
+        umath.log(x, y)
     y /= _log2
     return y
-    
+
