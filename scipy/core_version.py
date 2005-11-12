5ac24f59ed0053f58781d198e64e6ebe6dbe1d69
Travis Oliphant <oliphant@enthought.com>
2005-11-12 04:28:26 +0000

    Fixed problem.

--- scipy/core_version.py
+++ scipy/core_version.py
@@ -2,7 +2,7 @@ version='0.6.0'
 
 #import os
 #svn_version_file = os.path.join(os.path.dirname(__file__),
-                                'base','__svn_version__.py')
+#                                'base','__svn_version__.py')
 #if os.path.isfile(svn_version_file):
 #    import imp
 #    svn = imp.load_module('scipy.base.__svn_version__',
