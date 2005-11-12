ffa70cdb23d58eb7fb45bb73bfb4bad2d88ae852
Travis Oliphant <oliphant@enthought.com>
2005-11-12 05:01:45 +0000

    Changed core_version back.

--- scipy/core_version.py
+++ scipy/core_version.py
@@ -1,12 +1,12 @@
 version='0.6.0'
 
-#import os
-#svn_version_file = os.path.join(os.path.dirname(__file__),
+import os
+svn_version_file = os.path.join(os.path.dirname(__file__),
                                 'base','__svn_version__.py')
-#if os.path.isfile(svn_version_file):
-#    import imp
-#    svn = imp.load_module('scipy.base.__svn_version__',
-#                          open(svn_version_file),
-#                          svn_version_file,
-#                          ('.py','U',1))
-#    version += '.'+svn.version
+if os.path.isfile(svn_version_file):
+    import imp
+    svn = imp.load_module('scipy.base.__svn_version__',
+                          open(svn_version_file),
+                          svn_version_file,
+                          ('.py','U',1))
+    version += '.'+svn.version
