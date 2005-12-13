b5dbca15fc13e9714e57d24bb4a6e67fdeaacd2d
chanley <chanley@gmail.com>
2005-12-13 21:56:50 +0000

    quick fix for fromrecords unittest

--- scipy/base/tests/test_records.py
+++ scipy/base/tests/test_records.py
@@ -10,7 +10,7 @@ from scipy.base import records as rec
 class test_fromrecords(ScipyTestCase):
     def check_fromrecords(self):
         r = rec.fromrecords([[456,'dbe',1.2],[2,'de',1.3]],names='col1,col2,col3')
-        assert_equal(r[0],(456, 'dbe', 1.2))
+        assert_equal(r[0].toscalar(),(456, 'dbe', 1.2))
 
 del sys.path[0]
 if __name__ == "__main__":
