4caffb7c22b684ada3498a022846e5a45d7b1e8c
chanley <chanley@gmail.com>
2005-12-14 17:46:10 +0000

    added additional records unittests from numarray

--- scipy/base/tests/test_records.py
+++ scipy/base/tests/test_records.py
@@ -14,8 +14,16 @@ class test_fromrecords(ScipyTestCase):
 
     def check_method_array(ScipyTestCase):
         r = rec.array('abcdefg'*100,formats='i2,a3,i4',shape=3,byteorder='big')
-        assert_equal(r[1].toscalar(),(25444, 'efg', 1633837924)) 
+        assert_equal(r[1].toscalar(),(25444, 'efg', 1633837924))
 
+    def check_method_array2(ScipyTestCase):
+        r=rec.array([(1,11,'a'),(2,22,'b'),(3,33,'c'),(4,44,'d'),(5,55,'ex'),(6,66,'f'),(7,77,'g')],formats='u1,f4,a1')
+        assert_equal(r[1].toscalar(),(2, 22.0, 'b'))
+        
+    def check_recarray_slices(ScipyTestCase):
+        r=rec.array([(1,11,'a'),(2,22,'b'),(3,33,'c'),(4,44,'d'),(5,55,'ex'),(6,66,'f'),(7,77,'g')],formats='u1,f4,a1')
+        assert_equal(r[1::2][1].toscalar(),(4, 44.0, 'd'))
+        
 del sys.path[0]
 if __name__ == "__main__":
     ScipyTest().run()
