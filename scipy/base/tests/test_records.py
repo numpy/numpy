6a1ba6d51bd0840c241a8a3a5af3188f96dde531
chanley <chanley@gmail.com>
2005-12-14 15:45:35 +0000

    added array method test to records unittest

--- scipy/base/tests/test_records.py
+++ scipy/base/tests/test_records.py
@@ -12,6 +12,10 @@ class test_fromrecords(ScipyTestCase):
         r = rec.fromrecords([[456,'dbe',1.2],[2,'de',1.3]],names='col1,col2,col3')
         assert_equal(r[0].toscalar(),(456, 'dbe', 1.2))
 
+    def check_method_array(ScipyTestCase):
+        r = rec.array('abcdefg'*100,formats='i2,a3,i4',shape=3,byteorder='big')
+        assert_equal(r[1].toscalar(),(25444, 'efg', 1633837924)) 
+
 del sys.path[0]
 if __name__ == "__main__":
     ScipyTest().run()
