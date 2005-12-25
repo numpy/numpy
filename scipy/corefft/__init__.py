2ca38d27357363781d5e284b8cb81d70754c39da
Travis Oliphant <oliphant@enthought.com>
2005-12-25 11:14:53 +0000

    Added rudimentary code for over-riding functions in scipy_core with those from full scipy.

--- scipy/corefft/__init__.py
+++ scipy/corefft/__init__.py
@@ -4,5 +4,12 @@ from info import __doc__
 from fftpack import *
 from helper import *
 
+try:
+    import scipy.fftpack
+    fft = scipy.fftpack.fft
+    ifft = scipy.fftpack.ifft
+except ImportError:
+    pass
+
 from scipy.test.testing import ScipyTest 
 test = ScipyTest().test
--- scipy/corelinalg/__init__.py
+++ scipy/corelinalg/__init__.py
@@ -3,5 +3,12 @@ from info import __doc__
 
 from linalg import *
 
+try:
+    import scipy.linalg
+    inv = scipy.linalg.inv
+    svd = scipy.linalg.svd
+except ImportError:
+    pass
+
 from scipy.test.testing import ScipyTest 
 test = ScipyTest().test
