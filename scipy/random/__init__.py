e9d1483f0a5fa027d86f8c4084de9dfa4e5e4639
Travis Oliphant <oliphant@enthought.com>
2005-12-26 04:06:49 +0000

    Removed separate random.py file.

--- scipy/random/__init__.py
+++ scipy/random/__init__.py
@@ -1,7 +1,19 @@
 # To get sub-modules
 from info import __doc__
 
-from random import *
+from scipy.lib.mtrand import *
+
+# Some aliases:
+ranf = random_sample
+random = random_sample
+sample = random_sample
+
+def __RandomState_ctor():
+    """Return a RandomState instance.
+    
+    This function exists solely to assist (un)pickling.
+    """
+    return RandomState()
 
 from scipy.test.testing import ScipyTest 
 test = ScipyTest().test
