ac853f7e345d949a6be1a7e6a749ce31c4b0d7f9
Travis Oliphant <oliphant@enthought.com>
2005-12-30 00:08:43 +0000

    Fixed problem with mod.

--- scipy/base/function_base.py
+++ scipy/base/function_base.py
@@ -14,7 +14,7 @@ import numeric as _nx
 from numeric import ones, zeros, arange, concatenate, array, asarray, empty
 from numeric import ScalarType, dot, where
 from umath import pi, multiply, add, arctan2, maximum, minimum, frompyfunc, \
-     isnan, absolute, cos, less_equal, sqrt, sin
+     isnan, absolute, cos, less_equal, sqrt, sin, mod
 from oldnumeric import ravel, nonzero, choose, \
      sometrue, alltrue, reshape, any, all, typecodes, ArrayType, squeeze
 from type_check import ScalarType, isscalar
