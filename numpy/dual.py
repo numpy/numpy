f69c9a67449c1dc61ba24e1bc1319e87a89a6388
Travis Oliphant <oliphant@enthought.com>
2006-01-05 07:08:37 +0000

    Changed how to get dual numpy/scipy behavior.  Use explicit numpy.dual module.

---
+++ numpy/dual.py
@@ -0,0 +1,37 @@
+# This module should be used for functions both in numpy and scipy if
+#  you want to use the numpy version if available but the scipy version
+#  otherwise.
+#  Usage  --- import numpy.dual
+
+__all__ = ['fft','ifft','fftn','ifftn','fft2','ifft2',
+           'inv','svd','solve','det','eig','eigvals','lstsq',
+           'pinv','cholesky']
+
+try:
+    import scipy.linalg as linpkg
+except ImportError:
+    import numpy.linalg as linpkg
+
+try:
+    import scipy.fftpack as fftpkg
+except ImportError:
+    import numpy.dft as fftpkg
+
+
+fft = fftpkg.fft
+ifft = fftpkg.ifft
+fftn = fftpkg.fftn
+ifftn = fftpkg.ifftn
+fft2 = fftpkg.fft2
+ifft2 = fftpkg.ifft2
+
+inv = linpkg.inv
+svd = linpkg.svd
+solve = linpkg.solve
+det = linpkg.det
+eig = linpkg.eig
+eigvals = linpkg.eigvals
+lstsq = linpkg.lstsq
+pinv = linpkg.pinv
+cholesky = linpkg.cholesky
+    
--- numpy/linalg/__init__.py
+++ numpy/linalg/__init__.py
@@ -3,23 +3,5 @@ from info import __doc__
 
 from linalg import *
 
-# re-define duplicated functions if full scipy installed.
-try:
-    import scipy.linalg
-except ImportError:
-    pass
-else:
-    inv = scipy.linalg.inv
-    svd = scipy.linalg.svd
-    solve = scipy.linalg.solve
-    det = scipy.linalg.det
-    eig = scipy.linalg.eig
-    eigvals = scipy.linalg.eigvals
-    lstsq = scipy.linalg.lstsq
-    pinv = scipy.linalg.pinv
-    cholesky = scipy.linalg.cholesky
-    
-
-
 from numpy.testing import ScipyTest 
 test = ScipyTest().test
