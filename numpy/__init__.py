70216261ceea4ea801c7dbbcd487ba7c7733fec0
Pearu Peterson <pearu.peterson@gmail.com>
2006-01-05 11:09:18 +0000

    Doc updates and removing NUMPY_IMPORT_VERBOSE from numpy.

--- numpy/__init__.py
+++ numpy/__init__.py
@@ -14,11 +14,29 @@ Documentation is also available in the docstrings.
 
 Available subpackages
 ---------------------
-"""
 
-import os as _os
-import sys as _sys
-NUMPY_IMPORT_VERBOSE = int(_os.environ.get('NUMPY_IMPORT_VERBOSE','0'))
+core      - Defines a multi-dimensional array and useful procedures
+            for Numerical computation.
+lib       - Basic functions used by several sub-packages and useful
+            to have in the main name-space
+dft       - Core FFT routines
+linalg    - Core Linear Algebra Tools
+random    - Core Random Tools
+testing   - Scipy testing tools
+distutils - Enhanced distutils with Fortran compiler support and more
+f2py      - Fortran to Python interface generator
+dual      - Overwrite certain functions with high-performance Scipy tools
+
+Available tools
+---------------
+
+core, lib namespaces
+fft, ifft   - Functions for FFT and inverse FFT
+rand, randn - Functions for uniform and normal random numbers.
+test        - Method to run numpy unittests.
+__version__ - Numpy version string
+show_config - Show numpy build configuration
+"""
 
 try:
     from __config__ import show as show_config
@@ -32,7 +50,9 @@ except ImportError:
     pass
 
 if show_config is None:
+    import sys as _sys
     print >> _sys.stderr, 'Running from numpy source directory.'
+    del _sys
 else:
     from version import version as __version__
     from testing import ScipyTest
@@ -50,7 +70,3 @@ else:
     test = ScipyTest('numpy').test
 
     import add_newdocs
-
-    # TODO: Fix __doc__
-
-del _os, _sys
