a47cfdcf0dea1e9c3135296c229f36e45fe589a4
Pearu Peterson <pearu.peterson@gmail.com>
2006-01-24 19:02:24 +0000

    Fixed setup.py sdist.

--- numpy/distutils/command/build_py.py
+++ numpy/distutils/command/build_py.py
@@ -11,4 +11,14 @@ class build_py(old_build_py):
         modules += build_src.py_modules_dict.get(package,[])
 
         return modules
-            
+
+    def find_modules(self):
+        old_py_modules = self.py_modules[:]
+        new_py_modules = filter(lambda i:type(i) is type(str), self.py_modules)
+        self.py_modules[:] = new_py_modules
+        modules = old_build_py.find_modules(self)
+        self.py_modules[:] = old_py_modules
+        return modules
+
+    # XXX: Fix find_source_files for item in py_modules such that item is 3-tuple
+    # and item[2] is source file.
