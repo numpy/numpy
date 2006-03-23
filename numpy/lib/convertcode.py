4867af4f63044bbc487c8510b990dfa65c9531e0
Tim Leslie <tim.leslie@gmail.com>
2006-03-23 02:13:46 +0000

    change header comments into docstrings

--- numpy/lib/convertcode.py
+++ numpy/lib/convertcode.py
@@ -1,24 +1,23 @@
+"""
+This module converts code written for Numeric to run with numpy
 
-# This module converts code written for Numeric to run with numpy
-
-# Makes the following changes:
-#  * Converts typecharacters
-#  * Changes import statements (warns of use of from Numeric import *)
-#  * Changes import statements (using numerix) ...
-#  * Makes search and replace changes to:
-#    - .typecode()
-#    - .iscontiguous()
-#    - .byteswapped()
-#    - .itemsize()
-#  * Converts .flat to .ravel() except for .flat = xxx or .flat[xxx]
-#  * Change typecode= to dtype=
-#  * Eliminates savespace=xxx
-#  * Replace xxx.spacesaver() with True
-#  * Convert xx.savespace(?) to pass + ## xx.savespace(?)
-#  #### -- not * Convert a.shape = ? to a.reshape(?)
-#  * Prints warning for use of bool, int, float, copmlex, object, and unicode
-#
-
+Makes the following changes:
+ * Converts typecharacters
+ * Changes import statements (warns of use of from Numeric import *)
+ * Changes import statements (using numerix) ...
+ * Makes search and replace changes to:
+   - .typecode()
+   - .iscontiguous()
+   - .byteswapped()
+   - .itemsize()
+ * Converts .flat to .ravel() except for .flat = xxx or .flat[xxx]
+ * Change typecode= to dtype=
+ * Eliminates savespace=xxx
+ * Replace xxx.spacesaver() with True
+ * Convert xx.savespace(?) to pass + ## xx.savespace(?)
+ #### -- not * Convert a.shape = ? to a.reshape(?)
+ * Prints warning for use of bool, int, float, copmlex, object, and unicode
+"""
 __all__ = ['fromfile', 'fromstr']
 
 import sys
