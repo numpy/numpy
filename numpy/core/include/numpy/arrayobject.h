412bd52a7a295eee80f129a762b28923b30fe584
Travis Oliphant <oliphant@enthought.com>
2006-04-14 21:57:03 +0000

    Bump up the version number.

--- numpy/core/include/numpy/arrayobject.h
+++ numpy/core/include/numpy/arrayobject.h
@@ -79,7 +79,7 @@ extern "C" CONFUSE_EMACS
 #define PY_SUCCEED 1
 
         /* Helpful to distinguish what is installed */
-#define NDARRAY_VERSION 0x00090703
+#define NDARRAY_VERSION 0x00090704
 
 	/* Some platforms don't define bool, long long, or long double.
 	   Handle that here.
