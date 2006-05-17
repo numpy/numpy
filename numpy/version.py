4a514e0058568c363db0e240d838fe55ecbd30c0
Travis Oliphant <oliphant@enthought.com>
2006-05-17 22:58:02 +0000

    Fix versioning for release.

--- numpy/version.py
+++ numpy/version.py
@@ -1,12 +1,12 @@
-version='0.9.7'
+version='0.9.8'
 
-import os
-svn_version_file = os.path.join(os.path.dirname(__file__),
-                                'core','__svn_version__.py')
-if os.path.isfile(svn_version_file):
-    import imp
-    svn = imp.load_module('numpy.core.__svn_version__',
-                          open(svn_version_file),
-                          svn_version_file,
-                          ('.py','U',1))
-    version += '.'+svn.version
+#import os
+#svn_version_file = os.path.join(os.path.dirname(__file__),
+#                                'core','__svn_version__.py')
+#if os.path.isfile(svn_version_file):
+#    import imp
+#    svn = imp.load_module('numpy.core.__svn_version__',
+#                          open(svn_version_file),
+#                          svn_version_file,
+#                          ('.py','U',1))
+#    version += '.'+svn.version
