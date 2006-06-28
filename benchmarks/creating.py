145d10cd37fe61c5dcdf1d5841b7d23b09751038
Travis Oliphant <oliphant@enthought.com>
2006-06-28 01:10:10 +0000

    Add benchmark for creating new arrays.

---
+++ benchmarks/creating.py
@@ -0,0 +1,9 @@
+import timeit
+N = [1000,100]
+t1 = timeit.Timer('a=N.zeros(shape,type)','import numpy as N; shape=%s;type=float'%N)
+t2 = timeit.Timer('a=N.zeros(shape,type)','import Numeric as N; shape=%s;type=N.Float'%N)
+t3 = timeit.Timer('a=N.zeros(shape,type)',"import numarray as N; shape=%s;type=N.Float"%N)
+print "shape = ", N
+print "NumPy: ", t1.repeat(3,100)
+print "Numeric: ", t2.repeat(3,100)
+print "Numarray: ", t3.repeat(3,100)
