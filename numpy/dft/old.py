0d68afb5705489943e3ec611ebb1d8a515153583
Travis Oliphant <oliphant@enthought.com>
2006-07-12 06:42:35 +0000

    Fix names used in dft.old refft->rfft

--- numpy/dft/old.py
+++ numpy/dft/old.py
@@ -11,9 +11,9 @@ from fftpack import ifft as inverse_fft
 from fftpack import ifft2 as inverse_fft2d
 from fftpack import ifftn as inverse_fftnd
 from fftpack import ihfft as inverse_hermite_fft
-from fftpack import irefft as inverse_real_fft
-from fftpack import irefft2 as inverse_real_fft2d
-from fftpack import irefftn as inverse_real_fftnd
-from fftpack import refft as real_fft
-from fftpack import refft2 as real_fft2d
-from fftpack import refftn as real_fftnd
+from fftpack import irfft as inverse_real_fft
+from fftpack import irfft2 as inverse_real_fft2d
+from fftpack import irfftn as inverse_real_fftnd
+from fftpack import rfft as real_fft
+from fftpack import rfft2 as real_fft2d
+from fftpack import rfftn as real_fftnd
