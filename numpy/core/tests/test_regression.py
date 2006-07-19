20a9e65369d5a689be1991cc42ef6f91f91b49ef
Stefan van der Walt <stefan@sun.ac.za>
2006-07-19 16:37:08 +0000

    Add more regression tests, provided by Albert Strasheim.

--- numpy/core/tests/test_regression.py
+++ numpy/core/tests/test_regression.py
@@ -49,8 +49,83 @@ class test_regression(NumpyTestCase):
 
     def check_reduce(self,level=rlevel):
         """Ticket #40"""
-        assert_almost_equal(N.add.reduce([1.,.5],dtype=None),
-                            1.5)
+        assert_almost_equal(N.add.reduce([1.,.5],dtype=None), 1.5)
+
+    def check_zeros_order(self,level=rlevel):
+        """Ticket #43"""
+        N.zeros([3], int, 'C')
+        N.zeros([3], order='C')
+        N.zeros([3], int, order='C')
+
+    def check_sort_bigendian(self,level=rlevel):
+        """Ticket #47"""
+        a = N.linspace(0, 10, 11)
+        c = a.astype(N.dtype('<f8'))
+        c.sort()
+        assert_array_almost_equal(c, a)
+
+    def check_negative_nd_indexing(self,level=rlevel):
+        """Ticket #49"""
+        c = N.arange(125).reshape((5,5,5))
+        origidx = N.array([-1, 0, 1])
+        idx = N.array(origidx)
+        c[idx]
+        assert_array_equal(idx, origidx)
+
+    def check_bool(self,level=rlevel):
+        """Ticket #60"""
+        x = N.bool_(1)
+
+    def check_masked_array(self,level=rlevel):
+        """Ticket #61"""
+        x = N.core.ma.array(1,mask=[1])
+
+    def check_indexing1(self,level=rlevel):
+        """Ticket #64"""
+        descr = [('x', [('y', [('z', 'c16', (2,)),]),]),]
+        buffer = ((([6j,4j],),),)
+        h = N.array(buffer, dtype=descr)
+        h['x']['y']['z']
+
+    def check_indexing1(self,level=rlevel):
+        """Ticket #65"""
+        descr = [('x', 'i4', (2,))]
+        buffer = ([3,2],)
+        h = N.array(buffer, dtype=descr)
+        h['x']
+
+    def check_round(self,level=rlevel):
+        """Ticket #67"""
+        x = N.array([1+2j])
+        assert_almost_equal(x**(-1), [1/(1+2j)])
+
+    def check_scalar_compare(self,level=rlevel):
+        """Ticket #72"""
+        a = N.array(['test', 'auto'])
+        assert_array_equal(a == 'auto', N.array([False,True]))
+        self.assert_(a[1] == 'auto')
+        self.assert_(a[0] != 'auto')
+        b = N.linspace(0, 10, 11)
+        self.assert_(b != 'auto')
+        self.assert_(b[0] != 'auto')
+
+    def check_unicode_swapping(self,level=rlevel):
+        """Ticket #79"""
+        ulen = 1
+        ucs_value = u'\U0010FFFF'
+        ua = N.array([[[ucs_value*ulen]*2]*3]*4, dtype='U%s' % ulen)
+        ua2 = ua.newbyteorder()
+
+    def check_matrix_std_argmax(self,level=rlevel):
+        """Ticket #83"""
+        x = N.asmatrix(N.random.uniform(0,1,(3,3)))
+        self.assertEqual(x.std().shape, ())
+        self.assertEqual(x.argmax().shape, ())
+
+    def check_object_array_fill(self,level=rlevel):
+        """Ticket #86"""
+        x = N.zeros(1, 'O')
+        x.fill([])            
 
 if __name__ == "__main__":
     NumpyTest().run()
