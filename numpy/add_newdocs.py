3a4fd7e82ca5b200e7b7eeb92343e32b4e8891b6
Travis Oliphant <oliphant@enthought.com>
2006-07-20 20:57:30 +0000

    Add more documentation.  Fix #99

--- numpy/add_newdocs.py
+++ numpy/add_newdocs.py
@@ -2,15 +2,18 @@
 from lib import add_newdoc
 
 add_newdoc('numpy.core','dtype',
-           [('fields', "Fields of the data-typedescr if any."),
+           [('fields', "Fields of the data-type or None if no fields"),
+            ('names', "Names of fields or None if no fields"),
             ('alignment', "Needed alignment for this data-type"),
             ('byteorder',
              "Little-endian (<), big-endian (>), native (=), or "\
              "not-applicable (|)"),
-            ('char', "Letter typecode for this descriptor"),
-            ('dtype', "Typeobject associated with this descriptor"),
-            ('kind', "Character giving type-family of this descriptor"),
+            ('char', "Letter typecode for this data-type"),
+            ('type', "Typeobject associated with this data-type"),
+            ('kind', "Character giving type-family of this data-type"),
             ('itemsize', "Size of each item"),
+            ('hasobject', "Non-zero if Python objects are in "\
+             "this data-type"),
             ('num', "Internally-used number for builtin base")
             ]
            )
