83d1d47bb8e5df4c9578a42f9f2ce8db408ccd00
Travis Oliphant <oliphant@enthought.com>
2006-07-21 08:00:51 +0000

    Change c-api to 1.0

--- numpy/core/include/numpy/arrayobject.h
+++ numpy/core/include/numpy/arrayobject.h
@@ -36,7 +36,7 @@ extern "C" CONFUSE_EMACS
 #define NPY_SUCCEED 1
 
         /* Helpful to distinguish what is installed */
-#define NPY_VERSION 0x0009090D
+#define NPY_VERSION 0x01000000
 
 	/* Some platforms don't define bool, long long, or long double.
 	   Handle that here.
