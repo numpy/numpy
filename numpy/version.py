6dd69530d58ba65f4a37167ddec7c9cf19e44c18
Travis Oliphant <oliphant@enthought.com>
2006-07-21 08:48:36 +0000

    Remove svn number from 1.0b1 version

--- numpy/version.py
+++ numpy/version.py
@@ -1,12 +1,13 @@
 version='1.0b1'
 
-import os
-svn_version_file = os.path.join(os.path.dirname(__file__),
-                                'core','__svn_version__.py')
-if os.path.isfile(svn_version_file):
-    import imp
-    svn = imp.load_module('numpy.core.__svn_version__',
-                          open(svn_version_file),
-                          svn_version_file,
-                          ('.py','U',1))
-    version += '.'+svn.version
+#import os
+#svn_version_file = os.path.join(os.path.dirname(__file__),
+#                                'core','__svn_version__.py')
+#svn_version_file=
+#if os.path.isfile(svn_version_file):
+#    import imp
+#    svn = imp.load_module('numpy.core.__svn_version__',
+#                          open(svn_version_file),
+#                          svn_version_file,
+#                          ('.py','U',1))
+#    version += '.'+svn.version
