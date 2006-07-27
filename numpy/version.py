006cee584e54c3cfe68ad018bb71cba3d3ec614e
Travis Oliphant <oliphant@enthought.com>
2006-07-27 19:00:53 +0000

    Fix-up version.py so that a release just changes a variable to True.

--- numpy/version.py
+++ numpy/version.py
@@ -1,12 +1,14 @@
 version='1.0b2'
+release=False
 
-import os
-svn_version_file = os.path.join(os.path.dirname(__file__),
-                                'core','__svn_version__.py')
-if os.path.isfile(svn_version_file):
-    import imp
-    svn = imp.load_module('numpy.core.__svn_version__',
-                          open(svn_version_file),
-                          svn_version_file,
-                          ('.py','U',1))
-    version += '.dev'+svn.version
+if not release:
+    import os
+    svn_version_file = os.path.join(os.path.dirname(__file__),
+                                   'core','__svn_version__.py')
+    if os.path.isfile(svn_version_file):
+        import imp
+        svn = imp.load_module('numpy.core.__svn_version__',
+                              open(svn_version_file),
+                              svn_version_file,
+                              ('.py','U',1))
+        version += '.dev'+svn.version
