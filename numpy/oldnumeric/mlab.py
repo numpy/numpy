7b50608412dc41f79984aca28abd3019ac513394
Travis Oliphant <oliphant@enthought.com>
2006-08-14 07:32:22 +0000

    Remove un-needed functions.

--- numpy/oldnumeric/mlab.py
+++ numpy/oldnumeric/mlab.py
@@ -7,8 +7,8 @@ import random_array as RandomArray
 from numpy import tril, trapz as _Ntrapz, hanning, rot90, triu, diff, \
      angle, roots, ptp as _Nptp, kaiser, cumprod as _Ncumprod, \
      diag, msort, prod as _Nprod, std as _Nstd, hamming, flipud, \
-     amax as _Nmax, amin as _Nmin, blackman, bartlett, corrcoef as _Ncorrcoef,\
-     cov as _Ncov, squeeze, sinc, median, fliplr, mean as _Nmean, transpose
+     amax as _Nmax, amin as _Nmin, blackman, bartlett, \
+     squeeze, sinc, median, fliplr, mean as _Nmean, transpose
 
 from numpy.linalg import eig, svd
 from numpy.random import rand, randn
