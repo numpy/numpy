1f0508e85e320c486e88bbca329a70bcd467cdc1
Travis Oliphant <oliphant@enthought.com>
2006-09-20 00:10:42 +0000

    Remove UserArray.py and dft sub-package.

--- numpy/dft/__init__.py
+++
@@ -1,4 +0,0 @@
-import warnings
-warnings.warn("The dft subpackage will be removed by 1.0 final -- it is now called fft")
-from numpy.fft import *
-del warnings
--- numpy/lib/UserArray.py
+++
@@ -1,3 +0,0 @@
-from user_array import container as UserArray 
-import warnings
-warnings.warn('UserArray.UserArray will be removed by 1.0final use user_array.container')
