dff8d9e31223e31a0544ee743b7be80f47ce9ac3
Travis Oliphant <oliphant@enthought.com>
2006-09-23 03:47:27 +0000

    Fix the Python2.5-compatibility macros so that conflicts with other libraries are avoided.

--- numpy/core/include/numpy/ndarrayobject.h
+++ numpy/core/include/numpy/ndarrayobject.h
@@ -639,11 +639,18 @@ typedef Py_uintptr_t npy_uintp;
 #define NPY_SIZEOF_INTP SIZEOF_PY_INTPTR_T
 #define NPY_SIZEOF_UINTP SIZEOF_PY_INTPTR_T
 
+#ifdef constchar
+#undef constchar
+#endif
+
 #if (PY_VERSION_HEX < 0x02050000)
-typedef int Py_ssize_t;
-#define PY_SSIZE_T_MAX INT_MAX
-#define PY_SSIZE_T_MIN INT_MIN
+  #ifndef PY_SSIZE_T_MIN
+    typedef int Py_ssize_t;
+    #define PY_SSIZE_T_MAX INT_MAX
+    #define PY_SSIZE_T_MIN INT_MIN
+  #endif 
 #define NPY_SSIZE_T_PYFMT "i"
+#undef PyIndex_Check
 #define constchar const char
 #define PyIndex_Check(op) 0
 #else
