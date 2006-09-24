46bd2a34e3006a5047d637543f472893e418462f
Travis Oliphant <oliphant@enthought.com>
2006-09-24 07:08:26 +0000

    Fix doctests for new dtype

--- numpy/lib/tests/test_ufunclike.py
+++ numpy/lib/tests/test_ufunclike.py
@@ -18,26 +18,26 @@ array([[ 1.,  1.,  1.,  1.],
 Test isposinf, isneginf, sign
 >>> a = nx.array([nx.Inf, -nx.Inf, nx.NaN, 0.0, 3.0, -3.0])
 >>> U.isposinf(a)
-array([True, False, False, False, False, False], dtype=bool)
+array([True, False, False, False, False, False], dtype='bool')
 >>> U.isneginf(a)
-array([False, True, False, False, False, False], dtype=bool)
+array([False, True, False, False, False, False], dtype='bool')
 >>> nx.sign(a)
 array([ 1., -1.,  0.,  0.,  1., -1.])
 
 Same thing with an output array:
 >>> y = nx.zeros(a.shape, bool)
 >>> U.isposinf(a, y)
-array([True, False, False, False, False, False], dtype=bool)
+array([True, False, False, False, False, False], dtype='bool')
 >>> y
-array([True, False, False, False, False, False], dtype=bool)
+array([True, False, False, False, False, False], dtype='bool')
 >>> U.isneginf(a, y)
-array([False, True, False, False, False, False], dtype=bool)
+array([False, True, False, False, False, False], dtype='bool')
 >>> y
-array([False, True, False, False, False, False], dtype=bool)
+array([False, True, False, False, False, False], dtype='bool')
 >>> nx.sign(a, y)
-array([True, True, False, False, True, True], dtype=bool)
+array([True, True, False, False, True, True], dtype='bool')
 >>> y
-array([True, True, False, False, True, True], dtype=bool)
+array([True, True, False, False, True, True], dtype='bool')
 
 Now log2:
 >>> a = nx.array([4.5, 2.3, 6.5])
