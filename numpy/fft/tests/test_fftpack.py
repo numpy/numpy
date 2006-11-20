1ad56e631c29869d127931b555d0b366f7e75641
Stefan van der Walt <stefan@sun.ac.za>
2006-11-20 07:06:48 +0000

    Add test for fftpack.

---
+++ numpy/fft/tests/test_fftpack.py
@@ -0,0 +1,12 @@
+import sys
+from numpy.testing import *
+set_package_path()
+from numpy.fft import *
+restore_path()
+
+class test_fftshift(NumpyTestCase):
+    def check_fft_n(self):
+        self.failUnlessRaises(ValueError,fft,[1,2,3],0)
+
+if __name__ == "__main__":
+    NumpyTest().run()
