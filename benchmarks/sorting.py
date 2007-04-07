a083ce52b673c6dcadd800d64bb098d89f1e8e12
Charles Harris <charlesr.harris@gmail.com>
2007-04-07 20:19:17 +0000

    Make sorting benchmarks all sort integers

--- benchmarks/sorting.py
+++ benchmarks/sorting.py
@@ -5,21 +5,21 @@ b = Benchmark(modules,runs=3,reps=100)
 
 N = 10000
 b.title = 'Sorting %d elements' % N
-b['numarray'] = ('a=N.array(None,shape=%d);a.sort()'%N,'')
-b['numpy'] = ('a=N.empty(shape=%d);a.sort()'%N,'')
-b['Numeric'] = ('a=N.empty(shape=%d);N.sort(a)'%N,'')
+b['numarray'] = ('a=N.array(None,shape=%d,typecode="i");a.sort()'%N,'')
+b['numpy'] = ('a=N.empty(shape=%d, dtype="i");a.sort()'%N,'')
+b['Numeric'] = ('a=N.empty(shape=%d, typecode="i");N.sort(a)'%N,'')
 b.run()
 
 N1,N2 = 100,100
 b.title = 'Sorting (%d,%d) elements, last axis' % (N1,N2)
-b['numarray'] = ('a=N.array(None,shape=(%d,%d));a.sort()'%(N1,N2),'')
-b['numpy'] = ('a=N.empty(shape=(%d,%d));a.sort()'%(N1,N2),'')
-b['Numeric'] = ('a=N.empty(shape=(%d,%d));N.sort(a)'%(N1,N2),'')
+b['numarray'] = ('a=N.array(None,shape=(%d,%d),typecode="i");a.sort()'%(N1,N2),'')
+b['numpy'] = ('a=N.empty(shape=(%d,%d), dtype="i");a.sort()'%(N1,N2),'')
+b['Numeric'] = ('a=N.empty(shape=(%d,%d),typecode="i");N.sort(a)'%(N1,N2),'')
 b.run()
 
 N1,N2 = 100,100
 b.title = 'Sorting (%d,%d) elements, first axis' % (N1,N2)
-b['numarray'] = ('a=N.array(None,shape=(%d,%d));a.sort(0)'%(N1,N2),'')
-b['Numeric'] = ('a=N.empty(shape=(%d,%d));N.sort(a,0)'%(N1,N2),'')
-b['numpy'] = ('a=N.empty(shape=(%d,%d));N.sort(a,0)'%(N1,N2),'')
+b['numarray'] = ('a=N.array(None,shape=(%d,%d), typecode="i");a.sort(0)'%(N1,N2),'')
+b['numpy'] = ('a=N.empty(shape=(%d,%d),dtype="i");N.sort(a,0)'%(N1,N2),'')
+b['Numeric'] = ('a=N.empty(shape=(%d,%d),typecode="i");N.sort(a,0)'%(N1,N2),'')
 b.run()
