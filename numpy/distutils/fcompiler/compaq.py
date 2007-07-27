a39ebbcee92b6e5572d3159bf50126213e6bf887
Pearu Peterson <pearu.peterson@gmail.com>
2007-07-27 09:20:08 +0000

    Trying to fix Windows XP x86_64 MSVC build.

--- numpy/distutils/fcompiler/compaq.py
+++ numpy/distutils/fcompiler/compaq.py
@@ -79,7 +79,12 @@ class CompaqVisualFCompiler(FCompiler):
             m.initialize()
             ar_exe = m.lib
         except DistutilsPlatformError, msg:
-            print 'Ignoring %s (one should fix me in fcompiler/compaq.py)' % (msg)
+            print 'Ignoring "%s" (one should fix me in fcompiler/compaq.py)' % (msg)
+        except AttributeError, msg:
+            if '_MSVCCompiler__root' in str(msg):
+                print 'Ignoring "%s" (I think it is msvccompiler.py bug)' % (msg)
+            else:
+                raise
 
     executables = {
         'version_cmd'  : ['<F90>', "/what"],
