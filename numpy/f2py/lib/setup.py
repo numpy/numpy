2ff91f96ae8ed6c53c5822696e5527f12b9e1e2c
Stefan van der Walt <stefan@sun.ac.za>
2007-08-04 21:03:20 +0000

    Add extgen subpackage.

--- numpy/f2py/lib/setup.py
+++ numpy/f2py/lib/setup.py
@@ -3,6 +3,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('lib',parent_package,top_path)
     config.add_subpackage('parser')
+    config.add_subpackage('extgen')
     config.add_data_files('*.txt','parser/*.txt')
     config.add_data_dir('src')
     return config
