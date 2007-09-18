344670349e4a947b29e9252845692287510c8894
David Cournapeau <cournape@gmail.com>
2007-09-18 05:30:49 +0000

    Replace suncc call by cc on solaris for sun compilers

--- numpy/distutils/sunccompiler.py
+++ numpy/distutils/sunccompiler.py
@@ -5,9 +5,12 @@ from numpy.distutils.exec_command import find_executable
 class SunCCompiler(UnixCCompiler):
     """ A modified Sun compiler compatible with an gcc built Python. """
     compiler_type = 'sun'
-    # Use suncc instead of cc, because it makes it more obvious to follow
-    # what's going on when several compilers are available.
-    cc_exe = 'suncc'
+    if platform[:5] == 'linux':
+        # Use suncc instead of cc, because it makes it more obvious to follow
+        # what's going on when several compilers are available on linux.
+        cc_exe = 'suncc'
+    else:
+        cc_exe = 'cc'
 
     def __init__ (self, verbose=0, dry_run=0, force=0):
         UnixCCompiler.__init__ (self, verbose,dry_run, force)
