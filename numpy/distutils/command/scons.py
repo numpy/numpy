23ad96e7c49ef568274c5f03ef41e7dc003a3467
David Cournapeau <cournape@gmail.com>
2007-09-20 08:33:24 +0000

    Now, scons is launched for each scons scripts added by add_sconscript

--- numpy/distutils/command/scons.py
+++ numpy/distutils/command/scons.py
@@ -8,8 +8,16 @@ class scons(Command):
         pass
 
     def finalize_options(self):
-        pass
+        if self.distribution.has_scons_scripts():
+            print "Got it: scons scripts are %s" % self.distribution.scons_scripts
+            self.scons_scripts = self.distribution.scons_scripts
 
     def run(self):
-        pass
-            
+        # XXX: when a scons script is missing, scons only prints warnings, and
+        # does not return a failure (status is 0). We have to detect this from
+        # distutils (this cannot work for recursive scons builds...)
+        for i in self.scons_scripts:
+            cmd = "scons -f " + i + ' -I. '
+            import os
+            st = os.system(cmd)
+            print "status is %d" % st
