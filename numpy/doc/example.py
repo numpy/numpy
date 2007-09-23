76ab788ef20cfae3417b3d9a8513d6df13acd839
Jarrod Millman <millman@berkeley.edu>
2007-09-23 12:21:04 +0000

    more documentation

--- numpy/doc/example.py
+++ numpy/doc/example.py
@@ -1,3 +1,13 @@
+"""This is the docstring for the example.py module.  Modules names should
+have short, all-lowercase names.  The module name may have underscores if
+this improves readability.
+
+Every module should have a docstring at the very top of the file.  The
+module's docstring may extend over multiple lines.  If your docstring does
+extend over multiple lines, the closing three quotation marks must be on
+a line by itself, preferably preceeded by a blank line.
+
+"""
 
 __docformat__ = "restructuredtext en"
 
