d9ecd3db23e64f2f8acdc5256cbf9a7f184857a0
David Cournapeau <cournape@gmail.com>
2007-10-14 16:14:28 +0000

    * (scons) Better checker to get size of types

--- numpy/distutils/scons/__init__.py
+++ numpy/distutils/scons/__init__.py
@@ -1,4 +1,4 @@
 from numpyenv import GetNumpyEnvironment, GetNumpyOptions
 from libinfo_scons import NumpyCheckLib
-from custom_checkers import CheckSizeof
+from custom_checkers import CheckTypeSize
 from extension import get_python_inc
--- numpy/distutils/scons/custom_checkers.py
+++ numpy/distutils/scons/custom_checkers.py
@@ -1,59 +1,102 @@
-# size to try first for built-in types
-_PREF_SIZE = {
-    'short' : (2,),
-    'int' : (4,),
-    'long' : (4,8),
-    'long long' : (8,4),
-    'float' : (4,),
-    'double' : (8,),
-    'long double' : (12,),
-    'size_t' : (4,),
-    'Py_intptr_t' : (4,),
+# Sensible default for common types on common platforms.
+_DEFAULTS = {
+   'short' : [2,],
+   'int' : [4, 2],
+   'long' : [4, 8],
+   'long long' : [8, 4],
+   # Normally, there is no need to check unsigned types, because they are
+   # guaranteed to be of the same size than their signed counterpart.
+   'unsigned short' : [2,],
+   'unsigned int' : [4, 2],
+   'unsigned long' : [4, 8],
+   'unsigned long long' : [8, 4],
+   'float' : [4,],
+   'double' : [8,],
+   'long double' : [12,],
+   'size_t' : [4,],
 }
 
-def CheckSizeof(context, type, include = None, language = None):
-    """include should be a list of header."""
-    if language:
-        raise NotImplementedError("language arg not supported yet!")
+def CheckTypeSize(context, type, includes = None, language = 'C', size = None):
+   """This check can be used to get the size of a given type, or to check whether
+   the type is of expected size.
 
-    msg = context.Message('Checking size of type %s ... ' % type)
-    if include:
-        strinc = ["#include <%s>" % s for s in include]
-        src = "\n".join(strinc)
-    else:
-        src = ""
-    src += """
-typedef %s check_sizeof;
+   Arguments:
+       - type : str
+           the type to check
+       - includes : sequence
+           list of headers to include in the test code before testing the type
+       - language : str
+           'C' or 'C++'
+       - size : int
+           if given, will test wether the type has the given number of bytes.
+           If not given, will test against a list of sizes (all sizes between
+           0 and 16 bytes are tested).
+
+       Returns:
+           status : int
+               0 if the check failed, or the found size of the type if
+               the check succeeded."""
+   minsz = 0
+   maxsz = 16
+
+   if includes:
+       src = "\n".join([r"#include <%s>\n" % i for i in includes])
+   else:
+       src = ""
+
+   if language == 'C':
+       ext = '.c'
+   elif language == 'C++':
+       ext = '.cpp'
+   else:
+       raise NotImplementedError("%s is not a recognized language" % language)
+
+   # test code taken from autoconf: this is a pretty clever hack to find that
+   # a type is of a given size using only compilation. This speeds things up
+   # quite a bit compared to straightforward code using TryRun
+   src += r"""
+typedef %s scons_check_type;
 
 int main()
 {
-    static int test_array[1 - 2 * !( (long int) (sizeof(check_sizeof)) <= %d)];
-    test_array[0] = 0;
+   static int test_array[1 - 2 * !(((long int) (sizeof(scons_check_type))) <= %d)];
+   test_array[0] = 0;
 
-    return 0;
+   return 0;
 }
 """
 
-    st = 0
-    # First try sensible default
-    if _PREF_SIZE.has_key(type):
-        fs = _PREF_SIZE[type]
-        for i in fs:
-            st = context.TryCompile(src % (type, i), '.c')
-            if st:
-                break
-    # General 
-    maxsize = 16
-    if not st:
-        for i in xrange(0, maxsize):
-            st = context.TryCompile(src % (type, i), '.c')
-            if st:
-                break
+   if size:
+       # Only check if the given size is the right one
+       context.Message('Checking %s is %d bytes... ' % (type, size))
+       st = context.TryCompile(src % (type, size), ext)
+       context.Result(st)
 
-    if st:
-        context.Result('%d bytes' % i)
-    else:
-        context.Result('Failed !')
+       if st:
+           return size
+       else:
+           return 0
+   else:
+       # Check against a list of sizes.
+       context.Message('Checking size of %s ... ' % type)
 
-    return st
+       # Try sensible defaults first
+       try:
+           szrange = _DEFAULTS[type]
+       except KeyError:
+           szrange = []
+       szrange.extend(xrange(minsz, maxsz))
+       st = 0
 
+       # Actual test
+       for sz in szrange:
+           st = context.TryCompile(src % (type, sz), ext)
+           if st:
+               break
+
+       if st:
+           context.Result('%d' % sz)
+           return sz
+       else:
+           context.Result('Failed !')
+           return 0
