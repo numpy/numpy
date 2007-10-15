9b90b54e7b5104994e73148f8a07d41e950a399b
David Cournapeau <cournape@gmail.com>
2007-10-15 06:48:05 +0000

    Start working on the problem of adding scons generated files to the distutils disttribution

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-PREFIX=/usr/src/dsp/bzrversion/numpy.scons/tmp
+PREFIX=/usr/media/src/src/dsp/bzrversion/numpy.scons/tmp
 rm -rf build
 rm -rf $PREFIX
-python setupscons.py install --prefix=$PREFIX
-#python setup.py install --prefix=$PREFIX
-(cd $PREFIX && PYTHONPATH=$PREFIX/lib/python2.5/site-packages python -c "import numpy; print numpy; numpy.test(level = 9999)")
+python setupscons.py scons
+python setupscons.py build
+#--prefix=$PREFIX
+##python setup.py install --prefix=$PREFIX
+#(cd $PREFIX && PYTHONPATH=$PREFIX/lib/python2.5/site-packages python -c "import numpy; print numpy; numpy.test(level = 9999)")
