38081ddb789ae6c9a33a0ece0dcb3a25fe233285
David Cournapeau <cournape@gmail.com>
2007-10-18 12:40:30 +0000

    fft consified

--- numpy/fft/setupscons.py
+++ numpy/fft/setupscons.py
@@ -1,12 +1,13 @@
-
-
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration('fft',parent_package,top_path)
+def configuration(parent_package = '', top_path = None):
+    from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
+    config = Configuration('fft', parent_package, top_path)
 
     config.add_data_dir('tests')
 
+    print "!!!!!! %s !!!!!!!!!" % get_numpy_include_dirs()
+
     # Configure fftpack_lite
+    config.add_sconscript('SConstruct')
     config.add_extension('fftpack_lite',
                          sources=['fftpack_litemodule.c', 'fftpack.c']
                          )
