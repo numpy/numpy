a9fac4d56e11e917bab8cb6cc515e417ea1fbf26
David Cournapeau <cournape@gmail.com>
2007-10-25 05:09:12 +0000

    Make test script a one liner for numpy installation

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 PREFIX=$PWD
 rm -rf $PREFIX/build
 rm -rf $PREFIX/tmp
-python setup.py scons --jobs=4
-python setup.py build
-python setup.py install --prefix=$PREFIX/tmp
+python setup.py scons --jobs=4 install --prefix=$PREFIX/tmp
 (cd $PREFIX/tmp && PYTHONPATH=$PREFIX/tmp/lib/python2.5/site-packages python -c "import numpy; print numpy; numpy.test(level = 9999)")
