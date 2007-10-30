8c1e7c94998b827ecf4845ce298e8e4e7605de37
David Cournapeau <cournape@gmail.com>
2007-10-30 06:44:56 +0000

    Add checkers subpackage in distutils.scons

--- numpy/distutils/scons/setup.py
+++ numpy/distutils/scons/setup.py
@@ -4,6 +4,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('scons',parent_package,top_path)
     config.add_subpackage('tools')
+    config.add_subpackage('checkers')
     config.add_data_dir('tests')
     return config
 
