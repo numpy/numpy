9a80523a253feb6f080ffa5bdd44c727dec27b2c
David Cournapeau <cournape@gmail.com>
2007-10-30 04:12:38 +0000

    Add a test script for sdist command

---
+++ testsdist.sh
@@ -0,0 +1,8 @@
+PREFIX=$PWD
+SVNVER=4342
+rm -rf $PREFIX/build
+rm -rf $PREFIX/dist
+python setup.py sdist
+(cd $PREFIX/dist && tar -xzf numpy-1.0.4.dev$SVNVER.tar.gz)
+(cd $PREFIX/dist/numpy-1.0.4.dev$SVNVER && python setup.py scons)
+
