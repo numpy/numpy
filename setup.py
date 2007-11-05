47baaba716e4d58e81befd9afe057c8a0a20293a
Jarrod Millman <millman@berkeley.edu>
2007-11-05 20:36:03 +0000

    remove old reference to deleted file (r4384)

--- setup.py
+++ setup.py
@@ -50,7 +50,6 @@ def configuration(parent_package='',top_path=None):
 
     config.add_data_files(('numpy','*.txt'),
                           ('numpy','COMPATIBILITY'),
-                          ('numpy','scipy_compatibility'),
                           ('numpy','site.cfg.example'))
 
     config.get_version('numpy/version.py') # sets config.version
