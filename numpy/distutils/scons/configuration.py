1babc589731b36d370c576f39c02426c2132d5b5
David Cournapeau <cournape@gmail.com>
2007-11-22 04:55:44 +0000

    Avoid 'stringifying' configres for NUMPY_PKG_CONFIG from the beginning

--- numpy/distutils/scons/configuration.py
+++ numpy/distutils/scons/configuration.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python
-# Last Change: Thu Nov 01 06:00 PM 2007 J
+# Last Change: Thu Nov 22 12:00 PM 2007 J
 import os
 
 def add_info(env, name, opt):
     cfg = env['NUMPY_PKG_CONFIG']
-    cfg[name] = str(opt)
+    cfg[name] = opt
 
 def write_info(env):
     dir = os.path.dirname(env['NUMPY_PKG_CONFIG_FILE'])
     if not os.path.exists(dir):
         os.makedirs(dir)
+    cfg = env['NUMPY_PKG_CONFIG']
+    config_str = {}
+    for k, i in cfg.items():
+        config_str[k] = str(i)
     f = open(env['NUMPY_PKG_CONFIG_FILE'], 'w')
-    f.writelines("config = %s" % str(env['NUMPY_PKG_CONFIG']))
+    f.writelines("config = %s" % str(config_str))
     f.close()
