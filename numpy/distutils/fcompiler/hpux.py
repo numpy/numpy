ba07a502ccc27ad152e53ef86cc1429f296439c0
Pearu Peterson <pearu.peterson@gmail.com>
2008-02-12 11:49:11 +0000

    Fix type. Sorry about that.

--- numpy/distutils/fcompiler/hpux.py
+++ numpy/distutils/fcompiler/hpux.py
@@ -26,9 +26,9 @@ class HPUXFCompiler(FCompiler):
         return ['-O3']
     def get_libraries(self):
         return ['m']
-   def get_library_dirs(self):
-       opt = ['/usr/lib/hpux64']
-       return opt
+    def get_library_dirs(self):
+        opt = ['/usr/lib/hpux64']
+        return opt
     def get_version(self, force=0, ok_status=[256,0,1]):
         # XXX status==256 may indicate 'unrecognized option' or
         #     'no input file'. So, version_cmd needs more work.
