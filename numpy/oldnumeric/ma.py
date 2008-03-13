fd6d555f8e4cf04871c1c1ed71ad53e2b6009164
Travis Oliphant <oliphant@enthought.com>
2008-03-13 04:44:55 +0000

    Fix oldnumeric compatibility with ma

--- numpy/oldnumeric/ma.py
+++ numpy/oldnumeric/ma.py
@@ -1,8 +1,8 @@
 # Incompatibility in that getmask and a.mask returns nomask
 #  instead of None
 
-from numpy.core.ma import *
-import numpy.core.ma as nca
+from numpy.ma import *
+import numpy.ma as nca
 
 def repeat(a, repeats, axis=0):
     return nca.repeat(a, repeats, axis)
