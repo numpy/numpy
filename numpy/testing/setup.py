640018fac45026b3fd70846c62297bfedac5afa3
David Cournapeau <cournape@gmail.com>
2008-04-07 23:18:32 +0000

    Add the tests in setup file for testing package.

--- numpy/testing/setup.py
+++ numpy/testing/setup.py
@@ -3,6 +3,8 @@
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('testing',parent_package,top_path)
+
+    config.add_data_dir('tests')
     return config
 
 if __name__ == '__main__':
