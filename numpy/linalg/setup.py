56ec2be60ea1cfba72f79c96ee4151cec7be3d06
Stefan van der Walt <stefan@sun.ac.za>
2008-04-28 08:14:06 +0000

    Rename and reformat pythonxerbla.

--- numpy/linalg/setup.py
+++ numpy/linalg/setup.py
@@ -16,14 +16,14 @@ def configuration(parent_package='',top_path=None):
             return ext.depends[:-1]
         else:
             if sys.platform=='win32':
-                print "### Warning:  pythonxerbla.c is disabled ###"
+                print "### Warning:  python_xerbla.c is disabled ###"
                 return ext.depends[:1]
             return ext.depends[:2]
 
     config.add_extension('lapack_lite',
                          sources = [get_lapack_lite_sources],
                          depends=  ['lapack_litemodule.c',
-                                    'pythonxerbla.c',
+                                    'python_xerbla.c',
                                     'zlapack_lite.c', 'dlapack_lite.c',
                                     'blas_lite.c', 'dlamch.c',
                                     'f2c_lite.c','f2c.h'],
