1181f98d28b38385f3ca6a75af97649f7f01934a
David Cournapeau <cournape@gmail.com>
2008-05-22 03:14:53 +0000

    Remove trailing space.

--- numpy/core/src/multiarraymodule.c
+++ numpy/core/src/multiarraymodule.c
@@ -3895,15 +3895,15 @@ PyArray_TakeFrom(PyArrayObject *self0, PyObject *indices0, int axis,
         }
         obj = (PyArrayObject *)PyArray_FromArray(ret, self->descr,
                                                  flags);
-	if (obj == NULL) { 
- 		PyErr_SetString(PyExc_ValueError, 
- 		                "unable to create array of proper type from output array"); 
-		ret = NULL; 
-		Py_DECREF(self->descr); 
-		goto fail; 
-	} 
+	if (obj == NULL) {
+ 		PyErr_SetString(PyExc_ValueError,
+ 		                "unable to create array of proper type from output array");
+		ret = NULL;
+		Py_DECREF(self->descr);
+		goto fail;
+	}
  	else if (obj != ret) {
-		copyret = 1; 
+		copyret = 1;
 	}
         ret = obj;
     }
