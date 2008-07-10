f2abd6377ba69688713b7c6aef58005b31242f5a
Stefan van der Walt <stefan@sun.ac.za>
2008-07-10 18:26:29 +0000

    Sort numpy.doc names.

--- numpy/doc/reference/__init__.py
+++ numpy/doc/reference/__init__.py
@@ -4,6 +4,7 @@ ref_dir = os.path.join(os.path.dirname(__file__))
 
 __all__ = [f[:-3] for f in os.listdir(ref_dir) if f.endswith('.py') and
            not f.startswith('__')]
+__all__.sort()
 
 __doc__ = 'The following topics are available:\n' + \
           '\n - '.join([''] + __all__)
