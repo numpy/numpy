419cd5b72fd061fb5207e5c609d1ca33bda39ca9
David Cournapeau <cournape@gmail.com>
2008-11-07 08:14:06 +0000

    Disable looking for 3gf libraries; it causes more problem than it solves.

--- numpy/distutils/system_info.py
+++ numpy/distutils/system_info.py
@@ -520,8 +520,9 @@ class system_info:
             exts.append('.dylib')
         # Debian and Ubuntu added a g3f suffix to shared library to deal with
         # g77 -> gfortran ABI transition
-        if sys.platform[:5] == 'linux':
-            exts.append('.so.3gf')
+        # XXX: disabled, it hides more problem than it solves.
+        #if sys.platform[:5] == 'linux':
+        #    exts.append('.so.3gf')
         return exts
 
     def check_libs(self,lib_dir,libs,opt_libs =[]):
