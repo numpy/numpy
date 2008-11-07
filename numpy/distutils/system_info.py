db67966deec2965c5ba1aea865ed1690be901bed
David Cournapeau <cournape@gmail.com>
2008-11-07 07:50:45 +0000

    Fix typo (g3f->3gf).

--- numpy/distutils/system_info.py
+++ numpy/distutils/system_info.py
@@ -521,7 +521,7 @@ class system_info:
         # Debian and Ubuntu added a g3f suffix to shared library to deal with
         # g77 -> gfortran ABI transition
         if sys.platform[:5] == 'linux':
-            exts.append('.so.g3f')
+            exts.append('.so.3gf')
         return exts
 
     def check_libs(self,lib_dir,libs,opt_libs =[]):
