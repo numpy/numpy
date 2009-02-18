d52af7662c0a2c03d7708b3dafccd3b4de389d1d
David Cournapeau <cournape@gmail.com>
2009-02-18 17:20:50 +0000

    Check Python.h through compilation instead of looking for it on the FS + do not add python include, since it is already included by distutils.

--- numpy/core/setup.py
+++ numpy/core/setup.py
@@ -137,16 +137,12 @@ def configuration(parent_package='',top_path=None):
         if newer(__file__,target):
             config_cmd = config.get_config_cmd()
             log.info('Generating %s',target)
+
+            # Check we have the python header (-dev* packages on Linux)
+            config_cmd.check_header('Python.h')
+
             tc = generate_testcode(target)
-            from distutils import sysconfig
-            python_include = sysconfig.get_python_inc()
-            python_h = join(python_include, 'Python.h')
-            if not os.path.isfile(python_h):
-                raise SystemError,\
-                      "Non-existing %s. Perhaps you need to install"\
-                      " python-dev|python-devel." % (python_h)
-            result = config_cmd.try_run(tc,include_dirs=[python_include],
-                                        library_dirs = default_lib_dirs)
+            result = config_cmd.try_run(tc, library_dirs = default_lib_dirs)
             if not result:
                 raise SystemError,"Failed to test configuration. "\
                       "See previous error messages for more information."
