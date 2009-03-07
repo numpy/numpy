31f7df3dd1cceea282488c89cd087ab4361ada75
David Cournapeau <cournape@gmail.com>
2009-03-07 16:26:11 +0000

    Fix CPU macros for MS compilers.

--- numpy/core/include/numpy/npy_cpu.h
+++ numpy/core/include/numpy/npy_cpu.h
@@ -12,17 +12,18 @@
 #ifndef _NPY_CPUARCH_H_
 #define _NPY_CPUARCH_H_
 
-#if defined ( _i386_ ) || defined( __i386__ ) || defined(i386)
+#if defined( __i386__ ) || defined(i386) || defined(_M_IX86)
         /* 
 	 * __i386__ is defined by gcc and Intel compiler on Linux, 
-	 * _i386_ by VS compiler,
+	 * _M_IX86 by VS compiler,
 	 * i386 by Sun compilers on opensolaris at least
 	 */
         #define NPY_CPU_X86
-#elif defined(__x86_64__) || defined(__amd64__) || defined(__x86_64)
+#elif defined(__x86_64__) || defined(__amd64__) || defined(__x86_64) || defined(_M_AMD64)
         /* 
 	 * both __x86_64__ and __amd64__ are defined by gcc
 	 * __x86_64 defined by sun compiler on opensolaris at least
+	 * _M_AMD64 defined by MS compiler
 	 */
         #define NPY_CPU_AMD64
 #elif defined(__ppc__) || defined(__powerpc__)
