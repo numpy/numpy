7d75121a4d11bd31ec322523da9283790ac30f75
David Cournapeau <cournape@gmail.com>
2009-03-09 17:22:56 +0000

    Fix and re-enable regression test #947.

--- numpy/core/tests/test_regression.py
+++ numpy/core/tests/test_regression.py
@@ -1208,10 +1208,9 @@ class TestRegression(TestCase):
         a = np.array(1)
         self.failUnlessRaises(ValueError, lambda x: x.choose([]), a)
 
-    @dec.knownfailureif(True, "Overflowing ndmin arg of array ctor segfaults.")
     def test_array_ndmin_overflow(self):
         "Ticket #947."
-        a = np.array([1], ndmin=33)
+        self.assertRaises(ValueError, lambda: np.array([1], ndmin=33))
 
     def test_errobj_reference_leak(self, level=rlevel):
         """Ticket #955"""
