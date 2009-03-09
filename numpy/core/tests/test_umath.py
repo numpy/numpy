d771e4ee20a5970f20142816471f21761c127388
Pauli Virtanen <pav@iki.fi>
2009-03-09 23:09:55 +0000

    Fixed #836: ensure z**3 = z*z*z, z**2 == z*z and z**1 == z for complex pow+inf.
    C99 standard is silent on how complex pow should behave with inf/nan values,
    but gives leeway for handling special cases.

--- numpy/core/tests/test_umath.py
+++ numpy/core/tests/test_umath.py
@@ -27,13 +27,27 @@ class TestPower(TestCase):
         assert_equal(x**0, [1., 1., 1.])
         assert_equal(x**1, x)
         assert_equal(x**2, [-3+4j, -5+12j, -7+24j])
+        assert_equal(x**3, [(1+2j)**3, (2+3j)**3, (3+4j)**3])
+        assert_equal(x**4, [(1+2j)**4, (2+3j)**4, (3+4j)**4])
         assert_almost_equal(x**(-1), [1/(1+2j), 1/(2+3j), 1/(3+4j)])
+        assert_almost_equal(x**(-2), [1/(1+2j)**2, 1/(2+3j)**2, 1/(3+4j)**2])
         assert_almost_equal(x**(-3), [(-11+2j)/125, (-46-9j)/2197,
                                       (-117-44j)/15625])
         assert_almost_equal(x**(0.5), [ncu.sqrt(1+2j), ncu.sqrt(2+3j),
                                        ncu.sqrt(3+4j)])
         assert_almost_equal(x**14, [-76443+16124j, 23161315+58317492j,
                                     5583548873 +  2465133864j])
+
+        # Ticket #836
+        def assert_complex_equal(x, y):
+            assert_array_equal(x.real, y.real)
+            assert_array_equal(x.imag, y.imag)
+        
+        for z in [complex(0, np.inf), complex(1, np.inf)]:
+            z = np.array([z], dtype=np.complex_)
+            assert_complex_equal(z**1, z)
+            assert_complex_equal(z**2, z*z)
+            assert_complex_equal(z**3, z*z*z)
 
 class TestLog2(TestCase):
     def test_log2_values(self) :
