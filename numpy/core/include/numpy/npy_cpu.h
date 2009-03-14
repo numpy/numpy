5f9892390e1beb9263448086ad968a7205ef258a
Charles Harris <charlesr.harris@gmail.com>
2009-03-14 02:13:50 +0000

    Add macros for ia64

--- numpy/core/include/numpy/npy_cpu.h
+++ numpy/core/include/numpy/npy_cpu.h
@@ -7,42 +7,47 @@
  *              NPY_CPU_PPC64
  *              NPY_CPU_SPARC
  *              NPY_CPU_S390
+ *              NPY_CPU_IA64
  *              NPY_CPU_PARISC
  */
 #ifndef _NPY_CPUARCH_H_
 #define _NPY_CPUARCH_H_
 
 #if defined( __i386__ ) || defined(i386) || defined(_M_IX86)
-        /* 
-	 * __i386__ is defined by gcc and Intel compiler on Linux, 
-	 * _M_IX86 by VS compiler,
-	 * i386 by Sun compilers on opensolaris at least
-	 */
-        #define NPY_CPU_X86
+    /*
+     * __i386__ is defined by gcc and Intel compiler on Linux,
+     * _M_IX86 by VS compiler,
+     * i386 by Sun compilers on opensolaris at least
+     */
+    #define NPY_CPU_X86
 #elif defined(__x86_64__) || defined(__amd64__) || defined(__x86_64) || defined(_M_AMD64)
-        /* 
-	 * both __x86_64__ and __amd64__ are defined by gcc
-	 * __x86_64 defined by sun compiler on opensolaris at least
-	 * _M_AMD64 defined by MS compiler
-	 */
-        #define NPY_CPU_AMD64
+    /*
+     * both __x86_64__ and __amd64__ are defined by gcc
+     * __x86_64 defined by sun compiler on opensolaris at least
+     * _M_AMD64 defined by MS compiler
+     */
+    #define NPY_CPU_AMD64
 #elif defined(__ppc__) || defined(__powerpc__)
-        /* __ppc__ is defined by gcc, I remember having seen __powerpc__ once,
-         * but can't find it ATM */
-        #define NPY_CPU_PPC
+    /*
+     * __ppc__ is defined by gcc, I remember having seen __powerpc__ once,
+     * but can't find it ATM
+     */
+    #define NPY_CPU_PPC
 #elif defined(__ppc64__)
-        #define NPY_CPU_PPC64
+    #define NPY_CPU_PPC64
 #elif defined(__sparc__) || defined(__sparc)
-        /* __sparc__ is defined by gcc and Forte (e.g. Sun) compilers */
-        #define NPY_CPU_SPARC
+    /* __sparc__ is defined by gcc and Forte (e.g. Sun) compilers */
+    #define NPY_CPU_SPARC
 #elif defined(__s390__)
-        #define NPY_CPU_S390
+    #define NPY_CPU_S390
+#elif defined(__ia64)
+    #define NPY_CPU_IA64
 #elif defined(__parisc__)
-        /* XXX: Not sure about this one... */
-        #define NPY_CPU_PARISC
+    /* XXX: Not sure about this one... */
+    #define NPY_CPU_PARISC
 #else
-        #error Unknown CPU, please report this to numpy maintainers with \
-        information about your platform (OS, CPU and compiler)
+    #error Unknown CPU, please report this to numpy maintainers with \
+    information about your platform (OS, CPU and compiler)
 #endif
 
 #endif
--- numpy/core/include/numpy/npy_endian.h
+++ numpy/core/include/numpy/npy_endian.h
@@ -7,31 +7,31 @@
  */
 
 #ifdef NPY_HAVE_ENDIAN_H
-        /* Use endian.h if available */
-        #include <endian.h>
-        #define NPY_BYTE_ORDER __BYTE_ORDER
-        #if (__BYTE_ORDER == __LITTLE_ENDIAN)
-                #define NPY_LITTLE_ENDIAN
-        #elif (__BYTE_ORDER == __BIG_ENDIAN)
-                #define NPY_BIG_ENDIAN
-        #else
-                #error Unknown machine endianness detected.
-        #endif
+    /* Use endian.h if available */
+    #include <endian.h>
+    #define NPY_BYTE_ORDER __BYTE_ORDER
+    #if (__BYTE_ORDER == __LITTLE_ENDIAN)
+        #define NPY_LITTLE_ENDIAN
+    #elif (__BYTE_ORDER == __BIG_ENDIAN)
+        #define NPY_BIG_ENDIAN
+    #else
+        #error Unknown machine endianness detected.
+    #endif
 #else
-        /* Set endianness info using target CPU */
-        #include "npy_cpu.h"
+    /* Set endianness info using target CPU */
+    #include "npy_cpu.h"
 
-        #if defined(NPY_CPU_X86) || defined(NPY_CPU_AMD64)
-		#define NPY_LITTLE_ENDIAN
-		#define NPY_BYTE_ORDER 1234
-        #elif defined(NPY_CPU_PPC) || defined(NPY_CPU_SPARC) || \
-	      defined(NPY_CPU_S390) || \
-              defined(NPY_CPU_PARISC)
-		#define NPY_BIG_ENDIAN
-		#define NPY_BYTE_ORDER 4321
-        #else
-                #error Unknown CPU: can not set endianness
-        #endif
+    #if defined(NPY_CPU_X86) || defined(NPY_CPU_AMD64)\
+            || defined(NPY_CPU_IA64)
+        #define NPY_LITTLE_ENDIAN
+        #define NPY_BYTE_ORDER 1234
+    #elif defined(NPY_CPU_PPC) || defined(NPY_CPU_SPARC)\
+            || defined(NPY_CPU_S390) || defined(NPY_CPU_PARISC)
+        #define NPY_BIG_ENDIAN
+        #define NPY_BYTE_ORDER 4321
+    #else
+        #error Unknown CPU: can not set endianness
+    #endif
 #endif
 
 #endif
