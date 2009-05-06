cef776f141a1e11dd8efe56b6861f7a694e8b0d4
David Cournapeau <cournape@gmail.com>
2009-05-06 09:32:11 +0000

    Fix missing declaration.

--- numpy/core/src/multiarray/arraytypes.h
+++ numpy/core/src/multiarray/arraytypes.h
@@ -2,6 +2,7 @@
 #define _NPY_ARRAYTYPES_H_
 
 #ifdef NPY_ENABLE_SEPARATE_COMPILATION
+extern NPY_NO_EXPORT PyArray_Descr LONGLONG_Descr;
 extern NPY_NO_EXPORT PyArray_Descr LONG_Descr;
 extern NPY_NO_EXPORT PyArray_Descr INT_Descr;
 #endif
