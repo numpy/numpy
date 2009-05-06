f97b16539903d9cdf18fedde80d8c70e629f7935
David Cournapeau <cournape@gmail.com>
2009-05-06 08:54:03 +0000

    Include arraytypes.c early for the no  separate compilation case.

--- numpy/core/src/multiarray/multiarraymodule_onefile.c
+++ numpy/core/src/multiarray/multiarraymodule_onefile.c
@@ -1,9 +1,13 @@
 /*
  * This file includes all the .c files needed for a complete multiarray module.
  * This is used in the case where separate compilation is not enabled
+ *
+ * Note that the order of the includs matters
  */
 
 #include "common.c"
+
+#include "arraytypes.c"
 
 #include "hashdescr.c"
 #include "numpyos.c"
@@ -27,7 +31,6 @@
 #include "conversion_utils.c"
 #include "buffer.c"
 
-#include "arraytypes.c"
 #include "scalartypes.c"
 
 #ifndef Py_UNICODE_WIDE
