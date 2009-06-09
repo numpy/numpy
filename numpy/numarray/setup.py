988678cf6d0eb8459588e1067dd3a91468cbaa2d
Stefan van der Walt <stefan@sun.ac.za>
2009-06-09 15:35:38 +0000

    Fix installation of numarray headers on Windows.

--- numpy/numarray/setup.py
+++ numpy/numarray/setup.py
@@ -4,7 +4,7 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('numarray',parent_package,top_path)
 
-    config.add_data_files('numpy/')
+    config.add_data_files('numpy/*')
 
     config.add_extension('_capi',
                          sources=['_capi.c'],
