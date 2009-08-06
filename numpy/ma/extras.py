* fixed np.ma.average on integersw/ mask (bug #1188)

numpy/ma/extras.py
@@ -413,7 +413,7 @@ def average(a, axis=None, weights=None, returned=False):
         else:
             if weights is None:
                 n = a.filled(0).sum(axis=None)
-                d = umath.add.reduce((-mask).ravel().astype(int))
+                d = float(umath.add.reduce((~mask).ravel()))
             else:
                 w = array(filled(weights, 0.0), float, mask=mask).ravel()
                 n = add.reduce(a.ravel() * w)

numpy/ma/tests/test_extras.py
@@ -94,19 +94,19 @@ class TestAverage(TestCase):
     "Several tests of average. Why so many ? Good point..."
     def test_testAverage1(self):
         "Test of average."
-        ott = array([0.,1.,2.,3.], mask=[1,0,0,0])
-        assert_equal(2.0, average(ott,axis=0))
+        ott = array([0.,1.,2.,3.], mask=[True, False, False, False])
+        assert_equal(2.0, average(ott, axis=0))
         assert_equal(2.0, average(ott, weights=[1., 1., 2., 1.]))
-        result, wts = average(ott, weights=[1.,1.,2.,1.], returned=1)
+        result, wts = average(ott, weights=[1., 1., 2., 1.], returned=1)
         assert_equal(2.0, result)
         self.failUnless(wts == 4.0)
         ott[:] = masked
-        assert_equal(average(ott,axis=0).mask, [True])
-        ott = array([0.,1.,2.,3.], mask=[1,0,0,0])
-        ott = ott.reshape(2,2)
+        assert_equal(average(ott, axis=0).mask, [True])
+        ott = array([0., 1., 2., 3.], mask=[True, False, False, False])
+        ott = ott.reshape(2, 2)
         ott[:,1] = masked
-        assert_equal(average(ott,axis=0), [2.0, 0.0])
-        assert_equal(average(ott,axis=1).mask[0], [True])
+        assert_equal(average(ott, axis=0), [2.0, 0.0])
+        assert_equal(average(ott, axis=1).mask[0], [True])
         assert_equal([2.,0.], average(ott, axis=0))
         result, wts = average(ott, axis=0, returned=1)
         assert_equal(wts, [1., 0.])
@@ -122,43 +122,44 @@ class TestAverage(TestCase):
         assert_equal(average(y, None), np.add.reduce(np.arange(6))*3./12.)
         assert_equal(average(y, axis=0), np.arange(6) * 3./2.)
         assert_equal(average(y, axis=1),
-                     [average(x,axis=0), average(x,axis=0) * 2.0])
+                     [average(x, axis=0), average(x, axis=0) * 2.0])
         assert_equal(average(y, None, weights=w2), 20./6.)
         assert_equal(average(y, axis=0, weights=w2),
                      [0.,1.,2.,3.,4.,10.])
         assert_equal(average(y, axis=1),
-                     [average(x,axis=0), average(x,axis=0) * 2.0])
+                     [average(x, axis=0), average(x, axis=0) * 2.0])
         m1 = zeros(6)
-        m2 = [0,0,1,1,0,0]
-        m3 = [[0,0,1,1,0,0],[0,1,1,1,1,0]]
+        m2 = [0, 0, 1, 1, 0, 0]
+        m3 = [[0, 0, 1, 1, 0, 0], [0, 1, 1, 1, 1, 0]]
         m4 = ones(6)
         m5 = [0, 1, 1, 1, 1, 1]
-        assert_equal(average(masked_array(x, m1),axis=0), 2.5)
-        assert_equal(average(masked_array(x, m2),axis=0), 2.5)
-        assert_equal(average(masked_array(x, m4),axis=0).mask, [True])
-        assert_equal(average(masked_array(x, m5),axis=0), 0.0)
-        assert_equal(count(average(masked_array(x, m4),axis=0)), 0)
+        assert_equal(average(masked_array(x, m1), axis=0), 2.5)
+        assert_equal(average(masked_array(x, m2), axis=0), 2.5)
+        assert_equal(average(masked_array(x, m4), axis=0).mask, [True])
+        assert_equal(average(masked_array(x, m5), axis=0), 0.0)
+        assert_equal(count(average(masked_array(x, m4), axis=0)), 0)
         z = masked_array(y, m3)
         assert_equal(average(z, None), 20./6.)
         assert_equal(average(z, axis=0), [0.,1.,99.,99.,4.0, 7.5])
         assert_equal(average(z, axis=1), [2.5, 5.0])
-        assert_equal(average(z,axis=0, weights=w2), [0.,1., 99., 99., 4.0, 10.0])
+        assert_equal(average(z,axis=0, weights=w2),
+                     [0.,1., 99., 99., 4.0, 10.0])
 
     def test_testAverage3(self):
         "Yet more tests of average!"
         a = arange(6)
         b = arange(6) * 3
-        r1, w1 = average([[a,b],[b,a]], axis=1, returned=1)
+        r1, w1 = average([[a, b], [b, a]], axis=1, returned=1)
         assert_equal(shape(r1) , shape(w1))
         assert_equal(r1.shape , w1.shape)
-        r2, w2 = average(ones((2,2,3)), axis=0, weights=[3,1], returned=1)
+        r2, w2 = average(ones((2, 2, 3)), axis=0, weights=[3, 1], returned=1)
         assert_equal(shape(w2) , shape(r2))
-        r2, w2 = average(ones((2,2,3)), returned=1)
+        r2, w2 = average(ones((2, 2, 3)), returned=1)
         assert_equal(shape(w2) , shape(r2))
-        r2, w2 = average(ones((2,2,3)), weights=ones((2,2,3)), returned=1)
+        r2, w2 = average(ones((2, 2, 3)), weights=ones((2, 2, 3)), returned=1)
         assert_equal(shape(w2), shape(r2))
-        a2d = array([[1,2],[0,4]], float)
-        a2dm = masked_array(a2d, [[0,0],[1,0]])
+        a2d = array([[1, 2], [0, 4]], float)
+        a2dm = masked_array(a2d, [[False, False],[True, False]])
         a2da = average(a2d, axis=0)
         assert_equal(a2da, [0.5, 3.0])
         a2dma = average(a2dm, axis=0)
@@ -168,6 +169,12 @@ class TestAverage(TestCase):
         a2dma = average(a2dm, axis=1)
         assert_equal(a2dma, [1.5, 4.0])
 
+    def test_onintegers_with_mask(self):
+        "Test average on integers with mask"
+        a = average(array([1, 2]))
+        assert_equal(a, 1.5)
+        a = average(array([1, 2, 3, 4], mask=[False, False, True, True]))
+        assert_equal(a, 1.5)
 
 
 class TestConcatenator(TestCase):