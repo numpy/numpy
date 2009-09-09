24def43896bc3c7ae3baa1dbd969ea2704324322
David Cournapeau <cournape@gmail.com>
2009-09-09 00:41:35 +0000

    Do not reuse build_ext options - most do not make sense for scons command.

--- numpy/distutils/command/scons.py
+++ numpy/distutils/command/scons.py
@@ -288,19 +288,18 @@ def check_numscons(minver=(0, 10, 2)):
 class scons(old_build_ext):
     # XXX: add an option to the scons command for configuration (auto/force/cache).
     description = "Scons builder"
-    user_options = old_build_ext.user_options + \
-            [('jobs=', None,
-              "specify number of worker threads when executing scons"),
-             ('inplace', 'i', 'If specified, build in place.'),
-             ('scons-tool-path=', None, 'specify additional path '\
-                                    '(absolute) to look for scons tools'),
-             ('silent=', None, 'specify whether scons output should less verbose'\
-                               '(1), silent (2), super silent (3) or not (0, default)'),
-             ('log-level=', None, 'specify log level for numscons. Any value valid '\
-                                  'for the logging python module is valid'),
-             ('package-list=', None, 'If specified, only run scons on the given '\
-                 'packages (example: --package-list=scipy.cluster). If empty, '\
-                 'no package is built')]
+    user_options = [('jobs=', 'j', "specify number of worker threads when executing scons"),
+                    ('inplace', 'i', 'If specified, build in place.'),
+                    ('scons-tool-path=', None, 'specify additional path '\
+                                               '(absolute) to look for scons tools'),
+                    ('silent=', None, 'specify whether scons output should less verbose'\
+                                      '(1), silent (2), super silent (3) or not (0, default)'),
+                    ('log-level=', None, 'specify log level for numscons. Any value valid '\
+                                         'for the logging python module is valid'),
+                    ('package-list=', None, 
+                     'If specified, only run scons on the given '\
+                     'packages (example: --package-list=scipy.cluster). If empty, '\
+                     'no package is built')]
 
     def initialize_options(self):
         old_build_ext.initialize_options(self)
