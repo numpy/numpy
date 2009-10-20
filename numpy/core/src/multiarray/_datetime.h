51dbc63927c562e8536ac0c324b4de52815a9d10
Charles Harris <charlesr.harris@gmail.com>
2009-10-20 17:32:59 +0000

    Cleanup _datetime.h Remove trailing whitespace. Remove hard tabs.

--- numpy/core/src/multiarray/_datetime.h
+++ numpy/core/src/multiarray/_datetime.h
@@ -1,13 +1,13 @@
 #ifndef _NPY_PRIVATE__DATETIME_H_
 #define _NPY_PRIVATE__DATETIME_H_
 
-NPY_NO_EXPORT void 
-PyArray_DatetimeToDatetimeStruct(npy_datetime val, NPY_DATETIMEUNIT fr, 
-				 npy_datetimestruct *result);
+NPY_NO_EXPORT void
+PyArray_DatetimeToDatetimeStruct(npy_datetime val, NPY_DATETIMEUNIT fr,
+                                 npy_datetimestruct *result);
 
-NPY_NO_EXPORT void 
-PyArray_TimedeltaToTimedeltaStruct(npy_timedelta val, NPY_DATETIMEUNIT fr, 
-				 npy_timedeltastruct *result);
+NPY_NO_EXPORT void
+PyArray_TimedeltaToTimedeltaStruct(npy_timedelta val, NPY_DATETIMEUNIT fr,
+                                 npy_timedeltastruct *result);
 
 NPY_NO_EXPORT npy_datetime
 PyArray_DatetimeStructToDatetime(NPY_DATETIMEUNIT fr, npy_datetimestruct *d);
