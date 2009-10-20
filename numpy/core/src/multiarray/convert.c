58bee13a3c0d34b225a341edd6082f064a11fd3a
Charles Harris <charlesr.harris@gmail.com>
2009-10-20 17:32:51 +0000

    Cleanup convert.c Remove hard tabs.

--- numpy/core/src/multiarray/convert.c
+++ numpy/core/src/multiarray/convert.c
@@ -34,15 +34,15 @@ PyArray_ToList(PyArrayObject *self)
     sz = self->dimensions[0];
     lp = PyList_New(sz);
     for (i = 0; i < sz; i++) {
-	v = (PyArrayObject *)array_big_item(self, i);
-	if (PyArray_Check(v) && (v->nd >= self->nd)) {
-	    PyErr_SetString(PyExc_RuntimeError,
-			    "array_item not returning smaller-"	\
-			    "dimensional array");
-	    Py_DECREF(v);
-	    Py_DECREF(lp);
-	    return NULL;
-	}
+        v = (PyArrayObject *)array_big_item(self, i);
+        if (PyArray_Check(v) && (v->nd >= self->nd)) {
+            PyErr_SetString(PyExc_RuntimeError,
+                            "array_item not returning smaller-" \
+                            "dimensional array");
+            Py_DECREF(v);
+            Py_DECREF(lp);
+            return NULL;
+        }
         PyList_SetItem(lp, i, PyArray_ToList(v));
         Py_DECREF(v);
     }
