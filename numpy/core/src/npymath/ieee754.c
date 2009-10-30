c2485bd8831192f045097dc5eeb44b80bfaacf0e
David Cournapeau <cournape@gmail.com>
2009-10-30 10:25:11 +0000

    STY: fix indentation.

--- numpy/core/src/npymath/ieee754.c
+++ numpy/core/src/npymath/ieee754.c
@@ -12,24 +12,24 @@
 #ifndef HAVE_COPYSIGN
 double npy_copysign(double x, double y)
 {
-        npy_uint32 hx,hy;
-        GET_HIGH_WORD(hx,x);
-        GET_HIGH_WORD(hy,y);
-        SET_HIGH_WORD(x,(hx&0x7fffffff)|(hy&0x80000000));
-        return x;
+    npy_uint32 hx, hy;
+    GET_HIGH_WORD(hx, x);
+    GET_HIGH_WORD(hy, y);
+    SET_HIGH_WORD(x, (hx & 0x7fffffff) | (hy & 0x80000000));
+    return x;
 }
 #endif
 
 #if !defined(HAVE_DECL_SIGNBIT)
 #include "_signbit.c"
 
-int _npy_signbit_f (float x)
+int _npy_signbit_f(float x)
 {
-    return _npy_signbit_d((double)x);
+    return _npy_signbit_d((double) x);
 }
 
-int _npy_signbit_ld (long double x)
+int _npy_signbit_ld(long double x)
 {
-    return _npy_signbit_d((double)x);
+    return _npy_signbit_d((double) x);
 }
 #endif
