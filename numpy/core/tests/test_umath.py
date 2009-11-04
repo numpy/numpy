6713e50ff2338600dae6b73cb5c51dc61c302928
David Cournapeau <cournape@gmail.com>
2009-11-04 07:46:48 +0000

    Start adding test for C99 compliance of our complex functions.

--- numpy/core/tests/test_umath.py
+++ numpy/core/tests/test_umath.py
@@ -2,6 +2,36 @@ from numpy.testing import *
 import numpy.core.umath as ncu
 import numpy as np
 
+def assert_equal_spec(x, y):
+    # Handles nan and inf
+    if np.isnan(x) and np.isnan(y):
+        pass
+    elif np.isinf(x) and np.isinf(y):
+        if x * y > 0:
+            pass
+        else:
+            raise AssertError("""
+Items are not equal:
+ ACTUAL: %s
+ DESIRED: %s""" % (str(x), str(y)))
+    else:
+        assert_equal(x, y)
+
+def assert_almost_equal_spec(x, y):
+    # Handles nan
+    if np.isnan(x) and np.isnan(y):
+        pass
+    elif np.isinf(x) and np.isinf(y):
+        if x * y > 0:
+            pass
+        else:
+            raise AssertError("""
+Items are not almost equal:
+ ACTUAL: %s
+ DESIRED: %s""" % (str(x), str(y)))
+    else:
+        assert_almost_equal(x, y)
+
 class TestDivision(TestCase):
     def test_division_int(self):
         # int division should return the floor of the result, a la Python
@@ -740,6 +770,180 @@ class TestComplexFunctions(object):
     @dec.knownfailureif(is_longdouble_finfo_bogus(), "Bogus long double finfo")
     def test_loss_of_precision_longcomplex(self):
         self.check_loss_of_precision(np.longcomplex)
+
+class TestClog(TestCase):
+    def test_simple(self):
+        x = np.array([1+0j, 1+2j])
+        y_r = np.log(np.abs(x)) + 1j * np.angle(x)
+        y = np.log(x)
+        for i in range(len(x)):
+            assert_almost_equal_spec(y[i], y_r[i])
+
+    def test_special_values(self):
+        xl = []
+        yl = []
+
+        # From C99 std (Sec 6.3.2)
+        # XXX: check exceptions raised
+
+        # clog(-0 + i0) returns -inf + i pi and raises the 'divide-by-zero'
+        # floating-point exception.
+        x = np.array([-0 + 0j], dtype=np.complex)
+        y = np.complex(-np.inf, np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(+0 + i0) returns -inf + i0 and raises the 'divide-by-zero'
+        # floating-point exception.
+        x = np.array([0 + 0j], dtype=np.complex)
+        y = np.complex(np.inf, np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(x + i inf returns +inf + i pi /2, for finite x.
+        x = np.array([1 + np.inf * 1j], dtype=np.complex)
+        y = np.complex(np.inf, 0.5 * np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        x = np.array([-1 + np.inf * 1j], dtype=np.complex)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        x = np.array([np.inf + np.inf * 1j], dtype=np.complex)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        x = np.array([np.nan + np.inf * 1j], dtype=np.complex)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(x + iNaN) returns NaN + iNaN and optionally raises the
+        # 'invalid' floating- point exception, for finite x.
+        x = np.array([1. + np.nan * 1j], dtype=np.complex)
+        y = np.complex(np.nan, np.nan)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        x = np.array([np.inf + np.nan * 1j], dtype=np.complex)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(- inf + iy) returns +inf + ipi , for finite positive-signed y.
+        x = np.array([-np.inf + 1j], dtype=np.complex)
+        y = np.complex(np.inf, np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(+ inf + iy) returns +inf + i0, for finite positive-signed y.
+        x = np.array([np.inf + 1j], dtype=np.complex)
+        y = np.complex(np.inf, 0)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(- inf + i inf) returns +inf + i3pi /4.
+        x = np.array([-np.inf + 1j * np.inf], dtype=np.complex)
+        y = np.complex(np.inf, 0.75 * np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(+ inf + i inf) returns +inf + ipi /4.
+        x = np.array([np.inf + 1j * np.inf], dtype=np.complex)
+        y = np.complex(np.inf, 0.25 * np.pi)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(+/- inf + iNaN) returns +inf + iNaN.
+        x = np.array([+np.inf + 1j * np.nan], dtype=np.complex)
+        y = np.complex(np.inf, np.nan)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        x = np.array([-np.inf + 1j * np.nan], dtype=np.complex)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(NaN + iy) returns NaN + iNaN and optionally raises the
+        # 'invalid' floating-point exception, for finite y.
+        x = np.array([np.nan + 1j], dtype=np.complex)
+        y = np.complex(np.nan, np.nan)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(NaN + i inf) returns +inf + iNaN.
+        x = np.array([np.nan + 1j * np.inf], dtype=np.complex)
+        y = np.complex(np.inf, np.nan)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(NaN + iNaN) returns NaN + iNaN.
+        x = np.array([np.nan + 1j * np.nan], dtype=np.complex)
+        y = np.complex(np.nan, np.nan)
+        assert_almost_equal(np.log(x), y)
+        xl.append(x)
+        yl.append(y)
+
+        # clog(conj(z)) = conj(clog(z)).
+        xa = np.array(xl, dtype=np.complex)
+        ya = np.array(yl, dtype=np.complex)
+        for i in range(len(xa)):
+            assert_almost_equal(np.log(np.conj(x[i])), np.conj(np.log(x[i])))
+
+class TestCpow(TestCase):
+    def test_simple(self):
+        x = np.array([1+1j, 0+2j, 1+2j, np.inf, np.nan])
+        y_r = x ** 2
+        y = np.power(x, 2)
+        for i in range(len(x)):
+            assert_almost_equal_spec(y[i], y_r[i])
+
+class TestCabs(TestCase):
+    def test_simple(self):
+        x = np.array([1+1j, 0+2j, 1+2j, np.inf, np.nan])
+        y_r = np.array([np.sqrt(2.), 2, np.sqrt(5), np.inf, np.nan])
+        y = np.abs(x)
+        for i in range(len(x)):
+            assert_almost_equal_spec(y[i], y_r[i])
+
+    def test_fabs(self):
+        # Tesst that np.abs(x +- 0j) == np.abs(x) (as mandated by C99 for cabs)
+        x = np.array([1+0j], dtype=np.complex)
+        assert_array_equal(np.abs(x), np.real(x))
+
+        x = np.array([1-0j], dtype=np.complex)
+        assert_array_equal(np.abs(x), np.real(x))
+
+        x = np.array([np.inf-0j], dtype=np.complex)
+        assert_array_equal(np.abs(x), np.real(x))
+
+        x = np.array([np.nan-0j], dtype=np.complex)
+        assert_array_equal(np.abs(x), np.real(x))
+
+    @dec.knownfailureif(True, "Buggy behavior of abs with inf complex")
+    def test_cabs_inf_nan(self):
+        # According to C99 standard, cabs(inf + j * nan) should be inf
+        x = np.array([np.inf + 1j * np.nan, np.inf + 1j * np.inf,
+            np.nan + 1j * np.nan])
+        y_r = np.array([np.inf, np.inf, np.nan])
+        y = np.abs(x)
+        for i in range(len(x)):
+            assert_equal_spec(y_r[i], y[i])
 
 class TestAttributes(TestCase):
     def test_attributes(self):
