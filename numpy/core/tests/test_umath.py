1dec6da92fa355620acfccd8e18a6fe80e06054e
David Cournapeau <cournape@gmail.com>
2009-11-04 07:59:11 +0000

    Fix tests for complex power function.

--- numpy/core/tests/test_umath.py
+++ numpy/core/tests/test_umath.py
@@ -49,17 +49,19 @@ class TestPower(TestCase):
         x = np.array([1+2j, 2+3j, 3+4j])
         assert_equal(x**0, [1., 1., 1.])
         assert_equal(x**1, x)
-        assert_equal(x**2, [-3+4j, -5+12j, -7+24j])
-        assert_equal(x**3, [(1+2j)**3, (2+3j)**3, (3+4j)**3])
-        assert_equal(x**4, [(1+2j)**4, (2+3j)**4, (3+4j)**4])
+        assert_almost_equal(x**2, [-3+4j, -5+12j, -7+24j])
+        assert_almost_equal(x**3, [(1+2j)**3, (2+3j)**3, (3+4j)**3])
+        assert_almost_equal(x**4, [(1+2j)**4, (2+3j)**4, (3+4j)**4])
         assert_almost_equal(x**(-1), [1/(1+2j), 1/(2+3j), 1/(3+4j)])
         assert_almost_equal(x**(-2), [1/(1+2j)**2, 1/(2+3j)**2, 1/(3+4j)**2])
         assert_almost_equal(x**(-3), [(-11+2j)/125, (-46-9j)/2197,
                                       (-117-44j)/15625])
         assert_almost_equal(x**(0.5), [ncu.sqrt(1+2j), ncu.sqrt(2+3j),
                                        ncu.sqrt(3+4j)])
-        assert_almost_equal(x**14, [-76443+16124j, 23161315+58317492j,
-                                    5583548873 +  2465133864j])
+        norm = 1./((x**14)[0])
+        assert_almost_equal(x**14 * norm,
+                [i * norm for i in [-76443+16124j, 23161315+58317492j,
+                                    5583548873 +  2465133864j]])
 
         # Ticket #836
         def assert_complex_equal(x, y):
