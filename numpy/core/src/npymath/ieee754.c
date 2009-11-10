c52c2bfa691b5c3303f736de518a21a2c5c141ca
David Cournapeau <cournape@gmail.com>
2009-11-10 09:33:23 +0000

    STY: fix hard tab.

--- numpy/core/src/npymath/ieee754.c
+++ numpy/core/src/npymath/ieee754.c
@@ -165,50 +165,50 @@ npy_longdouble npy_nextafterl(npy_longdouble x, npy_longdouble y)
     uy.e = y;
 
     if ((ux.bits.exp == 0x7fff &&
-	 ((ux.bits.manh & ~LDBL_NBIT) | ux.bits.manl) != 0) ||
-	(uy.bits.exp == 0x7fff &&
-	 ((uy.bits.manh & ~LDBL_NBIT) | uy.bits.manl) != 0)) {
-	return x + y;		/* x or y is nan */
+         ((ux.bits.manh & ~LDBL_NBIT) | ux.bits.manl) != 0) ||
+        (uy.bits.exp == 0x7fff &&
+         ((uy.bits.manh & ~LDBL_NBIT) | uy.bits.manl) != 0)) {
+        return x + y;           /* x or y is nan */
     }
     if (x == y) {
-	return y;		/* x=y, return y */
+        return y;               /* x=y, return y */
     }
     if (x == 0.0) {
-	ux.bits.manh = 0;	/* return +-minsubnormal */
-	ux.bits.manl = 1;
-	ux.bits.sign = uy.bits.sign;
-	t = ux.e * ux.e;
-	if (t == ux.e) {
-	    return t;
+        ux.bits.manh = 0;       /* return +-minsubnormal */
+        ux.bits.manl = 1;
+        ux.bits.sign = uy.bits.sign;
+        t = ux.e * ux.e;
+        if (t == ux.e) {
+            return t;
         } else {
-	    return ux.e;	/* raise underflow flag */
+            return ux.e;        /* raise underflow flag */
         }
     }
-    if (x > 0.0 ^ x < y) {	/* x -= ulp */
-	if (ux.bits.manl == 0) {
-	    if ((ux.bits.manh & ~LDBL_NBIT) == 0) {
-		ux.bits.exp -= 1;
+    if (x > 0.0 ^ x < y) {      /* x -= ulp */
+        if (ux.bits.manl == 0) {
+            if ((ux.bits.manh & ~LDBL_NBIT) == 0) {
+                ux.bits.exp -= 1;
             }
-	    ux.bits.manh = (ux.bits.manh - 1) | (ux.bits.manh & LDBL_NBIT);
-	}
-	ux.bits.manl -= 1;
-    } else {			/* x += ulp */
-	ux.bits.manl += 1;
-	if (ux.bits.manl == 0) {
-	    ux.bits.manh = (ux.bits.manh + 1) | (ux.bits.manh & LDBL_NBIT);
-	    if ((ux.bits.manh & ~LDBL_NBIT) == 0) {
-		ux.bits.exp += 1;
+            ux.bits.manh = (ux.bits.manh - 1) | (ux.bits.manh & LDBL_NBIT);
+        }
+        ux.bits.manl -= 1;
+    } else {                    /* x += ulp */
+        ux.bits.manl += 1;
+        if (ux.bits.manl == 0) {
+            ux.bits.manh = (ux.bits.manh + 1) | (ux.bits.manh & LDBL_NBIT);
+            if ((ux.bits.manh & ~LDBL_NBIT) == 0) {
+                ux.bits.exp += 1;
             }
-	}
+        }
     }
     if (ux.bits.exp == 0x7fff) {
-	return x + x;		/* overflow  */
+        return x + x;           /* overflow  */
     }
-    if (ux.bits.exp == 0) {	/* underflow */
-	mask_nbit_l(ux);
-	t = ux.e * ux.e;
-	if (t != ux.e) {        /* raise underflow flag */
-	    return ux.e;
+    if (ux.bits.exp == 0) {     /* underflow */
+        mask_nbit_l(ux);
+        t = ux.e * ux.e;
+        if (t != ux.e) {        /* raise underflow flag */
+            return ux.e;
         }
     }
     return ux.e;
