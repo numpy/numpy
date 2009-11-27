eb9404fedd000791e56a09c99f54195d8494f9ba
David Cournapeau <cournape@gmail.com>
2009-11-27 19:10:11 +0000

    BUG: fix arch-dependent SIZEOF in npy_config.h for mac os x.
    (cherry picked from commit 6163b8d395c3c7be36da49d638cb120da7d95de2)

--- numpy/core/src/private/npy_config.h
+++ numpy/core/src/private/npy_config.h
@@ -14,4 +14,21 @@
 #undef HAVE_ATAN2
 #endif
 
+/* 
+ * On Mac OS X, because there is only one configuration stage for all the archs
+ * in universal builds, any macro which depends on the arch needs to be
+ * harcoded
+ */
+#ifdef __APPLE__
+    #undef SIZEOF_LONG
+    #undef SIZEOF_PY_INTPTR_T
+
+    #ifdef __LP64__
+        #define SIZEOF_LONG         8
+        #define SIZEOF_PY_INTPTR_T  8
+    #else
+        #define SIZEOF_LONG         4
+        #define SIZEOF_PY_INTPTR_T  4
+    #endif
+#endif
 #endif
