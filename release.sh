7ee0d5e3ab4ccb425d2e3e050781ebf041a0abc3
David Cournapeau <cournape@gmail.com>
2009-12-01 01:08:49 +0000

    REL: generate both 2.5 and 2.6 win and mac os x installers in release script.
    (cherry picked from commit ca2544215043e957ee96641294492b27d622683d)

--- release.sh
+++ release.sh
@@ -4,6 +4,8 @@ paver bootstrap
 source bootstrap/bin/activate
 CFLAGS="-arch x86_64" FFLAGS="-arch x86_64" python setupsconsegg.py install
 paver sdist
-paver dmg
-paver bdist_superpack
+paver dmg -p 2.5
+paver dmg -p 2.6
+paver bdist_superpack -p 2.5
+paver bdist_superpack -p 2.6
 paver write_release_and_log
