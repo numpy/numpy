b1b6e581144db477b9e8780b0f22f27f7eaddfa4
David Cournapeau <cournape@gmail.com>
2009-12-03 15:58:18 +0000

    Fix print statements in numpy.distutils.environment

--- numpy/distutils/environment.py
+++ numpy/distutils/environment.py
@@ -15,15 +15,15 @@ class EnvironmentConfig:
         hook, envvar, confvar, convert = conf_desc
         if not convert:
             convert = lambda x : x
-        print '%s.%s:' % (self._distutils_section, name)
+        print('%s.%s:' % (self._distutils_section, name))
         v = self._hook_handler(name, hook)
-        print '  hook   : %s' % (convert(v),)
+        print('  hook   : %s' % (convert(v),))
         if envvar:
             v = os.environ.get(envvar, None)
-            print '  environ: %s' % (convert(v),)
+            print('  environ: %s' % (convert(v),))
         if confvar and self._conf:
             v = self._conf.get(confvar, (None, None))[1]
-            print '  config : %s' % (convert(v),)
+            print('  config : %s' % (convert(v),))
 
     def dump_variables(self):
         for name in self._conf_keys:
--- numpy/setup.py
+++ numpy/setup.py
@@ -24,4 +24,4 @@ def configuration(parent_package='',top_path=None):
     return config
 
 if __name__ == '__main__':
-    print 'This is the wrong setup.py file to run'
+    print('This is the wrong setup.py file to run')
