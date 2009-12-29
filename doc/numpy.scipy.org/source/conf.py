7894a07c67b60821c837d9d36880081690e72867
Jarrod Millman <millman@berkeley.edu>
2009-12-29 20:24:10 +0000

    adding scipy icon bar to top of the page

--- doc/numpy.scipy.org/source/conf.py
+++ doc/numpy.scipy.org/source/conf.py
@@ -121,6 +121,7 @@ html_title = "Numpy"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
