34c59cfbd08db2f821f931c11e3b365e9f1493a8
Jarrod Millman <millman@berkeley.edu>
2010-01-16 03:17:29 +0000

    point everything to top-level dir

--- doc/numpy.scipy.org/conf.py
+++ doc/numpy.scipy.org/conf.py
@@ -99,7 +99,7 @@ html_theme = 'scipy'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["../_theme"]
+html_theme_path = ["_theme"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
