242eb69a2e0ef7df4413e18172ed6fde85913814
Jarrod Millman <millman@berkeley.edu>
2010-01-16 03:28:31 +0000

    point conf to the default static content

--- doc/numpy.scipy.org/conf.py
+++ doc/numpy.scipy.org/conf.py
@@ -116,12 +116,12 @@ html_title = "Numpy"
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['static']
-html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
