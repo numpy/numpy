755e6ebabdda26f8720d5881897d31379fe81586
mdroe <mdroe@localhost>
2010-02-22 19:36:24 +0000

    Add regression test for ticket #1405

--- numpy/core/tests/test_regression.py
+++ numpy/core/tests/test_regression.py
@@ -1265,5 +1265,11 @@ class TestRegression(TestCase):
         x = np.array([1,2,3], dtype=np.dtype('<i4'))
         assert_equal(md5(x).hexdigest(), '2a1dd1e1e59d0a384c26951e316cd7e6')
 
+    def test_numeric_handleError(self):
+        """Ticket #1405"""
+        from numpy import numarray
+        # Just make sure this doesn't throw an exception
+        numarray.handleError(0, "")
+
 if __name__ == "__main__":
     run_module_suite()
