5aa19838a8a58f82a8c8097be40115efc66d36ed
rgommers <ralf.gommers@googlemail.com>
2010-04-18 09:36:28 +0000

    REL: Remove 64-bit flags from release script.

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 # script to build tarballs, mac os x and windows installers on mac os x
 paver bootstrap
 source bootstrap/bin/activate
-CFLAGS="-arch x86_64" FFLAGS="-arch x86_64" python setupsconsegg.py install
+python setupegg.py install
 paver sdist
 paver dmg -p 2.5
 paver dmg -p 2.6
