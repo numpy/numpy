8a4fb936484cf0ba8b5e76706dc9b52b535549af
Pauli Virtanen <pav@iki.fi>
2010-07-17 18:14:46 +0000

    3K: f2py: catch a misc import error

--- numpy/f2py/__version__.py
+++ numpy/f2py/__version__.py
@@ -4,5 +4,5 @@ try:
     from __svn_version__ import version
     version_info = (major, version)
     version = '%s_%s' % version_info
-except ImportError:
+except (ImportError, ValueError):
     version = str(major)
