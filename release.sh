1d08d6a5e0f033be239688e385f9c4ec3df48afa
rgommers <ralf.gommers@googlemail.com>
2010-07-29 15:54:22 +0000

    REL: Update paver and release scripts for Python 2.7/3.1 binaries.
    Note that the increase in disk image size from 10Mb to 11Mb does not mean numpy
    is actually that large - more than half that is the included docs. It is the
    case though that the 2.7 binaries are slightly larger than the 2.5/2.6 ones, by
    100kb on Windows and 800kb on OS X.

--- release.sh
+++ release.sh
@@ -4,8 +4,10 @@ paver bootstrap
 source bootstrap/bin/activate
 python setupsconsegg.py install
 paver sdist
-paver dmg -p 2.5
+paver dmg -p 2.7
 paver dmg -p 2.6
-paver bdist_superpack -p 2.5
+paver dmg -p 2.5
+paver bdist_superpack -p 2.7
 paver bdist_superpack -p 2.6
+paver bdist_superpack -p 2.5
 paver write_release_and_log
