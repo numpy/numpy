7f731363803ec16722a307e48b1e0029a349ae1c
Pauli Virtanen <pav@iki.fi>
2010-08-04 19:54:58 +0000

    3K: (backport r8599) fix core/dotblas module initialization.

--- numpy/core/blasdot/_dotblas.c
+++ numpy/core/blasdot/_dotblas.c
@@ -2,6 +2,7 @@ static char module_doc[] =
 "This module provides a BLAS optimized\nmatrix multiply, inner product and dot for numpy arrays";
 
 #include "Python.h"
+#include "npy_config.h"
 #include "numpy/ndarrayobject.h"
 #ifndef CBLAS_HEADER
 #define CBLAS_HEADER "cblas.h"
@@ -1174,13 +1175,38 @@ static struct PyMethodDef dotblas_module_methods[] = {
     {NULL, NULL, 0, NULL}               /* sentinel */
 };
 
+#if defined(NPY_PY3K)
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "_dotblas",
+        NULL,
+        -1,
+        dotblas_module_methods,
+        NULL,
+        NULL,
+        NULL,
+        NULL
+};
+#endif
+
 /* Initialization function for the module */
-PyMODINIT_FUNC init_dotblas(void) {
+#if defined(NPY_PY3K)
+#define RETVAL m
+PyObject *PyInit__dotblas(void)
+#else
+#define RETVAL
+PyMODINIT_FUNC init_dotblas(void)
+#endif
+{
     int i;
-    PyObject *d, *s;
+    PyObject *d, *s, *m;
 
     /* Create the module and add the functions */
-    Py_InitModule3("_dotblas", dotblas_module_methods, module_doc);
+#if defined(NPY_PY3K)
+    m = PyModule_Create(&moduledef);
+#else
+    m = Py_InitModule3("_dotblas", dotblas_module_methods, module_doc);
+#endif
 
     /* Import the array object */
     import_array();
@@ -1195,4 +1221,5 @@ PyMODINIT_FUNC init_dotblas(void) {
     Py_DECREF(d);
     Py_DECREF(s);
 
+    return RETVAL;
 }
