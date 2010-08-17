c740bf818f645a3d7ffc39843e39859027a4db5a
rgommers <ralf.gommers@googlemail.com>
2010-08-17 16:10:14 +0000

    REL: Some more changes to release scripts.

--- pavement.py
+++ pavement.py
@@ -202,7 +202,7 @@ def bdist_superpack(options):
     pyver = options.python_version
     def copy_bdist(arch):
         # Copy the wininst in dist into the release directory
-        if pyver[0] >= 3:
+        if int(pyver[0]) >= 3:
             source = os.path.join('build', 'py3k', 'dist', wininst_name(pyver))
         else:
             source = os.path.join('dist', wininst_name(pyver))
--- release.sh
+++ release.sh
@@ -4,9 +4,11 @@ paver bootstrap
 source bootstrap/bin/activate
 python setupsconsegg.py install
 paver sdist
-paver dmg -p 2.7
 paver dmg -p 2.6
 paver dmg -p 2.5
+export MACOSX_DEPLOYMENT_TARGET=10.6
+paver dmg -p 2.7
+
 paver bdist_superpack -p 3.1
 paver bdist_superpack -p 2.7
 paver bdist_superpack -p 2.6
