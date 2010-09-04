3cb0eb32de3e01a6f6ff3778b5741d1dc1cbd522
Pauli Virtanen <pav@iki.fi>
2010-09-04 13:08:43 +0000

    sphinxext: be explicit about Sphinx version requirements

--- doc/sphinxext/numpydoc.py
+++ doc/sphinxext/numpydoc.py
@@ -16,6 +16,11 @@ It will:
 
 """
 
+import sphinx
+
+if sphinx.__version__ < '1.0.1':
+    raise RuntimeError("Sphinx 1.0.1 or newer is required")
+
 import os, re, pydoc
 from docscrape_sphinx import get_doc_object, SphinxDocString
 from sphinx.util.compat import Directive
