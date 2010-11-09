21c6094809969688231b676f879269021e579efd
Mark Wiebe <mwwiebe@gmail.com>
2010-11-09 15:42:31 -0800

    ENH: core: Add half/float16 finfo support

--- numpy/core/getlimits.py
+++ numpy/core/getlimits.py
@@ -131,6 +131,10 @@ class finfo(object):
             itype = ntypes.longlong
             fmt = '%s'
             precname = 'long double'
+        elif dtype is ntypes.half:
+            itype = ntypes.int16
+            fmt = '%12.5e'
+            precname = 'half'
         else:
             raise ValueError, repr(dtype)
 
