6fd71aeb7f5d4a67984044740827a223a3bcff64
David Cournapeau <cournape@gmail.com>
2011-03-07 20:26:04 +0900

    BENTO: add basic type checks.

--- numpy/build_utils/__init__.py
+++ numpy/build_utils/__init__.py
@@ -106,3 +106,117 @@ int main()
         return ret == 0
     return ret
 
+def do_binary_search(conf, type_name, kw):
+    code = """\
+typedef %(type)s waf_check_sizeof_type;
+int main ()
+{
+    static int test_array [1 - 2 * !(((long) (sizeof (waf_check_sizeof_type))) >= 0)];
+    test_array [0] = 0
+
+    ;
+    return 0;
+}
+""" % {"type": type_name}
+    kw["code"] = to_header(kw) + code
+
+    try:
+        conf.run_c_code(**kw)
+    except conf.errors.ConfigurationError, e:
+        conf.end_msg("failed !")
+        if waflib.Logs.verbose > 1:
+            raise
+        else:
+            conf.fatal("The configuration failed !")
+
+    body = r"""
+typedef %(type)s waf_check_sizeof_type;
+int main ()
+{
+    static int test_array [1 - 2 * !(((long) (sizeof (waf_check_sizeof_type))) <= %(size)s)];
+    test_array [0] = 0
+
+    ;
+    return 0;
+}
+"""
+    # The principle is simple: we first find low and high bounds
+    # of size for the type, where low/high are looked up on a log
+    # scale. Then, we do a binary search to find the exact size
+    # between low and high
+    low = 0
+    mid = 0
+    while True:
+        try:
+            kw["code"] = to_header(kw) + body % {"type": type_name, "size": mid}
+            validate_arguments(conf, kw)
+            conf.run_c_code(**kw)
+            break
+        except conf.errors.ConfigurationError:
+            #log.info("failure to test for bound %d" % mid)
+            low = mid + 1
+            mid = 2 * mid + 1
+
+    high = mid
+    ret = None
+    # Binary search:
+    while low != high:
+        mid = (high - low) / 2 + low
+        try:
+            kw["code"] = to_header(kw) + body % {"type": type_name, "size": mid}
+            validate_arguments(conf, kw)
+            ret = conf.run_c_code(**kw)
+            high = mid
+        except conf.errors.ConfigurationError:
+            low = mid + 1
+
+    return low
+
+@waflib.Configure.conf
+def check_type_size(conf, type_name, expected_sizes=None, **kw):
+    kw["define_name"] = "SIZEOF_%s" % sanitize_string(type_name)
+    kw["define_comment"] = "/* The size of `%s', as computed by sizeof. */" % type_name
+    kw["msg"] = "Checking sizeof(%s)" % type_name
+
+    validate_arguments(conf, kw)
+    conf.start_msg(kw["msg"])
+
+    if expected_sizes is not None:
+        try:
+            val = int(expected_sizes)
+        except TypeError:
+            values = expected_sizes
+        else:
+            values = [val]
+
+        size = None
+        for value in values:
+            code = """\
+    typedef %(type)s waf_check_sizeof_type;
+    int main ()
+    {
+        static int test_array [1 - 2 * !(((long) (sizeof (waf_check_sizeof_type))) == %(size)d)];
+        test_array [0] = 0
+
+        ;
+        return 0;
+    }
+    """ % {"type": type_name, "size": value}
+            kw["code"] = to_header(kw) + code
+            try:
+                conf.run_c_code(**kw)
+                size = value
+                break
+            except conf.errors.ConfigurationError:
+                pass
+        if size is None:
+            size = do_binary_search(conf, type_name, kw)
+    else:
+        size = do_binary_search(conf, type_name, kw)
+
+    kw["define_value"] = size
+    kw["success"] = 0
+    conf.end_msg(size)
+    conf.post_check(**kw)
+    return size
+
