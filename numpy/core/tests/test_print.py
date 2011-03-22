709e3573b02e88f86cc491ded104aa73476cff62
Mark Wiebe <mwwiebe@gmail.com>
2011-03-22 14:32:11 -0700

    ENH: Add scalar support for the format() function introduced in Python 2.6 (#1675)

--- numpy/core/tests/test_print.py
+++ numpy/core/tests/test_print.py
@@ -198,6 +198,32 @@ def test_complex_type_print():
     for t in [np.complex64, np.cdouble, np.clongdouble] :
         yield check_complex_type_print, t
 
+@dec.skipif(sys.version_info < (2,6))
+def test_scalar_format():
+    """Test the str.format method with NumPy scalar types"""
+    tests = [('{0}', True, np.bool_),
+            ('{0}', False, np.bool_),
+            ('{0:d}', 130, np.uint8),
+            ('{0:d}', 50000, np.uint16),
+            ('{0:d}', 3000000000, np.uint32),
+            ('{0:d}', 15000000000000000000, np.uint64),
+            ('{0:d}', -120, np.int8),
+            ('{0:d}', -30000, np.int16),
+            ('{0:d}', -2000000000, np.int32),
+            ('{0:d}', -7000000000000000000, np.int64),
+            ('{0:g}', 1.5, np.float16),
+            ('{0:g}', 1.5, np.float32),
+            ('{0:g}', 1.5, np.float64),
+            ('{0:g}', 1.5, np.longdouble),
+            ('{0:g}', 1.5+0.5j, np.complex64),
+            ('{0:g}', 1.5+0.5j, np.complex128),
+            ('{0:g}', 1.5+0.5j, np.clongdouble)]
+
+    for (fmat, val, valtype) in tests:
+        assert_equal(fmat.format(val), fmat.format(valtype(val)),
+                "failed with val %s, type %s" % (val, valtype))
+
+
 # Locale tests: scalar types formatting should be independent of the locale
 def in_foreign_locale(func):
     """
