d4db14bc7d235558f7677d7c3eaf5b41f6b4b4a2
Charles Harris <charlesr.harris@gmail.com>
2011-04-21 20:30:41 -0600

    BUG: ticket #1805, replace type char by signed char for tables.

--- numpy/core/src/multiarray/scalartypes.h
+++ numpy/core/src/multiarray/scalartypes.h
@@ -5,24 +5,24 @@
 #ifdef NPY_ENABLE_SEPARATE_COMPILATION
 extern NPY_NO_EXPORT unsigned char
 _npy_can_cast_safely_table[NPY_NTYPES][NPY_NTYPES];
-extern NPY_NO_EXPORT char
+extern NPY_NO_EXPORT signed char
 _npy_scalar_kinds_table[NPY_NTYPES];
-extern NPY_NO_EXPORT char
+extern NPY_NO_EXPORT signed char
 _npy_type_promotion_table[NPY_NTYPES][NPY_NTYPES];
-extern NPY_NO_EXPORT char
+extern NPY_NO_EXPORT signed char
 _npy_smallest_type_of_kind_table[NPY_NSCALARKINDS];
-extern NPY_NO_EXPORT char
+extern NPY_NO_EXPORT signed char
 _npy_next_larger_type_table[NPY_NTYPES];
 #else
 NPY_NO_EXPORT unsigned char
 _npy_can_cast_safely_table[NPY_NTYPES][NPY_NTYPES];
-NPY_NO_EXPORT char
+NPY_NO_EXPORT signed char
 _npy_scalar_kinds_table[NPY_NTYPES];
-NPY_NO_EXPORT char
+NPY_NO_EXPORT signed char
 _npy_type_promotion_table[NPY_NTYPES][NPY_NTYPES];
-NPY_NO_EXPORT char
+NPY_NO_EXPORT signed char
 _npy_smallest_type_of_kind_table[NPY_NSCALARKINDS];
-NPY_NO_EXPORT char
+NPY_NO_EXPORT signed char
 _npy_next_larger_type_table[NPY_NTYPES];
 #endif
 
