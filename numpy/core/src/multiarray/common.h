55787fa283c9f5121a45ce9647e9b3775be44709
Mark Wiebe <mwiebe@enthought.com>
2011-07-22 15:15:44 -0500

    ENH: missingdata: Define the standard Python arithmetic operations for NA

--- numpy/core/src/multiarray/common.h
+++ numpy/core/src/multiarray/common.h
@@ -6,6 +6,10 @@
 NPY_NO_EXPORT PyArray_Descr *
 _array_find_type(PyObject *op, PyArray_Descr *minitype, int max);
 
+/*
+ * Returns NULL without setting an exception if no scalar is matched, a
+ * new dtype reference otherwise.
+ */
 NPY_NO_EXPORT PyArray_Descr *
 _array_find_python_scalar_type(PyObject *op);
 
@@ -13,7 +17,7 @@ NPY_NO_EXPORT PyArray_Descr *
 _array_typedescr_fromstr(char *str);
 
 NPY_NO_EXPORT char *
-index2ptr(PyArrayObject *mp, intp i);
+index2ptr(PyArrayObject *mp, npy_intp i);
 
 NPY_NO_EXPORT int
 _zerofill(PyArrayObject *ret);
@@ -21,7 +25,7 @@ _zerofill(PyArrayObject *ret);
 NPY_NO_EXPORT int
 _IsAligned(PyArrayObject *ap);
 
-NPY_NO_EXPORT Bool
+NPY_NO_EXPORT npy_bool
 _IsWriteable(PyArrayObject *ap);
 
 #ifndef Py_UNICODE_WIDE
--- numpy/core/src/multiarray/na_singleton.h
+++ numpy/core/src/multiarray/na_singleton.h
@@ -13,7 +13,30 @@ typedef struct {
 } NpyNA_fieldaccess;
 
 NPY_NO_EXPORT NpyNA_fieldaccess _Npy_NASingleton;
-
 #define Npy_NA ((PyObject *)&_Npy_NASingleton)
+
+#define NPY_NA_NOPAYLOAD (255)
+
+static NPY_INLINE npy_uint8
+NpyNA_CombinePayloads(npy_uint p1, npy_uint p2)
+{
+    if (p1 == NPY_NA_NOPAYLOAD || p2 == NPY_NA_NOPAYLOAD) {
+        return NPY_NA_NOPAYLOAD;
+    }
+    else {
+        return (p1 < p2) ? p1 : p2;
+    }
+}
+
+/* Combines two NA values together, merging their payloads and dtypes. */
+NPY_NO_EXPORT NpyNA *
+NpyNA_CombineNA(NpyNA *na1, NpyNA *na2);
+
+/*
+ * Combines an NA with an object, raising an error if the object has
+ * no extractable NumPy dtype.
+ */
+NPY_NO_EXPORT NpyNA *
+NpyNA_CombineNAWithObject(NpyNA *na, PyObject *obj);
 
 #endif
