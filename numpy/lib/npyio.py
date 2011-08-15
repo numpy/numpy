046f2f726bb37f8c8f247d8c7bccdbdb91e55a53
Paul Anton Letnes <paul.anton.letnes@gmail.com>
2011-08-15 17:00:37 +0100

    added version numbers of new kwargs

--- numpy/lib/npyio.py
+++ numpy/lib/npyio.py
@@ -856,12 +856,15 @@ def savetxt(fname, X, fmt='%.18e', delimiter=' ', newline='\n', header='',
         .. versionadded:: 1.5.0
     header : str, optional
         String that will be written at the beginning of the file.
+        .. versionadded:: 2.0.0
     footer : str, optional
         String that will be written at the end of the file.
+        .. versionadded:: 2.0.0
     comments : str, optional
         String that will be prepended to the ``header`` and ``footer`` strings,
         to mark them as comments. Default: '# ',  as expected by e.g.
         ``numpy.loadtxt``.
+        .. versionadded:: 2.0.0
 
         Character separating lines.
 
