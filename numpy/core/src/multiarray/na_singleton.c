b974cf38fa0eff21270ba56795781c8549135cb3
Mark Wiebe <mwwiebe@gmail.com>
2011-08-19 08:41:19 -0700

    BLD: missingdata: Fixes for Python 3

--- numpy/core/src/multiarray/na_singleton.c
+++ numpy/core/src/multiarray/na_singleton.c
@@ -705,7 +705,12 @@ NPY_NO_EXPORT PyTypeObject NpyNA_Type = {
     0,                                          /* tp_getattro */
     0,                                          /* tp_setattro */
     0,                                          /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_CHECKTYPES,   /* tp_flags */
+    Py_TPFLAGS_DEFAULT                          /* tp_flags */
+#if !defined(NPY_PY3K)
+    | Py_TPFLAGS_CHECKTYPES,
+#else
+    ,
+#endif
     0,                                          /* tp_doc */
     0,                                          /* tp_traverse */
     0,                                          /* tp_clear */
