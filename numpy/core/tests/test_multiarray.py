d7b12a38aa602839b238c2105d0fae208fe891ef
Ben Walsh <b@wumpster.com>
2011-08-21 13:51:40 -0600

    BUG: Fix segfault when PyArray_VOID and no minitype in _array_find_type. The segfault showed up in numpy/testing/print_coercion_tables.py and 'lookfor'.

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -327,6 +327,10 @@ class TestCreation(TestCase):
             msg = 'String conversion for %s' % type
             assert_equal(array(nstr, dtype=type), result, err_msg=msg)
 
+    def test_void(self):
+        arr = np.array([np.void(0)], dtype='V')
+        assert_equal(arr.dtype.kind, 'V')
+
     def test_non_sequence_sequence(self):
         """Should not segfault.
 
