934e50b2f15f0613ce100a6a2ac3a87b095ddb5b
Mark Wiebe <mwwiebe@gmail.com>
2011-08-22 23:04:30 -0700

    ENH: missingdata: Move ReduceMaskNAArray out of the public API
    Keep this function private, to keep flexibility open for longer.
    Also added what will likely be the necessary parameters for future
    where mask and multi-NA expansion.

--- numpy/core/src/multiarray/na_mask.h
+++ numpy/core/src/multiarray/na_mask.h
@@ -43,4 +43,27 @@ PyArray_GetMaskAndFunction(
         npy_intp mask1_stride, PyArray_Descr *mask1_dtype, int invert_mask1,
         PyArray_StridedBinaryOp **out_binop, NpyAuxData **out_opdata);
 
+/*
+ * This function performs a reduction on the masks for an array.
+ *
+ * This is for use with a reduction where 'skipna=False'.
+ *
+ * operand: The operand for which the reduction is being done. This array
+ *          must have an NA mask.
+ * result: The result array, which should have the same 'ndim' as
+ *         'operand' but with dimensions of size one for every reduction
+ *         axis. This array must have an NA mask.
+ * wheremask: NOT SUPPORTED YET, but is for a boolean mask which can
+ *            broadcast to 'result', indicating where to do the reduction.
+ *            Should pass in NULL.
+ * skipwhichna: NOT SUPPORTED YET, but for future expansion to multi-NA,
+ *              where reductions can be done on NAs with a subset of
+ *              the possible payloads. Should pass in NULL.
+ *
+ * Returns 0 on success, -1 on failure.
+ */
+NPY_NO_EXPORT int
+PyArray_ReduceMaskNAArray(PyArrayObject *operand, PyArrayObject *result,
+                            PyArrayObject *wheremask, npy_bool *skipwhichna);
+
 #endif
