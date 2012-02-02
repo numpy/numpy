8a0e7cd26ea2803b8580dcca1d1306baa1d702dc
Charles Harris <charlesr.harris@gmail.com>
2012-02-01 18:42:25 -0700

    UPDATE: Replace macros in old_defines.h by new.
    With the exception of the numarray fixups these were all instances
    that were results of code generation.

--- numpy/core/code_generators/generate_umath.py
+++ numpy/core/code_generators/generate_umath.py
@@ -884,7 +884,7 @@ def make_arrays(funcdict):
                 funclist.append('%s_%s' % (tname, name))
 
             for x in t.in_ + t.out:
-                siglist.append('PyArray_%s' % (english_upper(chartoname[x]),))
+                siglist.append('NPY_%s' % (english_upper(chartoname[x]),))
 
             k += 1
 
--- numpy/numarray/include/numpy/arraybase.h
+++ numpy/numarray/include/numpy/arraybase.h
@@ -20,22 +20,22 @@ typedef npy_float64 Float64;
 
 typedef enum
 {
-  tAny=-1,
-  tBool=PyArray_BOOL,
-  tInt8=PyArray_INT8,
-  tUInt8=PyArray_UINT8,
-  tInt16=PyArray_INT16,
-  tUInt16=PyArray_UINT16,
-  tInt32=PyArray_INT32,
-  tUInt32=PyArray_UINT32,
-  tInt64=PyArray_INT64,
-  tUInt64=PyArray_UINT64,
-  tFloat32=PyArray_FLOAT32,
-  tFloat64=PyArray_FLOAT64,
-  tComplex32=PyArray_COMPLEX64,
-  tComplex64=PyArray_COMPLEX128,
-  tObject=PyArray_OBJECT,        /* placeholder... does nothing */
-  tMaxType=PyArray_NTYPES,
+  tAny = -1,
+  tBool = NPY_BOOL,
+  tInt8 = NPY_INT8,
+  tUInt8 = NPY_UINT8,
+  tInt16 = NPY_INT16,
+  tUInt16 = NPY_UINT16,
+  tInt32 = NPY_INT32,
+  tUInt32 = NPY_UINT32,
+  tInt64 = NPY_INT64,
+  tUInt64 = NPY_UINT64,
+  tFloat32 = NPY_FLOAT32,
+  tFloat64 = NPY_FLOAT64,
+  tComplex32 = NPY_COMPLEX64,
+  tComplex64 = NPY_COMPLEX128,
+  tObject = NPY_OBJECT,        /* placeholder... does nothing */
+  tMaxType = NPY_NTYPES,
   tDefault = tFloat64,
 #if NPY_BITSOF_LONG == 64
   tLong = tInt64,
