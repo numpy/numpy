f047010fa90d8186eeba59d8f18bc12523b3a27f
Charles Harris <charlesr.harris@gmail.com>
2012-02-11 18:32:44 -0700

    ENH: Check that npy_no_deprecated_api.h is included before it is needed.

--- numpy/core/include/numpy/npy_no_deprecated_api.h
+++ numpy/core/include/numpy/npy_no_deprecated_api.h
@@ -1,10 +1,19 @@
 /*
- * This file is for inclusion in cython *.pxd files in order to define
- * the NPY_NO_DEPRECATED_API macro. In order to include it, do
+ * This include file is provided for inclusion in Cython *.pyd files where
+ * one would like to define the NPY_NO_DEPRECATED_API macro. It can be
+ * included by
  *
- * cdef extern from "numpy/npy_no_deprecated_api.h": pass
+ * cdef extern from "npy_no_deprecated_api.h": pass
  *
  */
 #ifndef NPY_NO_DEPRECATED_API
+
+/* put this check here since there may be multiple includes in C extensions. */
+#if defined(NDARRAYTYPES_H) || defined(_NPY_DEPRECATED_API_H) || \
+    defined(OLD_DEFINES_H)
+#error "npy_no_deprecated_api.h" must be first among numpy includes.
+#else
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
+#endif
+
 #endif
