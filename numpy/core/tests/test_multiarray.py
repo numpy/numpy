1c226602af2bb9f1efef6d49231d8a8ed7463ab6
Charles Harris <charlesr.harris@gmail.com>
2012-05-04 14:49:20 -0600

    STY: Fix tabs in code.

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -1105,20 +1105,20 @@ class TestArgmin(TestCase):
 
     def test_minimum_signed_integers(self):
 
- 	a = np.array([1, -2**7, -2**7 + 1], dtype=np.int8)
- 	assert_equal(np.argmin(a), 1)
+        a = np.array([1, -2**7, -2**7 + 1], dtype=np.int8)
+        assert_equal(np.argmin(a), 1)
 
         a = np.array([1, -2**15, -2**15 + 1], dtype=np.int16)
- 	assert_equal(np.argmin(a), 1)
+        assert_equal(np.argmin(a), 1)
 
- 	a = np.array([1, -2**31, -2**31 + 1], dtype=np.int32)
- 	assert_equal(np.argmin(a), 1)
+        a = np.array([1, -2**31, -2**31 + 1], dtype=np.int32)
+        assert_equal(np.argmin(a), 1)
 
- 	a = np.array([1, -2**63, -2**63 + 1], dtype=np.int64)
- 	assert_equal(np.argmin(a), 1)
+        a = np.array([1, -2**63, -2**63 + 1], dtype=np.int64)
+        assert_equal(np.argmin(a), 1)
 
- 	a = np.array([1, 2**31 - 1, -2**31], dtype=np.int32)
- 	assert_equal(np.argmin(a), 2)
+        a = np.array([1, 2**31 - 1, -2**31], dtype=np.int32)
+        assert_equal(np.argmin(a), 2)
 
 
 class TestMinMax(TestCase):
