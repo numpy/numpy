baaf1819e3e9319c58269a7a06eb4b20e0a08eec
Nathaniel J. Smith <njs@pobox.com>
2012-05-22 09:41:18 +0100

    Move internal NPY_ARRAY_WARN_ON_WRITE flag into an internal header.

--- numpy/core/include/numpy/ndarraytypes.h
+++ numpy/core/include/numpy/ndarraytypes.h
@@ -918,17 +918,9 @@ typedef int (PyArray_FinalizeFunc)(PyArrayObject *, PyObject *);
 #define NPY_ARRAY_ALLOWNA         0x8000
 
 /*
- * This flag is used to mark arrays which we would like to, in the future,
- * turn into views. It causes a warning to be issued on the first attempt to
- * write to the array (but the write is allowed to succeed).
- *
- * This flag is for internal use only, and may be removed in a future release,
- * which is why the #define is not exposed to user code. Currently it is set
- * on arrays returned by ndarray.diagonal.
+ * NOTE: there are also internal flags defined in multiarray/arrayobject.h,
+ * which start at bit 31 and work down.
  */
-#ifdef _MULTIARRAYMODULE
-#define NPY_ARRAY_WARN_ON_WRITE  0x10000
-#endif
 
 #define NPY_ARRAY_BEHAVED      (NPY_ARRAY_ALIGNED | \
                                 NPY_ARRAY_WRITEABLE)
