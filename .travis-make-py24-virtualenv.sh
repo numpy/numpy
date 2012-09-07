fcacdcc253432c460d92dfaa7f216f7652944d88
Nathaniel J. Smith <njs@pobox.com>
2012-09-07 10:57:05 +0100

    FIX: use py24-compatible version of virtualenv on Travis
    Travis recently upgraded to virtualenv 1.8, which has dropped support
    for Python 2.4. So, in our Python 2.4 setup script, we need to
    explicitly fetch and use virtualenv 1.7. Likewise for pip 1.1.
    
    File imported from the already-fixed version for patsy:
      https://github.com/pydata/patsy/blob/0316d2901f4195db06e8091c15f37d9fe4ad09de/.travis-make-py24-virtualenv.sh

--- .travis-make-py24-virtualenv.sh
+++ .travis-make-py24-virtualenv.sh
@@ -15,4 +15,11 @@ EOF
 ./configure --prefix=$PWD/install
 make
 make install
-virtualenv -p install/bin/python2.4 --distribute $VIRTENV
+# This is the last version of virtualenv to support python 2.4:
+curl -O https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
+# And this is the last version of pip to support python 2.4. If
+# there's a file matching "^pip-.*(zip|tar.gz|tar.bz2|tgz|tbz)$" in
+# the current directory then virtualenv will take that as the pip
+# source distribution to install
+curl -O http://pypi.python.org/packages/source/p/pip/pip-1.1.tar.gz
+install/bin/python2.4 ./virtualenv.py --distribute $VIRTENV
