54b4dc7d40d013e388fabc81a4e36a4a8c0ed1a4
Han Genuit <hangenuit@gmail.com>
2012-09-16 22:03:49 +0200

    BUG: Make sure empty shapes are disallowed when data is specified

--- numpy/core/src/multiarray/ctors.c
+++ numpy/core/src/multiarray/ctors.c
@@ -2074,6 +2074,11 @@ PyArray_FromInterface(PyObject *origin)
     /* Case for data access through pointer */
     else if (attr && PyTuple_Check(attr)) {
         PyObject *dataptr;
+        if (n == 0) {
+            PyErr_SetString(PyExc_ValueError,
+                    "__array_interface__ shape must be at least size 1");
+            goto fail;
+        }
         if (PyTuple_GET_SIZE(attr) != 2) {
             PyErr_SetString(PyExc_TypeError,
                     "__array_interface__ data must be a 2-tuple with "
@@ -2107,6 +2112,11 @@ PyArray_FromInterface(PyObject *origin)
 
     /* Case for data access through buffer */
     else {
+        if (n == 0) {
+            PyErr_SetString(PyExc_ValueError,
+                    "__array_interface__ shape must be at least size 1");
+            goto fail;
+        }
         if (attr && (attr != Py_None)) {
             base = attr;
         }
