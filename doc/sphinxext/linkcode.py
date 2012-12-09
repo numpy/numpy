c5cb18f9486c5e6291e476890a3b6bef1424062c
Pauli Virtanen <pav@iki.fi>
2012-12-09 19:31:21 +0200

    DOC: numpydoc/linkcode: mention that the extension will be in Sphinx upstream

--- doc/sphinxext/linkcode.py
+++ doc/sphinxext/linkcode.py
@@ -10,8 +10,8 @@
 """
 
 import warnings
-warnings.warn("This extension has been submitted to Sphinx upstream. "
-              "Use the version from there if it is accepted "
+warnings.warn("This extension has been accepted to Sphinx upstream. "
+              "Use the version from there (Sphinx >= 1.2) "
               "https://bitbucket.org/birkenfeld/sphinx/pull-request/47/sphinxextlinkcode",
               FutureWarning, stacklevel=1)
 
