5de9999d82fa2353fce4518a83c187a168bf2aa3
David Warde-Farley <wardefar@iro.umontreal.ca>
2012-12-14 04:52:13 -0500

    TST: Add tests for non-int slice deprecation.

--- numpy/core/tests/test_deprecations.py
+++ numpy/core/tests/test_deprecations.py
@@ -28,7 +28,7 @@ def check_does_not_raise(f, category):
     np.testing.assert_(not raised)
 
 
-class TestFloatIndexDeprecation(object):
+class TestFloatScalarIndexDeprecation(object):
     """
     These test that DeprecationWarnings get raised when you
     try to use scalar indices that are not integers e.g. a[0.0],
@@ -41,11 +41,11 @@ class TestFloatIndexDeprecation(object):
     be removed from npy_pycompat.h and changed to just PyIndex_Check.
     """
     def setUp(self):
-        warnings.filterwarnings("error", message="non-integer",
+        warnings.filterwarnings("error", message="non-integer scalar index",
                                 category=DeprecationWarning)
 
     def tearDown(self):
-        warnings.filterwarnings("default", message="non-integer",
+        warnings.filterwarnings("default", message="non-integer scalar index",
                                 category=DeprecationWarning)
 
     def test_deprecations(self):
@@ -72,6 +72,10 @@ class TestFloatIndexDeprecation(object):
         yield check_for_warning, lambda: a[0, 0, -1.4], DeprecationWarning
         yield check_for_warning, lambda: a[-1.4, 0, 0], DeprecationWarning
         yield check_for_warning, lambda: a[0, -1.4, 0], DeprecationWarning
+        # Test that the slice parameter deprecation warning doesn't mask
+        # the scalar index warning.
+        yield check_for_warning, lambda: a[0.0:, 0.0], DeprecationWarning
+        yield check_for_warning, lambda: a[0.0:, 0.0, :], DeprecationWarning
 
     def test_valid_not_deprecated(self):
         a = np.array([[[5]]])
@@ -84,4 +88,65 @@ class TestFloatIndexDeprecation(object):
         yield (check_does_not_raise, lambda: a[:, 0, :],
                DeprecationWarning)
         yield (check_does_not_raise, lambda: a[:, :, :],
+               DeprecationWarning)
+
+
+class TestFloatSliceParameterDeprecation(object):
+    def setUp(self):
+        warnings.filterwarnings("error", message="non-integer slice param",
+                                category=DeprecationWarning)
+
+    def tearDown(self):
+        warnings.filterwarnings("default", message="non-integer slice param",
+                                category=DeprecationWarning)
+
+    def test_deprecations(self):
+        a = np.array([[5]])
+        if sys.version_info[:2] < (2, 5):
+            raise SkipTest()
+        # start as float.
+        yield check_for_warning, lambda: a[0.0:], DeprecationWarning
+        yield check_for_warning, lambda: a[0:, 0.0:2], DeprecationWarning
+        yield check_for_warning, lambda: a[0.0::2, :0], DeprecationWarning
+        yield check_for_warning, lambda: a[0.0:1:2, :], DeprecationWarning
+        yield check_for_warning, lambda: a[:, 0.0:], DeprecationWarning
+        # stop as float.
+        yield check_for_warning, lambda: a[:0.0], DeprecationWarning
+        yield check_for_warning, lambda: a[:0, 1:2.0], DeprecationWarning
+        yield check_for_warning, lambda: a[:0.0:2, :0], DeprecationWarning
+        yield check_for_warning, lambda: a[:0.0, :], DeprecationWarning
+        yield check_for_warning, lambda: a[:, 0:4.0:2], DeprecationWarning
+        # step as float.
+        yield check_for_warning, lambda: a[::1.0], DeprecationWarning
+        yield check_for_warning, lambda: a[0:, :2:2.0], DeprecationWarning
+        yield check_for_warning, lambda: a[1::4.0, :0], DeprecationWarning
+        yield check_for_warning, lambda: a[::5.0, :], DeprecationWarning
+        yield check_for_warning, lambda: a[:, 0:4:2.0], DeprecationWarning
+        # mixed.
+        yield check_for_warning, lambda: a[1.0:2:2.0], DeprecationWarning
+        yield check_for_warning, lambda: a[1.0::2.0], DeprecationWarning
+        yield check_for_warning, lambda: a[0:, :2.0:2.0], DeprecationWarning
+        yield check_for_warning, lambda: a[1.0:1:4.0, :0], DeprecationWarning
+        yield check_for_warning, lambda: a[1.0:5.0:5.0, :], DeprecationWarning
+        yield check_for_warning, lambda: a[:, 0.4:4.0:2.0], DeprecationWarning
+        # should still get the DeprecationWarning if step = 0.
+        yield check_for_warning, lambda: a[::0.0], DeprecationWarning
+
+    def test_valid_not_deprecated(self):
+        a = np.array([[[5]]])
+        yield (check_does_not_raise, lambda: a[::],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[0:],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[:2],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[0:2],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[::2],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[1::2],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[:2:2],
+               DeprecationWarning)
+        yield (check_does_not_raise, lambda: a[1:2:2],
                DeprecationWarning)
