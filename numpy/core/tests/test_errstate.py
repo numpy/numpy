bcbfea461a61a12ea91622d7569b256381570f9f
Ralf Gommers <ralf.gommers@googlemail.com>
2013-01-02 20:41:00 +0100

    MAINT: remove painful to look at exec statement from np.errstate test.

--- numpy/core/tests/test_errstate.py
+++ numpy/core/tests/test_errstate.py
@@ -1,41 +1,30 @@
-# The following exec statement (or something like it) is needed to
-# prevent SyntaxError on Python < 2.5. Even though this is a test,
-# SyntaxErrors are not acceptable; on Debian systems, they block
-# byte-compilation during install and thus cause the package to fail
-# to install.
-
-import sys
-if sys.version_info[:2] >= (2, 5):
-    exec """
-from __future__ import with_statement
 import platform
 
-from numpy.core import *
-from numpy.random import rand, randint
-from numpy.testing import *
+import numpy as np
+from numpy.testing import TestCase, assert_, run_module_suite, dec
 
 
 class TestErrstate(TestCase):
     @dec.skipif(platform.machine() == "armv5tel", "See gh-413.")
     def test_invalid(self):
-        with errstate(all='raise', under='ignore'):
-            a = -arange(3)
+        with np.errstate(all='raise', under='ignore'):
+            a = -np.arange(3)
             # This should work
-            with errstate(invalid='ignore'):
-                sqrt(a)
+            with np.errstate(invalid='ignore'):
+                np.sqrt(a)
             # While this should fail!
             try:
-                sqrt(a)
+                np.sqrt(a)
             except FloatingPointError:
                 pass
             else:
                 self.fail("Did not raise an invalid error")
 
     def test_divide(self):
-        with errstate(all='raise', under='ignore'):
-            a = -arange(3)
+        with np.errstate(all='raise', under='ignore'):
+            a = -np.arange(3)
             # This should work
-            with errstate(divide='ignore'):
+            with np.errstate(divide='ignore'):
                 a // 0
             # While this should fail!
             try:
@@ -48,14 +37,13 @@ class TestErrstate(TestCase):
     def test_errcall(self):
         def foo(*args):
             print(args)
-        olderrcall = geterrcall()
-        with errstate(call=foo):
-            assert_(geterrcall() is foo, 'call is not foo')
-            with errstate(call=None):
-                assert_(geterrcall() is None, 'call is not None')
-        assert_(geterrcall() is olderrcall, 'call is not olderrcall')
+        olderrcall = np.geterrcall()
+        with np.errstate(call=foo):
+            assert_(np.geterrcall() is foo, 'call is not foo')
+            with np.errstate(call=None):
+                assert_(np.geterrcall() is None, 'call is not None')
+        assert_(np.geterrcall() is olderrcall, 'call is not olderrcall')
 
-"""
 
 if __name__ == "__main__":
     run_module_suite()
