1d6f543eb85a2632a8dcee401e8ec76776f407e3
Charles Harris <charlesr.harris@gmail.com>
2013-01-05 15:15:44 -0700

    BUG: Replace unprefixed SIZEOF_* macros with prefixed versions.
    The sources don't define NPY_NO_PREFIX and consequently none of the
    unprefixed macros are defined. Using them can lead to some unexpected
    results.

--- numpy/core/src/multiarray/iterators.c
+++ numpy/core/src/multiarray/iterators.c
@@ -1716,7 +1716,7 @@ arraymultiter_dealloc(PyArrayMultiIterObject *multi)
 static PyObject *
 arraymultiter_size_get(PyArrayMultiIterObject *self)
 {
-#if NPY_SIZEOF_INTP <= SIZEOF_LONG
+#if NPY_SIZEOF_INTP <= NPY_SIZEOF_LONG
     return PyInt_FromLong((long) self->size);
 #else
     if (self->size < NPY_MAX_LONG) {
@@ -1731,7 +1731,7 @@ arraymultiter_size_get(PyArrayMultiIterObject *self)
 static PyObject *
 arraymultiter_index_get(PyArrayMultiIterObject *self)
 {
-#if NPY_SIZEOF_INTP <= SIZEOF_LONG
+#if NPY_SIZEOF_INTP <= NPY_SIZEOF_LONG
     return PyInt_FromLong((long) self->index);
 #else
     if (self->size < NPY_MAX_LONG) {
