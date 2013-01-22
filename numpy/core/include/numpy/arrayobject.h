0a34401441ba9ce2f1530537241429a254769964
Raul Cota <rcota@hotmail.com>
2013-01-22 14:33:03 -0700

    Fix for Python 3
    Take into account that PyInt_CheckExact  does not exits in Python 3

--- numpy/core/include/numpy/arrayobject.h
+++ numpy/core/include/numpy/arrayobject.h
@@ -1,6 +1,7 @@
 #ifndef Py_ARRAYOBJECT_H
 #define Py_ARRAYOBJECT_H
 
+#include <Python.h>
 #include "ndarrayobject.h"
 #include "npy_interrupt.h"
 
@@ -13,8 +14,12 @@
  * support array related methods. Useful for faster checks when
  * validating if an object supports these methods
  */
+#if PY_VERSION_HEX >= 0x03000000
+/* PyInt_CheckExact is not in Python 3 */
+#define ISEXACT_NATIVE_PYTYPE(op) (PyList_CheckExact(op) ||  (Py_None == op) || PyTuple_CheckExact(op) || PyFloat_CheckExact(op) || PyString_CheckExact(op) || PyUnicode_CheckExact(op))
+#else
 #define ISEXACT_NATIVE_PYTYPE(op) (PyList_CheckExact(op) ||  (Py_None == op) || PyTuple_CheckExact(op) || PyFloat_CheckExact(op) || PyInt_CheckExact(op) || PyString_CheckExact(op) || PyUnicode_CheckExact(op))
-
+#endif
 /* Check for exact native types that for sure do not
  * support buffer protocol. Useful for faster checks when 
  * validating if an object supports the buffer protocol.
