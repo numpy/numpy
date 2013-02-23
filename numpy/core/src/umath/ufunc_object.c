2e8fcc0814486accb71aacb7d60ebcb3203f3b7a
Sebastian Berg <sebastian@sipsolutions.net>
2013-02-23 12:32:58 +0100

    API: Make nditer support ndim == 0 for 0-d iterations.
    There are relatively few changes necessare here. However there is a
    conceptionally there are no axes for the 0-d case, and no axesdata
    needs to be used. This still uses the first axisdata. Which means that
    in a few places ndim == 0 is special cased or special cased to act like
    ndim == 1.
    It would probably be a little cleaner to to use the base pointers directly
    in the 0-d case and no axes iteration at all. That would require similar
    special cases though.
    
    This also makes oa_ndim == -1 the "correct" way to signal that no op_axes
    are given with oa_ndim == 0 being, for the time being, dual use. Either
    meaning that nothing may be given, or if something something was given
    enforcing a 0-d iteration.
    
    The necessary changes to the ufunc machinery are also done.
    
    Documented that the dtype transfer functions do not handle the scalar
    case unless even shape is set.

--- numpy/core/src/umath/ufunc_object.c
+++ numpy/core/src/umath/ufunc_object.c
@@ -1211,7 +1211,7 @@ iterator_loop(PyUFuncObject *ufunc,
                         NPY_ITER_DELAY_BUFALLOC,
                         order, NPY_UNSAFE_CASTING,
                         op_flags, dtype,
-                        0, NULL, NULL, buffersize);
+                        -1, NULL, NULL, buffersize);
     if (iter == NULL) {
         return -1;
     }
@@ -1509,7 +1509,7 @@ execute_fancy_ufunc_loop(PyUFuncObject *ufunc,
                         NPY_ITER_GROWINNER,
                         order, NPY_UNSAFE_CASTING,
                         op_flags, dtypes,
-                        0, NULL, NULL, buffersize);
+                        -1, NULL, NULL, buffersize);
     if (iter == NULL) {
         return -1;
     }
