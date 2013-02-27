ae4e3223e843ee38ea6f5cb9df192758a9b52bb2
Sebastian Berg <sebastian@sipsolutions.net>
2013-02-27 12:57:41 +0100

    ENH: Use NPY_ITER_USE_ZERO_OA_NDIM for einsum
    This allows for scalar multiplication with einsum, without any
    further special casing.

--- numpy/core/tests/test_einsum.py
+++ numpy/core/tests/test_einsum.py
@@ -239,6 +239,7 @@ class TestEinSum(TestCase):
             assert_equal(np.einsum(a, [0,0]), np.trace(a).astype(dtype))
 
         # multiply(a, b)
+        assert_equal(np.einsum("..., ...", 3, 4), 12) # check scalars
         for n in range(1,17):
             a = np.arange(3*n, dtype=dtype).reshape(3,n)
             b = np.arange(2*3*n, dtype=dtype).reshape(2,3,n)
