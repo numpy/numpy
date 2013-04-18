be7539e38bc5b182567c8cd47887e67e4bf1e286
andrew giessel <andrew.giessel@gmail.com>
2013-04-18 11:25:19 -0400

    iterdim() tests

--- numpy/core/tests/test_numeric.py
+++ numpy/core/tests/test_numeric.py
@@ -1525,6 +1525,39 @@ class TestRoll(TestCase):
         x = np.array([])
         assert_equal(np.roll(x, 1), np.array([]))
 
+class TestIterdim(TestCase):
+    def test_iterdim1d(self):
+        a = np.arange(10)
+        a_iter = np.iterdim(a)
+        for i, a_slice in enumerate(a_iter):
+            assert_equal(a_slice, ())
+        assert_equal(i, 9)
+    
+    def test_iterdimNd(self):
+        a = np.ones((100,10,3))
+
+        a_iter = np.iterdim(a, 1)
+        for i, a_slice in enumerate(a_iter):
+            assert_equal(a_slice, (10,3))
+        assert_equal(i, 99)
+
+        a_iter = np.iterdim(a, 1)
+        for a_slice in enumerate(a_iter):
+            assert_equal(a_slice, (100,3))
+        assert_equal(i, 9)
+
+        a_iter = np.iterdim(a, 2)
+        for a_slice in enumerate(a_iter):
+            assert_equal(a_slice, (100,10))
+        assert_equal(i, 2)
+                           
+    def test_axis_out_of_bounds_low(self):
+        a = np.ones((100,10))
+        assert_raises(ValueError, np.iterdim(a, -1))
+
+    def test_axis_out_of_bounds_high(self):
+        a = np.ones((100,10))
+        assert_raises(ValueError, np.iterdim(a, 2))
 
 if __name__ == "__main__":
     run_module_suite()
