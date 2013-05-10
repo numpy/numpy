Fix issue with broadcast_arrays() and user defined dtypes
broadcast_arrays() does not handle struct and custom dtypes correctly. Dtype of returned broadcasted arrays is always '|V8'. Fix broadcast_arrays() so that dtype of returned arrays is correct dtype for user defined dtypes.

numpy/lib/stride_tricks.py
@@ -27,7 +27,10 @@ def as_strided(x, shape=None, strides=None):
         interface['shape'] = tuple(shape)
     if strides is not None:
         interface['strides'] = tuple(strides)
-    return np.asarray(DummyArray(interface, base=x))
+    array = np.asarray(DummyArray(interface, base=x))
+    # Make sure dtype is correct in case of custom dtype
+    array.dtype = x.dtype
+    return array
 
 def broadcast_arrays(*args):
     """