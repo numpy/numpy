3beebbc0164afbbcc2b6840cf56174c6c073bb40
Charles Harris <charlesr.harris@gmail.com>
2013-08-18 18:40:28 -0600

    DEP: Remove deprecated modules numarray and oldnumeric.
    They were deprecated in 1.8 and scheduled for removal in 1.9.
    
    Closes #3637.

--- numpy/oldnumeric/user_array.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division, absolute_import, print_function
-
-from numpy.oldnumeric import *
-from numpy.lib.user_array import container as UserArray
-
-import numpy.oldnumeric as nold
-__all__ = nold.__all__[:]
-__all__ += ['UserArray']
-del nold
