acace4d98db4b7ab48b41f41a116def67208eaa5
Charles Harris <charlesr.harris@gmail.com>
2013-09-06 19:31:10 -0600

    MAINT: STY: Consolidate test_helper with test_fftpack.
    The tests in test_helper.py are moved to test_fftpack.py and
    test_helper.py deleted. The test_fftpack.py file is made PEP8 compliant.

--- numpy/fft/tests/test_fftpack.py
+++ numpy/fft/tests/test_fftpack.py
@@ -1,7 +1,10 @@
 from __future__ import division, absolute_import, print_function
 
 import numpy as np
+from numpy import fft
+from numpy import pi
 from numpy.testing import TestCase, run_module_suite, assert_array_almost_equal
+
 
 def fft1(x):
     L = len(x)
@@ -9,10 +12,36 @@ def fft1(x):
     phase = np.arange(L).reshape(-1, 1) * phase
     return np.sum(x*np.exp(phase), axis=1)
 
+
 class TestFFTShift(TestCase):
 
     def test_fft_n(self):
         self.assertRaises(ValueError, np.fft.fft, [1, 2, 3], 0)
+
+    def test_definition(self):
+        x = [0, 1, 2, 3, 4, -4, -3, -2, -1]
+        y = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
+        assert_array_almost_equal(fft.fftshift(x), y)
+        assert_array_almost_equal(fft.ifftshift(y), x)
+        x = [0, 1, 2, 3, 4, -5, -4, -3, -2, -1]
+        y = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4]
+        assert_array_almost_equal(fft.fftshift(x), y)
+        assert_array_almost_equal(fft.ifftshift(y), x)
+
+    def test_inverse(self):
+        for n in [1, 4, 9, 100, 211]:
+            x = np.random.random((n,))
+            assert_array_almost_equal(fft.ifftshift(fft.fftshift(x)), x)
+
+    def test_axes_keyword(self):
+        freqs = [[0,  1,  2], [3,  4, -4], [-3, -2, -1]]
+        shifted = [[-1, -3, -2], [2,  0,  1], [-4,  3,  4]]
+        assert_array_almost_equal(fft.fftshift(freqs, axes=(0, 1)), shifted)
+        assert_array_almost_equal(fft.fftshift(freqs, axes=0),
+                                  fft.fftshift(freqs, axes=(0,)))
+        assert_array_almost_equal(fft.ifftshift(shifted, axes=(0, 1)), freqs)
+        assert_array_almost_equal(fft.ifftshift(shifted, axes=0),
+                                  fft.ifftshift(shifted, axes=(0,)))
 
 
 class TestFFT1D(TestCase):
@@ -21,6 +50,28 @@ class TestFFT1D(TestCase):
         rand = np.random.random
         x = rand(30) + 1j*rand(30)
         assert_array_almost_equal(fft1(x), np.fft.fft(x))
+
+
+class TestRFFTFreq(TestCase):
+
+    def test_definition(self):
+        x = [0, 1, 2, 3, 4]
+        assert_array_almost_equal(9*fft.rfftfreq(9), x)
+        assert_array_almost_equal(9*pi*fft.rfftfreq(9, pi), x)
+        x = [0, 1, 2, 3, 4, 5]
+        assert_array_almost_equal(10*fft.rfftfreq(10), x)
+        assert_array_almost_equal(10*pi*fft.rfftfreq(10, pi), x)
+
+
+class TestIRFFTN(TestCase):
+
+    def test_not_last_axis_success(self):
+        ar, ai = np.random.random((2, 16, 8, 32))
+        a = ar + 1j*ai
+        axes = (-2,)
+
+        # Should not raise error
+        fft.irfftn(a, axes=axes)
 
 
 if __name__ == "__main__":
