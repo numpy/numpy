34fc33eb104305a9bb5ce6b659f87beb3d5a4737
Julian Taylor <jtaylor.debian@googlemail.com>
2013-09-29 20:09:24 +0200

    BUG: fix small issues found with cppcheck
    uninitialized use in _strided_to_strided_one_to_n_with_finish
    wrong error NULL checks in PyArray_MapIterNew and einsum_sub_op_from_str
    double free and memleak in umath_linalg.c.src init_@lapack_func@

--- numpy/core/src/multiarray/mapping.c
+++ numpy/core/src/multiarray/mapping.c
@@ -1970,12 +1970,12 @@ PyArray_MapIterNew(PyObject *indexobj, int oned, int fancy)
     }
 
     mit = (PyArrayMapIterObject *)PyArray_malloc(sizeof(PyArrayMapIterObject));
-    /* set all attributes of mapiter to zero */
-    memset(mit, 0, sizeof(PyArrayMapIterObject));
-    PyObject_Init((PyObject *)mit, &PyArrayMapIter_Type);
     if (mit == NULL) {
         return NULL;
     }
+    /* set all attributes of mapiter to zero */
+    memset(mit, 0, sizeof(PyArrayMapIterObject));
+    PyObject_Init((PyObject *)mit, &PyArrayMapIter_Type);
     /* initialize mapiter attributes */
     mit->consec = 1;
 
