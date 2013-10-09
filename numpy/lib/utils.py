293b9e01c5aae668e06522427870c24564a0ebc4
Joseph Martinot-Lagarde <joseph.martinot-lagarde@onera.fr>
2013-10-09 12:55:08 +0200

    BUG: Disable check for oldstyle classes in python3
    Fixes gh-2561

--- numpy/lib/utils.py
+++ numpy/lib/utils.py
@@ -561,7 +561,10 @@ def info(object=None,maxwidth=76,output=sys.stdout,toplevel='numpy'):
                     methstr, other = pydoc.splitdoc(inspect.getdoc(thisobj) or "None")
                 print("  %s  --  %s" % (meth, methstr), file=output)
 
-    elif isinstance(object, types.InstanceType): ## check for __call__ method
+    elif (sys.version_info[0] < 3
+            and isinstance(object, types.InstanceType)):
+        # check for __call__ method
+        # types.InstanceType is the type of the instances of oldstyle classes
         print("Instance of class: ", object.__class__.__name__, file=output)
         print(file=output)
         if hasattr(object, '__call__'):
