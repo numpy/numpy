4f01ab86902c019366939463dc4f3a059f990471
Julian Taylor <jtaylor.debian@googlemail.com>
2014-01-22 20:11:53 +0100

    DOC: add versionadded:: 1.8.0 to ufunc.at docstring

--- numpy/add_newdocs.py
+++ numpy/add_newdocs.py
@@ -5828,6 +5828,8 @@ add_newdoc('numpy.core', 'ufunc', ('at',
     increment the first element once because of buffering, whereas
     `add.at(a, [0,0], 1)` will increment the first element twice.
 
+    .. versionadded:: 1.8.0
+
     Parameters
     ----------
     a : array_like
