4d686a1e44462c34b60de9049dd499aed60de643
Charles Harris <charlesr.harris@gmail.com>
2014-02-10 11:04:37 -0700

    MAINT: Remove checks for presence of decimal module.
    The decimal module has been available since Python 2.4 and we no
    longer support Python earlier than 2.6.

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -3134,12 +3134,6 @@ class TestChoose(TestCase):
         A = np.choose(self.ind, (self.x, self.y2))
         assert_equal(A, [[2, 2, 3], [2, 2, 3]])
 
-def can_use_decimal():
-    try:
-        from decimal import Decimal
-        return True
-    except ImportError:
-        return False
 
 # TODO: test for multidimensional
 NEIGH_MODE = {'zero': 0, 'one': 1, 'constant': 2, 'circular': 3, 'mirror': 4}
@@ -3175,11 +3169,7 @@ class TestNeighborhoodIter(TestCase):
     def test_simple2d(self):
         self._test_simple2d(np.float)
 
-    @dec.skipif(not can_use_decimal(),
-            "Skip neighborhood iterator tests for decimal objects " \
-            "(decimal module not available")
     def test_simple2d_object(self):
-        from decimal import Decimal
         self._test_simple2d(Decimal)
 
     def _test_mirror2d(self, dt):
@@ -3195,11 +3185,7 @@ class TestNeighborhoodIter(TestCase):
     def test_mirror2d(self):
         self._test_mirror2d(np.float)
 
-    @dec.skipif(not can_use_decimal(),
-            "Skip neighborhood iterator tests for decimal objects " \
-            "(decimal module not available")
     def test_mirror2d_object(self):
-        from decimal import Decimal
         self._test_mirror2d(Decimal)
 
     # Simple, 1d tests
@@ -3221,11 +3207,7 @@ class TestNeighborhoodIter(TestCase):
     def test_simple_float(self):
         self._test_simple(np.float)
 
-    @dec.skipif(not can_use_decimal(),
-            "Skip neighborhood iterator tests for decimal objects " \
-            "(decimal module not available")
     def test_simple_object(self):
-        from decimal import Decimal
         self._test_simple(Decimal)
 
     # Test mirror modes
@@ -3240,11 +3222,7 @@ class TestNeighborhoodIter(TestCase):
     def test_mirror(self):
         self._test_mirror(np.float)
 
-    @dec.skipif(not can_use_decimal(),
-            "Skip neighborhood iterator tests for decimal objects " \
-            "(decimal module not available")
     def test_mirror_object(self):
-        from decimal import Decimal
         self._test_mirror(Decimal)
 
     # Circular mode
@@ -3258,11 +3236,7 @@ class TestNeighborhoodIter(TestCase):
     def test_circular(self):
         self._test_circular(np.float)
 
-    @dec.skipif(not can_use_decimal(),
-            "Skip neighborhood iterator tests for decimal objects " \
-            "(decimal module not available")
     def test_circular_object(self):
-        from decimal import Decimal
         self._test_circular(Decimal)
 
 # Test stacking neighborhood iterators
