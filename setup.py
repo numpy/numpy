23f8dcf86cf692fcc9dce48350d5d86c0bc63ada
Julian Taylor <jtaylor.debian@googlemail.com>
2014-03-02 14:10:59 +0000

    REL: Release 1.8.1rc1

--- setup.py
+++ setup.py
@@ -49,9 +49,9 @@ Operating System :: MacOS
 
 MAJOR               = 1
 MINOR               = 8
-MICRO               = 0
-ISRELEASED          = False
-VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+MICRO               = 1
+ISRELEASED          = True
+VERSION             = '%d.%d.%drc1' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
