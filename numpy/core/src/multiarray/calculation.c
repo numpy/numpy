5941fc9d9f15cd773b6c3989091a4b7ef96a1c30
Eric Moore <ewm@redtetrahedron.org>
2014-07-18 11:04:29 -0400

    BUG: object array np.conjugate, ndarray.conjugate inconsistent
    fixes gh-4730

--- numpy/core/src/multiarray/calculation.c
+++ numpy/core/src/multiarray/calculation.c
@@ -1182,7 +1182,7 @@ PyArray_Clip(PyArrayObject *self, PyObject *min, PyObject *max, PyArrayObject *o
 NPY_NO_EXPORT PyObject *
 PyArray_Conjugate(PyArrayObject *self, PyArrayObject *out)
 {
-    if (PyArray_ISCOMPLEX(self)) {
+    if (PyArray_ISCOMPLEX(self) || PyArray_ISOBJECT(self)) {
         if (out == NULL) {
             return PyArray_GenericUnaryFunction(self,
                                                 n_ops.conjugate);
