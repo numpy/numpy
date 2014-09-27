41d52fb98389963b1b16485624aa60a364c7bde2
Fei Liu <aaron11111.china@gmail.com>
2014-09-27 19:22:52 -0400

    test financial fixt

--- numpy/lib/tests/test_financial.py
+++ numpy/lib/tests/test_financial.py
@@ -47,9 +47,7 @@ class TestFinancial(TestCase):
         assert_almost_equal(np.pmt(0.0, 5*12, 15000),
                             -250.0, 3)
         # This one handles the case where we use broadcast and arguments passed in are arrays
-        assert_almost_equal(np.pmt([[0.0,0.8],[0.3, 0.8]], [12,3], [2000,20000]),np.array([[  -166.666, -19311.258],[  -626.908, -19311.258]]), 3)
--2000, 0, 100000.0),21.54, 2)
-
+        assert_almost_equal(np.pmt([[0.0,0.8],[0.3, 0.8]], [12,3], [2000,20000]),np.array([[  -166.666, -19311.258],[  -626.908, -19311.258]]), 3)        
     def test_nper2(self):
         assert_almost_equal(np.nper(0.0, -2000, 0, 100000.),
                             50.0, 1)
