6cf7ecefbfbc587647760b68fd7f803271fca3be
Julian Taylor <jtaylor.debian@googlemail.com>
2014-10-10 19:44:14 +0200

    REL: set version number to unreleased 1.9.1

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ Operating System :: MacOS
 
 MAJOR               = 1
 MINOR               = 9
-MICRO               = 0
-ISRELEASED          = True
+MICRO               = 1
+ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
