BUG: upcast weights to average result type to avoid inaccuracies
closes gh-5202

numpy/lib/function_base.py
@@ -510,8 +510,7 @@ def average(a, axis=None, weights=None, returned=False):
         scl = avg.dtype.type(a.size/avg.size)
     else:
         a = a + 0.0
-        wgt = np.array(weights, dtype=a.dtype, copy=0)
-
+        wgt = np.asarray(weights)
         # Sanity checks
         if a.shape != wgt.shape:
             if axis is None:
@@ -528,7 +527,7 @@ def average(a, axis=None, weights=None, returned=False):
             # setup wgt to broadcast along axis
             wgt = np.array(wgt, copy=0, ndmin=a.ndim).swapaxes(-1, axis)
 
-        scl = wgt.sum(axis=axis)
+        scl = wgt.sum(axis=axis, dtype=np.result_type(a.dtype, wgt.dtype))
         if (scl == 0.0).any():
             raise ZeroDivisionError(
                 "Weights sum to zero, can't be normalized")