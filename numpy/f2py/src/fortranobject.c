3202612006ce651913d2fd448e3eb6393ba8414b
Ralf Gommers <ralf.gommers@googlemail.com>
2014-11-23 22:15:51 +0100

    MAINT: remove NPY_NO_DEPRECATED_API define from f2py.
    See gh-5281 for discussion.  With the defines in, compiling scipy 0.14.0 and
    below isn't possible.

--- numpy/f2py/src/fortranobject.c
+++ numpy/f2py/src/fortranobject.c
@@ -1,5 +1,4 @@
 #define FORTRANOBJECT_C
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include "fortranobject.h"
 
 #ifdef __cplusplus
@@ -1033,6 +1032,4 @@ F2PyCapsule_Check(PyObject *ptr)
 #ifdef __cplusplus
 }
 #endif
-
-#undef NPY_NO_DEPRECATED_API
 /************************* EOF fortranobject.c *******************************/
