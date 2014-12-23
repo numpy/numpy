c683a43a23be09eda868a6be2427e4c9ef583447
alex <argriffi@ncsu.edu>
2014-12-22 19:52:14 -0500

    BLD: for PEP 440 compliance, change development version name format from e.g. 1.10.0.dev-243ab56 to 1.10.0.dev+243ab56

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ def get_version_info():
         GIT_REVISION = "Unknown"
 
     if not ISRELEASED:
-        FULLVERSION += '.dev-' + GIT_REVISION[:7]
+        FULLVERSION += '.dev+' + GIT_REVISION[:7]
 
     return FULLVERSION, GIT_REVISION
 
