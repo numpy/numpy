fde08cfc25e581988d41b8645b3bdacd0e50ddfd
F.P. Landes <francois.landes@gmail.com>
2015-01-06 20:53:06 +0100

    Update mtrand.pyx
    Docstring of "pareto" lightly changed, according to the discussion :
    https://github.com/numpy/numpy/issues/4181#issuecomment-68915452

--- numpy/random/mtrand/mtrand.pyx
+++ numpy/random/mtrand/mtrand.pyx
@@ -2611,14 +2611,14 @@ cdef class RandomState:
         --------
         Draw samples from the distribution:
 
-        >>> a, m = 3., 1. # shape and mode
-        >>> s = np.random.pareto(a, 1000) + m
+        >>> a, m = 3., 2. # shape and mode
+        >>> s = (np.random.pareto(a, 1000) +1) * m
 
         Display the histogram of the samples, along with
         the probability density function:
 
         >>> import matplotlib.pyplot as plt
-        >>> count, bins, ignored = plt.hist(s, 100, normed=True, align='center')
+        >>> count, bins, ignored = plt.hist(s, 100, normed=True)
         >>> fit = a*m**a/bins**(a+1)
         >>> plt.plot(bins, max(count)*fit/max(fit),linewidth=2, color='r')
         >>> plt.show()
