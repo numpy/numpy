63c06ec3f225fe126670ebaa0bf70041d2e3c511
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-02-18 13:56:27 -0500

    BUG: Fixes `numpy.where` to behave more consistently with `numpy.ma.where`. In other words, if the two arrays are provided as `None`, it treats this case as if only `condition` was provided.

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -4818,7 +4818,12 @@ class TestWhere(TestCase):
 
     def test_exotic(self):
         # object
-        assert_array_equal(np.where(True, None, None), np.array(None))
+        assert_array_equal(np.where(True), (np.array([0]),))
+        assert_array_equal(np.where(True, None, None), (np.array([0]),))
+        assert_array_equal(
+            np.where(True, np.array(None), np.array(None)),
+            np.array(None)
+        )
         # zero sized
         m = np.array([], dtype=bool).reshape(0, 3)
         b = np.array([], dtype=np.float64).reshape(0, 3)
