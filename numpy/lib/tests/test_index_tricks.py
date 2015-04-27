f06b1210d7171b4a452d0c9c67cde7b1a130303e
Jaime Fernandez <jaime.frio@gmail.com>
2015-04-27 06:15:03 -0700

    TST: Added tests for ix_

--- numpy/lib/tests/test_index_tricks.py
+++ numpy/lib/tests/test_index_tricks.py
@@ -7,7 +7,7 @@ from numpy.testing import (
     )
 from numpy.lib.index_tricks import (
     mgrid, ndenumerate, fill_diagonal, diag_indices, diag_indices_from,
-    index_exp, ndindex, r_, s_
+    index_exp, ndindex, r_, s_, ix_
     )
 
 
@@ -167,6 +167,30 @@ class TestIndexExpression(TestCase):
 
         assert_equal(a[:, :3, [1, 2]], a[index_exp[:, :3, [1, 2]]])
         assert_equal(a[:, :3, [1, 2]], a[s_[:, :3, [1, 2]]])
+
+
+class TestIx_(TestCase):
+    def test_regression_1(self):
+        # Empty inputs create ouputs of indexing type, gh-5804
+        a, = np.ix_(range(0, 0))
+        assert_equal(a.dtype, np.intp)
+
+    def test_shape_and_dtype(self):
+        sizes = (4, 5, 3, 2)
+        arrays = np.ix_(*[range(sz) for sz in sizes])
+        for k, (a, sz) in enumerate(zip(arrays, sizes)):
+            assert_equal(a.shape[k], sz)
+            assert_(all(sh == 1 for j, sh in enumerate(a.shape) if j != k))
+            assert_(np.issubdtype(a.dtype, int))
+
+    def test_bool(self):
+        bool_a = [True, False, True, True]
+        int_a, = np.nonzero(bool_a)
+        assert_equal(np.ix_(bool_a)[0], int_a)
+
+    def test_1d_only(self):
+        idx2d = [[1, 2, 3], [4, 5, 6]]
+        assert_raises(ValueError, np.ix_, idx2d)
 
 
 def test_c_():
