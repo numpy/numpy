81788b45e383d2f6494cf3daadbd7fb6bfc550b1
Charles Harris <charlesr.harris@gmail.com>
2015-06-21 12:31:06 -0600

    BUG: Fix wrong deprecation message for logical unary '-' operator.

--- numpy/core/src/umath/ufunc_type_resolution.c
+++ numpy/core/src/umath/ufunc_type_resolution.c
@@ -385,9 +385,9 @@ PyUFunc_NegativeTypeResolver(PyUFuncObject *ufunc,
     /* The type resolver would have upcast already */
     if (out_dtypes[0]->type_num == NPY_BOOL) {
         /* 2013-12-05, 1.9 */
-        if (DEPRECATE("numpy boolean negative (the unary `-` operator) is "
-                      "deprecated, use the bitwise_xor (the `^` operator) "
-                      "or the logical_xor function instead.") < 0) {
+        if (DEPRECATE("numpy boolean negative, the `-` operator, is "
+                      "deprecated, use the `~` operator or the logical_not "
+                      "function instead.") < 0) {
             return -1;
         }
     }
@@ -801,8 +801,8 @@ PyUFunc_SubtractionTypeResolver(PyUFuncObject *ufunc,
         /* The type resolver would have upcast already */
         if (out_dtypes[0]->type_num == NPY_BOOL) {
             /* 2013-12-05, 1.9 */
-            if (DEPRECATE("numpy boolean subtract (the binary `-` operator) is "
-                          "deprecated, use the bitwise_xor (the `^` operator) "
+            if (DEPRECATE("numpy boolean subtract, the `-` operator, is "
+                          "deprecated, use the bitwise_xor, the `^` operator, "
                           "or the logical_xor function instead.") < 0) {
                 return -1;
             }
