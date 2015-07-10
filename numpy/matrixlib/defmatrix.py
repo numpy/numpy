437ab97898f2ff51f916893413711b0029c403bf
Utkarsh Upadhyay <musically.ut@gmail.com>
2015-07-10 18:57:49 +0200

    ENH: Add todense function to numpy.matrix
    The implementation and tests have been adapted from scipy.
    Closes #6060.

--- numpy/matrixlib/defmatrix.py
+++ numpy/matrixlib/defmatrix.py
@@ -387,6 +387,59 @@ class matrix(N.ndarray):
         """
         return self.__array__().tolist()
 
+    def todense(self, order=None, out=None):
+        """
+        Return a dense matrix representation of this matrix.
+
+        See Also
+        --------
+        matrix.copy
+
+        Notes
+        -----
+        This mimics the interface provided by sparse matrices in scipy.
+
+        Parameters
+        ----------
+        order : {'C', 'F'}, optional
+            Whether to store multi-dimensional data in C (row-major)
+            or Fortran (column-major) order in memory. The default
+            is 'None', indicating the NumPy default of C-ordered.
+            Cannot be specified in conjunction with the `out`
+            argument.
+
+        out : ndarray, 2-dimensional, optional
+            If specified, uses this array (or `numpy.matrix`) as the
+            output buffer instead of allocating a new array to
+            return. The provided array must have the same shape and
+            dtype as the sparse matrix on which you are calling the
+            method.
+
+        Returns
+        -------
+        arr : numpy.matrix, 2-dimensional
+            A NumPy matrix object with the same shape and containing
+            the same data represented by the sparse matrix, with the
+            requested memory order. If `out` was passed and was an
+            array (rather than a `numpy.matrix`), it will be filled
+            with the appropriate values and returned wrapped in a
+            `numpy.matrix` object that shares the same memory.
+        """
+
+        if out is not None:
+            if order is not None:
+                raise ValueError('order cannot be specified if out'
+                                 'is not None')
+
+            if out.shape != self.shape or out.dtype != self.dtype:
+                raise ValueError('out array must be same dtype and shape as '
+                                 'sparse matrix')
+
+            out[...] = self
+            return asmatrix(out)
+        else:
+            return self.copy(order=order)
+
     # To preserve orientation of result...
     def sum(self, axis=None, dtype=None, out=None):
         """
