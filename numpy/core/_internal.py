b68a522e9e3a7d04edf79ffa24c6637dcaaf8786
Allan Haldane <allan.haldane@gmail.com>
2015-07-19 20:55:34 -0400

    MAINT: allow '?' in structured dtype commastring specification
    Code like `dtype('?,?,?')` now works.

--- numpy/core/_internal.py
+++ numpy/core/_internal.py
@@ -138,7 +138,7 @@ format_re = re.compile(asbytes(
                            r'(?P<order1>[<>|=]?)'
                            r'(?P<repeats> *[(]?[ ,0-9L]*[)]? *)'
                            r'(?P<order2>[<>|=]?)'
-                           r'(?P<dtype>[A-Za-z0-9.]*(?:\[[a-zA-Z0-9,.]+\])?)'))
+                           r'(?P<dtype>[A-Za-z0-9.?]*(?:\[[a-zA-Z0-9,.]+\])?)'))
 sep_re = re.compile(asbytes(r'\s*,\s*'))
 space_re = re.compile(asbytes(r'\s+$'))
 
