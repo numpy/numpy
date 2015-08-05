b268c2dd63f460a1a554dec6891879e2eec87193
Alex Willmer <alex@moreati.org.uk>
2015-08-05 10:29:39 +0100

    DOC: Add Trove classifiers for supported Python versions
    Adds missing `Programming Language :: Python :: 3` to bento.info
    Adds new `Programming Language :: Python :: {2..,3..,CPython..}` to both
    
    These versions are correct for the 1.10.x and 1.11.x releases.
    They reflect the commit b06dbc15d9db0e224d338c84ac98a925a7945d4c

--- setup.py
+++ setup.py
@@ -38,7 +38,15 @@ Intended Audience :: Developers
 License :: OSI Approved
 Programming Language :: C
 Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: Implementation :: CPython
 Topic :: Software Development
 Topic :: Scientific/Engineering
 Operating System :: Microsoft :: Windows
