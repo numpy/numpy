numpy/numpy

commit 4b1f508a57549d8031a23160b40c7f87f47892ed

MAINT: move the special case for string comparison before the regular case
The ndarray richcompare function has a special case for handling
string dtypes (which currently cannot be handled by
ufuncs). Traditionally this was handled by the ufuncs returning
NotImplemented, and then falling through to a special case. By moving
the special case to the top of the richcompare function, it becomes
unnecessary for the ufuncs to return NotImplemented in this case.

numpy/core/src/multiarray/arrayobject.c
@@ -1295,6 +1295,25 @@ array_richcompare(PyArrayObject *self, PyObject *other, int cmp_op)
     PyObject *obj_self = (PyObject *)self;
     PyObject *result = NULL;
 
+    /* Special case for string arrays (which don't and currently can't have
+     * ufunc loops defined, so there's no point in trying).
+     */
+    if (PyArray_ISSTRING(self)) {
+        array_other = (PyArrayObject *)PyArray_FromObject(other,
+                                                          NPY_NOTYPE, 0, 0);
+        if (array_other == NULL) {
+            PyErr_Clear();
+            /* Never mind, carry on, see what happens */
+        }
+        else if (!PyArray_ISSTRING(array_other)) {
+            Py_DECREF(array_other);
+            /* Never mind, carry on, see what happens */
+        }
+        else {
+            return _strings_richcompare(self, array_other, cmp_op, 0);
+        }
+    }
+
     switch (cmp_op) {
     case Py_LT:
         if (needs_right_binop_forward(obj_self, other, "__gt__", 0) &&
@@ -1483,24 +1502,6 @@ array_richcompare(PyArrayObject *self, PyObject *other, int cmp_op)
     default:
         result = Py_NotImplemented;
         Py_INCREF(result);
-    }
-    if (result == Py_NotImplemented) {
-        /* Try to handle string comparisons */
-        if (PyArray_TYPE(self) == NPY_OBJECT) {
-            return result;
-        }
-        array_other = (PyArrayObject *)PyArray_FromObject(other,
-                                                          NPY_NOTYPE, 0, 0);
-        if (array_other == NULL) {
-            PyErr_Clear();
-            return result;
-        }
-        if (PyArray_ISSTRING(self) && PyArray_ISSTRING(array_other)) {
-            Py_DECREF(result);
-            result = _strings_richcompare(self, (PyArrayObject *)
-                                          array_other, cmp_op, 0);
-        }
-        Py_DECREF(array_other);
     }
     return result;
 }

commit 02206a3652c5d92c03f82a4df578a1e5971db304

BUG: Fix refcounting for string comparison in array_richcompare
Regression introduced in 4b1f508a57549d8031a23160b40c7f87f47892ed

Closes gh-6250.

numpy/core/src/multiarray/arrayobject.c
@@ -1311,7 +1311,9 @@ array_richcompare(PyArrayObject *self, PyObject *other, int cmp_op)
             /* Never mind, carry on, see what happens */
         }
         else {
-            return _strings_richcompare(self, array_other, cmp_op, 0);
+            result = _strings_richcompare(self, array_other, cmp_op, 0);
+            Py_DECREF(array_other);
+            return result;
         }
         /* If we reach this point, it means that we are not comparing
          * string-to-string. It's possible that this will still work out,

numpy/core/tests/test_regression.py
@@ -2152,6 +2152,26 @@ class TestRegression(TestCase):
         assert_equal(uf(a), ())
         assert_array_equal(a, [[3, 2, 1], [5, 4], [9, 7, 8, 6]])
 
+    def test_leak_in_structured_dtype_comparison(self):
+        # gh-6250
+        recordtype = np.dtype([('a', np.float64),
+                               ('b', np.int32),
+                               ('d', (np.str, 5))])
+
+        # Simple case
+        a = np.zeros(2, dtype=recordtype)
+        for i in range(100):
+            a == a
+        assert_(sys.getrefcount(a) < 10)
+
+        # The case in the bug report.
+        before = sys.getrefcount(a)
+        u, v = a[0], a[1]
+        u == v
+        del u, v
+        gc.collect()
+        after = sys.getrefcount(a)
+        assert_equal(before, after)
 
 if __name__ == "__main__":
     run_module_suite()