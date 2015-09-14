688561972049c783f613f28d301080a3b86911d4
Dmitry Zagorny <dmitry.zagorny@intel.com>
2015-09-14 13:07:17 -0500

    Changed from distutils_MSVCCompiler to distutils.msvccompiler.MSVCCompiler

--- numpy/distutils/msvc9compiler.py
+++ numpy/distutils/msvc9compiler.py
@@ -1,16 +1,16 @@
 import os
-from distutils.msvccompiler import *
-from distutils.msvc9compiler import MSVCCompiler as distutils_MSVCCompiler
+import distutils.msvc9compiler
+from distutils.msvc9compiler import *
 
 
-class MSVCCompiler(distutils_MSVCCompiler):
+class MSVCCompiler(distutils.msvc9compiler.MSVCCompiler):
     def __init__(self, verbose=0, dry_run=0, force=0):
-        distutils_MSVCCompiler.__init__(self, verbose, dry_run, force)
+        distutils.msvc9compiler.MSVCCompiler.__init__(self, verbose, dry_run, force)
 
     def initialize(self, plat_name=None):
         environ_lib = os.getenv('lib')
         environ_include = os.getenv('include')
-        distutils_MSVCCompiler.initialize(self, plat_name)
+        distutils.msvc9compiler.MSVCCompiler.initialize(self, plat_name)
         if environ_lib is not None:
             os.environ['lib'] = environ_lib + os.environ['lib']
         if environ_include is not None:
@@ -18,5 +18,6 @@ class MSVCCompiler(distutils_MSVCCompiler):
 
     def manifest_setup_ldargs(self, output_filename, build_temp, ld_args):
         ld_args.append('/MANIFEST')
-        distutils_MSVCCompiler.manifest_setup_ldargs(self, output_filename,
-                                                     build_temp, ld_args)
+        distutils.msvc9compiler.MSVCCompiler.manifest_setup_ldargs(self,
+                                                                   output_filename,
+                                                                   build_temp, ld_args)
