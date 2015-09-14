7f792b848e48cbdf11214129394d328e5e960d84
Dmitry Zagorny <dmitry.zagorny@intel.com>
2015-09-14 13:06:19 -0500

    Align with pep8

--- numpy/distutils/intelccompiler.py
+++ numpy/distutils/intelccompiler.py
@@ -3,10 +3,10 @@ from __future__ import division, absolute_import, print_function
 import platform
 
 from distutils.unixccompiler import UnixCCompiler
-if platform.system() == 'Windows':
-    from numpy.distutils.msvc9compiler import MSVCCompiler
 from numpy.distutils.exec_command import find_executable
 from numpy.distutils.ccompiler import simple_version_match
+if platform.system() == 'Windows':
+    from numpy.distutils.msvc9compiler import MSVCCompiler
 
 
 class IntelCCompiler(UnixCCompiler):
@@ -17,7 +17,8 @@ class IntelCCompiler(UnixCCompiler):
 
     def __init__(self, verbose=0, dry_run=0, force=0):
         UnixCCompiler.__init__(self, verbose, dry_run, force)
-        self.cc_exe = 'icc -fPIC -fp-model strict -O3 -fomit-frame-pointer -openmp'
+        self.cc_exe = ('icc -fPIC -fp-model strict -O3 '
+                       '-fomit-frame-pointer -openmp')
         compiler = self.cc_exe
         self.set_executables(compiler=compiler,
                              compiler_so=compiler,
@@ -47,7 +48,8 @@ class IntelEM64TCCompiler(UnixCCompiler):
 
     def __init__(self, verbose=0, dry_run=0, force=0):
         UnixCCompiler.__init__(self, verbose, dry_run, force)
-        self.cc_exe = 'icc -m64 -fPIC -fp-model strict -O3 -fomit-frame-pointer -openmp -xSSE4.2'
+        self.cc_exe = ('icc -m64 -fPIC -fp-model strict -O3 '
+                       '-fomit-frame-pointer -openmp -xSSE4.2')
         compiler = self.cc_exe
         self.set_executables(compiler=compiler,
                              compiler_so=compiler,
