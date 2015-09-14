5ef26af9209f206c36329e0b2585b4ebdbb5f5fd
Dmitry Zagorny <dmitry.zagorny@intel.com>
2015-09-14 13:07:17 -0500

    Changed from distutils_MSVCCompiler to distutils.msvccompiler.MSVCCompiler

--- numpy/distutils/msvccompiler.py
+++ numpy/distutils/msvccompiler.py
@@ -1,16 +1,16 @@
 import os
+import distutils.msvccompiler
 from distutils.msvccompiler import *
-from distutils.msvccompiler import MSVCCompiler as distutils_MSVCCompiler
 
 
-class MSVCCompiler(distutils_MSVCCompiler):
+class MSVCCompiler(distutils.msvccompiler.MSVCCompiler):
     def __init__(self, verbose=0, dry_run=0, force=0):
-        distutils_MSVCCompiler.__init__(self, verbose, dry_run, force)
+        distutils.msvccompiler.MSVCCompiler.__init__(self, verbose, dry_run, force)
 
     def initialize(self, plat_name=None):
         environ_lib = os.getenv('lib')
         environ_include = os.getenv('include')
-        distutils_MSVCCompiler.initialize(self, plat_name)
+        distutils.msvccompiler.MSVCCompiler.initialize(self, plat_name)
         if environ_lib is not None:
             os.environ['lib'] = environ_lib + os.environ['lib']
         if environ_include is not None:
