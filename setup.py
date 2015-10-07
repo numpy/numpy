e905cad9646ad65316458bbc7f5c83a9d5ec8728
Charles Harris <charlesr.harris@gmail.com>
2015-10-07 13:34:37 -0600

    REL: 1.10.0.post2 release.

--- setup.py
+++ setup.py
@@ -58,8 +58,8 @@ Operating System :: MacOS
 MAJOR               = 1
 MINOR               = 10
 MICRO               = 0
-ISRELEASED          = False
-VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = True
+VERSION             = '%d.%d.%d.post2' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
