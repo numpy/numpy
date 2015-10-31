d07e84c499aaaa04b0723bf80e7c41aba7b5d51c
Charles Harris <charlesr.harris@gmail.com>
2015-10-31 12:21:14 -0600

    DOC: Update documentation for isfortran.
    Clarify isfortran checks if an array is both Fortran contiguous and
    *not* C contiguous. The suggests that it only checks if the array is F
    contiguous, but that is not the case.

--- numpy/core/numeric.py
+++ numpy/core/numeric.py
@@ -696,8 +696,12 @@ def require(a, dtype=None, requirements=None):
 
 def isfortran(a):
     """
-    Returns True if array is arranged in Fortran-order in memory
-    and not C-order.
+    Returns True if the array is Fortran contiguous but *not* C contiguous.
+
+    This function is obsolete and, because of changes due to relaxed stride
+    checking, its return value for the same array may differ for versions
+    of Numpy >= 1.10 and previous versions. If you only want to check if an
+    array is Fortran contiguous use ``a.flags.f_contiguous`` instead.
 
     Parameters
     ----------
