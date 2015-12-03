4dd71a3ab7bd019e36998e7c5f98ec2345539f18
Jonathan Underwood <jonathan.underwood@gmail.com>
2015-12-03 14:49:31 +0000

    TST: Add tests for check_fit with deg specified as list

--- numpy/polynomial/tests/test_classes.py
+++ numpy/polynomial/tests/test_classes.py
@@ -173,9 +173,16 @@ def check_fit(Poly):
     assert_almost_equal(p(x), y)
     assert_almost_equal(p.domain, d)
     assert_almost_equal(p.window, w)
+    p = Poly.fit(x, y, [0, 1, 2, 3], domain=d, window=w)
+    assert_almost_equal(p(x), y)
+    assert_almost_equal(p.domain, d)
+    assert_almost_equal(p.window, w)
 
     # check with class domain default
     p = Poly.fit(x, y, 3, [])
+    assert_equal(p.domain, Poly.domain)
+    assert_equal(p.window, Poly.window)
+    p = Poly.fit(x, y, [0, 1, 2, 3], [])
     assert_equal(p.domain, Poly.domain)
     assert_equal(p.window, Poly.window)
 
@@ -185,7 +192,9 @@ def check_fit(Poly):
     w[::2] = 1
     p1 = Poly.fit(x[::2], z[::2], 3)
     p2 = Poly.fit(x, z, 3, w=w)
+    p3 = Poly.fit(x, z, [0, 1, 2, 3], w=w)
     assert_almost_equal(p1(x), p2(x))
+    assert_almost_equal(p2(x), p3(x))
 
 
 def check_equal(Poly):
