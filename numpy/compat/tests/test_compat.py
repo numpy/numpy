4743f3b4454a736e2bbd8b6f116f7efaef13c406
Charles Harris <charlesr.harris@gmail.com>
2015-12-05 19:16:00 -0700

    MAINT: Include from __future__ boilerplate in some files missing it.
    Some newer *.py files are missing the `from __future__` boilerplate
    that helps assure Python2 and Python3 compatibility.

--- numpy/compat/tests/test_compat.py
+++ numpy/compat/tests/test_compat.py
@@ -1,3 +1,5 @@
+from __future__ import division, absolute_import, print_function
+
 from os.path import join
 
 from numpy.compat import isfileobj
--- numpy/distutils/msvccompiler.py
+++ numpy/distutils/msvccompiler.py
@@ -1,3 +1,5 @@
+from __future__ import division, absolute_import, print_function
+
 import os
 import distutils.msvccompiler
 from distutils.msvccompiler import *
