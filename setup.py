ea8aa221f98d0bbd24884e9d6e31fbb613f478f1
Charles Harris <charlesr.harris@gmail.com>
2016-01-06 15:35:20 -0700

    REL: Numpy 1.10.3 release

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ Operating System :: MacOS
 MAJOR               = 1
 MINOR               = 10
 MICRO               = 3
-ISRELEASED          = False
+ISRELEASED          = True
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
