1d1eaef896a66059163f0baaaaa162e1f7c225a9
Ralf Gommers <ralf.gommers@gmail.com>
2016-01-06 20:31:02 +0100

    MAINT: remove outdated Pyrex support from distutils (as far as possible).

--- numpy/distutils/tests/setup.py
+++ numpy/distutils/tests/setup.py
@@ -4,9 +4,7 @@ from __future__ import division, print_function
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('testnumpydistutils', parent_package, top_path)
-    config.add_subpackage('pyrex_ext')
     config.add_subpackage('f2py_ext')
-    #config.add_subpackage('f2py_f90_ext')
     config.add_subpackage('swig_ext')
     config.add_subpackage('gen_ext')
     return config
