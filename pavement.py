3d3c5621f00a18b00db4a4930ffdd072e9ffe3fc
Charles Harris <charlesr.harris@gmail.com>
2016-01-19 20:26:20 -0700

    REL: Update pavement.py END_LOG for 1.11.x branch.

--- pavement.py
+++ pavement.py
@@ -103,7 +103,7 @@ RELEASE_NOTES = 'doc/release/1.11.0-notes.rst'
 
 # Start/end of the log (from git)
 LOG_START = 'v1.10.0'
-LOG_END = 'master'
+LOG_END = 'maintenance/1.11.x'
 
 
 #-------------------------------------------------------
