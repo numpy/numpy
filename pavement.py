11a9b710481f5932835c1c0cb1e5e2747be8cde0
Charles Harris <charlesr.harris@gmail.com>
2016-01-19 19:53:20 -0700

    REL: Update master branch after 1.12.x branch has been made.
    * Drop testing of Python 2.6, 3.2, and 3.3
    * Create 1.12.0-notes.rst and add to source/documentation.
    * Update pavement.py to use 1.10.x as LOG_START
    * Update version numpy in setup.py

--- pavement.py
+++ pavement.py
@@ -99,10 +99,10 @@ finally:
 #-----------------------------------
 
 # Source of the release notes
-RELEASE_NOTES = 'doc/release/1.11.0-notes.rst'
+RELEASE_NOTES = 'doc/release/1.12.0-notes.rst'
 
 # Start/end of the log (from git)
-LOG_START = 'v1.10.0'
+LOG_START = 'maintenance/1.11.x'
 LOG_END = 'master'
 
 
