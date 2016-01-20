c4ccdcaca7a230abc475446c1147c0d9b5b04bf6
Charles Harris <charlesr.harris@gmail.com>
2016-01-19 19:53:20 -0700

    REL: Update master branch after 1.12.x branch has been made.
    * Drop testing of Python 2.6, 3.2, and 3.3
    * Create 1.12.0-notes.rst and add to source/documentation.
    * Update pavement.py to use 1.10.x as LOG_START
    * Update version numpy in setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ Operating System :: MacOS
 """
 
 MAJOR               = 1
-MINOR               = 11
+MINOR               = 12
 MICRO               = 0
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
