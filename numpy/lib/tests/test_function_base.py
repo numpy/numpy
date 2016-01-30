87cbb610baca7921b36af83016b051ce7761e1c6
Aditya Panchal <apanchal@bastula.org>
2016-01-29 18:44:59 -0600

    BUG: Fixed regressions in np.piecewise in ref to #5737 and #5729. Added unit tests for these conditions.

--- numpy/lib/tests/test_function_base.py
+++ numpy/lib/tests/test_function_base.py
@@ -1862,6 +1862,10 @@ class TestPiecewise(TestCase):
         x = piecewise([1, 2], [[True, False], [False, True]], [3, 4])
         assert_array_equal(x, [3, 4])
 
+    def test_scalar_domains_three_conditions(self):
+        x = piecewise(3, [True, False, False], [4, 2, 0])
+        assert_equal(x, 4)
+
     def test_default(self):
         # No value specified for x[1], should be 0
         x = piecewise([1, 2], [True, False], [2])
@@ -1885,6 +1889,13 @@ class TestPiecewise(TestCase):
     def test_0d_comparison(self):
         x = 3
         piecewise(x, [x <= 3, x > 3], [4, 0])  # Should succeed.
+
+    def test_multidimensional_extrafunc(self):
+        x = np.array([[-2.5, -1.5, -0.5],
+                      [0.5, 1.5, 2.5]])
+        y = piecewise(x, [x < 0, x >= 2], [-1, 1, 3])
+        assert_array_equal(y, np.array([[-1., -1., -1.],
+                                        [3., 3., 1.]]))
 
 
 class TestBincount(TestCase):
