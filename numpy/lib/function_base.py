46cb649aecc2d5e1a256f78fb3f7879666a7c879
Aditya Panchal <apanchal@bastula.org>
2016-01-31 16:22:02 -0600

    MAINT: Addressed comments in PR #7145

--- numpy/lib/function_base.py
+++ numpy/lib/function_base.py
@@ -944,9 +944,10 @@ def piecewise(x, condlist, funclist, *args, **kw):
             condlist = condlist.T
     if n == n2 - 1:  # compute the "otherwise" condition.
         totlist = np.logical_or.reduce(condlist, axis=0)
-        try:
+        # Only able to stack vertically if the array is 1d or less
+        if x.ndim <= 1:
             condlist = np.vstack([condlist, ~totlist])
-        except:
+        else:
             condlist = [asarray(c, dtype=bool) for c in condlist]
             totlist = condlist[0]
             for k in range(1, n):
