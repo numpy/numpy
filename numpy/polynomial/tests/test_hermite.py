707772433ca82df688ca8c7db8dfc5531a517edc
Dongjoon Hyun <dongjoon@apache.org>
2016-02-19 00:22:33 -0800

    MAINT: Fix typos in docs/comment of `ma` and `polynomial` modules.

--- numpy/polynomial/tests/test_hermite.py
+++ numpy/polynomial/tests/test_hermite.py
@@ -300,7 +300,7 @@ class TestDerivative(TestCase):
         assert_raises(ValueError, herm.hermder, [0], .5)
         assert_raises(ValueError, herm.hermder, [0], -1)
 
-        # check that zeroth deriviative does nothing
+        # check that zeroth derivative does nothing
         for i in range(5):
             tgt = [0]*i + [1]
             res = herm.hermder(tgt, m=0)
