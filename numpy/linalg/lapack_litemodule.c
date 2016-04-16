855c21df207b6490309ef9cd6345505cd6971c6f
Hasan Diwan <hd1@jsc.d8u.us>
2016-04-15 21:08:57 -0700

    Change return type to VOID, to pass travis

--- numpy/linalg/lapack_litemodule.c
+++ numpy/linalg/lapack_litemodule.c
@@ -17,7 +17,7 @@ typedef struct { float r, i; } f2c_complex;
 typedef struct { double r, i; } f2c_doublecomplex;
 /* typedef long int (*L_fp)(); */
 
-int FNAME(MAIN_)() { return 0; }
+void FNAME(MAIN_)() { return 0; }
 
 extern int FNAME(dgelsd)(int *m, int *n, int *nrhs,
                           double a[], int *lda, double b[], int *ldb,
