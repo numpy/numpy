5c6e26259b1b4e1405004f87079a9a72fe5a850e
Allan Haldane <allan.haldane@gmail.com>
2016-06-29 19:36:35 -0400

    BUG: construct ma.array from np.array which contains padding
    Structured dtypes with padding bytes fail in MaskedArrays
    because of padding bytes in `dtype.descr`.
    
    Fix is to avoid using dtype.descr.

--- numpy/ma/tests/test_core.py
+++ numpy/ma/tests/test_core.py
@@ -212,6 +212,12 @@ class TestMaskedArray(TestCase):
         assert_equal(data, [[0, 1, 2, 3, 4], [4, 3, 2, 1, 0]])
         self.assertTrue(data.mask is nomask)
 
+    def test_creation_from_ndarray_with_padding(self):
+        x = np.array([('A', 0)], dtype={'names':['f0','f1'], 
+                                        'formats':['S4','i8'], 
+                                        'offsets':[0,8]})
+        data = array(x) # used to fail due to 'V' padding field in x.dtype.descr
+
     def test_asarray(self):
         (x, y, a10, m1, m2, xm, ym, z, zm, xf) = self.d
         xm.fill_value = -9999
