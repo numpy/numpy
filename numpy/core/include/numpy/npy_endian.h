580b0dd6edde74a7dadfff4e44d329c23b3cca8d
Kamil Rytarowski <n54@gmx.com>
2016-07-31 18:10:52 +0200

    Handle NetBSD specific <sys/endian.h>

--- numpy/core/include/numpy/npy_endian.h
+++ numpy/core/include/numpy/npy_endian.h
@@ -6,9 +6,14 @@
  * endian.h
  */
 
-#ifdef NPY_HAVE_ENDIAN_H
+#if defined(NPY_HAVE_ENDIAN_H) || defined(NPY_HAVE_SYS_ENDIAN_H)
     /* Use endian.h if available */
+
+    #if defined(NPY_HAVE_ENDIAN_H)
     #include <endian.h>
+    #elif defined(NPY_HAVE_SYS_ENDIAN_H)
+    #include <sys/endian.h>
+    #endif
 
     #if defined(BYTE_ORDER) && defined(BIG_ENDIAN) && defined(LITTLE_ENDIAN)
         #define NPY_BYTE_ORDER    BYTE_ORDER
