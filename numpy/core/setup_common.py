627d433755374f2f053c760283f3c73566622f2d
Julian Taylor <jtaylor.debian@googlemail.com>
2016-08-27 10:21:40 +0200

    MAINT: add avx __builtin_cpu_supports and target attribute checks
    NPY_CPU_SUPPORTS_AVX2 checks at runtime if AVX2 is supported

--- numpy/core/setup_common.py
+++ numpy/core/setup_common.py
@@ -125,6 +125,7 @@ OPTIONAL_INTRINSICS = [("__builtin_isnan", '5.'),
                        ("__builtin_bswap64", '5u'),
                        ("__builtin_expect", '5, 0'),
                        ("__builtin_mul_overflow", '5, 5, (int*)5'),
+                       ("__builtin_cpu_supports", '"sse"'),
                        ("_mm_load_ps", '(float*)0', "xmmintrin.h"),  # SSE
                        ("_mm_prefetch", '(float*)0, _MM_HINT_NTA',
                         "xmmintrin.h"),  # SSE
@@ -141,6 +142,8 @@ OPTIONAL_FUNCTION_ATTRIBUTES = [('__attribute__((optimize("unroll-loops")))',
                                  'attribute_optimize_opt_3'),
                                 ('__attribute__((nonnull (1)))',
                                  'attribute_nonnull'),
+                                ('__attribute__((target ("avx2")))',
+                                 'attribute_target_avx2'),
                                 ]
 
 # variable attributes tested via "int %s a" % attribute
