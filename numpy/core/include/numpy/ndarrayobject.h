47cdeac105bda9d992e62b06309f53be17874f4f
Matti Picus <matti.picus@gmail.com>
2016-09-12 23:16:53 +0300

    ENH: a.resize(.., refcheck=True) is almost unusable on PyPy

--- numpy/core/include/numpy/ndarrayobject.h
+++ numpy/core/include/numpy/ndarrayobject.h
@@ -116,9 +116,10 @@ extern "C" CONFUSE_EMACS
 
 #define PyArray_FILLWBYTE(obj, val) memset(PyArray_DATA(obj), val, \
                                            PyArray_NBYTES(obj))
-
+#ifndef PYPY_VERSION
 #define PyArray_REFCOUNT(obj) (((PyObject *)(obj))->ob_refcnt)
 #define NPY_REFCOUNT PyArray_REFCOUNT
+#endif
 #define NPY_MAX_ELSIZE (2 * NPY_SIZEOF_LONGDOUBLE)
 
 #define PyArray_ContiguousFromAny(op, type, min_depth, max_depth) \
