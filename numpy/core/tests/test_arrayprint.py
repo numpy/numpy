2994141d5d5225614f57e869ebdc0a48e9022c95
wrwrwr <git@wr.waw.pl>
2016-10-01 21:44:38 +0200

    BUG: Fix array printing with precision=0.
    Values rounding to zero were formatted with negative precision.

--- numpy/core/tests/test_arrayprint.py
+++ numpy/core/tests/test_arrayprint.py
@@ -129,6 +129,16 @@ class TestPrintOptions:
         np.set_printoptions(precision=4)
         assert_equal(repr(x), "array([ 1.5   ,  0.    ,  1.2346])")
 
+    def test_precision_zero(self):
+        np.set_printoptions(precision=0)
+        for values, string in (
+                ([0.], " 0."), ([.3], " 0."), ([-.3], "-0."), ([.7], " 1."),
+                ([1.5], " 2."), ([-1.5], "-2."), ([-15.34], "-15."),
+                ([100.], " 100."), ([.2, -1, 122.51], "   0.,   -1.,  123."),
+                ([0], "0"), ([-12], "-12"), ([complex(.3, -.7)], " 0.-1.j")):
+            x = np.array(values)
+            assert_equal(repr(x), "array([%s])" % string)
+
     def test_formatter(self):
         x = np.arange(3)
         np.set_printoptions(formatter={'all':lambda x: str(x-1)})
