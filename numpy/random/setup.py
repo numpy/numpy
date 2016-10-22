0f667fd410265bc6e76ca6c0e1a153c0ccc41d2f
Charles Harris <charlesr.harris@gmail.com>
2016-10-22 11:32:08 -0600

    STY: Fix indentation to follow PEP8

--- numpy/random/setup.py
+++ numpy/random/setup.py
@@ -35,8 +35,8 @@ def configuration(parent_package='',top_path=None):
         defs = [('_LARGE_FILES', None)]
     else:
         defs = [('_FILE_OFFSET_BITS', '64'),
-            ('_LARGEFILE_SOURCE', '1'),
-            ('_LARGEFILE64_SOURCE', '1')]
+                ('_LARGEFILE_SOURCE', '1'),
+                ('_LARGEFILE64_SOURCE', '1')]
     if needs_mingw_ftime_workaround():
         defs.append(("NPY_NEEDS_MINGW_TIME_WORKAROUND", None))
 
