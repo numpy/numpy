9a850406cd52b91a674779cbec57d0e032f277af
Charles Harris <charlesr.harris@gmail.com>
2016-11-05 14:18:44 -0600

    MAINT: Update pavement.py and setup.py for 1.12.x.

--- pavement.py
+++ pavement.py
@@ -103,7 +103,7 @@ RELEASE_NOTES = 'doc/release/1.12.0-notes.rst'
 
 # Start/end of the log (from git)
 LOG_START = 'maintenance/1.11.x'
-LOG_END = 'master'
+LOG_END = 'maintenance/1.12.x'
 
 
 #-------------------------------------------------------
