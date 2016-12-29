22ffedc84940fa112f0d95b89125ff3ab36b1bd2
bennyrowland <bennyrowland@mac.com>
2016-12-29 10:41:38 +0000

    BUG: Closes issue #8419
    Fixes issue in apply_along_axis() where func1d() returns a non ndarray

--- numpy/lib/tests/test_shape_base.py
+++ numpy/lib/tests/test_shape_base.py
@@ -58,6 +58,10 @@ class TestApplyAlongAxis(TestCase):
         assert isinstance(res, MinimalSubclass)
         assert_array_equal(res, np.array([6, 6, 6]).view(MinimalSubclass))
 
+    def test_tuple_func1d(self):
+        res = np.apply_along_axis((lambda x: (x[1], x[0])), 1, np.array([[1, 2], [3, 4]]))
+        assert_array_equal(res, np.array([[2, 1], [4, 3]]))
+
 
 class TestApplyOverAxes(TestCase):
     def test_simple(self):
