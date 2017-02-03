a1dccaedd90a4fc186f2eb274429bda834dc7c44
MattHarrigan <harrigan.matthew@gmail.com>
2017-02-03 15:13:46 -0500

    added complex types

--- numpy/core/tests/test_logical_gufuncs.py
+++ numpy/core/tests/test_logical_gufuncs.py
@@ -5,36 +5,52 @@ from numpy.testing import (
     run_module_suite, assert_equal
 )
 
-dtypes = [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, 
-          np.uint64, np.float32, np.float64]
+float_types = [np.float32, np.float64, np.longdouble]
+complex_types = [np.cfloat, np.cdouble, np.clongdouble]
+int_types = [np.int8, np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, 
+          np.uint64, np.longlong, np.ulonglong]
 
-# helper function
-def check(f, x1, x2, dtype, expected):
-    result = f(x1.astype(dtype), x2.astype(dtype))
-    assert_equal(result, expected)
+# helper functions
+def check(f, x1, x2, expected):
+    assert_equal(f(x1, x2), expected)
 
 
-def test_generator():
+def check_all(x1, x2):
+    yield check, np.all_equal,          x1, x2, (x1==x2).all()
+    yield check, np.any_equal,          x1, x2, (x1==x2).any()
+    yield check, np.all_not_equal,      x1, x2, (x1!=x2).all()
+    yield check, np.any_not_equal,      x1, x2, (x1!=x2).any()
+    yield check, np.all_greater,        x1, x2, (x1>x2).all()
+    yield check, np.any_greater,        x1, x2, (x1>x2).any()
+    yield check, np.all_greater_equal,  x1, x2, (x1>=x2).all()
+    yield check, np.any_greater_equal,  x1, x2, (x1>=x2).any()
+    yield check, np.all_less,           x1, x2, (x1<x2).all()
+    yield check, np.any_less,           x1, x2, (x1<x2).any()
+    yield check, np.all_less_equal,     x1, x2, (x1<=x2).all()
+    yield check, np.any_less_equal,     x1, x2, (x1<=x2).any()
+
+
+def test_real():
     inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
     for i in range(inputs.shape[0]):
         for j in range(inputs.shape[0]):
-            x1 = inputs[i, :]
-            x2 = inputs[j, :]
+            for dtype in int_types + float_types + complex_types:
+                x1 = inputs[i, :].astype(dtype)
+                x2 = inputs[j, :].astype(dtype)
+                for x in check_all(x1, x2):
+                    yield x
 
-            for dtype in dtypes:
 
-                yield check, np.all_equal, x1, x2, dtype, (x1==x2).all()
-                yield check, np.any_equal, x1, x2, dtype, (x1==x2).any()
-                yield check, np.all_not_equal, x1, x2, dtype, (x1!=x2).all()
-                yield check, np.any_not_equal, x1, x2, dtype, (x1!=x2).any()
-                yield check, np.all_greater, x1, x2, dtype, (x1>x2).all()
-                yield check, np.any_greater, x1, x2, dtype, (x1>x2).any()
-                yield check, np.all_greater_equal, x1, x2, dtype, (x1>=x2).all()
-                yield check, np.any_greater_equal, x1, x2, dtype, (x1>=x2).any()
-                yield check, np.all_less, x1, x2, dtype, (x1<x2).all()
-                yield check, np.any_less, x1, x2, dtype, (x1<x2).any()
-                yield check, np.all_less_equal, x1, x2, dtype, (x1<=x2).all()
-                yield check, np.any_less_equal, x1, x2, dtype, (x1<=x2).any()
+def test_complex():
+    j = 1j
+    for m in range(-1, 2):
+        for n in range(-1, 2):
+            for dtype in complex_types:
+                x1 = np.zeros(2, dtype=dtype)
+                x2 = x1 + m + n * j
+                for x in check_all(x1, x2):
+                    yield x
+
 
 if __name__ == "__main__":
     run_module_suite()
