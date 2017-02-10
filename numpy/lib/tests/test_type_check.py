9d53b4657102e84667a6e968b4d4fa308dbc9f71
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2017-02-10 19:20:33 +0100

    BUG: Make iscomplexobj compatible with custom dtypes again
    This change makes iscomplexobj compatible with custom array types
    using custom dtypes, that are not fully compatible to Numpys dtypes,
    which can nevertheless be coerced to a numpy array with asarray
    again, as has been the behaviour before PR #7936.
    
    Fixes #8601

--- numpy/lib/tests/test_type_check.py
+++ numpy/lib/tests/test_type_check.py
@@ -183,6 +183,15 @@ class TestIscomplexobj(TestCase):
         dummy = DummyPd()
         assert_(iscomplexobj(dummy))
 
+    def test_custom_dtype_duck(self):
+        class MyArray(list):
+            @property
+            def dtype(self):
+                return complex
+
+        a = MyArray([1+0j, 2+0j, 3+0j])
+        assert_(iscomplexobj(a))
+
 
 class TestIsrealobj(TestCase):
     def test_basic(self):
