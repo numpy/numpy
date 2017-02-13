BUG: Copy meshgrid after broadcasting, fixing #8561
Also, remove some unused variables

numpy/lib/function_base.py
@@ -4498,23 +4498,20 @@ def meshgrid(*xi, **kwargs):
             "Valid values for `indexing` are 'xy' and 'ij'.")
 
     s0 = (1,) * ndim
-    output = [np.asanyarray(x).reshape(s0[:i] + (-1,) + s0[i + 1::])
+    output = [np.asanyarray(x).reshape(s0[:i] + (-1,) + s0[i + 1:])
               for i, x in enumerate(xi)]
-
-    shape = [x.size for x in output]
 
     if indexing == 'xy' and ndim > 1:
         # switch first and second axis
-        output[0].shape = (1, -1) + (1,)*(ndim - 2)
-        output[1].shape = (-1, 1) + (1,)*(ndim - 2)
-        shape[0], shape[1] = shape[1], shape[0]
+        output[0].shape = (1, -1) + s0[2:]
+        output[1].shape = (-1, 1) + s0[2:]
+
+    if not sparse:
+        # Return the full N-D matrix (not only the 1-D vector)
+        output = np.broadcast_arrays(*output, subok=True)
 
     if copy_:
         output = [x.copy() for x in output]
-
-    if not sparse and len(output) > 0:
-        # Return the full N-D matrix (not only the 1-D vector)
-        output = np.broadcast_arrays(*output, subok=True)
 
     return output
 