ab322ec3ada3e4d4fcc832cfd5cf1e46fa5c71da
Matthew Brett <matthew.brett@gmail.com>
2017-02-21 23:04:45 +0000

    BUG: PPC64el machines are POWER for Fortran
    Fix Fortran kind detection for PPC64el.
    
    See: gh-3424.

--- numpy/f2py/crackfortran.py
+++ numpy/f2py/crackfortran.py
@@ -2392,7 +2392,8 @@ def _selected_real_kind_func(p, r=0, radix=0):
         return 4
     if p < 16:
         return 8
-    if platform.machine().lower().startswith('power'):
+    machine = platform.machine().lower()
+    if machine.startswith('power') or machine.startswith('ppc64'):
         if p <= 20:
             return 16
     else:
