fc340b1b17520f2500e148f5c4d128f06adf57dc
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2017-03-12 14:56:44 -0400

    MAINT: allow __array_ufunc__ = None to force binops to defer.
    In previous versions, one could force ndarray binops to defer by
    setting a high __array_priority__.  With __array_ufunc__ this gets
    ignored, and this commit ensures it is still possible to avoid
    using the standard python language feature that setting something
    to None means it is not implemented. In consequence, inside a
    ufunc, if __array_ufunc__ is None, it will be treated as if it
    had returned NotImplemented (leading to a TypeError if no other
    object had a functioning __array_ufunc__ override).

--- numpy/core/src/private/binop_override.h
+++ numpy/core/src/private/binop_override.h
@@ -46,22 +46,16 @@
  *   all use cases. (See https://github.com/numpy/numpy/issues/5844 for lots
  *   of discussion of this, and in particular
  *     https://github.com/numpy/numpy/issues/5844#issuecomment-112014014
- *   for a summary of some conclusions.)
+ *   for a summary of some conclusions.) Also, python 3.6 defines a standard
+ *   where setting a special-method name to None is a signal that that method
+ *   cannot be used.
  *
- * So for 1.10, we are going to try the following rules. a.__add__(b) will
+ * So for 1.13, we are going to try the following rules. a.__add__(b) will
  * be implemented as follows:
  * - If b does not define __array_ufunc__, apply the legacy rule:
  *   - If not isinstance(b, a.__class__), and b.__array_priority__ is higher
  *     than a.__array_priority__, return NotImplemented
- *   - Otherwise, fall through.
- * - If b->ob_type["__module__"].startswith("scipy.sparse."), then return
- *   NotImplemented. (Rationale: scipy.sparse defines __mul__ and np.multiply
- *   to do two totally different things. We want to grandfather this behavior
- *   in, but we don't want to support it in the long run, as per PEP
- *   465. Additionally, several versions of scipy.sparse were released with
- *   __array_ufunc__ implementations that don't match the final interface, and
- *   we don't want dense + sparse to suddenly start erroring out because
- *   dense.__add__ dispatched to a broken sparse.__array_ufunc__.)
+ * - If b does define __array_ufunc__ but it is None, return NotImplemented
  * - Otherwise, call the corresponding ufunc.
  *
  * For reversed operations like b.__radd__(a), and for in-place operations
@@ -76,7 +70,7 @@
  * overridden this method. In this case, Python will never call a.__add__
  * (because it's identical to b.__radd__), so we have no-one to defer to;
  * there's no reason to return NotImplemented. The second possibility is that
- * a.__add__ has already been called and returned NotImplemented. Again, in
+ * b.__add__ has already been called and returned NotImplemented. Again, in
  * this case there is no point in returning NotImplemented.
  *
  * Rationale for __iadd__: In-place operations do not take all the trouble
@@ -88,100 +82,8 @@
  *
  * In the future we might change these rules further. For example, we plan to
  * eventually deprecate __array_priority__ in cases where __array_ufunc__ is
- * not present, and we might decide that we need somewhat more flexible
- * dispatch rules where the ndarray binops sometimes return NotImplemented
- * rather than always dispatching to ufuncs.
- *
- * Note that these rules are also implemented by ABCArray, so any changes here
- * should also be reflected there.
+ * not present.
  */
-
-static int
-binop_override_has_ufunc_attr(PyObject *obj) {
-    PyObject *attr;
-    int result;
-
-    /* attribute check is expensive for scalar operations, avoid if possible */
-    if (PyArray_CheckExact(obj) || PyArray_CheckAnyScalarExact(obj) ||
-        _is_basic_python_type(obj)) {
-        return 0;
-    }
-
-    attr = PyArray_GetAttrString_SuppressException(obj, "__array_ufunc__");
-    if (attr == NULL) {
-        return 0;
-    }
-    else {
-        /*
-         * Pretend that non-callable __array_ufunc__ isn't there. This is an
-         * escape hatch in case we want to assign some special meaning to
-         * something like __array_ufunc__ = None, later on. (And can be
-         * deleted if we decide we don't want to do that.) See these two
-         * comments:
-         *   https://github.com/numpy/numpy/issues/5844#issuecomment-105081603
-         *   https://github.com/numpy/numpy/issues/5844#issuecomment-105170926
-         */
-        result = PyCallable_Check(attr);
-        Py_DECREF(attr);
-        return result;
-    }
-}
-
-static int
-binop_override_is_scipy_sparse(PyObject *obj) {
-    PyObject *module_name = NULL;
-    PyObject *bytes = NULL;
-    int result = 0;
-    char *contents;
-
-    module_name = PyArray_GetAttrString_SuppressException(
-        (PyObject*) Py_TYPE(obj),
-        "__module__");
-    if (module_name == NULL) {
-        goto done;
-    }
-    if (PyBytes_CheckExact(module_name)) {
-        contents = PyBytes_AS_STRING(module_name);
-    }
-#if PY_VERSION_HEX >= 0x03020000
-    else if (PyUnicode_CheckExact(module_name)) {
-#if (PY_VERSION_HEX >= 0x03020000) && (PY_VERSION_HEX < 0x03030000)
-        /* Python 3.2: unicode, but old API */
-        bytes = PyUnicode_AsLatin1String(module_name);
-        if (bytes == NULL) {
-            PyErr_Clear();
-            goto done;
-        }
-        contents = PyString_AS_STRING(bytes);
-#endif /* cpython == 3.2.x */
-#if PY_VERSION_HEX >= 0x03030000
-        /* Python 3.3+: new unicode API */
-        if (PyUnicode_READY(module_name) < 0) {
-            PyErr_Clear();
-            goto done;
-        }
-        /*
-         * We assume that scipy.sparse modules will always have ascii names
-         */
-        if (PyUnicode_KIND(module_name) != PyUnicode_1BYTE_KIND) {
-            goto done;
-        }
-        contents = (char*) PyUnicode_1BYTE_DATA(module_name);
-#endif /* cpython >= 3.3 */
-    }
-#endif /* cpython >= 3.2 */
-    else {
-        goto done;
-    }
-    if (strncmp("scipy.sparse", contents, 12) == 0) {
-        result = 1;
-    }
-
-  done:
-    Py_XDECREF(module_name);
-    Py_XDECREF(bytes);
-    return result;
-}
 
 static int
 binop_override_forward_binop_should_defer(PyObject *self, PyObject *other)
@@ -201,39 +103,41 @@ binop_override_forward_binop_should_defer(PyObject *self, PyObject *other)
      * which should possibly be updated when this is.
      */
 
+    PyObject *attr;
+    double self_prio, other_prio;
+    int defer;
+    /*
+     * attribute check is expensive for scalar operations, avoid if possible
+     */
     if (other == NULL ||
         self == NULL ||
         Py_TYPE(self) == Py_TYPE(other) ||
         PyArray_CheckExact(other) ||
-        PyArray_CheckAnyScalar(other)) {
-        /*
-         * Quick cases
-         */
+        PyArray_CheckAnyScalarExact(other) ||
+	_is_basic_python_type(other)) {
         return 0;
     }
-
     /*
-     * Classes with __array_ufunc__ are living in the future, and don't need
-     * a check for the legacy __array_priority__. And if other.__class__ is a
-     * subtype of self.__class__, then it's already had a chance to run, so no
-     * need to defer to it.
+     * Classes with __array_ufunc__ are living in the future, and only need to
+     * check whether __array_ufunc__ equals None.
      */
-    if (!binop_override_has_ufunc_attr(other) &&
-        !PyType_IsSubtype(Py_TYPE(other), Py_TYPE(self))) {
-        double self_prio = PyArray_GetPriority((PyObject *)self,
-                                               NPY_SCALAR_PRIORITY);
-        double other_prio = PyArray_GetPriority((PyObject *)other,
-                                                NPY_SCALAR_PRIORITY);
-        if (self_prio < other_prio) {
-            return 1;
-        }
+    attr = PyArray_GetAttrString_SuppressException(other, "__array_ufunc__");
+    if (attr) {
+        defer = (attr == Py_None);
+        Py_DECREF(attr);
+        return defer;
     }
-    if (binop_override_is_scipy_sparse(other)) {
-        /* Special case grandfathering in scipy.sparse */
-        return 1;
+    /*
+     * Otherwise, we need to check for the legacy __array_priority__. But if
+     * other.__class__ is a subtype of self.__class__, then it's already had
+     * a chance to run, so no need to defer to it.
+     */
+    if(PyType_IsSubtype(Py_TYPE(other), Py_TYPE(self))) {
+        return 0;
     }
-
-    return 0;
+    self_prio = PyArray_GetPriority((PyObject *)self, NPY_SCALAR_PRIORITY);
+    other_prio = PyArray_GetPriority((PyObject *)other, NPY_SCALAR_PRIORITY);
+    return self_prio < other_prio;
 }
 
 /*
--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -3936,13 +3936,14 @@ class MaskedArray(ndarray):
         #     private/binop_override.h:forward_binop_should_defer
         if isinstance(other, type(self)):
             return False
-        if not hasattr(other, "__array_ufunc__"):
+        array_ufunc = getattr(other, "__array_ufunc__", False)
+        if array_ufunc is False:
             other_priority = getattr(other, "__array_priority__", -1000000)
-            if self.__array_priority__ < other_priority:
-                return True
-        if other.__class__.__module__.startswith("scipy.sparse"):
-            return True
-        return False
+            return self.__array_priority__ < other_priority
+        else:
+            # If array_ufunc is not None, it will be called inside the ufunc;
+            # None explicitly tells us to not call the ufunc, i.e., defer.
+            return array_ufunc is None
 
     def _comparison(self, other, compare):
         """Compare self with other using operator.eq or operator.ne.
