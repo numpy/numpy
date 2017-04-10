4d30cffcd4aaf380a1ed64e542e10daed28970d1
Eric Wieser <wieser.eric@gmail.com>
2017-04-10 12:07:31 +0100

    API: Change default axis argument for argsort
    Now matches:
     * its own documentation
     * everything else
    
    Fixes #8701

--- numpy/ma/tests/test_deprecations.py
+++ numpy/ma/tests/test_deprecations.py
@@ -8,34 +8,5 @@ from numpy.testing import TestCase, run_module_suite, assert_warns
 from numpy.ma.testutils import assert_equal
 
 
-class TestArgsort(TestCase):
-	""" gh-8701 """
-	def _test_base(self, argsort, cls):
-		arr_0d = np.array(1).view(cls)
-		argsort(arr_0d)
-
-		arr_1d = np.array([1, 2, 3]).view(cls)
-		argsort(arr_1d)
-
-		# argsort has a bad default for >1d arrays
-		arr_2d = np.array([[1, 2], [3, 4]]).view(cls)
-		result = assert_warns(
-			np.ma.core.MaskedArrayFutureWarning, argsort, arr_2d)
-		assert_equal(result, argsort(arr_2d, axis=None))
-
-		# should be no warnings for explictly specifiying it
-		argsort(arr_2d, axis=None)
-		argsort(arr_2d, axis=-1)
-
-	def test_function_ndarray(self):
-		return self._test_base(np.ma.argsort, np.ndarray)
-
-	def test_function_maskedarray(self):
-		return self._test_base(np.ma.argsort, np.ma.MaskedArray)
-
-	def test_method(self):
-		return self._test_base(np.ma.MaskedArray.argsort, np.ma.MaskedArray)
-
-
 if __name__ == "__main__":
     run_module_suite()
