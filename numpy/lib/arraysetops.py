8805bbbe4a46ac1b69f367286de23a0d086be44b
B R S Recht <brstone@gmail.com>
2017-04-12 21:44:31 -0400

    More docstring changes

--- numpy/lib/arraysetops.py
+++ numpy/lib/arraysetops.py
@@ -507,10 +507,10 @@ def isin(element, test_elements, assume_unique=False, invert=False):
         If True, the input arrays are both assumed to be unique, which
         can speed up the calculation.  Default is False.
     invert : bool, optional
-        If True, the values in the returned array are inverted (that is,
-        False where an element of `ar1` is in `ar2` and True otherwise).
-        Default is False. ``np.isin(a, b, invert=True)`` is equivalent
-        to (but is faster than) ``np.invert(np.isin(a, b))``.
+        If True, the values in the returned array are inverted, as if 
+        calculating `element not in test_elements`. Default is False. 
+        ``np.isin(a, b, invert=True)`` is equivalent to (but faster 
+        than) ``np.invert(np.isin(a, b))``.
 
     Returns
     -------
@@ -533,15 +533,18 @@ def isin(element, test_elements, assume_unique=False, invert=False):
     `element` and `test_elements` are converted to arrays if they are not
     already. If `test_elements` is a set (or other non-sequence collection)
     it will be converted to an object array with one element, rather than an
-    array of the values contained in `test_elements`. This is a consequence of
-    the `array` constructor's way of handling non-sequence collections.
+    array of the values contained in `test_elements`. This is a consequence 
+    of the `array` constructor's way of handling non-sequence collections.
     Converting the set to a list usually gives the desired behavior.
 
     .. versionadded:: 1.13.0
 
     Examples
     --------
-    >>> element = np.array([[0, 2], [4, 6]])
+    >>> element = 2*np.arange(4).reshape((2, 2))
+    >>> element
+    array([[0, 2],
+           [4, 6]])
     >>> test_elements = [1, 2, 4, 8]
     >>> mask = np.isin(element, test_elements)
     >>> mask
@@ -554,7 +557,22 @@ def isin(element, test_elements, assume_unique=False, invert=False):
     array([[ True, False],
            [ False, True]], dtype=bool)
     >>> element[mask]
-    array([0, 6])"""
+    array([0, 6])
+    
+    Because of how `array` handles sets, the following does not 
+    work as expected:
+    
+    >>> test_set = {1, 2, 4, 8}
+    >>> np.isin(element, test_set)
+    array([[ False, False],
+           [ False, False]], dtype=bool)
+    
+    Casting the set to a list gives the expected result:
+    
+    >>> np.isin(element, list(test_set))
+    array([[ False,  True],
+           [ True,  False]], dtype=bool)    
+    """
     element = np.asarray(element)
     return in1d(element, test_elements, assume_unique=assume_unique,
                 invert=invert).reshape(element.shape)
