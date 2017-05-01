dc88f1970842854454767215ca089c6fa8cc92f0
Stephan Hoyer <shoyer@google.com>
2017-04-30 21:33:21 -0700

    ENH: add __pos__ to NDArrayOperatorsMixin

--- numpy/lib/tests/test_mixins.py
+++ numpy/lib/tests/test_mixins.py
@@ -143,7 +143,7 @@ class TestNDArrayOperatorsMixin(TestCase):
         array = np.array([-1, 0, 1, 2])
         array_like = ArrayLike(array)
         for op in [operator.neg,
-                   # pos is not yet implemented
+                   operator.pos,
                    abs,
                    operator.invert]:
             _assert_equal_type_and_value(op(array_like), ArrayLike(op(array)))
