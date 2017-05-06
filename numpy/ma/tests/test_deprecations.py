0f7fc5056da04e4ffb9587631ec144ef8fa7b89d
Eric Wieser <wieser.eric@gmail.com>
2017-05-06 01:33:57 +0100

    STY: Tabs to spaces
    Introduced by me in #8918

--- numpy/ma/tests/test_deprecations.py
+++ numpy/ma/tests/test_deprecations.py
@@ -9,32 +9,32 @@ from numpy.ma.testutils import assert_equal
 from numpy.ma.core import MaskedArrayFutureWarning
 
 class TestArgsort(TestCase):
-	""" gh-8701 """
-	def _test_base(self, argsort, cls):
-		arr_0d = np.array(1).view(cls)
-		argsort(arr_0d)
+    """ gh-8701 """
+    def _test_base(self, argsort, cls):
+        arr_0d = np.array(1).view(cls)
+        argsort(arr_0d)
 
-		arr_1d = np.array([1, 2, 3]).view(cls)
-		argsort(arr_1d)
+        arr_1d = np.array([1, 2, 3]).view(cls)
+        argsort(arr_1d)
 
-		# argsort has a bad default for >1d arrays
-		arr_2d = np.array([[1, 2], [3, 4]]).view(cls)
-		result = assert_warns(
-			np.ma.core.MaskedArrayFutureWarning, argsort, arr_2d)
-		assert_equal(result, argsort(arr_2d, axis=None))
+        # argsort has a bad default for >1d arrays
+        arr_2d = np.array([[1, 2], [3, 4]]).view(cls)
+        result = assert_warns(
+            np.ma.core.MaskedArrayFutureWarning, argsort, arr_2d)
+        assert_equal(result, argsort(arr_2d, axis=None))
 
-		# should be no warnings for explictly specifiying it
-		argsort(arr_2d, axis=None)
-		argsort(arr_2d, axis=-1)
+        # should be no warnings for explictly specifiying it
+        argsort(arr_2d, axis=None)
+        argsort(arr_2d, axis=-1)
 
-	def test_function_ndarray(self):
-		return self._test_base(np.ma.argsort, np.ndarray)
+    def test_function_ndarray(self):
+        return self._test_base(np.ma.argsort, np.ndarray)
 
-	def test_function_maskedarray(self):
-		return self._test_base(np.ma.argsort, np.ma.MaskedArray)
+    def test_function_maskedarray(self):
+        return self._test_base(np.ma.argsort, np.ma.MaskedArray)
 
-	def test_method(self):
-		return self._test_base(np.ma.MaskedArray.argsort, np.ma.MaskedArray)
+    def test_method(self):
+        return self._test_base(np.ma.MaskedArray.argsort, np.ma.MaskedArray)
 
 
 class TestMinimumMaximum(TestCase):
