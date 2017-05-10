0db096d6f84463ec7b4d73d53938b35fb0fa7a40
Eric Wieser <wieser.eric@gmail.com>
2017-05-10 02:20:54 +0100

    MAINT: Improve error message from sorting with duplicate key
    Fixes #9082

--- numpy/core/_internal.py
+++ numpy/core/_internal.py
@@ -281,20 +281,26 @@ class _ctypes(object):
     _as_parameter_ = property(get_as_parameter, None, doc="_as parameter_")
 
 
-# Given a datatype and an order object
-#  return a new names tuple
-#  with the order indicated
 def _newnames(datatype, order):
+    """
+    Given a datatype and an order object, return a new names tuple, with the
+    order indicated
+    """
     oldnames = datatype.names
     nameslist = list(oldnames)
     if isinstance(order, str):
         order = [order]
+    seen = set()
     if isinstance(order, (list, tuple)):
         for name in order:
             try:
                 nameslist.remove(name)
             except ValueError:
-                raise ValueError("unknown field name: %s" % (name,))
+                if name in seen:
+                    raise ValueError("duplicate field name: %s" % (name,))
+                else:
+                    raise ValueError("unknown field name: %s" % (name,))
+            seen.add(name)
         return tuple(list(order) + nameslist)
     raise ValueError("unsupported order value: %s" % (order,))
 
