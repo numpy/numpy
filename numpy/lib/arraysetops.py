07b925b03016679b7198888efd4f7e4b42986aa0
k_kapp@yahoo.com <k_kapp@yahoo.com>
2017-05-20 13:52:40 +0200

    MAINT: optimize _unique1d in arraysetops.py
    See discussion at pull request #9142

--- numpy/lib/arraysetops.py
+++ numpy/lib/arraysetops.py
@@ -257,26 +257,16 @@ def _unique1d(ar, return_index=False, return_inverse=False,
     optional_indices = return_index or return_inverse
     optional_returns = optional_indices or return_counts
 
-    if ar.size == 0:
-        if not optional_returns:
-            ret = ar
-        else:
-            ret = (ar,)
-            if return_index:
-                ret += (np.empty(0, np.intp),)
-            if return_inverse:
-                ret += (np.empty(0, np.intp),)
-            if return_counts:
-                ret += (np.empty(0, np.intp),)
-        return ret
-
     if optional_indices:
         perm = ar.argsort(kind='mergesort' if return_index else 'quicksort')
         aux = ar[perm]
     else:
         ar.sort()
         aux = ar
-    flag = np.concatenate(([True], aux[1:] != aux[:-1]))
+
+    flag = np.empty(aux.shape, dtype=np.bool)
+    flag[:1] = True
+    np.not_equal(aux[:-1], aux[1:], out=flag[1:])
 
     if not optional_returns:
         ret = aux[flag]
