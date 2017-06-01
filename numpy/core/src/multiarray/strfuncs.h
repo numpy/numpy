d681d9fd061eaa51765e3956b0406d2f459ed69a
Eric Wieser <wieser.eric@gmail.com>
2017-06-01 17:27:51 +0100

    MAINT: Move string functions to their own file

---
+++ numpy/core/src/multiarray/strfuncs.h
@@ -0,0 +1,13 @@
+#ifndef _NPY_ARRAY_STRFUNCS_H_
+#define _NPY_ARRAY_STRFUNCS_H_
+
+NPY_NO_EXPORT void
+PyArray_SetStringFunction(PyObject *op, int repr);
+
+NPY_NO_EXPORT PyObject *
+array_repr(PyArrayObject *self);
+
+NPY_NO_EXPORT PyObject *
+array_str(PyArrayObject *self);
+
+#endif
