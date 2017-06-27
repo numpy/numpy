1b44baf2a828974142e255fb7a1023f8da430855
Sergey Kojoian <sergeyyerevan@gmail.com>
2017-06-27 03:57:44 -0700

    Removed the extra  stub function initially made only for the docstring.

--- numpy/distutils/misc_util.py
+++ numpy/distutils/misc_util.py
@@ -2349,37 +2349,3 @@ def msvc_version(compiler):
         raise ValueError("Compiler instance is not msvc (%s)"\
                          % compiler.compiler_type)
     return compiler._MSVCCompiler__version
-
-def show_config():
-    """
-    Print information about various resources (libraries, library directories,
-    include directories, etc.) in the system on which NumPy was built.
-
-    See Also
-    --------
-    get_include : Returns the include directory containing NumPy \\*.h header files.
-
-    Notes
-    -----
-    Classes specifying the information to be printed are defined
-    in the `numpy.distutils.system_info` module.
-
-    Information may include:
-
-    * ``language``: language used to write the libraries (mostly C or f77)
-    * ``libraries``: names of libraries found in the system
-    * ``library_dirs``: directories containing the libraries
-    * ``include_dirs``: directories containing library header files
-    * ``src_dirs``: directories containing library source files
-    * ``define_macros``: preprocessor macros used by ``distutils.setup``
-
-    Examples
-    --------
-    >>> np.show_config()
-    blas_opt_info:
-        language = c
-        define_macros = [('HAVE_CBLAS', None)]
-        libraries = ['openblas', 'openblas']
-        library_dirs = ['/usr/local/lib']
-    """
-    pass
