604d21792b6e1125dae54f1905bcd8b4a70e912f
Charles Harris <charlesr.harris@gmail.com>
2017-07-07 19:55:52 -0600

    ENH: Make numpy.testing pytest aware.
    When the environment has NPY_PYTEST=1, the testing utilities will be
    pytest compatible. Typical usage would be something like
    
    $ NPY_PYTEST=1 pytest ...

--- numpy/testing/nosetester.py
+++ numpy/testing/nosetester.py
@@ -3,7 +3,13 @@ Back compatibility nosetester module. It will import the appropriate
 set of tools
 
 """
-from .nose_tools.nosetester import *
+import os
+
+if int(os.getenv('NPY_PYTEST', '0')):
+    from .pytest_tools.nosetester import *
+else:
+    from .nose_tools.nosetester import *
+
 
 __all__ = ['get_package_name', 'run_module_suite', 'NoseTester',
            '_numpy_tester', 'get_package_name', 'import_nose',
