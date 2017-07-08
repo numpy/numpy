024a5ce06f314469e81341d7c6c78d6a22006686
Charles Harris <charlesr.harris@gmail.com>
2017-07-07 19:55:52 -0600

    ENH: Make numpy.testing pytest aware.
    When the environment has NPY_PYTEST=1, the testing utilities will be
    pytest compatible. Typical usage would be something like
    
    $ NPY_PYTEST=1 pytest ...

--- numpy/testing/decorators.py
+++ numpy/testing/decorators.py
@@ -3,4 +3,9 @@ Back compatibility decorators module. It will import the appropriate
 set of tools
 
 """
-from .nose_tools.decorators import *
+import os
+
+if int(os.getenv('NPY_PYTEST', '0')):
+    from .pytest_tools.decorators import *
+else:
+    from .nose_tools.decorators import *
