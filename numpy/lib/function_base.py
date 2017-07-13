BUG: Allow 0d arrays instead of scalars in gradient
This fixes gh-8292

numpy/lib/function_base.py
@@ -1674,6 +1674,7 @@ def gradient(f, *varargs, **kwargs):
             S0025-5718-1988-0935077-0/S0025-5718-1988-0935077-0.pdf>`_.
     """
     f = np.asanyarray(f)
+    varargs = [asanyarray(d) for d in varargs]
     N = f.ndim  # number of dimensions
 
     axes = kwargs.pop('axis', None)
@@ -1685,11 +1686,16 @@ def gradient(f, *varargs, **kwargs):
     len_axes = len(axes)
     n = len(varargs)
     if n == 0:
-        dx = [1.0] * len_axes
-    elif n == len_axes or (n == 1 and np.isscalar(varargs[0])):
-        dx = list(varargs)
+        # no spacing argument - use 1 in all axes
+        dx = [np.array(1.0)] * len_axes
+    elif n == 1 and varargs[0].ndim == 0:
+        # single scalar for all axes
+        dx = varargs * len_axes
+    elif n == len_axes:
+        # scalar or 1d array for each axis
+        dx = varargs[:]
         for i, distances in enumerate(dx):
-            if np.isscalar(distances):
+            if distances.ndim == 0:
                 continue
             if len(distances) != f.shape[axes[i]]:
                 raise ValueError("distances must be either scalars or match "
@@ -1700,8 +1706,6 @@ def gradient(f, *varargs, **kwargs):
             if (diffx == diffx[0]).all():
                 diffx = diffx[0]
             dx[i] = diffx
-        if len(dx) == 1:
-            dx *= len_axes
     else:
         raise TypeError("invalid number of arguments")
 
@@ -1751,7 +1755,7 @@ def gradient(f, *varargs, **kwargs):
         # result allocation
         out = np.empty_like(y, dtype=otype)
 
-        uniform_spacing = np.isscalar(dx[i])
+        uniform_spacing = dx[i].ndim == 0
 
         # Numerical differentiation: 2nd order interior
         slice1[axis] = slice(1, -1)

numpy/lib/tests/test_function_base.py
@@ -742,8 +742,11 @@ class TestGradient(TestCase):
 
         # distances must be scalars or have size equal to gradient[axis]
         gradient(np.arange(5), 3.)
+        gradient(np.arange(5), np.array(3.))
         gradient(np.arange(5), dx)
-        gradient(f_2d, 1.5)  # dy is set equal to dx because scalar
+        # dy is set equal to dx because scalar
+        gradient(f_2d, 1.5)
+        gradient(f_2d, np.array(1.5))
 
         gradient(f_2d, dx_uneven, dx_uneven)
         # mix between even and uneven spaces and