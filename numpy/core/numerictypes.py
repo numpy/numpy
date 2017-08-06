2d4a09ca8b0a9781e2c164f5ea233a9794e2b78b
Eric Wieser <wieser.eric@gmail.com>
2017-08-05 22:19:47 -0500

    BUG: Make issubclass(np.number, numbers.Number) return true
    An omission from 2d73ff34f48980778b4e808afc76c0c1fa841ba7

--- numpy/core/numerictypes.py
+++ numpy/core/numerictypes.py
@@ -958,6 +958,7 @@ def _register_types():
     numbers.Integral.register(integer)
     numbers.Complex.register(inexact)
     numbers.Real.register(floating)
+    numbers.Number.register(number)
 
 _register_types()
 
