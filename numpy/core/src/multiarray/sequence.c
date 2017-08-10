4521d22b695279edaae41820bac992d853471a49
Allan Haldane <allan.haldane@gmail.com>
2017-08-10 16:34:37 -0400

    MAINT: array_any_nonzero now uses new iterator API

--- numpy/core/src/multiarray/sequence.c
+++ numpy/core/src/multiarray/sequence.c
@@ -69,29 +69,66 @@ NPY_NO_EXPORT PySequenceMethods array_as_sequence = {
 static int
 array_any_nonzero(PyArrayObject *arr)
 {
-    npy_intp counter;
-    PyArrayIterObject *it;
-    npy_bool anyTRUE = NPY_FALSE;
+    PyArray_NonzeroFunc *nonzero;
+    int ret = NPY_FALSE;
     int needs_api = 0;
 
-    it = (PyArrayIterObject *)PyArray_IterNew((PyObject *)arr);
-    if (it == NULL) {
-        return anyTRUE;
-    }
-    needs_api = NpyIter_IterationNeedsAPI(it);
+    NpyIter *iter;
+    NpyIter_IterNextFunc *iternext;
+    char **dataptr;
+    npy_intp *strideptr, *innersizeptr;
+    NPY_BEGIN_THREADS_DEF;
 
-    counter = it->size;
-    while (counter--) {
-        if (PyArray_DESCR(arr)->f->nonzero(it->dataptr, arr)) {
-            anyTRUE = NPY_TRUE;
-            break;
-        }
-        if (needs_api && PyErr_Occurred()) {
-            Py_DECREF(it);
-            return -1;
-        }
-        PyArray_ITER_NEXT(it);
+    nonzero = PyArray_DESCR(arr)->f->nonzero;
+
+    iter = NpyIter_New(arr, NPY_ITER_READONLY |
+                            NPY_ITER_EXTERNAL_LOOP |
+                            NPY_ITER_REFS_OK,
+                            NPY_KEEPORDER, NPY_NO_CASTING,
+                            NULL);
+    if (iter == NULL) {
+        return ret;
     }
-    Py_DECREF(it);
-    return anyTRUE;
+    needs_api = NpyIter_IterationNeedsAPI(iter);
+
+    /* Get the pointers for inner loop iteration */
+    iternext = NpyIter_GetIterNext(iter, NULL);
+    if (iternext == NULL) {
+        NpyIter_Deallocate(iter);
+        return -1;
+    }
+
+    NPY_BEGIN_THREADS_NDITER(iter);
+
+    dataptr = NpyIter_GetDataPtrArray(iter);
+    strideptr = NpyIter_GetInnerStrideArray(iter);
+    innersizeptr = NpyIter_GetInnerLoopSizePtr(iter);
+
+    /* Iterate over all the elements to count the nonzeros */
+    do {
+        char *data = *dataptr;
+        npy_intp stride = *strideptr;
+        npy_intp count = *innersizeptr;
+
+        while (count--) {
+            if (nonzero(data, arr)) {
+                ret = NPY_TRUE;
+                goto finish;
+            }
+            if (needs_api && PyErr_Occurred()) {
+                ret = -1;
+                goto finish;
+            }
+            data += stride;
+        }
+
+    } while(iternext(iter));
+
+
+finish:
+    NPY_END_THREADS;
+
+    NpyIter_Deallocate(iter);
+
+    return ret;
 }
