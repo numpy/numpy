687d7b8675f8cbc216c822574eb5bcb92007b412
Spencer Hill <spencerahill@gmail.com>
2017-09-19 21:31:06 -0700

    [ci skip] requested updates
    [ci skip]

--- numpy/core/numeric.py
+++ numpy/core/numeric.py
@@ -2436,7 +2436,7 @@ def allclose(a, b, rtol=1.e-5, atol=1.e-8, equal_nan=False):
 
     See Also
     --------
-    isclose, all, any, equals
+    isclose, all, any, equal
 
     Notes
     -----
@@ -2446,12 +2446,13 @@ def allclose(a, b, rtol=1.e-5, atol=1.e-8, equal_nan=False):
      absolute(`a` - `b`) <= (`atol` + `rtol` * absolute(`b`))
 
     The above equation is not symmetric in `a` and `b`, so that
-    `allclose(a, b)` might be different from `allclose(b, a)` in
+    ``allclose(a, b)`` might be different from ``allclose(b, a)`` in
     some rare cases.
-        
-    The comparison of `a` and `b` includes standard broadcasting,
-    which means that `a` and `b` need not have the same shape in order for
-    allclose(a, b) to evaluate to True.  (The same is true for `equals`.)
+
+    The comparison of `a` and `b` uses standard broadcasting, which
+    means that `a` and `b` need not have the same shape in order for
+    ``allclose(a, b)`` to evaluate to True.  The same is true for
+    ``equal`` but not ``array_equal``.
 
     Examples
     --------
