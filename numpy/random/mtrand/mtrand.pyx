d15f5e7797ee11fbadaa89d99bf3e61fc2efd38b
Ryan Peach <ryanpeach@users.noreply.github.com>
2017-10-12 09:35:54 -0400

    Restricting to pop_size>3
    Below this number, the formula explodes to infinity or is less than 1.

--- numpy/random/mtrand/mtrand.pyx
+++ numpy/random/mtrand/mtrand.pyx
@@ -1186,7 +1186,8 @@ cdef class RandomState:
                     n_uniq += new.size
                 idx = found
             else:
-                if ((pop_size+.5)*np.log(pop_size+1)-pop_size-0.60091006582)/np.log(pop_size) > size and not legacy:
+                if pop_size >= 3 and not legacy and \
+                   ((pop_size+.5)*np.log(pop_size+1)-pop_size-0.60091006582)/np.log(pop_size) > size:
                     idx = set()
                     while len(idx)<size:
                         idx.update(self.randint(0, pop_size, size=size - len(idx)))
