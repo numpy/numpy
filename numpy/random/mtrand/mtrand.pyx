741502dfe68967d738d93aff18dacc753c37db2f
Ryan Peach <ryanpeach@users.noreply.github.com>
2017-10-12 14:46:39 -0400

    Fixed math, used loggam
    Fixed some math, made cleaner by using loggam. Not sure if loggam is included however?

--- numpy/random/mtrand/mtrand.pyx
+++ numpy/random/mtrand/mtrand.pyx
@@ -1183,7 +1183,7 @@ cdef class RandomState:
                 idx = found
             else:
                 if pop_size >= 3 and self.version > 0 and \
-                   ((pop_size+.5)*np.log(pop_size+1)-pop_size-0.60091006582)/np.log(pop_size) > size:
+                   (loggam(pop_size+1)-loggam(n-k))/np.log(n) > size:
                     idx = set()
                     while len(idx)<size:
                         idx.update(self.randint(0, pop_size, size=size - len(idx)))
