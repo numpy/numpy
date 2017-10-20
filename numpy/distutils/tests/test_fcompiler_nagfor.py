7956677dbd7b8795465b6144bb7e3fdcf3dee3e8
Andrew Lawson <andrew.lawson@nag.co.uk>
2017-10-20 14:12:19 +0100

    TST: Improved tests for nag fortran compiler

--- numpy/distutils/tests/test_fcompiler_nagfor.py
+++ numpy/distutils/tests/test_fcompiler_nagfor.py
@@ -4,21 +4,24 @@ from numpy.testing import assert_, run_module_suite
 
 import numpy.distutils.fcompiler
 
-nagfor_version_strings = [('NAG Fortran Compiler Release 6.2(Chiyoda) Build 6200', '6.2')]
+nagfor_version_strings = [('nagfor', 'NAG Fortran Compiler Release '
+                           '6.2(Chiyoda) Build 6200', '6.2'),
+                          ('nagfor', 'NAG Fortran Compiler Release '
+                           '6.1(Tozai) Build 6136', '6.1'),
+                          ('nagfor', 'NAG Fortran Compiler Release '
+                           '6.0(Hibiya) Build 1021', '6.0'),
+                          ('nagfor', 'NAG Fortran Compiler Release '
+                           '5.3.2(971)', '5.3.2')]
 
-nag_version_strings = [('NAGWare Fortran 95 compiler Release 5.1(347,355-367,375,'
-                        '380-383,389,394,399,401-402,407,431,435,437,446,459-460,'
-                        '463,472,494,496,503,508,511,517,529,555,557,565)', '5.1')]
+nag_version_strings = [('nag', 'NAGWare Fortran 95 compiler Release 5.1'
+                        '(347,355-367,375,380-383,389,394,399,401-402,407,'
+                        '431,435,437,446,459-460,463,472,494,496,503,508,'
+                        '511,517,529,555,557,565)', '5.1')]
 
 class TestNagFCompilerVersions(object):
     def test_version_match(self):
-        fc = numpy.distutils.fcompiler.new_fcompiler(compiler='nagfor')
-        for vs, version in nagfor_version_strings:
-            v = fc.version_match(vs)
-            assert_(v == version)
-
-        fc = numpy.distutils.fcompiler.new_fcompiler(compiler='nag')
-        for vs, version in nag_version_strings:
+        for comp, vs, version in nagfor_version_strings:
+            fc = numpy.distutils.fcompiler.new_fcompiler(compiler=comp)
             v = fc.version_match(vs)
             assert_(v == version)
 
