4a985b9e91923f5e6c4c1b0b2370b5a79a7fe0d0
Allan Haldane <allan.haldane@gmail.com>
2017-10-29 15:40:40 -0400

    TST: update tests to account for dragon4 fixes

--- numpy/core/tests/test_scalarprint.py
+++ numpy/core/tests/test_scalarprint.py
@@ -5,25 +5,26 @@
 from __future__ import division, absolute_import, print_function
 
 import numpy as np
-from numpy.testing import assert_, run_module_suite
+from numpy.testing import assert_equal, run_module_suite
 
 
 class TestRealScalars(object):
     def test_str(self):
         svals = [0.0, -0.0, 1, -1, np.inf, -np.inf, np.nan]
         styps = [np.float16, np.float32, np.float64, np.longdouble]
-        actual = [str(f(c)) for c in svals for f in styps]
         wanted = [
-             '0.0',  '0.0',  '0.0',  '0.0',
-             '-0.0', '-0.0', '-0.0', '-0.0',
-             '1.0',  '1.0',  '1.0',  '1.0',
-             '-1.0', '-1.0', '-1.0', '-1.0',
-             'inf',  'inf',  'inf',  'inf',
-             '-inf', '-inf', '-inf', '-inf',
-             'nan',  'nan',  'nan',  'nan']
+             ['0.0',  '0.0',  '0.0',  '0.0' ],
+             ['-0.0', '-0.0', '-0.0', '-0.0'],
+             ['1.0',  '1.0',  '1.0',  '1.0' ],
+             ['-1.0', '-1.0', '-1.0', '-1.0'],
+             ['inf',  'inf',  'inf',  'inf' ],
+             ['-inf', '-inf', '-inf', '-inf'],
+             ['nan',  'nan',  'nan',  'nan']]
 
-        for res, val in zip(actual, wanted):
-            assert_(res == val)
+        for wants, val in zip(wanted, svals):
+            for want, styp in zip(wants, styps):
+                msg = 'for str({}({}))'.format(np.dtype(styp).name, repr(val))
+                assert_equal(str(styp(val)), want, err_msg=msg)
 
 
 if __name__ == "__main__":
