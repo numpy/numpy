1039c2d2e8ff01048dd8be5515443cdd9993f1fc
Allan Haldane <allan.haldane@gmail.com>
2017-11-06 15:14:42 -0500

    BUG: cast to str_ should not convert to pure-python intermediate

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -451,6 +451,14 @@ class TestAssignment(object):
             arr = np.array([np.array(tinya)])
             assert_equal(arr[0], tinya)
 
+    def test_cast_to_string(self):
+        # cast to str should do "str(scalar)", not "str(scalar.item())"
+        # Example: In python2, str(float) is truncated, so we want to avoid
+        # str(np.float64(...).item()) as this would incorrectly truncate.
+        a = np.zeros(1, dtype='S20')
+        a[:] = np.array(['1.12345678901234567890'], dtype='f8')
+        assert_equal(a[0], b"1.1234567890123457")
+
 
 class TestDtypedescr(object):
     def test_construction(self):
