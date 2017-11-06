2eb11f0181d03f5add62073d9f7bffb59298d7d4
xoviat <xoviat@users.noreply.github.com>
2017-11-06 09:22:51 -0600

    [distutils:fcompiler:pg] cleanup

--- numpy/distutils/fcompiler/pg.py
+++ numpy/distutils/fcompiler/pg.py
@@ -57,7 +57,5 @@ class PGroupFCompiler(FCompiler):
 if __name__ == '__main__':
     from distutils import log
     log.set_verbosity(2)
-    from numpy.distutils.fcompiler import new_fcompiler
-    compiler = new_fcompiler(compiler='pg')
-    compiler.customize()
-    print(compiler.get_version())
+    from numpy.distutils import customized_fcompiler
+    print(customized_fcompiler(compiler='pg').get_version())
