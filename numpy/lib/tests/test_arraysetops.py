a3552604857c92c65f0d262a22492ab4c8f0dd73
Joerg <joerg.doepfert@flixbus.com>
2017-11-10 18:58:25 +0100

    add tests

--- numpy/lib/tests/test_arraysetops.py
+++ numpy/lib/tests/test_arraysetops.py
@@ -208,10 +208,26 @@ class TestSetOps(object):
         assert_array_equal(in1d(a, long_b, assume_unique=True), ec)
         assert_array_equal(in1d(a, long_b, assume_unique=False), ec)
 
-    def test_in1d_unorderable_types(self):
+    def test_in1d_first_array_is_object(self):
+        ar1 = [None]
+        ar2 = np.array([1]*10)
+        expected = np.array([False])
+        result = np.in1d(ar1, ar2)
+        assert_equal(result, expected)
+
+    def test_in1d_second_array_is_object(self):
         ar1 = 1
         ar2 = np.array([None]*10)
-        np.in1d(ar1, ar2)
+        expected = np.array([False])
+        result = np.in1d(ar1, ar2)
+        assert_equal(result, expected)
+
+    def test_in1d_both_arrays_are_object(self):
+        ar1 = [None]
+        ar2 = np.array([None]*10)
+        expected = np.array([True])
+        result = np.in1d(ar1, ar2)
+        assert_equal(result, expected)
 
     def test_union1d(self):
         a = np.array([5, 4, 7, 1, 2])
