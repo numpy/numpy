a39c928789f44bf48e8b75b9579487300219433d
Joerg <joerg.doepfert@flixbus.com>
2017-11-10 19:03:06 +0100

    format code

--- numpy/lib/arraysetops.py
+++ numpy/lib/arraysetops.py
@@ -448,11 +448,13 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
     ar1 = np.asarray(ar1).ravel()
     ar2 = np.asarray(ar2).ravel()
 
+    # Check if one of the arrays is of type object
+    is_object = ar1.dtype == np.dtype('O') or ar2.dtype == np.dtype('O')
+
     # This code is run when
     # a) the first condition is true, making the code significantly faster
-    # b) the second condition is true (i.e. `ar1` or `ar2` are of dtype
-    # `object`), since sorting object arrays is not guaranteed to work
-    is_object = ar1.dtype == np.dtype('O') or ar2.dtype == np.dtype('O')
+    # b) the second condition is true (i.e. `ar1` or `ar2` are of type
+    #    object), since sorting object arrays is not guaranteed to work
     if len(ar2) < 10 * len(ar1) ** 0.145 or is_object:
         if invert:
             mask = np.ones(len(ar1), dtype=bool)
