b95bc921a8863deb18f6b011e96472191ba7104b
Eric Wieser <wieser.eric@gmail.com>
2017-11-14 00:37:30 -0800

    BUG: longdouble(int) does not work
    Fixes gh-9968

--- numpy/core/src/common/npy_longdouble.h
+++ numpy/core/src/common/npy_longdouble.h
@@ -14,4 +14,14 @@
 NPY_VISIBILITY_HIDDEN PyObject *
 npy_longdouble_to_PyLong(npy_longdouble ldval);
 
+/* Convert a python `long` integer to a npy_longdouble
+ *
+ * This performs the same task as PyLong_AsDouble, but for long doubles
+ * which have a greater range.
+ *
+ * Returns -1 if an error occurs.
+ */
+NPY_VISIBILITY_HIDDEN npy_longdouble
+npy_longdouble_from_PyLong(PyObject *long_obj);
+
 #endif
