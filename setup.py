4652e39c564d7fa361b208acc3eb317159edee30
Charles Harris <charlesr.harris@gmail.com>
2017-12-11 16:26:35 -0700

    MAINT: Update development branch version to 1.15.0.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ Operating System :: MacOS
 """
 
 MAJOR               = 1
-MINOR               = 14
+MINOR               = 15
 MICRO               = 0
 ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
