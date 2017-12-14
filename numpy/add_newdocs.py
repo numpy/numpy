aa6453b2b4bd6fb6758f6b9ee47c3846e7fbf9ef
mattip <matti.picus@gmail.com>
2017-12-14 20:41:38 +0200

    convert 0D arrays to scalars for less visual clutter

--- numpy/add_newdocs.py
+++ numpy/add_newdocs.py
@@ -498,10 +498,10 @@ add_newdoc('numpy.core', 'nested_iters',
     >>> i, j = np.nested_iters(a, [[0], [1, 2]])
     >>> vals = []
     >>> for x in i:
-    ...    vals.append([y for y in j])
+    ...    vals.append([y[()] for y in j])
     >>> vals
-    [[array(0), array(1), array(2), array(3), array( 4), array( 5)],
-     [array(6), array(7), array(8), array(9), array(10), array(11)]]
+    [[0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11], 
+     [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]]
     """)
 
 
