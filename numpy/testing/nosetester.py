0f7a8b3dfbd9c99162074aeebc80981cfd3decbb
xoviat <xoviat@users.noreply.github.com>
2017-12-22 15:57:04 -0600

    ENH: do backward compatibility correctly

--- numpy/testing/nosetester.py
+++ numpy/testing/nosetester.py
@@ -5,10 +5,7 @@ set of tools
 """
 import os
 
-if int(os.getenv('NPY_PYTEST', '0')):
-    from .pytest_tools.nosetester import *
-else:
-    from .nose_tools.nosetester import *
+from .nose_tools.nosetester import *
 
 
 __all__ = ['get_package_name', 'run_module_suite', 'NoseTester',
