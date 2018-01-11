f0dc4e336c8997c7fa3b2db7f83e9fd8dc4eca81
Eric Wieser <wieser.eric@gmail.com>
2018-01-10 23:26:39 -0800

    ENH: Preserve norm dtype for order 0

--- numpy/linalg/linalg.py
+++ numpy/linalg/linalg.py
@@ -2277,7 +2277,7 @@ def norm(x, ord=None, axis=None, keepdims=False):
             return abs(x).min(axis=axis, keepdims=keepdims)
         elif ord == 0:
             # Zero norm
-            return (x != 0).astype(float).sum(axis=axis, keepdims=keepdims)
+            return (x != 0).astype(x.real.dtype).sum(axis=axis, keepdims=keepdims)
         elif ord == 1:
             # special case for speedup
             return add.reduce(abs(x), axis=axis, keepdims=keepdims)
