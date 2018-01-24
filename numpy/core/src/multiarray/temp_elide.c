9a4a3a37925771502d953484678ab8e2d15ef509
Charles Harris <charlesr.harris@gmail.com>
2018-01-23 20:49:07 -0700

    MAINT: Fix miscellaneous sign-compare warnings.
    Small fixes scattered over six files.
    
    - numpy/core/src/multiarray/array_assign_scalar.c
    - numpy/core/src/multiarray/common.c
    - numpy/core/src/multiarray/multiarray_tests.c.src
    - numpy/core/src/multiarray/refcount.c
    - numpy/core/src/multiarray/shape.c
    - numpy/core/src/multiarray/temp_elide.c

--- numpy/core/src/multiarray/temp_elide.c
+++ numpy/core/src/multiarray/temp_elide.c
@@ -7,6 +7,7 @@
 #include "numpy/arrayobject.h"
 
 #define NPY_NUMBER_MAX(a, b) ((a) > (b) ? (a) : (b))
+#define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
 /*
  * Functions used to try to avoid/elide temporaries in python expressions
@@ -181,6 +182,7 @@ check_callers(int * cannot)
         Dl_info info;
         int in_python = 0;
         int in_multiarray = 0;
+
 #if NPY_ELIDE_DEBUG >= 2
         dladdr(buffer[i], &info);
         printf("%s(%p) %s(%p)\n", info.dli_fname, info.dli_fbase,
@@ -242,14 +244,14 @@ check_callers(int * cannot)
             }
             if (info.dli_sname &&
                     strcmp(info.dli_sname, PYFRAMEEVAL_FUNC) == 0) {
-                if (n_pyeval < sizeof(pyeval_addr) / sizeof(pyeval_addr[0])) {
+                if (n_pyeval < (npy_intp)ARRAY_SIZE(pyeval_addr)) {
                     /* store address to not have to dladdr it again */
                     pyeval_addr[n_pyeval++] = buffer[i];
                 }
                 ok = 1;
                 break;
             }
-            else if (n_py_addr < sizeof(py_addr) / sizeof(py_addr[0])) {
+            else if (n_py_addr < (npy_intp)ARRAY_SIZE(py_addr)) {
                 /* store other py function to not have to dladdr it again */
                 py_addr[n_py_addr++] = buffer[i];
             }
