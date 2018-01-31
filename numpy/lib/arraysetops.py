59ad226ff339540692e80483d3b4deb1fd34cdaa
Eric Wieser <wieser.eric@gmail.com>
2018-01-31 01:12:06 -0800

    DOC: Use a bulleted list to show the outputs of `unique`, for clarity

--- numpy/lib/arraysetops.py
+++ numpy/lib/arraysetops.py
@@ -124,10 +124,11 @@ def unique(ar, return_index=False, return_inverse=False,
     Find the unique elements of an array.
 
     Returns the sorted unique elements of an array. There are three optional
-    outputs in addition to the unique elements: the indices of the input array
-    that give the unique values, the indices of the unique array that
-    reconstruct the input array, and the number of times each unique value
-    comes up in the input array.
+    outputs in addition to the unique elements:
+
+    * the indices of the input array that give the unique values
+    * the indices of the unique array that reconstruct the input array
+    * the number of times each unique value comes up in the input array
 
     Parameters
     ----------
