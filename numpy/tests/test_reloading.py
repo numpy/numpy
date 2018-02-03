0d70db6ccf344eaddbd08f86f2c02032f6aca26c
Eric Wieser <wieser.eric@gmail.com>
2018-02-03 12:04:46 -0800

    ENH: Add a repr to np._NoValue
    This change _NoValue from a class to an instance, which is more inline with the builtin None.
    
    Fixes gh-8991, closes gh-9592

--- numpy/tests/test_reloading.py
+++ numpy/tests/test_reloading.py
@@ -1,8 +1,9 @@
 from __future__ import division, absolute_import, print_function
 
 import sys
+import pickle
 
-from numpy.testing import assert_raises, assert_, run_module_suite
+from numpy.testing import assert_raises, assert_, assert_equal, run_module_suite
 
 if sys.version_info[:2] >= (3, 4):
     from importlib import reload
@@ -28,6 +29,11 @@ def test_numpy_reloading():
     assert_(_NoValue is np._NoValue)
     assert_(ModuleDeprecationWarning is np.ModuleDeprecationWarning)
     assert_(VisibleDeprecationWarning is np.VisibleDeprecationWarning)
+
+def test_novalue():
+    import numpy as np
+    assert_equal(repr(np._NoValue), '<no value>')
+    assert_(pickle.loads(pickle.dumps(np._NoValue)) is np._NoValue)
 
 
 if __name__ == "__main__":
