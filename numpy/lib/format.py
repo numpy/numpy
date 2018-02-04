019cb642b993947d5c9402459d952cd1293eb660
Dennis Weyland <dennisweyland@gmail.com>
2018-02-05 00:42:33 +0100

    python 2.7.5 bugfix

--- numpy/lib/format.py
+++ numpy/lib/format.py
@@ -518,7 +518,9 @@ def _filter_header(s):
 
     tokens = []
     last_token_was_number = False
-    for token in tokenize.generate_tokens(StringIO(asstr(s)).read):
+    # adding newline as python 2.7.5 workaround
+    s += "\n"
+    for token in tokenize.generate_tokens(StringIO(asstr(s)).readline):
         token_type = token[0]
         token_string = token[1]
         if (last_token_was_number and
@@ -528,7 +530,8 @@ def _filter_header(s):
         else:
             tokens.append(token)
         last_token_was_number = (token_type == tokenize.NUMBER)
-    return tokenize.untokenize(tokens)
+    # removing newline (see above) as python 2.7.5 workaround
+    return tokenize.untokenize(tokens)[:-1]
 
 
 def _read_array_header(fp, version):
