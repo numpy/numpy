9f68fee30c9d859815d055c9fbf5875b4b879f5d
Kirit Thadaka <kirit.thadaka@gmail.com>
2018-02-21 07:30:31 +0530

    MAINT: Improve range error messages in np.histogram (#10603)

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -219,18 +219,20 @@ def _get_outer_edges(a, range):
     """
     if range is not None:
         first_edge, last_edge = range
+        if first_edge > last_edge:
+            raise ValueError(
+                'max must be larger than min in range parameter.')
+        if not (np.isfinite(first_edge) and np.isfinite(last_edge)):
+            raise ValueError(
+                "supplied range of [{}, {}] is not finite".format(first_edge, last_edge))
     elif a.size == 0:
         # handle empty arrays. Can't determine range, so use 0-1.
         first_edge, last_edge = 0, 1
     else:
         first_edge, last_edge = a.min(), a.max()
-
-    if first_edge > last_edge:
-        raise ValueError(
-            'max must be larger than min in range parameter.')
-    if not (np.isfinite(first_edge) and np.isfinite(last_edge)):
-        raise ValueError(
-            'range parameter must be finite.')
+        if not (np.isfinite(first_edge) and np.isfinite(last_edge)):
+            raise ValueError(
+                "autodetected range of [{}, {}] is not finite".format(first_edge, last_edge))
 
     # expand empty range to avoid divide by zero
     if first_edge == last_edge:
