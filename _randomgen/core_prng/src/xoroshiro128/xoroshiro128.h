aa9060ce67f90b806b90fae1e445b62c73c0a11d
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-02-24 19:18:38 +0000

    CLN: Simplify xoroshiro state
    Simplify xoroshiro state

--- _randomgen/core_prng/src/xoroshiro128/xoroshiro128.h
+++ _randomgen/core_prng/src/xoroshiro128/xoroshiro128.h
@@ -1,23 +1,19 @@
 #include <stdint.h>
 
-typedef struct s_xoroshiro128_state {
-  uint64_t s[2];
-} xoroshiro128_state;
-
 static inline uint64_t rotl(const uint64_t x, int k) {
   return (x << k) | (x >> (64 - k));
 }
 
-static inline uint64_t xoroshiro128_next(xoroshiro128_state *state) {
-  const uint64_t s0 = state->s[0];
-  uint64_t s1 = state->s[1];
+static inline uint64_t xoroshiro128_next(uint64_t *s) {
+  const uint64_t s0 = s[0];
+  uint64_t s1 = s[1];
   const uint64_t result = s0 + s1;
 
   s1 ^= s0;
-  state->s[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
-  state->s[1] = rotl(s1, 36);                   // c
+  s[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
+  s[1] = rotl(s1, 36);                   // c
 
   return result;
 }
 
-void xoroshiro128_jump(xoroshiro128_state *state);
+void xoroshiro128_jump(uint64_t *s);
--- _randomgen/demo.py
+++ _randomgen/demo.py
@@ -4,7 +4,9 @@ from core_prng.xoroshiro128 import Xoroshiro128
 
 print(RandomGenerator().random_integer())
 print(RandomGenerator(Xoroshiro128()).random_integer())
+x = Xoroshiro128()
+print(x.get_state())
 print(RandomGenerator(SplitMix64()).random_integer())
 print(RandomGenerator(SplitMix64()).random_integer())
 print(RandomGenerator(SplitMix64(1)).random_integer())
-print(RandomGenerator(SplitMix64([1.0,2.0])).random_integer())
+print(RandomGenerator(SplitMix64([1.0, 2.0])).random_integer())
