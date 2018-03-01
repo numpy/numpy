a944360e96cbdee873befa3d461c9eddc5702f3a
Spoorthy Bhat <spoorthy.bhat.77@gmail.com>
2018-03-01 23:18:47 +0530

    BUG: fixes #4701 related to numpy.asscalar
    Allows numpy.asscalar to pass scalars

--- numpy/lib/type_check.py
+++ numpy/lib/type_check.py
@@ -486,7 +486,10 @@ def asscalar(a):
     24
 
     """
-    return a.item()
+    try:
+        return a.item()
+    except AttributeError:
+        return np.asarray(a).item()
 
 #-----------------------------------------------------------------------------
 
