218814d7f67470fd32eececed1295f32280a5348
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2018-03-05 12:21:08 +0000

    BUG: Fix bugs which prevented building on Linux

--- _randomgen/core_prng/src/pcg64/pcg64.c
+++ _randomgen/core_prng/src/pcg64/pcg64.c
@@ -24,66 +24,67 @@
 
 #include "pcg64.h"
 
-extern inline void pcg_setseq_128_step_r(pcg_state_setseq_128* rng);
+extern inline void pcg_setseq_128_step_r(pcg_state_setseq_128 *rng);
 extern inline uint64_t pcg_output_xsl_rr_128_64(pcg128_t state);
-extern inline void pcg_setseq_128_srandom_r(pcg_state_setseq_128* rng,
-        pcg128_t initstate, pcg128_t initseq);
+extern inline void pcg_setseq_128_srandom_r(pcg_state_setseq_128 *rng,
+                                            pcg128_t initstate,
+                                            pcg128_t initseq);
 extern inline uint64_t
-pcg_setseq_128_xsl_rr_64_random_r(pcg_state_setseq_128* rng);
+pcg_setseq_128_xsl_rr_64_random_r(pcg_state_setseq_128 *rng);
 extern inline uint64_t
-pcg_setseq_128_xsl_rr_64_boundedrand_r(pcg_state_setseq_128* rng,
+pcg_setseq_128_xsl_rr_64_boundedrand_r(pcg_state_setseq_128 *rng,
                                        uint64_t bound);
-extern inline void pcg_setseq_128_advance_r(pcg_state_setseq_128* rng, pcg128_t delta);
+extern inline void pcg_setseq_128_advance_r(pcg_state_setseq_128 *rng,
+                                            pcg128_t delta);
 
 /* Multi-step advance functions (jump-ahead, jump-back)
-*
-* The method used here is based on Brown, "Random Number Generation
-* with Arbitrary Stride,", Transactions of the American Nuclear
-* Society (Nov. 1994).  The algorithm is very similar to fast
-* exponentiation.
-*
-* Even though delta is an unsigned integer, we can pass a
-* signed integer to go backwards, it just goes "the long way round".
-*/
+ *
+ * The method used here is based on Brown, "Random Number Generation
+ * with Arbitrary Stride,", Transactions of the American Nuclear
+ * Society (Nov. 1994).  The algorithm is very similar to fast
+ * exponentiation.
+ *
+ * Even though delta is an unsigned integer, we can pass a
+ * signed integer to go backwards, it just goes "the long way round".
+ */
 
 #ifndef PCG_EMULATED_128BIT_MATH
 
 pcg128_t pcg_advance_lcg_128(pcg128_t state, pcg128_t delta, pcg128_t cur_mult,
-                             pcg128_t cur_plus)
-{
-    pcg128_t acc_mult = 1u;
-    pcg128_t acc_plus = 0u;
-    while (delta > 0) {
-        if (delta & 1) {
-            acc_mult *= cur_mult;
-            acc_plus = acc_plus * cur_mult + cur_plus;
-        }
-        cur_plus = (cur_mult + 1) * cur_plus;
-        cur_mult *= cur_mult;
-        delta /= 2;
+                             pcg128_t cur_plus) {
+  pcg128_t acc_mult = 1u;
+  pcg128_t acc_plus = 0u;
+  while (delta > 0) {
+    if (delta & 1) {
+      acc_mult *= cur_mult;
+      acc_plus = acc_plus * cur_mult + cur_plus;
     }
-    return acc_mult * state + acc_plus;
+    cur_plus = (cur_mult + 1) * cur_plus;
+    cur_mult *= cur_mult;
+    delta /= 2;
+  }
+  return acc_mult * state + acc_plus;
 }
 
 #else
 
 pcg128_t pcg_advance_lcg_128(pcg128_t state, pcg128_t delta, pcg128_t cur_mult,
-                             pcg128_t cur_plus)
-{
-    pcg128_t acc_mult = PCG_128BIT_CONSTANT(0u, 1u);
-    pcg128_t acc_plus = PCG_128BIT_CONSTANT(0u, 0u);
-    while ((delta.high > 0) || (delta.low > 0)) {
-        if (delta.low & 1) {
-            acc_mult = _pcg128_mult(acc_mult, cur_mult);
-            acc_plus = _pcg128_add(_pcg128_mult(acc_plus, cur_mult), cur_plus);
-        }
-        cur_plus = _pcg128_mult(_pcg128_add(cur_mult, PCG_128BIT_CONSTANT(0u, 1u)), cur_plus);
-        cur_mult = _pcg128_mult(cur_mult, cur_mult);
-        delta.low >>= 1;
-        delta.low += delta.high & 1;
-        delta.high >>= 1;
+                             pcg128_t cur_plus) {
+  pcg128_t acc_mult = PCG_128BIT_CONSTANT(0u, 1u);
+  pcg128_t acc_plus = PCG_128BIT_CONSTANT(0u, 0u);
+  while ((delta.high > 0) || (delta.low > 0)) {
+    if (delta.low & 1) {
+      acc_mult = _pcg128_mult(acc_mult, cur_mult);
+      acc_plus = _pcg128_add(_pcg128_mult(acc_plus, cur_mult), cur_plus);
     }
-    return _pcg128_add(_pcg128_mult(acc_mult, state), acc_plus);
+    cur_plus = _pcg128_mult(_pcg128_add(cur_mult, PCG_128BIT_CONSTANT(0u, 1u)),
+                            cur_plus);
+    cur_mult = _pcg128_mult(cur_mult, cur_mult);
+    delta.low >>= 1;
+    delta.low += delta.high & 1;
+    delta.high >>= 1;
+  }
+  return _pcg128_add(_pcg128_mult(acc_mult, state), acc_plus);
 }
 
 #endif
@@ -91,15 +92,13 @@ pcg128_t pcg_advance_lcg_128(pcg128_t state, pcg128_t delta, pcg128_t cur_mult,
 extern inline uint64_t pcg64_next64(pcg64_state *state);
 extern inline uint32_t pcg64_next32(pcg64_state *state);
 
-#if __SIZEOF_INT128__ && !defined(PCG_FORCE_EMULATED_128BIT_MATH)
-extern void pcg64_advance(pcg64_state *state, pcg128_t step) {
-    pcg64_advance_r(state->pcg_state, step);
-}
-#else
 extern void pcg64_advance(pcg64_state *state, uint64_t *step) {
-    pcg128_t delta;
-    delta.high = step[0];
-    delta.low = step[1];
-    pcg64_advance_r(state->pcg_state, delta);
-}
+  pcg128_t delta;
+#if __SIZEOF_INT128__ && !defined(PCG_FORCE_EMULATED_128BIT_MATH)
+  delta = (((pcg128_t)step[0]) << 64) | step[1];
+#else
+  delta.high = step[0];
+  delta.low = step[1];
 #endif
+  pcg64_advance_r(state->pcg_state, delta);
+}
