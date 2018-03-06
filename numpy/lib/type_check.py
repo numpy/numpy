440f2d8d10a7214a71f0537da9ae78e7d6de4843
Spoorthy Bhat <spoorthy.bhat.77@gmail.com>
2018-03-06 09:59:34 +0530

    Update type_check.py

--- numpy/lib/type_check.py
+++ numpy/lib/type_check.py
@@ -486,10 +486,9 @@ def asscalar(a):
     24
 
     """
-    try:
-        return a.item()
-    except AttributeError:
-        return np.asarray(a).item()
+
+    return a.item()
+
 
 #-----------------------------------------------------------------------------
 
