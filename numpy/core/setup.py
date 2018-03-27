bbfb4034f782e7610b67df9c29c492f0d5bc9b57
Julian Taylor <jtaylor.debian@googlemail.com>
2018-03-27 22:29:17 +0200

    BUG: verify the OS supports avx instruction
    On some systems you can disable avx registers but the gcc builtin does
    only checks if the cpu has the feature.
    Before using avx functions check the OS support with xgetbv.
    Closes gh-10787
    Closes gh-9534

--- numpy/core/setup.py
+++ numpy/core/setup.py
@@ -888,6 +888,7 @@ def configuration(parent_package='',top_path=None):
             join('src', 'umath', 'loops.c.src'),
             join('src', 'umath', 'ufunc_object.c'),
             join('src', 'umath', 'extobj.c'),
+            join('src', 'umath', 'cpuid.c'),
             join('src', 'umath', 'scalarmath.c.src'),
             join('src', 'umath', 'ufunc_type_resolution.c'),
             join('src', 'umath', 'override.c'),
