2589616ca6a9b6270c9621af038d90972154914b
Charles Harris <charlesr.harris@gmail.com>
2018-03-27 09:41:03 -0600

    MAINT: Warn when importing numpy.testing.<module>.
    Downstream projects were importing directly from the testing modules
    rather than from testing. Discourage this.

--- numpy/testing/noseclasses.py
+++ numpy/testing/noseclasses.py
@@ -2,4 +2,10 @@
 Back compatibility noseclasses module. It will import the appropriate
 set of tools
 """
+import warnings
+
+warnings.warn(ImportWarning,
+    "Import from numpy.testing, not numpy.testing.noseclasses")
+
 from ._private.noseclasses import *
+
--- numpy/testing/nosetester.py
+++ numpy/testing/nosetester.py
@@ -3,10 +3,12 @@ Back compatibility nosetester module. It will import the appropriate
 set of tools
 
 """
-import os
+import warnings
+
+warnings.warn(ImportWarning,
+    "Import from numpy.testing, not numpy.testing.nosetester")
 
 from ._private.nosetester import *
-
 
 __all__ = ['get_package_name', 'run_module_suite', 'NoseTester',
            '_numpy_tester', 'get_package_name', 'import_nose',
