77618830e559062fe7e98274fbaa1d9fb53cd6c3
raunaks13 <foleys.raunak@gmail.com>
2018-03-30 09:52:19 +0530

    TST: added test for genfromtxt exception

--- numpy/lib/tests/test_io.py
+++ numpy/lib/tests/test_io.py
@@ -1272,6 +1272,15 @@ M   33  21.99
             assert_(w[0].category is np.VisibleDeprecationWarning)
         assert_equal(test, ctrl)
 
+    def test_names_and_comments_none(self):
+        #tests case when names is true but comments is None
+        data = TextIO('col1 col2\n 1 2\n 3 4')
+        with warnings.catch_warnings(record=True) as w:
+            warnings.filterwarnings('always', '', np.VisibleDeprecationWarning)
+        test = np.genfromtxt(data, dtype=(int, int), comments=None, names=True)
+        control = np.array([(1, 2), (3, 4)], dtype=[('col1', int), ('col2', int)])
+        assert_equal(test, control)
+
     def test_autonames_and_usecols(self):
         # Tests names and usecols
         data = TextIO('A B C D\n aaaa 121 45 9.1')
