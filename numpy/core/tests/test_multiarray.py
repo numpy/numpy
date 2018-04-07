3baef5c8db91ea5eb65ea67bae3a862ddb5eef8c
mattip <matti.picus@gmail.com>
2018-04-07 22:51:31 +0300

    fix from review

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -7266,22 +7266,26 @@ class TestWritebackIfCopy(TestCase):
 
     def test_view_assign(self):
         from numpy.core.multiarray_tests import npy_create_writebackifcopy, npy_resolve
+
         arr = np.arange(9).reshape(3, 3).T
         arr_wb = npy_create_writebackifcopy(arr)
         assert_(arr_wb.flags.writebackifcopy)
         assert_(arr_wb.base is arr)
-        arr_wb[:] = -100
+        arr_wb[...] = -100
         npy_resolve(arr_wb)
+        # arr changes after resolve, even though we assigned to arr_wb
         assert_equal(arr, -100)
-        # after resolve, the two arrays no longer reference eachother
-        assert_(not arr_wb.ctypes.data == 0)
+        # after resolve, the two arrays no longer reference each other
+        assert_(arr_wb.ctypes.data != 0)
         assert_equal(arr_wb.base, None)
-        arr_wb[:] = 100
+        # assigning to arr_wb does not get transfered to arr
+        arr_wb[...] = 100
         assert_equal(arr, -100)
 
 
     def test_view_discard_refcount(self):
         from numpy.core._multiarray_tests import npy_create_writebackifcopy, npy_discard
+
         arr = np.arange(9).reshape(3, 3).T
         orig = arr.copy()
         if HAS_REFCOUNT:
@@ -7289,16 +7293,17 @@ class TestWritebackIfCopy(TestCase):
         arr_wb = npy_create_writebackifcopy(arr)
         assert_(arr_wb.flags.writebackifcopy)
         assert_(arr_wb.base is arr)
-        arr_wb[:] = -100
+        arr_wb[...] = -100
         npy_discard(arr_wb)
         # arr remains unchanged after discard
         assert_equal(arr, orig)
         # after discard, the two arrays no longer reference each other
-        assert_(not arr_wb.ctypes.data == 0)
+        assert_(arr_wb.ctypes.data != 0)
         assert_equal(arr_wb.base, None)
         if HAS_REFCOUNT:
             assert_equal(arr_cnt, sys.getrefcount(arr))
-        arr_wb[:] = 100
+        # assigning to arr_wb does not get transfered to arr
+        arr_wb[...] = 100
         assert_equal(arr, orig)
 
 
