a1e1741d6b679fcf96990e5bddd3c7c012082047
krithi07 <krithi710@gmail.com>
2018-05-03 11:43:40 +0530

    __deepcopy__ on MaskedConstant

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -6317,6 +6317,9 @@ class MaskedConstant(MaskedArray):
         # precedent for this with `np.bool_` scalars.
         return self
 
+	def __deepcopy__(self, *args, **kwargs):
+		return self
+	
     def __setattr__(self, attr, value):
         if not self.__has_singleton():
             # allow the singleton to be initialized
