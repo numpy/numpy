c5ab245430c00ef7761c8acbe2a70b5d40d588de
mattip <matti.picus@gmail.com>
2018-05-06 00:04:16 +0300

    BUG: reduce using SSE only warns if inside SSE loop

--- numpy/core/tests/test_umath.py
+++ numpy/core/tests/test_umath.py
@@ -1329,14 +1329,15 @@ class TestMinMax(object):
         assert_equal(d.min(), d[0])
 
     def test_reduce_warns(self):
-        # gh 10370 Some compilers reorder the call to npy_getfloatstatus and
-        # put it before the call to an intrisic function that causes invalid
-        # status to be set
+        # gh 10370, 11029 Some compilers reorder the call to npy_getfloatstatus
+        # and put it before the call to an intrisic function that causes
+        # invalid status to be set. Also make sure warnings are emitted
         for n in (2, 4, 8, 16, 32):
             with suppress_warnings() as sup:
                 sup.record(RuntimeWarning)
                 for r in np.diagflat([np.nan] * n):
                     assert_equal(np.min(r), np.nan)
+                assert_equal(len(sup.log), n)
 
 
 class TestAbsoluteNegative(object):
