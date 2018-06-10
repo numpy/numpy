818a710e5c23804e307072c18b0bcd36be1473d9
RCCG <r.greif@outlook.com>
2018-06-10 22:52:00 +0200

    changed sum requirement from 2 -> 'sum'

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -605,13 +605,13 @@ def histogram(a, bins=10, range=None, normed=False, weights=None,
         (instead of 1). If `density` is True, the weights are
         normalized, so that the integral of the density over the range
         remains 1.
-    density : bool or int, optional
+    density : bool or str, optional
         If ``False``, the result will contain the number of samples in
         each bin. If ``True``, the result is the value of the
         probability *density* function at the bin, normalized such that
         the *integral* over the range is 1. 
-        If ``2``, the result will be normalized such that the sum of the 
-        histogram values will be equal to 1.
+        If ``sum``, the result will be normalized such that the sum of 
+        the histogram values will be equal to 1.
 
         Overrides the ``normed`` keyword if given.
 
@@ -779,12 +779,11 @@ def histogram(a, bins=10, range=None, normed=False, weights=None,
     if density is not None:
         normed = False
 
-    if density==1:
+    if density=='sum':
+        return n/n.sum(), bin_edges
+    elif density:
         db = np.array(np.diff(bin_edges), float)
         return n/db/n.sum(), bin_edges
-    elif density==2:
-        # normalize for fractionals
-        return n/n.sum(), bin_edges
     elif normed:
         # deprecated, buggy behavior. Remove for NumPy 2.0.0
         db = np.array(np.diff(bin_edges), float)
