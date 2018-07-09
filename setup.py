9f5f1685fe35e63a0f40dbeec152d9d37f2111ec
Charles Harris <charlesr.harris@gmail.com>
2018-07-09 14:46:15 -0600

    REL: prepare 1.15.x for further development

--- setup.py
+++ setup.py
@@ -65,8 +65,8 @@ Operating System :: MacOS
 MAJOR               = 1
 MINOR               = 15
 MICRO               = 0
-ISRELEASED          = True
-VERSION             = '%d.%d.%drc2' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = False
+VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
