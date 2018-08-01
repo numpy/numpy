872508760a447352a680d89359d1a1b262aec164
mattip <matti.picus@gmail.com>
2018-08-01 11:36:54 -0700

    DOC: add docstring to new python files

--- numpy/core/multiarray.py
+++ numpy/core/multiarray.py
@@ -1,3 +1,10 @@
+"""
+Create the numpy.core.multiarray namespace for backward compatibility. In v1.16
+the multiarray and umath c-extension modules were merged into a single
+_multiarray_umath extension module. So we replicate the old namespace
+by importing from the extension module.
+"""
+
 from . import _multiarray_umath
 from numpy.core._multiarray_umath import *
 from numpy.core._multiarray_umath import (_fastCopyAndTranspose, _flagdict, _insert,
