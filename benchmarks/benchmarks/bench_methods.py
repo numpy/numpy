ae5d3fc2a2ce72128a4dc72a2c4245e68f0c0725
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-08-17 14:48:54 -0700

    DEBUG: bench demonstrates speedup, but ref counting issue persists, and sorting op dominates in larger counts [ci skip].

--- benchmarks/benchmarks/bench_methods.py
+++ benchmarks/benchmarks/bench_methods.py
@@ -6,22 +6,38 @@ import numpy as np
 
 class FieldHandling(Benchmark):
 
-    repeat = 1
+    # there's some kind of reference counting
+    # issue in C version of _newnames()
+    # so restrict to a single iteration
+    # with many repeats (and NO warmup)
+    # so that self.r may be defined in setup
+    # only and its reference isn't messed with
+    repeat = 40
     number = 1
+    warmup_time = 0.0
 
-    def setup(self):
-        self.x1 = np.array([21, 32, 14])
-        self.x2 = np.array(['my', 'first', 'name'])
-        self.x3 = np.array([3.1, 4.5, 6.2])
-        self.names = 'id,word,number'
-        self.r = np.rec.fromarrays([self.x1, 
-                                    self.x2,
-                                    self.x3],
-                                    names=self.names)
+    params = [(1, 3, 5, 10, 20, 200)]
+    param_names = 'num_fields'
 
-    def time_sort_array_fields(self):
+    def setup(self, num_fields):
+        num_fields = int(num_fields)
+        self.x = np.array([21, 32, 14])
+        self.arrays = []
+
+        for field_num in range(num_fields):
+            self.arrays.append(self.x)
+
+        self.names = ','.join(np.array(['field' + str(val) for
+                                val in range(num_fields)]))
+
+        self.order = self.names.split(',')[::-1]
+
+        self.r = np.rec.fromarrays(self.arrays,
+                                   names=self.names)
+
+    def time_sort_array_fields(self, num_fields):
         # time sorting recarray by field
         # why do I get "unknown field name" ValueError
         # from C code version when self.r is defined
         # above vs. here?? Ref counting issue again??
-        self.r.sort(order=['word'])
+        self.r.sort(order=self.order)
