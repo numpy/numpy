2b100ec773408deef3ac150f342a6d87c9caede1
Jeffrey Yancey <jeffrey@octane5.com>
2018-08-23 16:45:07 -0600

    add missing lagpow test

--- numpy/polynomial/tests/test_laguerre.py
+++ numpy/polynomial/tests/test_laguerre.py
@@ -5,6 +5,7 @@ from __future__ import division, absolute_import, print_function
 
 import numpy as np
 import numpy.polynomial.laguerre as lag
+import numpy.polynomial.polyutils as pu
 from numpy.polynomial.polynomial import polyval
 from numpy.testing import (
     assert_almost_equal, assert_raises, assert_equal, assert_,
@@ -95,6 +96,19 @@ class TestArithmetic(object):
                 quo, rem = lag.lagdiv(tgt, ci)
                 res = lag.lagadd(lag.lagmul(quo, ci), rem)
                 assert_almost_equal(trim(res), trim(tgt), err_msg=msg)
+
+    def test_lagpow(self):
+        for i in range(5):
+            for j in range(5):
+                msg = "At i=%d, j=%d" % (i, j)
+                c = list(range(i+1))
+                [c] = pu.as_series([c])
+                power = j+1
+                tgt = c
+                for i in range(2, power + 1):
+                    tgt = lag.lagmul(tgt, c)
+                res = lag.lagpow(c, power) 
+                assert_equal(trim(res), trim(tgt), err_msg=msg)
 
 
 class TestEvaluation(object):
