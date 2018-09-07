8c1815c0083629e8f9f0caaad4f653f8af6d2b1d
Charles Harris <charlesr.harris@gmail.com>
2018-09-06 18:57:11 -0600

    BUG: Fix matrix PendingDeprecationWarning suppression for pytest 3.8+.
    Pytest < 3.8 ignored warnings issued during test collection, but that
    changed in pytest 3.8 and the method NumPy used to suppress the
    PendingDeprecationWarning for matrices no longer worked, or rather, was
    exposed as not working. The fix here is to suppress the warning in
    pytest.ini and pytesttester.py , which should work as long as the tests
    are the only places left where NumPy uses matrices.
    
    An alternate fix is to delay the construction of matrices in the tests
    until they are actually run, which has the virtue of test localization
    but is a bit more complicated.
    
    See pytest-dev/pytest#3945 for discussion.

--- numpy/matrixlib/tests/test_masked_matrix.py
+++ numpy/matrixlib/tests/test_masked_matrix.py
@@ -1,14 +1,7 @@
 from __future__ import division, absolute_import, print_function
 
-# As we are testing matrices, we ignore its PendingDeprecationWarnings
-try:
-    import pytest
-    pytestmark = pytest.mark.filterwarnings(
-        'ignore:the matrix subclass is not:PendingDeprecationWarning')
-except ImportError:
-    pass
-
 import pickle
+import pytest
 
 import numpy as np
 from numpy.ma.testutils import (assert_, assert_equal, assert_raises,
