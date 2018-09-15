47e7e68643a47671db92beb27aabfe78d4ec17dc
Mark Harfouche <mark.harfouche@gmail.com>
2018-09-15 07:38:13 -0400

    Modify bench_lib so that page faulting happens in setup

--- benchmarks/benchmarks/bench_lib.py
+++ benchmarks/benchmarks/bench_lib.py
@@ -13,13 +13,22 @@ class Pad(Benchmark):
 
     param_names = ["shape", "pad_width", "mode"]
     params = [
-        [(1000,), (10, 100), (10, 10, 10)],
+        [(1000,),
+         (10, 100),
+         (10, 10, 10),
+         (64, 64),
+         (512, 512),
+         (10, 512, 512)],
         [1, 3, (0, 5)],
-        ["constant", "edge", "linear_ramp", "mean", "reflect", "wrap"],
+        ["constant", "edge", "linear_ramp",
+         "mean", "median", "minimum", "maximum",
+         "reflect", "symmetric", 'wrap']
     ]
 
     def setup(self, shape, pad_width, mode):
-        self.array = np.empty(shape)
+        # This should not be zeros or empty as the OS
+        # might defer page faulting to the time we pad the array
+        self.array = np.ones(shape)
 
     def time_pad(self, shape, pad_width, mode):
-        np.pad(self.array, pad_width, mode)
+        _ = np.pad(self.array, pad_width, mode)
