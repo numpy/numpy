ff56a7e54ec6e29da5f8fd2fd6f86da74b79d5cd
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-10-10 14:58:42 -0700

    TST: error tests for diag_indices_from()
    * add unit tests for previously uncovered
    error handling code in diag_indices_from()

--- numpy/lib/tests/test_index_tricks.py
+++ numpy/lib/tests/test_index_tricks.py
@@ -365,11 +365,23 @@ def test_diag_indices():
         )
 
 
-def test_diag_indices_from():
-    x = np.random.random((4, 4))
-    r, c = diag_indices_from(x)
-    assert_array_equal(r, np.arange(4))
-    assert_array_equal(c, np.arange(4))
+class TestDiagIndicesFrom(object):
+
+    def test_diag_indices_from(self):
+        x = np.random.random((4, 4))
+        r, c = diag_indices_from(x)
+        assert_array_equal(r, np.arange(4))
+        assert_array_equal(c, np.arange(4))
+
+    def test_error_small_input(self):
+        x = np.ones(7)
+        with assert_raises_regex(ValueError, "at least 2-d"):
+            diag_indices_from(x)
+
+    def test_error_shape_mismatch(self):
+        x = np.zeros((3, 3, 2, 3), int)
+        with assert_raises_regex(ValueError, "equal length"):
+            diag_indices_from(x)
 
 
 def test_ndindex():
