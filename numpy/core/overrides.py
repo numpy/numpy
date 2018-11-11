1f4e6db822aab94631009b599ff4934908b52028
Stephan Hoyer <shoyer@google.com>
2018-11-11 13:35:58 -0800

    DOC: add docstring for override_module

--- numpy/core/overrides.py
+++ numpy/core/overrides.py
@@ -151,7 +151,16 @@ def verify_matching_signatures(implementation, dispatcher):
 
 
 def override_module(module):
-    """Decorator for overriding __module__ on a function or class."""
+    """Decorator for overriding __module__ on a function or class.
+
+    Example usage::
+
+        @override_module('numpy')
+        def example():
+            pass
+
+        assert example.__module__ == 'numpy'
+    """
     def decorator(func):
         if module is not None:
             func.__module__ = module
