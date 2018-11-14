4b505c5709ca0974b20176d3ef133e7ed2a7d3fa
Stephan Hoyer <shoyer@google.com>
2018-11-13 22:04:40 -0800

    MAINT: separate builtins and undocumented functions

--- numpy/tests/test_public_api.py
+++ numpy/tests/test_public_api.py
@@ -18,39 +18,43 @@ def check_dir(module, module_name=None):
 
 def test_numpy_namespace():
     # None of these objects are publicly documented.
-    whitelist = {
+    undocumented = {
         'Tester': 'numpy.testing._private.nosetester.NoseTester',
         '_add_newdoc_ufunc': 'numpy.core._multiarray_umath._add_newdoc_ufunc',
         'add_docstring': 'numpy.core._multiarray_umath.add_docstring',
         'add_newdoc': 'numpy.core.function_base.add_newdoc',
         'add_newdoc_ufunc': 'numpy.core._multiarray_umath._add_newdoc_ufunc',
-        'bool': 'builtins.bool',
         'byte_bounds': 'numpy.lib.utils.byte_bounds',
         'compare_chararrays': 'numpy.core._multiarray_umath.compare_chararrays',
-        'complex': 'builtins.complex',
         'deprecate': 'numpy.lib.utils.deprecate',
         'deprecate_with_doc': 'numpy.lib.utils.<lambda>',
         'disp': 'numpy.lib.function_base.disp',
         'fastCopyAndTranspose': 'numpy.core._multiarray_umath._fastCopyAndTranspose',
-        'float': 'builtins.float',
         'get_array_wrap': 'numpy.lib.shape_base.get_array_wrap',
         'get_include': 'numpy.lib.utils.get_include',
-        'int': 'builtins.int',
         'int_asbuffer': 'numpy.core._multiarray_umath.int_asbuffer',
-        'long': 'builtins.int',
         'mafromtxt': 'numpy.lib.npyio.mafromtxt',
         'maximum_sctype': 'numpy.core.numerictypes.maximum_sctype',
         'ndfromtxt': 'numpy.lib.npyio.ndfromtxt',
-        'object': 'builtins.object',
         'recfromcsv': 'numpy.lib.npyio.recfromcsv',
         'recfromtxt': 'numpy.lib.npyio.recfromtxt',
         'safe_eval': 'numpy.lib.utils.safe_eval',
         'set_string_function': 'numpy.core.arrayprint.set_string_function',
         'show_config': 'numpy.__config__.show',
-        'str': 'builtins.str',
-        'unicode': 'builtins.str',
         'who': 'numpy.lib.utils.who',
     }
+    # These built-in types are re-exported by numpy.
+    builtins = {
+        'bool': 'builtins.bool',
+        'complex': 'builtins.complex',
+        'float': 'builtins.float',
+        'int': 'builtins.int',
+        'long': 'builtins.int',
+        'object': 'builtins.object',
+        'str': 'builtins.str',
+        'unicode': 'builtins.str',
+    }
+    whitelist = dict(undocumented, **builtins)
     bad_results = check_dir(np)
     # pytest gives better error messages with the builtin assert than with
     # assert_equal
