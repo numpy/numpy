15cbdc26f051c62ef66191d9f03989810547dd83
Mircea-Akos Brumă <bruma.mircea.a@gmail.com>
2018-11-15 14:53:53 +0100

    BUG: Fix for #10533 np.dtype(ctype) does not respect endianness (#12380)
    * BUG: Fix for #10533 np.dtype(ctype) does not respect endianness
    
    See: #10533
    Added some code that uses the __name__ of the ctypes type. Also
    added some new test to make sure we'll know if the ctypes data
    that allows this workaround breaks in the future.
    
    * Fix for #10533 np.dtype(ctype) does not respect endianness
    
    Removed the TODO for this bug.
    
    * Fix for #10533 np.dtype(ctype) does not respect endianness
    
    Solved the recursiveness issue that failed the automated test.
    Also did some of Eric's requested changes.
    
    * Small mistake in the new function fixed.
    
    * Changed 'if t.__name__.endswith('_be') ' with
    'if t.__ctypes_be__ is t' on Eric's suggestion.
    
    * Code cleanup on Eric's recommendations.
    
    * Cleaned up the tests on Eric's suggestions.

--- numpy/core/_dtype_ctypes.py
+++ numpy/core/_dtype_ctypes.py
@@ -33,7 +33,6 @@ def _from_ctypes_array(t):
 
 
 def _from_ctypes_structure(t):
-    # TODO: gh-10533
     for item in t._fields_:
         if len(item) > 2:
             raise TypeError(
@@ -67,6 +66,18 @@ def _from_ctypes_structure(t):
         return np.dtype(fields, align=True)
 
 
+def dtype_from_ctypes_scalar(t):
+    """
+    Return the dtype type with endianness included if it's the case
+    """
+    if t.__ctype_be__ is t:
+        return np.dtype('>' + t._type_)
+    elif t.__ctype_le__ is t:
+        return np.dtype('<' + t._type_)
+    else:
+        return np.dtype(t._type_)
+
+
 def dtype_from_ctypes_type(t):
     """
     Construct a dtype object from a ctypes type
@@ -83,7 +94,7 @@ def dtype_from_ctypes_type(t):
             "conversion from ctypes.Union types like {} to dtype"
             .format(t.__name__))
     elif isinstance(t._type_, str):
-        return np.dtype(t._type_)
+        return dtype_from_ctypes_scalar(t)
     else:
         raise NotImplementedError(
             "Unknown ctypes type {}".format(t.__name__))
