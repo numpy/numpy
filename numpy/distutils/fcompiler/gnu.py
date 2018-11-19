19cd7d0c14aaf3b1f4606431d6ee23ac979d4d6e
Ayappan P <ayappap2@in.ibm.com>
2018-11-19 20:45:30 +0530

    gfortran needs -lpthread & -maix64(64 build) in AIX

--- numpy/distutils/fcompiler/gnu.py
+++ numpy/distutils/fcompiler/gnu.py
@@ -315,6 +315,12 @@ class Gnu95FCompiler(GnuFCompiler):
     module_dir_switch = '-J'
     module_include_switch = '-I'
 
+    if sys.platform[:3] == 'aix':
+        executables['linker_so'].append('-lpthread')
+        if platform.architecture()[0][:2] == '64':
+            for key in ['compiler_f77', 'compiler_f90','compiler_fix','linker_so', 'linker_exe']:
+                executables[key].append('-maix64')
+
     g2c = 'gfortran'
 
     def _universal_flags(self, cmd):
