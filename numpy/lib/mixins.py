ae4a421703ce173b3d0240c8fef2bb9ae2466f23
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-12-04 14:09:19 -0800

    MAINT: revert numpy/lib/mixins to master branch status -- refguide passes in any case now.

--- numpy/lib/mixins.py
+++ numpy/lib/mixins.py
@@ -78,55 +78,58 @@ class NDArrayOperatorsMixin(object):
     class that simply wraps a NumPy array and ensures that the result of any
     arithmetic operation is also an ``ArrayLike`` object::
 
-    >>> import numpy.lib
-    >>> import numbers
-    >>> class ArrayLike(np.lib.mixins.NDArrayOperatorsMixin):
-    ...    def __init__(self, value):
-    ...        self.value = np.asarray(value)
-    ...    # One might also consider adding the built-in list type to this
-    ...    # list, to support operations like np.add(array_like, list)
-    ...    _HANDLED_TYPES = (np.ndarray, numbers.Number)
-    ...    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
-    ...        out = kwargs.get('out', ())
-    ...        for x in inputs + out:
-    ...            # Only support operations with instances of _HANDLED_TYPES.
-    ...            # Use ArrayLike instead of type(self) for isinstance to
-    ...            # allow subclasses that don't override __array_ufunc__ to
-    ...            # handle ArrayLike objects.
-    ...            if not isinstance(x, self._HANDLED_TYPES + (ArrayLike,)):
-    ...                return NotImplemented
-    ...        # Defer to the implementation of the ufunc on unwrapped values.
-    ...        inputs = tuple(x.value if isinstance(x, ArrayLike) else x
-    ...                       for x in inputs)
-    ...        if out:
-    ...            kwargs['out'] = tuple(
-    ...                x.value if isinstance(x, ArrayLike) else x
-    ...                for x in out)
-    ...        result = getattr(ufunc, method)(*inputs, **kwargs)
-    ...        if type(result) is tuple:
-    ...            # multiple return values
-    ...            return tuple(type(self)(x) for x in result)
-    ...        elif method == 'at':
-    ...            # no return value
-    ...            return None
-    ...        else:
-    ...            # one return value
-    ...            return type(self)(result)
-    ...    def __repr__(self):
-    ...        return '%s(%r)' % (type(self).__name__, self.value)
+        class ArrayLike(np.lib.mixins.NDArrayOperatorsMixin):
+            def __init__(self, value):
+                self.value = np.asarray(value)
 
-    >>> # In interactions between ``ArrayLike`` objects and numbers or numpy arrays,
-    >>> # the result is always another ``ArrayLike``:
+            # One might also consider adding the built-in list type to this
+            # list, to support operations like np.add(array_like, list)
+            _HANDLED_TYPES = (np.ndarray, numbers.Number)
 
-    >>> x = ArrayLike([1, 2, 3])
-    >>> x - 1
-    ArrayLike(array([0, 1, 2]))
-    >>> 1 - x
-    ArrayLike(array([ 0, -1, -2]))
-    >>> np.arange(3) - x
-    ArrayLike(array([-1, -1, -1]))
-    >>> x - np.arange(3)
-    ArrayLike(array([1, 1, 1]))
+            def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+                out = kwargs.get('out', ())
+                for x in inputs + out:
+                    # Only support operations with instances of _HANDLED_TYPES.
+                    # Use ArrayLike instead of type(self) for isinstance to
+                    # allow subclasses that don't override __array_ufunc__ to
+                    # handle ArrayLike objects.
+                    if not isinstance(x, self._HANDLED_TYPES + (ArrayLike,)):
+                        return NotImplemented
+
+                # Defer to the implementation of the ufunc on unwrapped values.
+                inputs = tuple(x.value if isinstance(x, ArrayLike) else x
+                               for x in inputs)
+                if out:
+                    kwargs['out'] = tuple(
+                        x.value if isinstance(x, ArrayLike) else x
+                        for x in out)
+                result = getattr(ufunc, method)(*inputs, **kwargs)
+
+                if type(result) is tuple:
+                    # multiple return values
+                    return tuple(type(self)(x) for x in result)
+                elif method == 'at':
+                    # no return value
+                    return None
+                else:
+                    # one return value
+                    return type(self)(result)
+
+            def __repr__(self):
+                return '%s(%r)' % (type(self).__name__, self.value)
+
+    In interactions between ``ArrayLike`` objects and numbers or numpy arrays,
+    the result is always another ``ArrayLike``:
+
+        >>> x = ArrayLike([1, 2, 3])
+        >>> x - 1
+        ArrayLike(array([0, 1, 2]))
+        >>> 1 - x
+        ArrayLike(array([ 0, -1, -2]))
+        >>> np.arange(3) - x
+        ArrayLike(array([-1, -1, -1]))
+        >>> x - np.arange(3)
+        ArrayLike(array([1, 1, 1]))
 
     Note that unlike ``numpy.ndarray``, ``ArrayLike`` does not allow operations
     with arbitrary, unrecognized types. This ensures that interactions with
