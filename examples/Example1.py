30fcd4fee04a8278bea0341c138cdc47cca4ab6c
Czapkaman <kubicki.mateusz97@gmail.com>
2018-12-08 22:57:59 +0100

    Add example2

--- examples/Example1.py
+++ examples/Example1.py
@@ -4,12 +4,9 @@ arrbool = np.full(9, True, dtype=bool)
 
 arr = np.arange(1,10)
 
-##for a in arr:
-##    print(a, end='')
-##    if (a+1)%3 == 0:
-##        print(end='\n')
-
 arr2 = np.random.random(9)
+
+#ustawienie precyzji wypisywania to 3 znaków po przecinku
 np.set_printoptions(precision=3)
 print(arr * arr2)
 
@@ -21,4 +18,5 @@ arr5 = np.array([7,2,10,2,7,4,9,4,9,8])
 print(arr4+arr5)
 print(np.setdiff1d(arr4,arr5))
 
+#setdiff1d służy do usuwania powtórzeń
 arr6 = np.setdiff1d(arr4,arr5) * np.random.randint(1,9, 9).sum()
