c9424106d678a05a0520e74d529a448241223f32
Charles Harris <charlesr.harris@gmail.com>
2018-12-19 14:15:29 -0700

    REL: NumPy 1.16.0rc1 release.

--- setup.py
+++ setup.py
@@ -62,8 +62,8 @@ Operating System :: MacOS
 MAJOR               = 1
 MINOR               = 16
 MICRO               = 0
-ISRELEASED          = False
-VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+ISRELEASED          = True
+VERSION             = '%d.%d.%drc1' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
