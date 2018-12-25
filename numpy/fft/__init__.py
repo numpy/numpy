f8d9ab132eafadfcc523151377127d71cd40f22e
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-12-25 21:19:08 +0100

    replace fftpack with pocketfft

--- numpy/fft/__init__.py
+++ numpy/fft/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division, absolute_import, print_function
 # To get sub-modules
 from .info import __doc__
 
-from .fftpack import *
+from .pocketfft import *
 from .helper import *
 
 from numpy._pytesttester import PytestTester
