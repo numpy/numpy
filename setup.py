81927111589946319137117f88e656eeaa63c9b3
Charles Harris <charlesr.harris@gmail.com>
2019-01-13 19:37:40 -0700

    REL: prepare 1.16.x for further development

--- setup.py
+++ setup.py
@@ -61,8 +61,8 @@ Operating System :: MacOS
 
 MAJOR               = 1
 MINOR               = 16
-MICRO               = 0
-ISRELEASED          = True
+MICRO               = 1
+ISRELEASED          = False
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
