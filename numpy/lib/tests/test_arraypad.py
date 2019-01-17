3f602fc671e953a846ac9bd64359752df11558fc
Lars Grueter <lagru@mailbox.org>
2019-01-17 16:57:49 +0100

    TST: Move testing pad_width as ndarray
    Can be grouped in class TestPadWidth as this test checks if an ndarray
    is accepted as the value to pad_width

--- numpy/lib/tests/test_arraypad.py
+++ numpy/lib/tests/test_arraypad.py
@@ -1155,27 +1155,6 @@ class TestLegacyVectorFunction(object):
         assert_array_equal(a, b)
 
 
-class TestNdarrayPadWidth(object):
-    def test_check_simple(self):
-        a = np.arange(12)
-        a = np.reshape(a, (4, 3))
-        a = pad(a, np.array(((2, 3), (3, 2))), 'edge')
-        b = np.array(
-            [[0,  0,  0,    0,  1,  2,    2,  2],
-             [0,  0,  0,    0,  1,  2,    2,  2],
-
-             [0,  0,  0,    0,  1,  2,    2,  2],
-             [3,  3,  3,    3,  4,  5,    5,  5],
-             [6,  6,  6,    6,  7,  8,    8,  8],
-             [9,  9,  9,    9, 10, 11,   11, 11],
-
-             [9,  9,  9,    9, 10, 11,   11, 11],
-             [9,  9,  9,    9, 10, 11,   11, 11],
-             [9,  9,  9,    9, 10, 11,   11, 11]]
-            )
-        assert_array_equal(a, b)
-
-
 class TestUnicodeInput(object):
     def test_unicode_mode(self):
         constant_mode = u'constant'
@@ -1233,6 +1212,25 @@ class TestPadWidth(object):
         match = "`pad_width` must be of integral type."
         with pytest.raises(TypeError, match=match):
             np.pad(arr, pad_width, mode)
+
+    def test_pad_width_as_ndarray(self):
+        a = np.arange(12)
+        a = np.reshape(a, (4, 3))
+        a = pad(a, np.array(((2, 3), (3, 2))), 'edge')
+        b = np.array(
+            [[0,  0,  0,    0,  1,  2,    2,  2],
+             [0,  0,  0,    0,  1,  2,    2,  2],
+
+             [0,  0,  0,    0,  1,  2,    2,  2],
+             [3,  3,  3,    3,  4,  5,    5,  5],
+             [6,  6,  6,    6,  7,  8,    8,  8],
+             [9,  9,  9,    9, 10, 11,   11, 11],
+
+             [9,  9,  9,    9, 10, 11,   11, 11],
+             [9,  9,  9,    9, 10, 11,   11, 11],
+             [9,  9,  9,    9, 10, 11,   11, 11]]
+            )
+        assert_array_equal(a, b)
 
 
 @pytest.mark.parametrize("mode", _all_modes.keys())
