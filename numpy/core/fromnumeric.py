e9c5e3ad9220ea4ff613654578bcaa5e090d085e
Dominic Jack <thedomjack@gmail.com>
2019-01-30 19:36:50 +1000

    BUG: squeeze corner case

--- numpy/core/fromnumeric.py
+++ numpy/core/fromnumeric.py
@@ -1385,7 +1385,7 @@ def squeeze(a, axis=None):
     try:
         squeeze = a.squeeze
     except AttributeError:
-        return _wrapit(a, 'squeeze')
+        return _wrapit(a, 'squeeze', axis=axis)
     if axis is None:
         return squeeze()
     else:
