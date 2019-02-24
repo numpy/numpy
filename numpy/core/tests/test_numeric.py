3fba5ae7a657b006afb02620927e501eb8ef48de
Eric Wieser <wieser.eric@gmail.com>
2019-02-24 01:34:21 -0800

    Update test_numeric.py

--- numpy/core/tests/test_numeric.py
+++ numpy/core/tests/test_numeric.py
@@ -2748,7 +2748,9 @@ class TestTensordot(object):
         td = np.tensordot(a, b, (1, 0))
         assert_array_equal(td, np.dot(a, b))
         assert_array_equal(td, np.einsum('ij,jk', a, b))
-        # Test resolution to issue #12130
+        
+    def test_zero_dimensional(self):
+        # gh-12130
         arr_0d = np.array(1)
         ret = np.tensordot(arr_0d, arr_0d, ([], []))  # contracting no axes is well defined
         assert_array_equal(ret, arr_0d)
