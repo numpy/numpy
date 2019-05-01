08767fc0a4f3d585dca492b9390454d6fa943b4b
Tyler Reddy <tyler.je.reddy@gmail.com>
2019-05-01 16:24:05 -0700

    MAINT: more adjustments in PR 10723
    * adjust test_longdouble_from_int to properly
    handle platforms where np.longdouble and
    np.double have roughly equivalent limits

--- numpy/core/tests/test_longdouble.py
+++ numpy/core/tests/test_longdouble.py
@@ -1,5 +1,6 @@
 from __future__ import division, absolute_import, print_function
 
+import warnings
 import pytest
 
 import numpy as np
@@ -213,7 +214,18 @@ class TestCommaDecimalPointLocale(CommaDecimalPointLocale):
 def test_longdouble_from_int(int_val):
     # for issue gh-9968
     str_val = str(int_val)
-    assert np.longdouble(int_val) == np.longdouble(str_val)
+    # we'll expect a RuntimeWarning on platforms
+    # with np.longdouble equivalent to np.double
+    # for large integer input
+    with warnings.catch_warnings(record=True) as w:
+        warnings.filterwarnings('always', '', RuntimeWarning)
+        # can be inf==inf on some platforms
+        assert np.longdouble(int_val) == np.longdouble(str_val)
+        # we can't directly compare the int and
+        # max longdouble value on all platforms
+        if np.allclose(np.finfo(np.longdouble).max,
+                       np.finfo(np.double).max) and w:
+            assert w[0].category is RuntimeWarning
 
 @pytest.mark.parametrize("bool_val", [
     True, False])
