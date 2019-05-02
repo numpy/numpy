1f1e8f94ea343084889bae2e9081c3fa3d581b65
mattip <matti.picus@gmail.com>
2019-05-01 22:53:54 -0400

    BUG: missing git raises an OSError

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def git_version():
     try:
         out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
         GIT_REVISION = out.strip().decode('ascii')
-    except subprocess.SubprocessError:
+    except (subprocess.SubprocessError, OSError):
         GIT_REVISION = "Unknown"
 
     return GIT_REVISION
