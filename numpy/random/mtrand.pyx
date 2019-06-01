68c81c10aa8b40a60a95968a1fb4b57c08b1340f
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-06-01 14:23:23 +0900

    MAINT: Correct masked aliases
    Expose alias that have correct docstrings
    
    closes #10587

--- numpy/random/mtrand.pyx
+++ numpy/random/mtrand.pyx
@@ -4196,7 +4196,7 @@ power = _rand.power
 rand = _rand.rand
 randint = _rand.randint
 randn = _rand.randn
-random = _rand.random_sample
+random = _rand.random
 random_integers = _rand.random_integers
 random_sample = _rand.random_sample
 rayleigh = _rand.rayleigh
@@ -4261,6 +4261,7 @@ __all__ = [
     'rand',
     'randint',
     'randn',
+    'random',
     'random_integers',
     'random_sample',
     'ranf',
