70f1c78ae17db12ab0ef0037ce025bdf04f1eb3f
Maksim Shabunin <maksim.shabunin@gmail.com>
2019-06-06 16:27:39 +0300

    BUG: fix compilation of 3rdparty modules with Py_LIMITED_API enabled
    There are no macros PyTuple_GET_SIZE and PyTuple_GET_ITEM available when compiling with enabled Py_LIMITED_API.

--- numpy/core/include/numpy/ndarrayobject.h
+++ numpy/core/include/numpy/ndarrayobject.h
@@ -233,10 +233,10 @@ static NPY_INLINE int
 NPY_TITLE_KEY_check(PyObject *key, PyObject *value)
 {
     PyObject *title;
-    if (PyTuple_GET_SIZE(value) != 3) {
+    if (PyTuple_Size(value) != 3) {
         return 0;
     }
-    title = PyTuple_GET_ITEM(value, 2);
+    title = PyTuple_GetItem(value, 2);
     if (key == title) {
         return 1;
     }
