51ee4543823457b03bda56e33801177f31fc58f6
Eric Wieser <wieser.eric@gmail.com>
2019-06-07 15:23:51 -0700

    ENH: Allow dtype objects to be indexed with multiple fields at once (#10417)
    This returns a dtype of the same size with other fields removed, just like that used in gh-6053
    and is used internally for the indexing code in mapping.c now.

--- numpy/core/src/multiarray/descriptor.h
+++ numpy/core/src/multiarray/descriptor.h
@@ -14,6 +14,18 @@ _arraydescr_from_dtype_attr(PyObject *obj);
 NPY_NO_EXPORT int
 is_dtype_struct_simple_unaligned_layout(PyArray_Descr *dtype);
 
+/*
+ * Filter the fields of a dtype to only those in the list of strings, ind.
+ *
+ * No type checking is performed on the input.
+ *
+ * Raises:
+ *   ValueError - if a field is repeated
+ *   KeyError - if an invalid field name (or any field title) is used
+ */
+NPY_NO_EXPORT PyArray_Descr *
+arraydescr_field_subset_view(PyArray_Descr *self, PyObject *ind);
+
 extern NPY_NO_EXPORT char *_datetime_strings[];
 
 #endif
