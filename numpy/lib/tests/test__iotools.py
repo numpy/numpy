c2e20bc33368ba168dc111b18f11e321fc925e9b
Sebastian Berg <sebastian@sipsolutions.net>
2019-06-29 19:31:00 -0700

    FIXUP: iotools StringConverter._mapper

--- numpy/lib/tests/test__iotools.py
+++ numpy/lib/tests/test__iotools.py
@@ -205,15 +205,17 @@ class TestStringConverter(object):
         "Tests updatemapper"
         dateparser = _bytes_to_date
         _original_mapper = StringConverter._mapper[:]
-        StringConverter.upgrade_mapper(dateparser, date(2000, 1, 1))
-        convert = StringConverter(dateparser, date(2000, 1, 1))
-        test = convert('2001-01-01')
-        assert_equal(test, date(2001, 1, 1))
-        test = convert('2009-01-01')
-        assert_equal(test, date(2009, 1, 1))
-        test = convert('')
-        assert_equal(test, date(2000, 1, 1))
-        StringConverter._mapper = _original_mapper
+        try:
+            StringConverter.upgrade_mapper(dateparser, date(2000, 1, 1))
+            convert = StringConverter(dateparser, date(2000, 1, 1))
+            test = convert('2001-01-01')
+            assert_equal(test, date(2001, 1, 1))
+            test = convert('2009-01-01')
+            assert_equal(test, date(2009, 1, 1))
+            test = convert('')
+            assert_equal(test, date(2000, 1, 1))
+        finally:
+            StringConverter._mapper = _original_mapper
 
     def test_string_to_object(self):
         "Make sure that string-to-object functions are properly recognized"
