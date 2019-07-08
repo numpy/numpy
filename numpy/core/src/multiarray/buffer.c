6d47d311b4da287c24baa971330d0ae3a4f38d32
Sebastian Berg <sebastian@sipsolutions.net>
2019-07-08 13:18:44 -0700

    DEP: Speed up WarnOnWrite deprecation in buffer interface
    When a buffer interface does not request a writeable buffer,
    simply pass a read-only one when the warn on write flag is set.
    
    This is to give an easier way forward with avoiding the deprecation
    warnings: Simply do not ask for a writeable buffer.
    
    It will break code that expects writeable buffers but does not
    ask for them specifically a bit harder than would be nice.
    But since such code probably should ask for it specifically, this
    is likely fine (an RC release has to find out).
    
    The main reason for this is, that this way it plays very will with
    cython, which requests writeable buffers explicitly and if declared
    `const` is happy about read-only (so that using `const` is the best
    way to avoid the warning and makes code cleaner).
    
    Closes gh-13929, gh-13974

--- numpy/core/src/multiarray/buffer.c
+++ numpy/core/src/multiarray/buffer.c
@@ -771,17 +771,6 @@ array_getbuffer(PyObject *obj, Py_buffer *view, int flags)
             goto fail;
         }
     }
-    /*
-     * If a read-only buffer is requested on a read-write array, we return a
-     * read-write buffer, which is dubious behavior. But that's why this call
-     * is guarded by PyArray_ISWRITEABLE rather than (flags &
-     * PyBUF_WRITEABLE).
-     */
-    if (PyArray_ISWRITEABLE(self)) {
-        if (array_might_be_written(self) < 0) {
-            goto fail;
-        }
-    }
 
     if (view == NULL) {
         PyErr_SetString(PyExc_ValueError, "NULL view in getbuffer");
@@ -797,7 +786,14 @@ array_getbuffer(PyObject *obj, Py_buffer *view, int flags)
     view->buf = PyArray_DATA(self);
     view->suboffsets = NULL;
     view->itemsize = PyArray_ITEMSIZE(self);
-    view->readonly = !PyArray_ISWRITEABLE(self);
+    /*
+     * Set a requested buffer to readonly also if the array will be readonly
+     * after a deprecation. This jumps the deprecation, but avoiding the
+     * warning is not convenient here and a warning is given if a writeable
+     * buffer is requested. (Warn on write is cleared if writeable is requested)
+     */
+    view->readonly = (!PyArray_ISWRITEABLE(self) ||
+                      PyArray_CHKFLAGS(self, NPY_ARRAY_WARN_ON_WRITE));
     view->internal = NULL;
     view->len = PyArray_NBYTES(self);
     if ((flags & PyBUF_FORMAT) == PyBUF_FORMAT) {
