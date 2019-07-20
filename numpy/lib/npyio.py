b60d3ae356d81a84f5f92f2e091635a771e30648
Omar Merghany <omar.merghany95@gmail.com>
2019-07-21 01:37:38 +0200

    d

--- numpy/lib/npyio.py
+++ numpy/lib/npyio.py
@@ -512,8 +512,8 @@ def save(file, arr, allow_pickle=True, fix_imports=True):
     Example:
     --------
     >>> with open('test.npy', 'wb') as f: 
-            np.save(f, np.array([1, 2]))
-            np.save(f, np.array([1, 3]))
+        np.save(f, np.array([1, 2]))
+        np.save(f, np.array([1, 3]))
     >>> with open('test.npy', 'rb') as f:
             a = np.load(f)
             b = np.load(f)
