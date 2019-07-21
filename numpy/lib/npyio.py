d49b9e57148d2864dc7c2ad1ce25f36b036ea845
Omar Merghany <omar.merghany95@gmail.com>
2019-07-21 18:59:37 +0200

    adjust open block

--- numpy/lib/npyio.py
+++ numpy/lib/npyio.py
@@ -511,12 +511,14 @@ def save(file, arr, allow_pickle=True, fix_imports=True):
     It has similar behavior to append not overwrite since the file is not closed yet.
     Example:
     --------
-    >>>with open('test.npy', 'wb') as f: 
-    np.save(f, np.array([1, 2]))
-    np.save(f, np.array([1, 3]))
+    >>> with open('test.npy', 'wb') as f:
+    ...     np.save(f, np.array([1, 2]))
+    ...     np.save(f, np.array([1, 3]))    
+    
+
     >>> with open('test.npy', 'rb') as f:
-            a = np.load(f)
-            b = np.load(f)
+    ...     a = np.load(f)
+    ...     b = np.load(f)
     >>> print(a, b)
     # [1 2] [1 3]
 
