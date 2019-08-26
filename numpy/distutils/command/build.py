41f74db4e835ae0cff95ce3ad1f6800fbc4b192d
Hugo <hugovk@users.noreply.github.com>
2019-08-26 21:06:41 +0300

    Use *sys.version_info[:2]

--- numpy/distutils/command/build.py
+++ numpy/distutils/command/build.py
@@ -38,7 +38,7 @@ class build(old_build):
                 raise ValueError("--parallel/-j argument must be an integer")
         build_scripts = self.build_scripts
         old_build.finalize_options(self)
-        plat_specifier = ".{}-{}.{}".format(get_platform(), *sys.version_info)
+        plat_specifier = ".{}-{}.{}".format(get_platform(), *sys.version_info[:2])
         if build_scripts is None:
             self.build_scripts = os.path.join(self.build_base,
                                               'scripts' + plat_specifier)
