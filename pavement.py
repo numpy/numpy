3dc2b01192d4ef54e0507601cc3d5ecf9330da56
Charles Harris <charlesr.harris@gmail.com>
2019-08-26 18:14:31 -0600

    REL: prepare 1.17.x for further development

--- pavement.py
+++ pavement.py
@@ -42,7 +42,7 @@ from paver.easy import Bunch, options, task, sh
 #-----------------------------------
 
 # Path to the release notes
-RELEASE_NOTES = 'doc/release/1.17.1-notes.rst'
+RELEASE_NOTES = 'doc/release/1.17.2-notes.rst'
 
 
 #-------------------------------------------------------
