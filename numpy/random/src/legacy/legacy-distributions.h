3a5d416fcc055014805545047ec8687c5e6660ab
Warren Weckesser <warren.weckesser@gmail.com>
2019-09-15 22:02:38 -0400

    MAINT: random: Remove a few duplicated C function prototypes.

--- numpy/random/src/legacy/legacy-distributions.h
+++ numpy/random/src/legacy/legacy-distributions.h
@@ -16,20 +16,15 @@ extern double legacy_pareto(aug_bitgen_t *aug_state, double a);
 extern double legacy_weibull(aug_bitgen_t *aug_state, double a);
 extern double legacy_power(aug_bitgen_t *aug_state, double a);
 extern double legacy_gamma(aug_bitgen_t *aug_state, double shape, double scale);
-extern double legacy_pareto(aug_bitgen_t *aug_state, double a);
-extern double legacy_weibull(aug_bitgen_t *aug_state, double a);
 extern double legacy_chisquare(aug_bitgen_t *aug_state, double df);
 extern double legacy_noncentral_chisquare(aug_bitgen_t *aug_state, double df,
                                           double nonc);
-
 extern double legacy_noncentral_f(aug_bitgen_t *aug_state, double dfnum,
                                   double dfden, double nonc);
 extern double legacy_wald(aug_bitgen_t *aug_state, double mean, double scale);
 extern double legacy_lognormal(aug_bitgen_t *aug_state, double mean,
                                double sigma);
 extern double legacy_standard_t(aug_bitgen_t *aug_state, double df);
-extern int64_t legacy_negative_binomial(aug_bitgen_t *aug_state, double n,
-                                        double p);
 extern double legacy_standard_cauchy(aug_bitgen_t *state);
 extern double legacy_beta(aug_bitgen_t *aug_state, double a, double b);
 extern double legacy_f(aug_bitgen_t *aug_state, double dfnum, double dfden);
