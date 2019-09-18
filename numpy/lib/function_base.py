0a293a3554a7c32ea3b6cb5b328b5854fc8ab3d9
Joseph R. Fox-Rabinovitz <jfoxrabinovitz@gmail.com>
2019-09-18 01:38:06 -0400

    DOC: Added missing versionadded to diff(prepend)

--- numpy/lib/function_base.py
+++ numpy/lib/function_base.py
@@ -1167,11 +1167,13 @@ def diff(a, n=1, axis=-1, prepend=np._NoValue, append=np._NoValue):
         The axis along which the difference is taken, default is the
         last axis.
     prepend, append : array_like, optional
-        Values to prepend or append to "a" along axis prior to
+        Values to prepend or append to `a` along axis prior to
         performing the difference.  Scalar values are expanded to
         arrays with length 1 in the direction of axis and the shape
         of the input array in along all other axes.  Otherwise the
-        dimension and shape must match "a" except along axis.
+        dimension and shape must match `a` except along axis.
+
+        .. versionadded:: 1.16.0
 
     Returns
     -------
