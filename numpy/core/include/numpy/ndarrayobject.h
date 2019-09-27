0299ac0edef0d8955cd1a64f82c983368c1f692e
mattip <matti.picus@gmail.com>
2019-09-27 09:21:49 +0300

    BUG: properly define PyArray_DescrCheck

--- numpy/core/include/numpy/ndarrayobject.h
+++ numpy/core/include/numpy/ndarrayobject.h
@@ -23,7 +23,7 @@ extern "C" {
 
 /* C-API that requires previous API to be defined */
 
-#define PyArray_DescrCheck(op) (((PyObject*)(op))->ob_type==&PyArrayDescr_Type)
+#define PyArray_DescrCheck(op) PyObject_TypeCheck(op, &PyArrayDescr_Type)
 
 #define PyArray_Check(op) PyObject_TypeCheck(op, &PyArray_Type)
 #define PyArray_CheckExact(op) (((PyObject*)(op))->ob_type == &PyArray_Type)
