0182549c5039ba222c2196af6e3ccfc4783509d3
disha4u <disha.97.agarwal@gmail.com>
2019-10-06 21:59:44 +0530

    Update nanfunctions.py

--- numpy/lib/nanfunctions.py
+++ numpy/lib/nanfunctions.py
@@ -1365,9 +1365,8 @@ def _nanquantile_unchecked(a, q, axis=None, out=None, overwrite_input=False,
     # so deal them upfront
     if a.size == 0:
         #to return a nan for all quantiles
-        ans=[np.nanmean(a, axis, out=out, keepdims=keepdims) for _ in range(q.size)]
+        ans = [np.nanmean(a, axis, out=out, keepdims=keepdims) for _ in range(q.size)]
         return np.array(ans)
-
     r, k = function_base._ureduce(
         a, func=_nanquantile_ureduce_func, q=q, axis=axis, out=out,
         overwrite_input=overwrite_input, interpolation=interpolation
