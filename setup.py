34ac92a6efb4e40385f6fce83e4b3de4e0e9fbb9
mattip <matti.picus@gmail.com>
2019-10-10 14:11:27 +0300

    BUILD: remove python 3.5 for 1.18, as per NEP 29

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ License :: OSI Approved
 Programming Language :: C
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3 :: Only
