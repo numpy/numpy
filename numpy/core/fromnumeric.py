728dfebca6f2fa6a37c553565163904a9198b726
mproszewska <38814059+mproszewska@users.noreply.github.com>
2019-11-01 01:09:18 +0100

    Update numpy/core/fromnumeric.py
    Co-Authored-By: Eric Wieser <wieser.eric@gmail.com>

--- numpy/core/fromnumeric.py
+++ numpy/core/fromnumeric.py
@@ -1163,7 +1163,7 @@ def argmax(a, axis=None, out=None):
     1
 
     >>> x = np.array([[4,2,3],[1,0,3]])
-    >>> np.take_along_axis(x, np.expand_dims(np.argmax(x, axis=-1), axis=-1), axis=-1)
+    >>> np.take_along_axis(x, np.expand_dims(np.argmax(x, axis=-1), axis=-1), axis=-1)  # same as `np.max(x, axis=-1, keepdims=True)`
     array([[4],
        [3]])
     >>> np.take_along_axis(x, np.expand_dims(np.argmax(x, axis=-1), axis=-1), axis=-1).flatten() # same as np.max(x, axis=-1)
