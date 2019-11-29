bcd950f0a4b6617bc3111a0ffd8102475ad1c3be
mattip <matti.picus@gmail.com>
2019-11-29 14:12:19 +0200

    DOC, TST: refactor CFFI test, add file names to documentation

--- doc/source/reference/random/examples/numba.rst
+++ doc/source/reference/random/examples/numba.rst
@@ -1,5 +1,7 @@
 Extending via Numba
 -------------------
 
+The source of ``numpy/random/_examples/numba/extending.py``
+
 .. literalinclude:: ../../../../../numpy/random/_examples/numba/extending.py
     :language: python
--- numpy/random/_examples/cffi/extending.py
+++ numpy/random/_examples/cffi/extending.py
@@ -1,9 +1,10 @@
 """
-Use cffi to access the underlying C functions from distributions.h
+Use cffi to access any of the underlying C functions from distributions.h
 """
 import os
 import numpy as np
 import cffi
+from .parse import parse_distributions_h
 ffi = cffi.FFI()
 
 inc_dir = os.path.join(np.get_include(), 'numpy')
@@ -15,40 +16,7 @@ ffi.cdef('''
 
 ''')
 
-with open(os.path.join(inc_dir, 'random', 'bitgen.h')) as fid:
-    s = []
-    for line in fid:
-        # massage the include file
-        if line.strip().startswith('#'):
-            continue
-        s.append(line)
-    ffi.cdef('\n'.join(s))
-        
-with open(os.path.join(inc_dir, 'random', 'distributions.h')) as fid:
-    s = []
-    in_skip = 0
-    for line in fid:
-        # massage the include file
-        if line.strip().startswith('#'):
-            continue
-
-        # skip any inlined function definition
-        # which starts with 'static NPY_INLINE xxx(...) {'
-        # and ends with a closing '}'
-        if line.strip().startswith('static NPY_INLINE'):
-            in_skip += line.count('{')
-            continue
-        elif in_skip > 0:
-            in_skip += line.count('{')
-            in_skip -= line.count('}')
-            continue
-
-        # replace defines with their value or remove them
-        line = line.replace('DECLDIR', '')
-        line = line.replace('NPY_INLINE', '')
-        line = line.replace('RAND_INT_TYPE', 'int64_t')
-        s.append(line)
-    ffi.cdef('\n'.join(s))
+parse_distributions_h(ffi, inc_dir)
 
 lib = ffi.dlopen(np.random._generator.__file__)
 
