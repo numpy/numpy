02d6910faf1f222c5ec40f5150cc70aa2d1ac306
Anne Bonner <bonn0062@yahoo.com>
2019-11-28 22:24:46 -0800

    DOC: update and fix matplotlib img/plots, add plots

---
+++ doc/source/user/plots/matplotlib3.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = Axes3D(fig)
+X = np.arange(-5, 5, 0.15)
+Y = np.arange(-5, 5, 0.15)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis')
+
+plt.colorbar()
+
+plt.show()
