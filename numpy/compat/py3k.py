9da75115529b88bd96e6d5eede002ccda09471af
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-29 10:55:39 -0500

    Keep PathLike and Path imports

--- numpy/compat/py3k.py
+++ numpy/compat/py3k.py
@@ -7,12 +7,12 @@ from __future__ import division, absolute_import, print_function
 __all__ = ['bytes', 'asbytes', 'isfileobj', 'getexception', 'strchar',
            'unicode', 'asunicode', 'asbytes_nested', 'asunicode_nested',
            'asstr', 'open_latin1', 'long', 'basestring', 'sixu',
-           'integer_types', 'is_pathlib_path', 'npy_load_module',
+           'integer_types', 'is_pathlib_path', 'npy_load_module', 'PathLike',
            'pickle', 'contextlib_nullcontext', 'os_fspath', 'os_PathLike']
 
 import sys
 import os
-from warnings import warn
+from pathlib import Path, PurePath
 
 if sys.version_info[0] >= 3:
     import io
@@ -101,14 +101,6 @@ def is_pathlib_path(obj):
 
     Prefer using `isinstance(obj, os_PathLike)` instead of this function.
     """
-    from pathlib import Path
-    # 2019-08-25, 1.18
-    warn("The use of is_pathlib_path is deprecated. "
-         "For Python 3.5, you should prefer using "
-         "`isinstance(obj, os_PathLike)`. For Python 3.6 and above consider "
-         "using `isinstance(obj, os.PathLike)`. "
-         "This function will be removed in v1.20 of numpy.",
-         category=DeprecationWarning, stacklevel=2)
     return isinstance(obj, Path)
 
 # from Python 3.7
@@ -205,10 +197,6 @@ if sys.version_info[:2] >= (3, 6):
 else:
     def _PurePath__fspath__(self):
         return str(self)
-
-    # Pathlib isn't exactly a cheap import. Lazy import it here in case
-    # users are running python older than 3.6
-    from pathlib import PurePath
 
     class os_PathLike(abc_ABC):
         """Abstract base class for implementing the file system path protocol."""
