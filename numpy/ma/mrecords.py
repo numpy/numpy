749897caa286ef0d1f75b035c5be5c070f526a71
Eric Wieser <wieser.eric@gmail.com>
2019-12-03 14:01:34 +0000

    BUG: Exceptions tracebacks are dropped
    For some reason this code reconstructed brand new exception objects with
    no traceback, rather than just re-raising

--- numpy/ma/mrecords.py
+++ numpy/ma/mrecords.py
@@ -260,8 +260,7 @@ class MaskedRecords(MaskedArray, object):
             fielddict = ndarray.__getattribute__(self, 'dtype').fields or {}
             optinfo = ndarray.__getattribute__(self, '_optinfo') or {}
             if not (attr in fielddict or attr in optinfo):
-                exctype, value = sys.exc_info()[:2]
-                raise exctype(value)
+                raise
         else:
             # Get the list of names
             fielddict = ndarray.__getattribute__(self, 'dtype').fields or {}
