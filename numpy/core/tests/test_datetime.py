e023ec53b7137cffdf324b35c53c4b1d28e009db
tirthasheshpatel <tirthasheshpatel@gmail.com>
2019-12-07 13:28:28 +0530

    [MRG] FIX: Add support to sort timedelta64 `NaT` to end of the array

--- numpy/core/tests/test_datetime.py
+++ numpy/core/tests/test_datetime.py
@@ -145,11 +145,9 @@ class TestDateTime(object):
         assert_(np.datetime64('NaT') != np.datetime64('NaT', 'us'))
         assert_(np.datetime64('NaT', 'us') != np.datetime64('NaT'))
 
-
-
     @pytest.mark.parametrize("size", [
         3, 21, 217, 1000])
-    def test_nat_argsort_stability(self, size):
+    def test_datetime_nat_argsort_stability(self, size):
         # NaT < NaT should be False internally for
         # sort stability
         expected = np.arange(size)
@@ -172,7 +170,7 @@ class TestDateTime(object):
          np.array([[-220, 51, 'NaT'],
                    [-90, -17, 'NaT']], dtype='M8[us]')),
         ])
-    def test_sort_nat(self, arr, expected):
+    def test_datetime_sort_nat(self, arr, expected):
         # fix for gh-12629; NaT sorting to end of array
         arr.sort()
         assert_equal(arr, expected)
@@ -399,6 +397,38 @@ class TestDateTime(object):
         a = datetime.timedelta()
         assert_raises(TypeError, np.timedelta64, a, 'M')
         assert_raises(TypeError, np.timedelta64, a, 'Y')
+
+    @pytest.mark.parametrize("size", [
+        3, 21, 217, 1000])
+    def test_timedelta64_nat_argsort_stability(self, size):
+        # NaT < NaT should be False internally for
+        # sort stability
+        expected = np.arange(size)
+        arr = np.tile(np.timedelta64('NaT'), size)
+        assert_equal(np.argsort(arr, kind='mergesort'), expected)
+
+    @pytest.mark.parametrize("arr, expected", [
+        # the example provided in gh-12629
+        (np.array(['NaT', 1, 2, 3], dtype='m8[ns]'),
+         np.array([1, 2, 3, 'NaT'], dtype='m8[ns]')),
+        # multiple NaTs
+        (np.array(['NaT', 9, 'NaT', -707], dtype='m8[s]'),
+         np.array([-707, 9, 'NaT', 'NaT'], dtype='m8[s]')),
+        # this sort explores another code path for NaT
+        (np.array([1, -2, 3, 'NaT'], dtype='m8[ns]'),
+         np.array([-2, 1, 3, 'NaT'], dtype='m8[ns]')),
+        # 2-D array
+        (np.array([[51, -220, 'NaT'],
+                   [-17, 'NaT', -90]], dtype='m8[us]'),
+         np.array([[-220, 51, 'NaT'],
+                   [-90, -17, 'NaT']], dtype='m8[us]')),
+        ])
+    def test_datetime_sort_nat(self, arr, expected):
+        # fix for gh-12629; NaT sorting to end of array
+        # a follow up of gh-12658. NaT sorting to the
+        # end of array for `timedelta64` dtype.
+        arr.sort()
+        assert_equal(arr, expected)
 
     def test_timedelta_object_array_conversion(self):
         # Regression test for gh-11096
