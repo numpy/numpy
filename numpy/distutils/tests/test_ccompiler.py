16e9770e2bbbbad3003876545ed2aea96f910531
mattip <matti.picus@gmail.com>
2019-12-30 15:50:34 +0200

    MAINT: document the test, from review

--- numpy/distutils/tests/test_ccompiler.py
+++ numpy/distutils/tests/test_ccompiler.py
@@ -5,9 +5,16 @@ from distutils.ccompiler import new_compiler
 from numpy.distutils.numpy_distribution import NumpyDistribution
 
 def test_ccompiler():
+    '''
+    scikit-image/scikit-image issue 4369
+    We unconditionally add ``-std-c99`` to the gcc compiler in order
+    to support c99 with very old gcc compilers. However the same call
+    is used to get the flags for the c++ compiler, just with a kwarg.
+    Make sure in this case the option is **not** added.
+    '''
     dist = NumpyDistribution()
     compiler = new_compiler()
-    compiler.customize(dist, need_cxx=False)
+    compiler.customize(dist)
     if hasattr(compiler, 'compiler') and 'gcc' in compiler.compiler[0]:
         assert 'c99' in ' '.join(compiler.compiler)
 
