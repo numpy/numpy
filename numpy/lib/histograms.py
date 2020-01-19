dd120d75a8d57345d78890a776b6b4608afb62fb
Kirill Zinovjev <kzinovjev@gmail.com>
2020-01-19 19:13:32 +0000

    MAINT: Fix mistype in histogramdd docstring

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -954,9 +954,9 @@ def histogramdd(sample, bins=10, range=None, normed=None, weights=None,
         Note the unusual interpretation of sample when an array_like:
 
         * When an array, each row is a coordinate in a D-dimensional space -
-          such as ``histogramgramdd(np.array([p1, p2, p3]))``.
+          such as ``histogramdd(np.array([p1, p2, p3]))``.
         * When an array_like, each element is the list of values for single
-          coordinate - such as ``histogramgramdd((X, Y, Z))``.
+          coordinate - such as ``histogramdd((X, Y, Z))``.
 
         The first form should be preferred.
 
