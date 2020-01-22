4fe9ff45abde5d510bfb20701be4f5bd7057f296
MLK97 <maximilianlukaskonrad@hotmail.de>
2020-01-22 12:09:39 +0100

    DOC: Updating Chararry Buffer datatypes #15360

--- numpy/core/defchararray.py
+++ numpy/core/defchararray.py
@@ -1908,7 +1908,7 @@ class chararray(ndarray):
     unicode : bool, optional
         Are the array elements of type unicode (True) or string (False).
         Default is False.
-    buffer : int or str or bytes, optional
+    buffer : object exposing the buffer interface or str, optional
         Memory address of the start of the array data.  Default is None,
         in which case a new array is created.
     offset : int, optional
