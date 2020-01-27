b7cbf64b1640ad59c86797a2b6710f9d847ad028
Eric Wieser <wieser.eric@gmail.com>
2020-01-27 09:51:48 +0000

    BUG: Reject nonsense arguments to scalar constructors
    Fixes #11304

--- numpy/core/tests/test_scalar_ctors.py
+++ numpy/core/tests/test_scalar_ctors.py
@@ -1,6 +1,8 @@
 """
 Test the scalar constructors, which also do type-coercion
 """
+import pytest
+
 import numpy as np
 from numpy.testing import (
     assert_equal, assert_almost_equal, assert_warns,
@@ -35,6 +37,39 @@ class TestFromString:
         assert_equal(fdouble, -np.inf)
         flongdouble = assert_warns(RuntimeWarning, np.longdouble, '-1e10000')
         assert_equal(flongdouble, -np.inf)
+
+
+class TestExtraArgs:
+    def test_superclass(self):
+        # try both positional and keyword arguments
+        s = np.str_(b'\\x61', encoding='unicode-escape')
+        assert s == 'a'
+        s = np.str_(b'\\x61', 'unicode-escape')
+        assert s == 'a'
+
+        # previously this would return '\\xx'
+        with pytest.raises(UnicodeDecodeError):
+            np.str_(b'\\xx', encoding='unicode-escape')
+        with pytest.raises(UnicodeDecodeError):
+            np.str_(b'\\xx', 'unicode-escape')
+
+        # superclass fails, but numpy succeeds
+        assert np.bytes_(-2) == b'-2'
+
+    def test_datetime(self):
+        dt = np.datetime64('2000-01', ('M', 2))
+        assert np.datetime_data(dt) == ('M', 2)
+
+        with pytest.raises(TypeError):
+            np.datetime64('2000', garbage=True)
+
+    def test_bool(self):
+        with pytest.raises(TypeError):
+            np.bool(False, garbage=True)
+
+    def test_void(self):
+        with pytest.raises(TypeError):
+            np.void(b'test', garbage=True)
 
 
 class TestFromInt:
