9bbe90d82506d24a71e6b6001726d798d6643854
Eric Mariasis <ericmariasis829@gmail.com>
2020-02-01 10:12:45 -0500

    Update numpy/core/src/multiarray/descriptor.c
    Co-Authored-By: Eric Wieser <wieser.eric@gmail.com>

--- numpy/core/src/multiarray/descriptor.c
+++ numpy/core/src/multiarray/descriptor.c
@@ -1492,7 +1492,7 @@ _convert_from_any(PyObject *obj, int align)
             return ret;
         }
         Py_DECREF(ret);
-        PyErr_Format(PyExc_TypeError, "Cannot interpret %R as a data type");
+        PyErr_Format(PyExc_TypeError, "Cannot interpret %R as a data type", obj);
         return NULL;
     }
 }
