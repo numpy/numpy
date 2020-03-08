30a4572552add84434fba127b75c22e0abce670b
mattip <matti.picus@gmail.com>
2020-03-08 05:41:41 +0200

    BUG: fix logic error when nm fails on 32-bit

--- numpy/distutils/tests/test_mingw32ccompiler.py
+++ numpy/distutils/tests/test_mingw32ccompiler.py
@@ -19,12 +19,15 @@ def test_build_import():
     except FileNotFoundError:
         pytest.skip("'nm.exe' not on path, is mingw installed?")
     supported = out[out.find(b'supported targets:'):]
-    if sys.maxsize < 2**32 and b'pe-i386' not in supported:
-        raise ValueError("'nm.exe' found but it does not support 32-bit "
-                         "dlls when using 32-bit python")
+    if sys.maxsize < 2**32:
+        if b'pe-i386' not in supported:
+            raise ValueError("'nm.exe' found but it does not support 32-bit "
+                             "dlls when using 32-bit python. Supported "
+                             "formats: '%s'" % supported)
     elif b'pe-x86-64' not in supported:
         raise ValueError("'nm.exe' found but it does not support 64-bit "
-                         "dlls when using 64-bit python")
+                         "dlls when using 64-bit python. Supported "
+                         "formats: '%s'" % supported)
     # Hide the import library to force a build
     has_import_lib, fullpath = mingw32ccompiler._check_for_import_lib()
     if has_import_lib: 
