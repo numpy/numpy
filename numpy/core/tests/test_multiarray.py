54a1ea69673314bceb6bb21742cdecd11a4c8d6c
m-podhajski <36967358+m-podhajski@users.noreply.github.com>
2020-03-23 00:53:55 +0100

    Update numpy/core/tests/test_multiarray.py
    Co-Authored-By: Eric Wieser <wieser.eric@gmail.com>

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -1743,7 +1743,7 @@ class TestMethods:
         setattr(ai, part, 1)
         setattr(bi, part, 1)
         for kind in self.sort_kinds:
-            msg = "complex sort, " + message + " part == 1, kind=%s" % kind
+            msg = "complex sort, %s part == 1, kind=%s" % (kind, part)
             c = ai.copy()
             c.sort(kind=kind)
             assert_equal(c, ai, msg)
