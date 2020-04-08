3bd693b1cb5be2def4771c2e590da462da6d6dd7
dustanlevenstein <43019642+dustanlevenstein@users.noreply.github.com>
2020-04-07 21:36:34 -0500

    extra indentation
    Co-Authored-By: Eric Wieser <wieser.eric@gmail.com>

--- numpy/core/records.py
+++ numpy/core/records.py
@@ -637,7 +637,7 @@ def fromarrays(arrayList, dtype=None, shape=None, formats=None,
 ...         dtype=np.dtype([('a', np.int32), ('b', 'S3'), ('c',np.float32)]))
     >>> r # names is ignored in favor of dtype
     rec.array([(1, b'a', 1.1), (2, b'dd', 2. ), (3, b'xyz', 3. ),
-           (4, b'12', 4. )],
+               (4, b'12', 4. )],
           dtype=[('a', '<i4'), ('b', 'S3'), ('c', '<f4')])
     """
 
