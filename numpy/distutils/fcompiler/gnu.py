42281e9523572e299ab90446f4d83cb50f1149e4
mattip <matti.picus@gmail.com>
2020-04-08 08:58:18 +0300

    ENH: update default MACOSX_DEPLOYMENT_TARGET to 10.9 (from review)

--- numpy/distutils/fcompiler/gnu.py
+++ numpy/distutils/fcompiler/gnu.py
@@ -123,15 +123,14 @@ class GnuFCompiler(FCompiler):
             if not target:
                 # If MACOSX_DEPLOYMENT_TARGET is not set in the environment,
                 # we try to get it first from sysconfig and then
-                # fall back to setting it to 10.7 to maximize the set of
-                # versions we can work with.  This is a reasonable default
+                # fall back to setting it to 10.9 This is a reasonable default
                 # even when using the official Python dist and those derived
                 # from it.
                 import sysconfig
                 target = sysconfig.get_config_var('MACOSX_DEPLOYMENT_TARGET')
                 if not target:
-                    target = '10.7'
-                    s = 'Env. variable MACOSX_DEPLOYMENT_TARGET set to 10.7'
+                    target = '10.9'
+                    s = f'Env. variable MACOSX_DEPLOYMENT_TARGET set to {target}'
                     warnings.warn(s, stacklevel=2)
                 os.environ['MACOSX_DEPLOYMENT_TARGET'] = target
             opt.extend(['-undefined', 'dynamic_lookup', '-bundle'])
