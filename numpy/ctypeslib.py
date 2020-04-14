180a0d3fd0ce5831c4d0dc765b98cc704688e42d
marload <rladhkstn8@gmail.com>
2020-04-14 20:44:03 +0900

    refactoring: early return 'if else' -> 'if'

--- numpy/ctypeslib.py
+++ numpy/ctypeslib.py
@@ -453,10 +453,9 @@ if ctypes is not None:
     def _ctype_from_dtype(dtype):
         if dtype.fields is not None:
             return _ctype_from_dtype_structured(dtype)
-        elif dtype.subdtype is not None:
+        if dtype.subdtype is not None:
             return _ctype_from_dtype_subarray(dtype)
-        else:
-            return _ctype_from_dtype_scalar(dtype)
+        return _ctype_from_dtype_scalar(dtype)
 
 
     def as_ctypes_type(dtype):
