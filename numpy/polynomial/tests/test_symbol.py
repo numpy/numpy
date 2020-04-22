dbd325932146fe0b8e84b0774ff0fce821207d99
Ross Barnowski <rossbar@berkeley.edu>
2020-04-21 18:01:06 -0700

    Added tests for bad symbol input to __init__

--- numpy/polynomial/tests/test_symbol.py
+++ numpy/polynomial/tests/test_symbol.py
@@ -5,13 +5,21 @@ Tests related to the ``symbol`` attribute of the ABCPolyBase class.
 import pytest
 import numpy.polynomial as poly
 from numpy.polynomial._polybase import ABCPolyBase
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_raises
 
 class TestInit:
     """
     Test polynomial creation with symbol kwarg.
     """
-    
+    c = [1, 2, 3]
     def test_default_symbol(self):
-        p = poly.Polynomial([1, 2, 3])
+        p = poly.Polynomial(self.c)
         assert_equal(p.symbol, 'x')
+
+    def test_symbol_None(self):
+        with pytest.raises(TypeError):
+            p = poly.Polynomial(self.c, symbol=None)
+
+    def test_symbol_empty(self):
+        with pytest.raises(ValueError):
+            p = poly.Polynomial(self.c, symbol='')
