7f8e6bc586312040315cbfa15dcd40f89c40813e
Ross Barnowski <rossbar@berkeley.edu>
2020-04-23 11:40:07 -0700

    Test that symbol is kept for creation/manipulation methods

--- numpy/polynomial/tests/test_symbol.py
+++ numpy/polynomial/tests/test_symbol.py
@@ -135,3 +135,39 @@ class TestEquality:
     def test_neq(self):
         other = poly.Polynomial([1, 2, 3], symbol='y')
         assert_(not self.p == other)
+
+
+class TestExtraMethods:
+    """
+    Test other methods for manipulating/creating polynomial objects.
+    """
+    p = poly.Polynomial([1, 2, 3, 0], symbol='z')
+
+    def test_copy(self):
+        other = self.p.copy()
+        assert_equal(other.symbol, 'z')
+
+    def test_trim(self):
+        other = self.p.trim()
+        assert_equal(other.symbol, 'z')
+
+    def test_truncate(self):
+        other = self.p.truncate(2)
+        assert_equal(other.symbol, 'z')
+
+    @pytest.mark.parametrize('kwarg', (
+        {'domain' : [-10, 10]},
+        {'window' : [-10, 10]},
+        {'kind' : poly.Chebyshev},
+    ))
+    def test_convert(self, kwarg):
+        other = self.p.convert(**kwarg)
+        assert_equal(other.symbol, 'z')
+
+    def test_integ(self):
+        other = self.p.integ()
+        assert_equal(other.symbol, 'z')
+
+    def test_deriv(self):
+        other = self.p.deriv()
+        assert_equal(other.symbol, 'z')
