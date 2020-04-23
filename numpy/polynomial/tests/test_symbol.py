49184f26e492245f2cb808c409d2f249cad52638
Ross Barnowski <rossbar@berkeley.edu>
2020-04-23 10:57:52 -0700

    Added tests that math ops fail for poly of diff. symb.

--- numpy/polynomial/tests/test_symbol.py
+++ numpy/polynomial/tests/test_symbol.py
@@ -110,3 +110,16 @@ class TestBinaryOperatorsSameSymbol:
         for out in divmod(rhs, self.p):
             assert_equal(out.symbol, 'z')
 
+
+class TestBinaryOperatorsDifferentSymbol:
+    """
+    Ensure that a ValueError is raised for binary operations between 
+    polynomials of the same kind with different symbols.
+    """
+    p = poly.Polynomial([1, 2, 3], symbol='x')
+    other = poly.Polynomial([4, 5, 6], symbol='y')
+    ops = (p.__add__, p.__sub__, p.__mul__, p.__floordiv__, p.__mod__)
+
+    @pytest.mark.parametrize('f', ops)
+    def test_binops_fails(self, f):
+        assert_raises(ValueError, f, self.other)
