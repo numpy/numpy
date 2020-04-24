6ccc7d32c1f8cd312c517f53263c6bca0c3196fe
Chris Holland <chrisholland3553@gmail.com>
2020-04-24 10:15:18 -0700

    Chained exceptions

--- numpy/core/memmap.py
+++ numpy/core/memmap.py
@@ -209,10 +209,10 @@ class memmap(ndarray):
         import os.path
         try:
             mode = mode_equivalents[mode]
-        except KeyError:
+        except KeyError as e:
             if mode not in valid_filemodes:
                 raise ValueError("mode must be one of %s" %
-                                 (valid_filemodes + list(mode_equivalents.keys())))
+                                 (valid_filemodes + list(mode_equivalents.keys()))) from e
 
         if mode == 'w+' and shape is None:
             raise ValueError("shape must be given")
