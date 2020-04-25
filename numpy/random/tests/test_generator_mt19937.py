c3ac6669cba897c972bff7dcbebf5adcb389c289
Warren Weckesser <warren.weckesser@gmail.com>
2020-04-25 12:28:57 -0400

    BUG: random: Generator.integers(2**32) always returned 0.
    When the input to Generator.integers was 2**32, the value 2**32-1
    was being passed as the `rng` argument to the 32-bit Lemire method,
    but that method requires `rng` be strictly less then 2**32-1.
    
    The fix was to handle 2**32-1 by calling next_uint32 directly.
    This also works for the legacy code without changing the stream
    of random integers from `randint`.
    
    Closes gh-16066.

--- numpy/random/tests/test_generator_mt19937.py
+++ numpy/random/tests/test_generator_mt19937.py
@@ -520,6 +520,21 @@ class TestIntegers:
 
             assert_array_equal(val, val_bc)
 
+    @pytest.mark.parametrize(
+        'bound, expected',
+        [(2**32 - 1, np.array([517043486, 1364798665, 1733884389, 1353720612,
+                               3769704066, 1170797179, 4108474671])),
+         (2**32, np.array([517043487, 1364798666, 1733884390, 1353720613,
+                           3769704067, 1170797180, 4108474672])),
+         (2**32 + 1, np.array([517043487, 1733884390, 3769704068, 4108474673,
+                               1831631863, 1215661561, 3869512430]))]
+    )
+    def test_repeatability_32bit_boundary(self, bound, expected):
+        for size in [None, len(expected)]:
+            random = Generator(MT19937(1234))
+            x = random.integers(bound, size=size)
+            assert_equal(x, expected if size is not None else expected[0])
+
     def test_int64_uint64_broadcast_exceptions(self, endpoint):
         configs = {np.uint64: ((0, 2**65), (-1, 2**62), (10, 9), (0, 0)),
                    np.int64: ((0, 2**64), (-(2**64), 2**62), (10, 9), (0, 0),
