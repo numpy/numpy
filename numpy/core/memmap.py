741c232c2da580ce5316a64a5684f45a62723fcd
Chris Holland <41524756+ChrisAHolland@users.noreply.github.com>
2020-04-25 15:29:14 -0700

    shorten lines

--- numpy/core/memmap.py
+++ numpy/core/memmap.py
@@ -211,8 +211,10 @@ class memmap(ndarray):
             mode = mode_equivalents[mode]
         except KeyError as e:
             if mode not in valid_filemodes:
-                raise ValueError("mode must be one of %s (got %r)" %
-                                 (valid_filemodes + list(mode_equivalents.keys())), mode) from None
+                raise ValueError(
+                    "mode must be one of %s (got %r)" %
+                    (valid_filemodes + list(mode_equivalents.keys())), mode
+                ) from None
 
         if mode == 'w+' and shape is None:
             raise ValueError("shape must be given")
