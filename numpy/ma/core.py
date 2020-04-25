901514787faa3412593d1b71f7b3f604feeb23a2
RenaRuirui <renaxu@umich.edu>
2020-04-24 23:17:12 -0400

    Revert "Update core.py, replace assert with if statement."
    This reverts commit b3098821a9da2f82379f891c1a8c5b0a775efed3.

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -3305,10 +3305,8 @@ class MaskedArray(ndarray):
                     # which should have shape () to avoid breaking several
                     # methods. There is no great way out, so set to
                     # first element.  See issue #6723.
-                    # something like gh-15895 has happened if this check fails.
-                    # _fill_value should always be an ndarray.
-                    if not isinstance(dout._fill_value, np.ndarray):
-                        raise RuntimeError('Internal NumPy error.')
+                    # something like gh-15895 has happened if this fails
+                    assert isinstance(dout._fill_value, np.ndarray)
                     if dout._fill_value.ndim > 0:
                         if not (dout._fill_value ==
                                 dout._fill_value.flat[0]).all():
