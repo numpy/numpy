d019c9f8aaf4eff79cf5748253d8ff8e92b70c56
Flaviu Gostin <pfg@posteo.org>
2020-04-27 12:35:46 +0100

    DOC: add note on type casting to numpy.left_shift(). (gh-12310)
    Intuitively, one expects the dtype of the first argument in
    np.left_shift() to be preserved and assigned to the output.  However,
    this may be changed by the dtype of the second argument and lead to
    confusing results.  Added note to draw attention to this.  Added three
    examples.  Closes gh-12310.

--- numpy/core/code_generators/ufunc_docstrings.py
+++ numpy/core/code_generators/ufunc_docstrings.py
@@ -1835,10 +1835,9 @@ add_newdoc('numpy.core.umath', 'left_shift',
     >>> np.left_shift(5, [1,2,3])
     array([10, 20, 40])
 
-    Note: As explained in :ref:`Casting Rules <ufuncs.casting>`, if a ufunc
-    does not have a core loop implementation for the input types provided,
-    it will cast one or more inputs to a different type.  This may lead to
-    an unexpected result in some cases.
+    Note: The dtype of the second argument may change the dtype of the first
+    argument (see :ref:`Casting Rules <ufuncs.casting>`) and lead to unexpected
+    results in some cases.
 
     >>> np.left_shift(np.array([255], dtype='u1'), np.array([1], dtype='u1'))
     array([254], dtype=uint8)
