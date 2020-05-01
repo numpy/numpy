d7cec65f6a37a52463efa2b348f2b57094ff844b
MatteoRaso <mraso@uoguelph.ca>
2020-04-30 23:02:26 -0400

    Improved one of the error messages for histogramdd.py as outlined in issue #15984

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -1047,7 +1047,12 @@ def histogramdd(sample, bins=10, range=None, normed=None, weights=None,
                 raise ValueError(
                     '`bins[{}]` must be positive, when an integer'.format(i))
             smin, smax = _get_outer_edges(sample[:,i], range[i])
-            edges[i] = np.linspace(smin, smax, bins[i] + 1)
+            try:
+                edges[i] = np.linspace(smin, smax, bins[i] + 1)
+            except TypeError:
+                bins[i] = int(bins[i])
+                edges[i] = np.linspace(smin, smax, bins[i] + 1)
+                
         elif np.ndim(bins[i]) == 1:
             edges[i] = np.asarray(bins[i])
             if np.any(edges[i][:-1] > edges[i][1:]):
