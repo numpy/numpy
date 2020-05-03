f92608a6c1a8bfe1e1ee805913ee282f551e9aca
MatteoRaso <33975162+MatteoRaso@users.noreply.github.com>
2020-05-03 13:09:29 -0400

    Made the suggested changes

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -1051,9 +1051,9 @@ def histogramdd(sample, bins=10, range=None, normed=None, weights=None,
                 n = operator.index(bins[i])
             
             except TypeError as e:
-                raise TypeError("bins[i] must be an integer")
+                raise TypeError("bins[i] must be an integer") from e
                 
-            edges[i] = np.linspace(smin, smax, bins[i] + 1)    
+            edges[i] = np.linspace(smin, smax, n + 1)    
         elif np.ndim(bins[i]) == 1:
             edges[i] = np.asarray(bins[i])
             if np.any(edges[i][:-1] > edges[i][1:]):
