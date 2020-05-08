bef1b0048fd21d97968250f45f2c834db0a6cb38
Tina Oberoi <55803680+tinaoberoi@users.noreply.github.com>
2020-05-08 15:51:54 +0530

    Update numpy/core/_add_newdocs.py
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- numpy/core/_add_newdocs.py
+++ numpy/core/_add_newdocs.py
@@ -5043,7 +5043,7 @@ add_newdoc('numpy.core', 'ufunc', ('outer',
 
     See Also
     --------
-    numpy.outer : ``np.outer`` only exists at this point for compatibility with old code.
+    numpy.outer : A less powerful version of ``np.multiply.outer`` that `ravel`\ s all inputs to 1D. This exists primarily for compatibility with old code.
 
     tensordot : ``np.tensordot(a, b, axes=((), ()))`` and ``np.multiply.outer(a, b)`` behave same for all dimensions of a and b.
 
