845b1ba913e32de07196858cd0ae7f479c222798
Doja Frat <dojafrat@gmail.com>
2020-05-10 16:35:24 -0500

    DOC: add documentation to numpy.core.records
    Respond to review comments
    See #15853

--- numpy/core/records.py
+++ numpy/core/records.py
@@ -777,14 +777,14 @@ def fromstring(datastring, dtype=None, shape=None, offset=0, formats=None,
 
     Parameters
     ----------
-    datastring : str
+    datastring : bytes-like
         Binary data contained in a string
     dtype : data-type, optional
         Valid dtype for all arrays
     shape : int or tuple of ints, optional
         Shape of each array.
     offset : int, optional
-        Position in the file to start reading from.
+        Position in the buffer to start reading from.
     formats, names, titles, aligned, byteorder :
         If `dtype` is ``None``, these arguments are passed to
         `numpy.format_parser` to construct a dtype. See that function for
@@ -794,18 +794,26 @@ def fromstring(datastring, dtype=None, shape=None, offset=0, formats=None,
     Returns
     -------
     np.recarray
-        record array consisting of data in datastring.
+        Read-only record array consisting of data in datastring.
 
     Examples
     --------
-    >>> a = np.empty(10,dtype='f8,i4,a5')
-    >>> a[5] = (0.5,10,'abcde')
-    >>> b=np.core.records.fromstring(a.tostring(), formats='f8,i4,a5', shape=10,
-    ... byteorder='<')
-    >>> print(b[5])
-    (0.5, 10, 'abcde')
-    >>> b.shape
-    (10,)
+    >>> a = b'\x01\x02\x03abc'
+    >>> np.core.records.fromstring(a,dtype='u1,u1,u1,S3')
+    rec.array([(1, 2, 3, b'abc')],
+              dtype=[('f0', 'u1'), ('f1', 'u1'), ('f2', 'u1'), ('f3', 'S3')])
+
+    >>> grades_dtype = [('Name', (np.str_, 10)), ('Marks', np.float64), ('GradeLevel', np.int32)]
+    >>> grades_array = np.array([('Sam', 33.3, 3), ('Mike', 44.4, 5), ('Aadi', 66.6, 6)], dtype=grades_dtype)
+    >>> np.core.records.fromstring(grades_array.tobytes(), dtype=dtype)
+    rec.array([('Sam', 33.3, 3), ('Mike', 44.4, 5), ('Aadi', 66.6, 6)],
+          dtype=[('Name', '<U10'), ('Marks', '<f8'), ('GradeLevel', '<i4')])
+
+    >>> s = '\x01\x02\x03abc'
+    >>> np.core.records.fromstring(s, dtype='u1,u1,u1,S3')
+    Traceback (most recent call last)
+       ...
+    TypeError: a bytes-like object is required, not 'str'
     """
 
     if dtype is None and formats is None:
