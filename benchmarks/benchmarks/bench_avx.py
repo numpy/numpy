73649c17fe9b046646a4b275217a4cd943573be0
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2020-05-22 16:02:28 -0700

    ENH: Use AVX-512 for float and double np.ldexp

--- benchmarks/benchmarks/bench_avx.py
+++ benchmarks/benchmarks/bench_avx.py
@@ -60,6 +60,22 @@ class AVX_BFunc(Benchmark):
     def time_ufunc(self, ufuncname, dtype, stride):
         self.f(self.arr1[::stride], self.arr2[::stride])
 
+class AVX_ldexp(Benchmark):
+
+    params = [dtype, stride]
+    param_names = ['dtype', 'stride']
+    timeout = 10
+
+    def setup(self, dtype, stride):
+        np.seterr(all='ignore')
+        self.f = getattr(np, 'ldexp')
+        N = 10000
+        self.arr1 = np.array(np.random.rand(stride*N), dtype=dtype)
+        self.arr2 = np.array(np.random.rand(stride*N), dtype='i')
+
+    def time_ufunc(self, ufuncname, dtype, stride):
+        self.f(self.arr1[::stride], self.arr2[::stride])
+
 cmplx_bfuncs = ['add',
                 'subtract',
                 'multiply',
