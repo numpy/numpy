c11ee6f52105ec653296088856a8f358177189f1
DWesl <22566757+DWesl@users.noreply.github.com>
2020-05-25 13:59:17 -0400

    Remove reference to np.complex256 in the other test
    I changed this for the one test, but forgot to do so for the other.  Hopefully this actually fixes the problem.

--- numpy/core/tests/test_scalarmath.py
+++ numpy/core/tests/test_scalarmath.py
@@ -699,7 +699,7 @@ class TestAbs:
                         raises=RuntimeWarning,
                         strict=True,
                     )
-                    if dtype == np.complex256 else ()
+                    if dtype().dtype.str.endswith("c32") else ()
                 )
             )
             for dtype in floating_types + complex_floating_types
