ea257d82d2072c9a5fb6d84af1b8c4efd005b836
Ben Nathanson <github@bigriver.xyz>
2020-05-29 15:16:41 -0400

    DOC: Fix description of dtype default in linspace
    Fixes gh-8597

--- numpy/core/function_base.py
+++ numpy/core/function_base.py
@@ -52,8 +52,9 @@ def linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None,
         If True, return (`samples`, `step`), where `step` is the spacing
         between samples.
     dtype : dtype, optional
-        The type of the output array.  If `dtype` is not given, infer the data
-        type from the other input arguments.
+        The type of the output array.  If `dtype` is not given, the data type
+        is inferred from `start`, `stop`, and `num`, but it will never be smaller
+        than float.
 
         .. versionadded:: 1.9.0
 
