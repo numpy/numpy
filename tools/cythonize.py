66975565484c4390cfaa6302b4f459c775753a14
Charles Harris <charlesr.harris@gmail.com>
2020-05-31 19:18:40 -0600

    MAINT: Update required Cython version.
    Increase from 0.29.14 to 0.29.19. It is not clear that the most
    recent version is needed, but it cannot hurt.

--- tools/cythonize.py
+++ tools/cythonize.py
@@ -70,7 +70,7 @@ def process_pyx(fromfile, tofile):
         # other fixes in the 0.29 series that are needed even for earlier
         # Python versions.
         # Note: keep in sync with that in pyproject.toml
-        required_version = LooseVersion('0.29.14')
+        required_version = LooseVersion('0.29.19')
 
         if LooseVersion(cython_version) < required_version:
             raise RuntimeError(f'Building {VENDOR} requires Cython >= {required_version}')
