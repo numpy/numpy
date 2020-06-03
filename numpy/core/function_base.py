5487847a0b6c6e06072fae3292cc469f6f683ff9
Ben Nathanson <github@bigriver.xyz>
2020-06-03 06:30:10 -0400

    DOC: Drop 'num' from inference rules in PR #16433
    Per https://github.com/numpy/numpy/pull/16433#discussion_r434295779,
    now says the inferred dtype depends only on 'start' and 'stop'.

--- numpy/core/function_base.py
+++ numpy/core/function_base.py
@@ -53,9 +53,9 @@ def linspace(start, stop, num=50, endpoint=True, retstep=False, dtype=None,
         between samples.
     dtype : dtype, optional
         The type of the output array.  If `dtype` is not given, the data type
-        is inferred from `start`, `stop`, and `num`. The inferred dtype will
-        never be an integer; `float` is chosen even if all three arguments are
-        integers.
+        is inferred from `start` and `stop`. The inferred dtype will
+        never be an integer; `float` is chosen even if the arguments would
+        produce an array of integers.
 
         .. versionadded:: 1.9.0
 
