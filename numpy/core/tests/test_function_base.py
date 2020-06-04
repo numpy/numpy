8cdbe30377c806cef30b0c5c9d54370e8deed4b7
t301285 <kaspar.thommen@ubs.com>
2020-06-04 12:21:53 +0200

    only keep code in the 'with' clause that actually needs it

--- numpy/core/tests/test_function_base.py
+++ numpy/core/tests/test_function_base.py
@@ -136,14 +136,16 @@ class TestGeomspace:
     def test_nan_interior(self):
         with errstate(invalid='ignore'):
             y = geomspace(-3, 3, num=4)
-            assert_equal(y[0], -3.0)
-            assert_(isnan(y[1:-1]).all())
-            assert_equal(y[3], 3.0)
+
+        assert_equal(y[0], -3.0)
+        assert_(isnan(y[1:-1]).all())
+        assert_equal(y[3], 3.0)
 
         with errstate(invalid='ignore'):
             y = geomspace(-3, 3, num=4, endpoint=False)
-            assert_equal(y[0], -3.0)
-            assert_(isnan(y[1:]).all())
+
+        assert_equal(y[0], -3.0)
+        assert_(isnan(y[1:]).all())
 
     def test_complex(self):
         # Purely imaginary
