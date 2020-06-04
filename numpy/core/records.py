489de42a9585615ca9962a83882896069357ab97
Zuhair Ali-Khan <zaak7179@gmail.com>
2020-06-04 00:28:58 -0500

    MAINT: Chain some exceptions. (#16418)
    * ENH: Chain extensions in numpy and numpy/core
    
    Co-authored-by: Ross Barnowski <rossbar@berkeley.edu>
    Co-authored-by: Zuhair Ali-Khan <54608785+zalikh2@users.noreply.github.com>

--- numpy/core/records.py
+++ numpy/core/records.py
@@ -461,8 +461,8 @@ class recarray(ndarray):
         fielddict = ndarray.__getattribute__(self, 'dtype').fields
         try:
             res = fielddict[attr][:2]
-        except (TypeError, KeyError):
-            raise AttributeError("recarray has no attribute %s" % attr)
+        except (TypeError, KeyError) as e:
+            raise AttributeError("recarray has no attribute %s" % attr) from e
         obj = self.getfield(*res)
 
         # At this point obj will always be a recarray, since (see
@@ -509,8 +509,10 @@ class recarray(ndarray):
                     return ret
         try:
             res = fielddict[attr][:2]
-        except (TypeError, KeyError):
-            raise AttributeError("record array has no attribute %s" % attr)
+        except (TypeError, KeyError) as e:
+            raise AttributeError(
+                "record array has no attribute %s" % attr
+            ) from e
         return self.setfield(val, *res)
 
     def __getitem__(self, indx):
