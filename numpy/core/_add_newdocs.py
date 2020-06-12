8190116aa2fa58aba628b0a5cea768017acf82bf
Josh Wilson <person142@users.noreply.github.com>
2020-06-11 20:59:35 -0700

    DOC: correct name of first dtype constructor parameter

--- numpy/core/_add_newdocs.py
+++ numpy/core/_add_newdocs.py
@@ -5142,7 +5142,7 @@ add_newdoc('numpy.core', 'ufunc', ('at',
 
 add_newdoc('numpy.core.multiarray', 'dtype',
     """
-    dtype(obj, align=False, copy=False)
+    dtype(dtype, align=False, copy=False)
 
     Create a data type object.
 
@@ -5152,7 +5152,7 @@ add_newdoc('numpy.core.multiarray', 'dtype',
 
     Parameters
     ----------
-    obj
+    dtype
         Object to be converted to a data type object.
     align : bool, optional
         Add padding to the fields to match what a C compiler would output
