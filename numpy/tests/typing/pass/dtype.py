53e84ab4999deb1be1d83b775a3385904ccae832
Bas van Beek <b.f.van.beek@vu.nl>
2020-06-16 21:49:33 +0200

    Ran black

--- numpy/tests/typing/pass/dtype.py
+++ numpy/tests/typing/pass/dtype.py
@@ -2,7 +2,7 @@ import numpy as np
 
 np.dtype(dtype=np.int64)
 np.dtype(int)
-np.dtype('int')
+np.dtype("int")
 np.dtype(None)
 
 np.dtype((int, 1))
@@ -10,13 +10,17 @@ np.dtype((int, (1,)))
 np.dtype([(int, 1)])
 np.dtype([(int, (1,))])
 
-np.dtype({'names': ['a', 'b'], 'formats': [int, float]})
-np.dtype({'names': ['a', 'b'],
-          'formats': [int, float],
-          'itemsize': 9,
-          'aligned': False,
-          'titles': ['x', 'y'],
-          'offsets': [0, 1]})
+np.dtype({"names": ["a", "b"], "formats": [int, float]})
+np.dtype(
+    {
+        "names": ["a", "b"],
+        "formats": [int, float],
+        "itemsize": 9,
+        "aligned": False,
+        "titles": ["x", "y"],
+        "offsets": [0, 1],
+    }
+)
 
-np.dtype({'field1': (float, 1), 'field2': (int, 3)})
+np.dtype({"field1": (float, 1), "field2": (int, 3)})
 np.dtype((np.float_, float))
