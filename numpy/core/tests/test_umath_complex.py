959071af8571ba833f81c844cff36116ac59ef30
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2020-06-22 14:51:01 -0700

    TST: Add test to validate complex operations on mixed dtype arrays

--- numpy/core/tests/test_umath_complex.py
+++ numpy/core/tests/test_umath_complex.py
@@ -577,3 +577,34 @@ class TestComplexAbsoluteAVX(object):
         arr = np.ones(arraysize, dtype=astype)
         abs_true = np.ones(arraysize, dtype=arr.real.dtype)
         assert_equal(np.abs(arr[::stride]), abs_true[::stride])
+
+# Testcase taken as is from https://github.com/numpy/numpy/issues/16660
+class TestComplexAbsoluteMixedDTypes(object):
+    @pytest.mark.parametrize("stride", [-4,-3,-2,-1,1,2,3,4])
+    @pytest.mark.parametrize("astype", [np.complex64, np.complex128])
+    @pytest.mark.parametrize("func", ['abs', 'square', 'conjugate'])
+    
+    def test_array(self, stride, astype, func):
+        dtype = [('template_id', '<i8'), ('bank_chisq','<f4'),
+                 ('bank_chisq_dof','<i8'), ('chisq', '<f4'), ('chisq_dof','<i8'),
+                 ('cont_chisq', '<f4'), ('psd_var_val', '<f4'), ('sg_chisq','<f4'),
+                 ('mycomplex', astype), ('time_index', '<i8')]
+        vec = np.array([
+               (0, 0., 0, -31.666483, 200, 0., 0.,  1.      ,  3.0+4.0j   ,  613090),
+               (1, 0., 0, 260.91525 ,  42, 0., 0.,  1.      ,  5.0+12.0j  ,  787315),
+               (1, 0., 0,  52.15155 ,  42, 0., 0.,  1.      ,  8.0+15.0j  ,  806641),
+               (1, 0., 0,  52.430195,  42, 0., 0.,  1.      ,  7.0+24.0j  , 1363540),
+               (2, 0., 0, 304.43646 ,  58, 0., 0.,  1.      ,  20.0+21.0j ,  787323),
+               (3, 0., 0, 299.42108 ,  52, 0., 0.,  1.      ,  12.0+35.0j ,  787332),
+               (4, 0., 0,  39.4836  ,  28, 0., 0.,  9.182192,  9.0+40.0j  ,  787304),
+               (4, 0., 0,  76.83787 ,  28, 0., 0.,  1.      ,  28.0+45.0j, 1321869),
+               (5, 0., 0, 143.26366 ,  24, 0., 0., 10.996129,  11.0+60.0j ,  787299)], dtype=dtype)
+        myfunc = getattr(np, func)
+        a = vec['mycomplex']
+        g = myfunc(a[::stride])
+        
+        b = vec['mycomplex'].copy()
+        h = myfunc(b[::stride])
+        
+        assert_array_max_ulp(h.real, g.real, 1)
+        assert_array_max_ulp(h.imag, g.imag, 1)
