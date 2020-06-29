107c02951d400e69df92d2d5c5d96ef2964aa315
sidhant007 <sidhbansal@gmail.com>
2020-06-29 10:24:07 +0800

    Refactor: Remove isfilelikeobj function

--- numpy/compat/tests/test_compat.py
+++ numpy/compat/tests/test_compat.py
@@ -1,11 +1,9 @@
 from os.path import join
 
-from numpy.compat import isfileobj, isfilelikeobj
+from numpy.compat import isfileobj
 from numpy.testing import assert_
 from numpy.testing import tempdir
 
-import io
-import gzip
 
 def test_isfileobj():
     with tempdir(prefix="numpy_test_compat_") as folder:
@@ -19,17 +17,3 @@ def test_isfileobj():
 
         with open(filename, 'rb') as f:
             assert_(isfileobj(f))
-
-def test_isfilelikeobj():
-    with tempdir(prefix="numpy_test_compat_") as folder:
-        filename = join(folder, 'a.bin')
-
-        with open(filename, 'wb') as f:
-            assert_(isfilelikeobj(f))
-
-        with gzip.open(filename, 'rb') as f:
-            assert_(isfilelikeobj(f))
-
-        assert_(isfilelikeobj(io.BytesIO()))
-
-        assert_(not isfilelikeobj(io.StringIO()))
--- numpy/core/records.py
+++ numpy/core/records.py
@@ -40,7 +40,7 @@ from collections import Counter, OrderedDict
 from . import numeric as sb
 from . import numerictypes as nt
 from numpy.compat import (
-    isfilelikeobj, os_fspath, contextlib_nullcontext
+    os_fspath, contextlib_nullcontext
 )
 from numpy.core.overrides import set_module
 from .arrayprint import get_printoptions
@@ -848,10 +848,11 @@ def fromstring(datastring, dtype=None, shape=None, offset=0, formats=None,
 
 def get_remaining_size(fd):
     pos = fd.tell()
-    fd.seek(0, 2)
-    size = fd.tell() - pos
-    fd.seek(pos, 0)
-    return size
+    try:
+        fd.seek(0, 2)
+        return fd.tell() - pos
+    finally:
+        fd.seek(pos, 0)
 
 def fromfile(fd, dtype=None, shape=None, offset=0, formats=None,
              names=None, titles=None, aligned=False, byteorder=None):
@@ -909,7 +910,7 @@ def fromfile(fd, dtype=None, shape=None, offset=0, formats=None,
     elif isinstance(shape, int):
         shape = (shape,)
 
-    if isfilelikeobj(fd):
+    if hasattr(fd, 'readinto'):
         # file already opened
         ctx = contextlib_nullcontext(fd)
     else:
@@ -1034,7 +1035,7 @@ def array(obj, dtype=None, shape=None, offset=0, strides=None, formats=None,
     array('def', dtype='<U3')
     """
 
-    if ((isinstance(obj, (type(None), str)) or isfilelikeobj(obj)) and
+    if ((isinstance(obj, (type(None), str)) or hasattr(obj, 'readinto')) and
            formats is None and dtype is None):
         raise ValueError("Must define formats (or dtype) if object is "
                          "None, string, or an open file")
@@ -1076,7 +1077,7 @@ def array(obj, dtype=None, shape=None, offset=0, strides=None, formats=None,
             new = new.copy()
         return new
 
-    elif isfilelikeobj(obj):
+    elif hasattr(obj, 'readinto'):
         return fromfile(obj, dtype=dtype, shape=shape, offset=offset)
 
     elif isinstance(obj, ndarray):
