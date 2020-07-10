e97d91325567cacc64fe5112a4b933b425f4aee8
Charles Harris <charlesr.harris@gmail.com>
2020-07-10 17:32:33 -0600

    MAINT: Move typing tests
    Move them into a new `numpy/typing/tests directory`

--- numpy/typing/setup.py
+++ numpy/typing/setup.py
@@ -1,7 +1,8 @@
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('tests', parent_package, top_path)
-    config.add_data_dir('typing')
+    config = Configuration('typing', parent_package, top_path)
+    config.add_subpackage('tests')
+    config.add_data_dir('tests/data')
     return config
 
 
