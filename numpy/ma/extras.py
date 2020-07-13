8a38084a12544505918b5f8afada73e8f15ffb79
marload <rladhkstn8@gmail.com>
2020-07-14 01:45:05 +0900

    MAINT: While to For loop

--- numpy/ma/extras.py
+++ numpy/ma/extras.py
@@ -361,11 +361,9 @@ diagflat = _fromnxfunction_single('diagflat')
 #####--------------------------------------------------------------------------
 def flatten_inplace(seq):
     """Flatten a sequence in place."""
-    k = 0
-    while (k != len(seq)):
+    for k in range(len(seq)):
         while hasattr(seq[k], '__iter__'):
             seq[k:(k + 1)] = seq[k]
-        k += 1
     return seq
 
 
