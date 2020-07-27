6e7af3fb2ddced1f05f4eea4ebbee2a97025dcdc
Noman Arshad <noman.arshad@northbaysolutions.com>
2020-07-27 14:49:31 +0500

    update numpy/lib/arraypad.py with appropriate chain exception

--- numpy/lib/arraypad.py
+++ numpy/lib/arraypad.py
@@ -779,11 +779,11 @@ def pad(array, pad_width, mode='constant', **kwargs):
     }
     try:
         unsupported_kwargs = set(kwargs) - set(allowed_kwargs[mode])
-    except KeyError:
-        raise ValueError("mode '{}' is not supported".format(mode))
+    except KeyError as e:
+        raise ValueError("mode '{}' is not supported".format(mode)) from e
     if unsupported_kwargs:
         raise ValueError("unsupported keyword arguments for mode '{}': {}"
-                         .format(mode, unsupported_kwargs))
+                         .format(mode, unsupported_kwargs)) from None
 
     stat_functions = {"maximum": np.amax, "minimum": np.amin,
                       "mean": np.mean, "median": np.median}
