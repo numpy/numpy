d646001de26d70bf7e1854b847f013b94bc20950
Noman Arshad <noman.arshad@northbaysolutions.com>
2020-07-27 15:31:31 +0500

    update numpy/lib/arraypad.py with required change

--- numpy/lib/arraypad.py
+++ numpy/lib/arraypad.py
@@ -779,7 +779,7 @@ def pad(array, pad_width, mode='constant', **kwargs):
     }
     try:
         unsupported_kwargs = set(kwargs) - set(allowed_kwargs[mode])
-    except KeyError as e:
+    except KeyError:
         raise ValueError("mode '{}' is not supported".format(mode)) from None
     if unsupported_kwargs:
         raise ValueError("unsupported keyword arguments for mode '{}': {}"
