e91e31eab3f6a4af1498635c6c80d4ef4263e1d9
Bas van Beek <b.f.van.beek@vu.nl>
2020-08-19 15:45:17 +0200

    REV: Removed two more (accidently included) builtin modules

--- numpy/tests/typing/reveal/modules.py
+++ numpy/tests/typing/reveal/modules.py
@@ -13,13 +13,11 @@ reveal_type(np.linalg)  # E: ModuleType
 reveal_type(np.ma)  # E: ModuleType
 reveal_type(np.math)  # E: ModuleType
 reveal_type(np.matrixlib)  # E: ModuleType
-reveal_type(np.os)  # E: ModuleType
 reveal_type(np.polynomial)  # E: ModuleType
 reveal_type(np.random)  # E: ModuleType
 reveal_type(np.rec)  # E: ModuleType
 reveal_type(np.testing)  # E: ModuleType
 reveal_type(np.version)  # E: ModuleType
-reveal_type(np.warnings)  # E: ModuleType
 
 # TODO: Remove when annotations have been added to `np.linalg.norm`
 reveal_type(np.linalg.norm)  # E: Any
