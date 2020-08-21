be813b83b2a03f887be5268615e10b7afb3fe9a6
Raghav Khanna <37894186+raghavkhanna18@users.noreply.github.com>
2020-08-21 23:07:24 +0800

    MAINT: chain axis error line 281 arraysetops.py to be from None

--- numpy/lib/arraysetops.py
+++ numpy/lib/arraysetops.py
@@ -278,7 +278,7 @@ def unique(ar, return_index=False, return_inverse=False,
         ar = np.moveaxis(ar, axis, 0)
     except np.AxisError:
         # this removes the "axis1" or "axis2" prefix from the error message
-        raise np.AxisError(axis, ar.ndim)
+        raise np.AxisError(axis, ar.ndim) from None
 
     # Must reshape to a contiguous 2D array for this to work...
     orig_shape, orig_dtype = ar.shape, ar.dtype
