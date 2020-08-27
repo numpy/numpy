954e642a82d6138d22aead5d0ae608b8bd77cd48
Qiyu8 <fangchunlin@huawei.com>
2020-08-27 10:19:57 +0800

    revert to faster simd code

--- numpy/core/src/multiarray/einsum_sumprod.h
+++ numpy/core/src/multiarray/einsum_sumprod.h
@@ -1,10 +1,8 @@
 #ifndef _NPY_MULTIARRAY_EINSUM_SUMPROD_H
 #define _NPY_MULTIARRAY_EINSUM_SUMPROD_H
 
-#include "simd/simd.h"
-#include "common.h"
-
-#define EINSUM_IS_ALIGNED(x) npy_is_aligned(x, NPY_SIMD_WIDTH)
+#include "npy_cpu_dispatch.h"
+#include <numpy/npy_common.h>
 
 #ifndef NPY_DISABLE_OPTIMIZATION
     #include "einsum.dispatch.h"
@@ -12,7 +10,7 @@
 
 typedef void (*sum_of_products_fn)(int, char **, npy_intp const*, npy_intp);
 
-NPY_CPU_DISPATCH_DECLARE(NPY_NO_EXPORT sum_of_products_fn einsum_get_sum_of_products_function,
+NPY_CPU_DISPATCH_DECLARE(NPY_VISIBILITY_HIDDEN sum_of_products_fn einsum_get_sum_of_products_function,
     (int nop, int type_num, npy_intp itemsize, npy_intp const *fixed_strides))
 
 #endif
