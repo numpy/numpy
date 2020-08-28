3893b93b1bf38caf6ea6b509b142b8544f89141e
Bas van Beek <b.f.van.beek@vu.nl>
2020-08-28 16:08:32 +0200

    TST: Added an `__array__` test

--- numpy/tests/typing/fail/flatiter.py
+++ numpy/tests/typing/fail/flatiter.py
@@ -1,5 +1,8 @@
 from typing import Any
+
 import numpy as np
+from numpy.typing import DtypeLike, _SupportsArray
+
 
 class Index:
     def __index__(self) -> int:
@@ -7,6 +10,7 @@ class Index:
 
 
 a: "np.flatiter[np.ndarray]"
+supports_array: _SupportsArray
 
 a.base = Any  # E: Property "base" defined in "flatiter" is read-only
 a.copy = Any  # E: Property "copy" defined in "flatiter" is read-only
@@ -14,3 +18,4 @@ a.coords = Any  # E: Property "coords" defined in "flatiter" is read-only
 a.index = Any  # E: Property "index" defined in "flatiter" is read-only
 a[np.bool_()]  # E: No overload variant of "__getitem__"
 a[Index()]  # E: No overload variant of "__getitem__"
+a[supports_array]  # E: No overload variant of "__getitem__"
