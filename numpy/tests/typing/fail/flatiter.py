b6bf3a177e5f6337502ba5fe492a601abc3a3edb
Bas van Beek <b.f.van.beek@vu.nl>
2020-08-28 16:12:47 +0200

    TST,MAINT: Fixed a few tests

--- numpy/tests/typing/fail/flatiter.py
+++ numpy/tests/typing/fail/flatiter.py
@@ -13,9 +13,9 @@ a: "np.flatiter[np.ndarray]"
 supports_array: _SupportsArray
 
 a.base = Any  # E: Property "base" defined in "flatiter" is read-only
-a.copy = Any  # E: Property "copy" defined in "flatiter" is read-only
 a.coords = Any  # E: Property "coords" defined in "flatiter" is read-only
 a.index = Any  # E: Property "index" defined in "flatiter" is read-only
+a.copy(order='C')  # E: Unexpected keyword argument
 a[np.bool_()]  # E: No overload variant of "__getitem__"
 a[Index()]  # E: No overload variant of "__getitem__"
 a[supports_array]  # E: No overload variant of "__getitem__"
--- numpy/tests/typing/reveal/flatiter.py
+++ numpy/tests/typing/reveal/flatiter.py
@@ -4,9 +4,9 @@ a: "np.flatiter[np.ndarray]"
 
 reveal_type(a.base)  # E: numpy.ndarray*
 reveal_type(a.copy)  # E: numpy.ndarray*
-reveal_type(a.coords)  # E: tuple[bultins.int]
+reveal_type(a.coords)  # E: tuple[builtins.int]
 reveal_type(a.index)  # E: int
-reveal_type(iter(a))  # E: typing.Iterator[numpy.generic]
+reveal_type(iter(a))  # E: Iterator[numpy.generic*]
 reveal_type(next(a))  # E: numpy.generic
 reveal_type(a[0])  # E: numpy.generic
 reveal_type(a[[0, 1, 2]])  # E: numpy.ndarray*
