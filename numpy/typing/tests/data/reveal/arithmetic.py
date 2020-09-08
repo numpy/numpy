73104bfad5f0a55bf66609cefdc71a1356d86238
Bas van Beek <b.f.van.beek@vu.nl>
2020-09-08 14:04:20 +0200

    MAINT: Arithmetic operations on 0d arrays return scalars

--- numpy/typing/tests/data/reveal/arithmetic.py
+++ numpy/typing/tests/data/reveal/arithmetic.py
@@ -62,7 +62,7 @@ reveal_type(c16 + b)  # E: complexfloating
 reveal_type(c16 + c)  # E: complexfloating
 reveal_type(c16 + f)  # E: complexfloating
 reveal_type(c16 + i)  # E: complexfloating
-reveal_type(c16 + AR)  # E: ndarray
+reveal_type(c16 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(c16 + c16)  # E: complexfloating
 reveal_type(f8 + c16)  # E: complexfloating
@@ -75,7 +75,7 @@ reveal_type(b + c16)  # E: complexfloating
 reveal_type(c + c16)  # E: complexfloating
 reveal_type(f + c16)  # E: complexfloating
 reveal_type(i + c16)  # E: complexfloating
-reveal_type(AR + c16)  # E: ndarray
+reveal_type(AR + c16)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(c8 + c16)  # E: complexfloating
 reveal_type(c8 + f8)  # E: complexfloating
@@ -88,7 +88,7 @@ reveal_type(c8 + b)  # E: complexfloating
 reveal_type(c8 + c)  # E: complexfloating
 reveal_type(c8 + f)  # E: complexfloating
 reveal_type(c8 + i)  # E: complexfloating
-reveal_type(c8 + AR)  # E: ndarray
+reveal_type(c8 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(c16 + c8)  # E: complexfloating
 reveal_type(f8 + c8)  # E: complexfloating
@@ -101,7 +101,7 @@ reveal_type(b + c8)  # E: complexfloating
 reveal_type(c + c8)  # E: complexfloating
 reveal_type(f + c8)  # E: complexfloating
 reveal_type(i + c8)  # E: complexfloating
-reveal_type(AR + c8)  # E: ndarray
+reveal_type(AR + c8)  # E: Union[numpy.ndarray, numpy.generic]
 
 # Float
 
@@ -114,7 +114,7 @@ reveal_type(f8 + b)  # E: floating
 reveal_type(f8 + c)  # E: complexfloating
 reveal_type(f8 + f)  # E: floating
 reveal_type(f8 + i)  # E: floating
-reveal_type(f8 + AR)  # E: ndarray
+reveal_type(f8 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(f8 + f8)  # E: floating
 reveal_type(i8 + f8)  # E: floating
@@ -125,7 +125,7 @@ reveal_type(b + f8)  # E: floating
 reveal_type(c + f8)  # E: complexfloating
 reveal_type(f + f8)  # E: floating
 reveal_type(i + f8)  # E: floating
-reveal_type(AR + f8)  # E: ndarray
+reveal_type(AR + f8)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(f4 + f8)  # E: floating
 reveal_type(f4 + i8)  # E: floating
@@ -136,7 +136,7 @@ reveal_type(f4 + b)  # E: floating
 reveal_type(f4 + c)  # E: complexfloating
 reveal_type(f4 + f)  # E: floating
 reveal_type(f4 + i)  # E: floating
-reveal_type(f4 + AR)  # E: ndarray
+reveal_type(f4 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(f8 + f4)  # E: floating
 reveal_type(i8 + f4)  # E: floating
@@ -147,7 +147,7 @@ reveal_type(b + f4)  # E: floating
 reveal_type(c + f4)  # E: complexfloating
 reveal_type(f + f4)  # E: floating
 reveal_type(i + f4)  # E: floating
-reveal_type(AR + f4)  # E: ndarray
+reveal_type(AR + f4)  # E: Union[numpy.ndarray, numpy.generic]
 
 # Int
 
@@ -160,7 +160,7 @@ reveal_type(i8 + b)  # E: signedinteger
 reveal_type(i8 + c)  # E: complexfloating
 reveal_type(i8 + f)  # E: floating
 reveal_type(i8 + i)  # E: signedinteger
-reveal_type(i8 + AR)  # E: ndarray
+reveal_type(i8 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(u8 + u8)  # E: unsignedinteger
 reveal_type(u8 + i4)  # E: Union[numpy.signedinteger, numpy.floating]
@@ -170,7 +170,7 @@ reveal_type(u8 + b)  # E: unsignedinteger
 reveal_type(u8 + c)  # E: complexfloating
 reveal_type(u8 + f)  # E: floating
 reveal_type(u8 + i)  # E: Union[numpy.signedinteger, numpy.floating]
-reveal_type(u8 + AR)  # E: ndarray
+reveal_type(u8 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(i8 + i8)  # E: signedinteger
 reveal_type(u8 + i8)  # E: Union[numpy.signedinteger, numpy.floating]
@@ -181,7 +181,7 @@ reveal_type(b + i8)  # E: signedinteger
 reveal_type(c + i8)  # E: complexfloating
 reveal_type(f + i8)  # E: floating
 reveal_type(i + i8)  # E: signedinteger
-reveal_type(AR + i8)  # E: ndarray
+reveal_type(AR + i8)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(u8 + u8)  # E: unsignedinteger
 reveal_type(i4 + u8)  # E: Union[numpy.signedinteger, numpy.floating]
@@ -191,14 +191,14 @@ reveal_type(b + u8)  # E: unsignedinteger
 reveal_type(c + u8)  # E: complexfloating
 reveal_type(f + u8)  # E: floating
 reveal_type(i + u8)  # E: Union[numpy.signedinteger, numpy.floating]
-reveal_type(AR + u8)  # E: ndarray
+reveal_type(AR + u8)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(i4 + i8)  # E: signedinteger
 reveal_type(i4 + i4)  # E: signedinteger
 reveal_type(i4 + i)  # E: signedinteger
 reveal_type(i4 + b_)  # E: int32
 reveal_type(i4 + b)  # E: signedinteger
-reveal_type(i4 + AR)  # E: ndarray
+reveal_type(i4 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(u4 + i8)  # E: Union[numpy.signedinteger, numpy.floating]
 reveal_type(u4 + i4)  # E: Union[numpy.signedinteger, numpy.floating]
@@ -207,14 +207,14 @@ reveal_type(u4 + u4)  # E: unsignedinteger
 reveal_type(u4 + i)  # E: Union[numpy.signedinteger, numpy.floating]
 reveal_type(u4 + b_)  # E: uint32
 reveal_type(u4 + b)  # E: unsignedinteger
-reveal_type(u4 + AR)  # E: ndarray
+reveal_type(u4 + AR)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(i8 + i4)  # E: signedinteger
 reveal_type(i4 + i4)  # E: signedinteger
 reveal_type(i + i4)  # E: signedinteger
 reveal_type(b_ + i4)  # E: int32
 reveal_type(b + i4)  # E: signedinteger
-reveal_type(AR + i4)  # E: ndarray
+reveal_type(AR + i4)  # E: Union[numpy.ndarray, numpy.generic]
 
 reveal_type(i8 + u4)  # E: Union[numpy.signedinteger, numpy.floating]
 reveal_type(i4 + u4)  # E: Union[numpy.signedinteger, numpy.floating]
@@ -223,4 +223,4 @@ reveal_type(u4 + u4)  # E: unsignedinteger
 reveal_type(b_ + u4)  # E: uint32
 reveal_type(b + u4)  # E: unsignedinteger
 reveal_type(i + u4)  # E: Union[numpy.signedinteger, numpy.floating]
-reveal_type(AR + u4)  # E: ndarray
+reveal_type(AR + u4)  # E: Union[numpy.ndarray, numpy.generic]
