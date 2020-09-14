92a4693cb0df392003cf99a2f1abe8927cdd58b4
Sayed Adel <seiko@imavr.com>
2020-09-14 15:52:27 +0200

    TST: Add testing unit for fused NPYV intrinsics

--- numpy/core/tests/test_simd.py
+++ numpy/core/tests/test_simd.py
@@ -131,7 +131,25 @@ class _SIMD_FP(_Test_Utility):
     """
     To test all float vector types at once
     """
-    pass
+    def test_arithmetic_fused(self):
+        vdata_a, vdata_b, vdata_c = [self.load(self._data())]*3
+        vdata_cx2 = self.add(vdata_c, vdata_c)
+        # multiply and add, a*b + c
+        data_fma = self.load([a * b + c for a, b, c in zip(vdata_a, vdata_b, vdata_c)])
+        fma = self.muladd(vdata_a, vdata_b, vdata_c)
+        assert fma == data_fma
+        # multiply and subtract, a*b - c
+        fms = self.mulsub(vdata_a, vdata_b, vdata_c)
+        data_fms = self.sub(data_fma, vdata_cx2)
+        assert fms == data_fms
+        # negate multiply and add, -(a*b) + c
+        nfma = self.nmuladd(vdata_a, vdata_b, vdata_c)
+        data_nfma = self.sub(vdata_cx2, data_fma)
+        assert nfma == data_nfma
+        # negate multiply and subtract, -(a*b) - c
+        nfms = self.nmulsub(vdata_a, vdata_b, vdata_c)
+        data_nfms = self.mul(data_fma, self.setall(-1))
+        assert nfms == data_nfms
 
 class _SIMD_ALL(_Test_Utility):
     """
