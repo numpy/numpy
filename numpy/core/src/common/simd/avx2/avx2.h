1b8637d4af9b27735629d5371b9d92a0fd1c147d
Sayed Adel <seiko@imavr.com>
2020-09-17 12:11:07 +0200

    ENH, SIMD: Add partial/non-contig load and store intrinsics for 32/64-bit
       This patch improves the implementation of memory load/store for VSX

--- numpy/core/src/common/simd/avx2/avx2.h
+++ numpy/core/src/common/simd/avx2/avx2.h
@@ -5,6 +5,8 @@
 #define NPY_SIMD 256
 #define NPY_SIMD_WIDTH 32
 #define NPY_SIMD_F64 1
+// Enough limit to allow us to use _mm256_i32gather_*
+#define NPY_SIMD_MAXLOAD_STRIDE32 (0x7fffffff / 8)
 
 typedef __m256i npyv_u8;
 typedef __m256i npyv_s8;
