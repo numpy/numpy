f691175d8fd836d1f65f95db4774370cc1ec3a88
danbeibei <57632244+danbeibei@users.noreply.github.com>
2020-10-02 15:57:18 +0200

    Update numpy/distutils/fcompiler/nv.py
    Co-authored-by: Eric Wieser <wieser.eric@gmail.com>

--- numpy/distutils/fcompiler/nv.py
+++ numpy/distutils/fcompiler/nv.py
@@ -1,15 +1,18 @@
-# https://developer.nvidia.com/hpc-sdk
 import sys
 
 from numpy.distutils.fcompiler import FCompiler
 
 compilers = ['NVHPCFCompiler']
 
-"""
-Since august 2020 the NVIDIA HPC SDK includes the compilers formely known as The Portland Group compilers.
-https://www.pgroup.com/index.htm
-"""
 class NVHPCFCompiler(FCompiler):
+   """ NVIDIA High Performance Computing (HPC) SDK Fortran Compiler
+   
+   https://developer.nvidia.com/hpc-sdk
+   
+   Since august 2020 the NVIDIA HPC SDK includes the compilers formerly known as The Portland Group compilers,
+   https://www.pgroup.com/index.htm.
+   See also `numpy.distutils.fcompiler.pg`.
+   """
 
     compiler_type = 'nv'
     description = 'NVIDIA HPC SDK'
