35c67f6d33aa82d1ee8bd4fbdc5b925ef560de2a
Alex Rogozhnikov <iamfullofspam@gmail.com>
2020-10-03 02:28:51 -0700

    DOC: add references to einops and opt_einsum
    Following discussion in mailing list, this PR
    adds mentions to third-party libs einops and opt_einsum
    to einsum documentation

--- numpy/core/einsumfunc.py
+++ numpy/core/einsumfunc.py
@@ -1062,6 +1062,17 @@ def einsum(*operands, out=None, optimize=False, **kwargs):
     --------
     einsum_path, dot, inner, outer, tensordot, linalg.multi_dot
 
+    einops:
+        similar verbose interface is provided by
+        `einops <https://github.com/arogozhnikov/einops>`_ package to cover
+        additional operations: transpose, reshape/flatten, repeat/tile,
+        squeeze/unsqueeze and reductions.
+
+    opt_einsum:
+        `opt_einsum <https://optimized-einsum.readthedocs.io/en/stable/>`_
+        optimizes contraction order for einsum-like expressions
+        in backend-agnostic manner.
+
     Notes
     -----
     .. versionadded:: 1.6.0
