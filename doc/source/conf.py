5ee77370b92f24b06a221774c6b1c4292e6b09ef
Eric Wieser <wieser.eric@gmail.com>
2020-10-04 19:15:08 +0100

    DOC: Improve [source] links for C extension types
    Previously, these would all link to `numpy/core/__init__.py`.
    Now the scalar type and `ndarray` link to the files where the `PyTypeObject` is defined.
    In future, we should do this for all extension types, probably automatically.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -310,6 +310,17 @@ for name in ['sphinx.ext.linkcode', 'numpydoc.linkcode']:
 else:
     print("NOTE: linkcode extension not found -- no links to source generated")
 
+
+def _get_c_source_file(obj):
+    if issubclass(obj, numpy.generic):
+        return r"core/src/multiarray/scalartypes.c.src"
+    elif obj is numpy.ndarray:
+        return r"core/src/multiarray/arrayobject.c"
+    else:
+        # todo: come up with a better way to generate these
+        return None
+
+
 def linkcode_resolve(domain, info):
     """
     Determine the URL corresponding to Python object
@@ -340,24 +351,32 @@ def linkcode_resolve(domain, info):
     else:
         obj = unwrap(obj)
 
-    try:
-        fn = inspect.getsourcefile(obj)
-    except Exception:
-        fn = None
-    if not fn:
-        return None
+    fn = None
+    lineno = None
 
-    try:
-        source, lineno = inspect.getsourcelines(obj)
-    except Exception:
-        lineno = None
+    # Make a poor effort at linking C extension types
+    if isinstance(obj, type) and obj.__module__ == 'numpy':
+        fn = _get_c_source_file(obj)
+
+    if fn is None:
+        try:
+            fn = inspect.getsourcefile(obj)
+        except Exception:
+            fn = None
+        if not fn:
+            return None
+
+        try:
+            source, lineno = inspect.getsourcelines(obj)
+        except Exception:
+            lineno = None
+
+        fn = relpath(fn, start=dirname(numpy.__file__))
 
     if lineno:
         linespec = "#L%d-L%d" % (lineno, lineno + len(source) - 1)
     else:
         linespec = ""
-
-    fn = relpath(fn, start=dirname(numpy.__file__))
 
     if 'dev' in numpy.__version__:
         return "https://github.com/numpy/numpy/blob/master/numpy/%s%s" % (
