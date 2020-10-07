30e037cf51ff476ef249926f365212e991886a27
Bas van Beek <b.f.van.beek@vu.nl>
2020-10-07 12:52:44 +0200

    MAINT: Ignore the private `np.compat` module

--- tools/functions_missing_types.py
+++ tools/functions_missing_types.py
@@ -37,6 +37,7 @@ EXCLUDE_LIST = {
         "add_newdoc",
         "add_newdoc_ufunc",
         "core",
+        "compat",
         "fastCopyAndTranspose",
         "get_array_wrap",
         "int_asbuffer",
