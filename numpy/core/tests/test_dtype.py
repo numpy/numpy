4e3fcf642a0aeae03c12fad325ceaf75235b24bc
Ganesh Kathiresan <ganesh3597@gmail.com>
2020-10-16 21:14:22 +0530

    BUG: Added tuple tests

--- numpy/core/tests/test_dtype.py
+++ numpy/core/tests/test_dtype.py
@@ -767,11 +767,18 @@ class TestMonsterType:
             ('yi', np.dtype((a, (3, 2))))])
         assert_dtype_equal(c, d)
 
-    def test_array_recursion(self):
+    def test_list_recursion(self):
         l = list()
         l.append(('f', l))
         with pytest.raises(RecursionError):
             np.dtype(l)
+
+    def test_tuple_recursion(self):
+        d = np.int32
+        for i in range(100000):
+            d = (d, (1,))
+        with pytest.raises(RecursionError):
+            np.dtype(d)
 
     def test_dict_recursion(self):
         d = dict(names=['self'], formats=[None], offsets=[0])
