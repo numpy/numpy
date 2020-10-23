0c31540433cf7478df0d8cc34ab1639176664cc1
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-10-23 18:25:55 +0200

    Add writeable test

--- numpy/lib/tests/test_stride_tricks.py
+++ numpy/lib/tests/test_stride_tricks.py
@@ -473,6 +473,19 @@ class TestSlidingWindowView:
                 match='window shape cannot be larger than input array'):
             sliding_window_view(arr, (5, 5))
 
+    def test_writeable(self):
+        arr = np.arange(5)
+        view = sliding_window_view(arr, 2, writeable=False)
+        assert_(not view.flags.writeable)
+        with pytest.raises(
+                ValueError,
+                match='assignment destination is read-only'):
+            view[0, 0] = 3
+        view = sliding_window_view(arr, 2, writeable=True)
+        assert_(view.flags.writeable)
+        view[0, 1] = 3
+        assert_array_equal(arr, np.array([0, 3, 2, 3, 4]))
+
 
 def as_strided_writeable():
     arr = np.ones(10)
