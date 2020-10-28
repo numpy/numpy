eceb8c1c01cd6ce60a3e0f0cd6a8439391e254eb
Charles Harris <charlesr.harris@gmail.com>
2020-10-27 21:48:20 -0600

    TST: Simplify source path names in test_extending.
    This is a fix for Python 3.9 Azure windows builds that were failing
    on account of too long temporary file names.

--- numpy/random/tests/test_extending.py
+++ numpy/random/tests/test_extending.py
@@ -41,8 +41,6 @@ else:
         cython = None
 
 
-@pytest.mark.skipif(sys.platform == "win32" and sys.version_info[:2] == (3, 9),
-                    reason="temp filename problem with Python 3.9 on Windows")
 @pytest.mark.skipif(cython is None, reason="requires cython")
 @pytest.mark.slow
 def test_cython(tmp_path):
