77c447079af9ef8e855c41b4f4578d91b776d216
svorfolomeev <vorfol@yandex.ru>
2020-11-18 12:39:42 +0500

    fsync is required

--- numpy/core/memmap.py
+++ numpy/core/memmap.py
@@ -109,7 +109,7 @@ class memmap(ndarray):
     The memmap object can be used anywhere an ndarray is accepted.
     Given a memmap ``fp``, ``isinstance(fp, numpy.ndarray)`` returns
     ``True``.
-    
+
     Memory-mapped files cannot be larger than 2GB on 32-bit systems.
 
     When a memmap causes a file to be created or extended beyond its
@@ -250,6 +250,7 @@ class memmap(ndarray):
                 fid.seek(bytes - 1, 0)
                 fid.write(b'\0')
                 fid.flush()
+                os.fsync(fid.fileno())
 
             if mode == 'c':
                 acc = mmap.ACCESS_COPY
