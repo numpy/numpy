5f316a85dd205cb0a6134a5a1e6b5bc76943f17c
Sebastian Berg <sebastian@sipsolutions.net>
2020-11-19 12:53:42 -0600

    BUG: Fix pickling of scalars with NPY_LISTPICKLE
    Structured void scalars (or potentially other dtypes using LISTPICKLE)
    were incorrectly pickled and unpickled previously.
    While the pickled value was valid and could have been used to
    reconstruct the data, the reconstruction itself would always fail.
    
    The approach here is to always pickle the full array and use the
    arrays pickle/unpickle machinery which handles structured void
    correctly.  Future work could improve on this probably.
    
    In the (hopefully impossible) case that someone tries to unpickle
    a numpy object scalar, the code is now fixed to return the pickled
    value unmodified (but INCREF'd), however a deprecation-warning
    is given, since such an object should never be created.

--- numpy/core/tests/test_records.py
+++ numpy/core/tests/test_records.py
@@ -424,7 +424,15 @@ class TestRecord:
         # make sure we did not pickle the address
         assert not isinstance(obj, bytes)
 
-        assert_raises(TypeError, ctor, dtype, 13)
+        assert_raises(RuntimeError, ctor, dtype, 13)
+
+        # Test roundtrip:
+        dump = pickle.dumps(a[0])
+        unpickled = pickle.loads(dump)
+        assert a[0] == unpickled
+
+        # Also check the similar (impossible) "object scalar" path:
+        assert ctor(np.dtype("O"), data) is data
 
     def test_objview_record(self):
         # https://github.com/numpy/numpy/issues/2599
