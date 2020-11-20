7ad74fb975f0479879582d12e8026afc2d3f9380
Eric Wieser <wieser.eric@gmail.com>
2020-11-20 14:15:18 +0000

    Update numpy/linalg/linalg.py

--- numpy/linalg/linalg.py
+++ numpy/linalg/linalg.py
@@ -363,8 +363,8 @@ def solve(a, b):
     --------
     Solve the system of equations ``x0 + 2 * x1 = 1`` and ``3 * x0 + 5 * x1 = 2``:
 
-    >>> a = np.array([[1,2], [3,5]])
-    >>> b = np.array([1,2])
+    >>> a = np.array([[1, 2], [3, 5]])
+    >>> b = np.array([1, 2])
     >>> x = np.linalg.solve(a, b)
     >>> x
     array([-1.,  1.])
