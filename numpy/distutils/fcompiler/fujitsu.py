ee6def389247028175cf47c3c6470585092851eb
t-karatsu <fj6085ge@fujitsu.com>
2020-12-03 16:59:50 +0900

    DOC: Add docstring about Fujitsu compiler.

--- numpy/distutils/fcompiler/fujitsu.py
+++ numpy/distutils/fcompiler/fujitsu.py
@@ -1,3 +1,9 @@
+"""
+fujitsu
+
+Supports Fujitsu compiler function.
+This compiler is developed by Fujitsu and is used in A64FX on Fugaku.
+"""
 from numpy.distutils.fcompiler import FCompiler
 
 compilers = ['FujitsuFCompiler']
