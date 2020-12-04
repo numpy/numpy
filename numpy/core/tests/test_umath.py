bbde6360d61ec832e0bb0c9618803d25dfef9c76
svorfolomeev <vorfol@yandex.ru>
2020-12-04 18:06:34 +0500

    merge try one

--- numpy/core/tests/test_umath.py
+++ numpy/core/tests/test_umath.py
@@ -395,6 +395,14 @@ class TestRemainder:
                 rem = np.remainder(finf, fone)
                 if sys.platform != 'OpenVMS':   # OpenVMS returns zero
                     assert_(np.isnan(rem), 'dt: %s, rem: %s' % (dt, rem))
+                rem = np.remainder(fnan, fzer)
+                fmod = np.fmod(fnan, fzer)
+                assert_(np.isnan(rem), 'dt: %s, rem: %s' % (dt, rem))
+                assert_(np.isnan(fmod), 'dt: %s, fmod: %s' % (dt, rem))
+                rem = np.remainder(fnan, fone)
+                fmod = np.fmod(fnan, fone)
+                assert_(np.isnan(rem), 'dt: %s, rem: %s' % (dt, rem))
+                assert_(np.isnan(fmod), 'dt: %s, fmod: %s' % (dt, rem))
 
 
 class TestCbrt:
