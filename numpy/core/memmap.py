fab8a561320564ddaf52cc19c257d5925f3a945c
YoongHM <yoonghm@users.noreply.github.com>
2020-12-10 18:47:43 +0800

    Update numpy/core/memmap.py
    Co-authored-by: Bas van Beek <43369155+BvB93@users.noreply.github.com>

--- numpy/core/memmap.py
+++ numpy/core/memmap.py
@@ -222,7 +222,7 @@ class memmap(ndarray):
     Use context manager to read the file into memmap:
     
     >>> with np.memmap(filename, dtype='float32', mode='r') as fpcon:
-    >>>     fpcon
+    ...     fpcon
     memmap([[  0.,   1.,   2.,   3.],
             [  4.,   5.,   6.,   7.],
             [  8.,   9.,  10.,  11.]], dtype=float32)
