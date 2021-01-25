59ca3b5e97e2186f439f3f2fc82259ba56a3b78f
Bas van Beek <b.f.van.beek@vu.nl>
2021-01-25 20:44:01 +0100

    TST: Add module-based tests to the `pass` tests

---
+++ numpy/typing/tests/data/pass/modules.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+np.char
+np.ctypeslib
+np.emath
+np.fft
+np.lib
+np.linalg
+np.ma
+np.matrixlib
+np.polynomial
+np.random
+np.rec
+np.testing
+np.version
+
+np.__all__
+np.__path__
+np.__version__
+np.__git_version__
+np.__NUMPY_SETUP__
+np.__deprecated_attrs__
+np.__expired_functions__
