570710aafab435024ee17440fb3207740a0d39c2
Jamie <dodgerbarker@gmail.com>
2021-02-10 18:16:02 +0000

    Adjusted formulas to estimator and removed repeat

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -563,8 +563,7 @@ def histogram_bin_edges(a, bins=10, range=None, weights=None):
     bins. All estimators that compute bin counts are recast to bin width
     using the `ptp` of the data. The final bin count is obtained from
     ``np.round(np.ceil(range / h))``. The final bin width is often less 
-    than what is returned by the formulas below, in order to allow the 
-    bins to fit the entire range of the data.
+    than what is returned by the estimators below.
 
     'auto' (maximum of the 'sturges' and 'fd' estimators)
         A compromise to get a good value. For small datasets the Sturges
