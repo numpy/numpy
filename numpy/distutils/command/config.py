540168d53440588290bf3079b5bd72b8b9f22d5b
Abhay Raghuvanshi <abhayaman669@gmail.com>
2021-02-12 10:15:22 +0530

    Updated raise chain exception

--- numpy/distutils/command/config.py
+++ numpy/distutils/command/config.py
@@ -64,7 +64,7 @@ class config(old_config):
                         % (e, self.compiler.__class__.__name__)
                     print(textwrap.dedent("""\
                         ============================================================================"""))
-                    raise distutils.errors.DistutilsPlatformError(msg) from None
+                    raise distutils.errors.DistutilsPlatformError(msg) from e
 
             # After MSVC is initialized, add an explicit /MANIFEST to linker
             # flags.  See issues gh-4245 and gh-4101 for details.  Also
--- numpy/distutils/fcompiler/compaq.py
+++ numpy/distutils/fcompiler/compaq.py
@@ -87,11 +87,11 @@ class CompaqVisualFCompiler(FCompiler):
         except IOError as e:
             if not "vcvarsall.bat" in str(e):
                 print("Unexpected IOError in", __file__)
-                raise e from None
+                raise
         except ValueError as e:
             if not "'path'" in str(e):
                 print("Unexpected ValueError in", __file__)
-                raise e from None
+                raise
 
     executables = {
         'version_cmd'  : ['<F90>', "/what"],
