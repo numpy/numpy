21b2759e964b72a78df53d52c53081366f953ce9
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2021-02-15 12:41:37 +0000

    ENH: Improve broadcasting in multinomial
    Improve robustness fo broadcasting
    Explicitly rule out impossible cases

--- numpy/random/tests/test_generator_mt19937.py
+++ numpy/random/tests/test_generator_mt19937.py
@@ -2385,6 +2385,17 @@ class TestBroadcast:
         random = Generator(MT19937(self.seed))
         pvals = [[1 / 6] * 6, [1 / 4] * 6]
         assert_raises(ValueError, random.multinomial, 1, pvals)
+        assert_raises(ValueError, random.multinomial, 6, 0.5)
+
+    def test_empty_outputs(self):
+        random = Generator(MT19937(self.seed))
+        actual = random.multinomial(np.empty((10, 0, 6), "i8"), [1 / 6] * 6)
+        assert actual.shape == (10, 0, 6, 6)
+        actual = random.multinomial(12, np.empty((10, 0, 10)))
+        assert actual.shape == (10, 0, 10)
+        actual = random.multinomial(np.empty((3, 0, 7), "i8"),
+                                    np.empty((3, 0, 7, 4)))
+        assert actual.shape == (3, 0, 7, 4)
 
 
 class TestThread:
