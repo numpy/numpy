c21889f3c57b508c91895fa883d1e4cdc1a0193d
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2021-02-18 07:21:48 +0000

    MAINT: Restructure imports in pyi file

--- numpy/random/__init__.pyi
+++ numpy/random/__init__.pyi
@@ -8,56 +8,58 @@ from numpy.random._philox import Philox as Philox
 from numpy.random._sfc64 import SFC64 as SFC64
 from numpy.random.bit_generator import BitGenerator as BitGenerator
 from numpy.random.bit_generator import SeedSequence as SeedSequence
-from numpy.random.mtrand import RandomState as RandomState
-from numpy.random.mtrand import beta as beta
-from numpy.random.mtrand import binomial as binomial
-from numpy.random.mtrand import bytes as bytes
-from numpy.random.mtrand import chisquare as chisquare
-from numpy.random.mtrand import choice as choice
-from numpy.random.mtrand import dirichlet as dirichlet
-from numpy.random.mtrand import exponential as exponential
-from numpy.random.mtrand import f as f
-from numpy.random.mtrand import gamma as gamma
-from numpy.random.mtrand import geometric as geometric
-from numpy.random.mtrand import get_state as get_state
-from numpy.random.mtrand import gumbel as gumbel
-from numpy.random.mtrand import hypergeometric as hypergeometric
-from numpy.random.mtrand import laplace as laplace
-from numpy.random.mtrand import logistic as logistic
-from numpy.random.mtrand import lognormal as lognormal
-from numpy.random.mtrand import logseries as logseries
-from numpy.random.mtrand import multinomial as multinomial
-from numpy.random.mtrand import multivariate_normal as multivariate_normal
-from numpy.random.mtrand import negative_binomial as negative_binomial
-from numpy.random.mtrand import noncentral_chisquare as noncentral_chisquare
-from numpy.random.mtrand import noncentral_f as noncentral_f
-from numpy.random.mtrand import normal as normal
-from numpy.random.mtrand import pareto as pareto
-from numpy.random.mtrand import permutation as permutation
-from numpy.random.mtrand import poisson as poisson
-from numpy.random.mtrand import power as power
-from numpy.random.mtrand import rand as rand
-from numpy.random.mtrand import randint as randint
-from numpy.random.mtrand import randn as randn
-from numpy.random.mtrand import random as random
-from numpy.random.mtrand import random_integers as random_integers
-from numpy.random.mtrand import random_sample as random_sample
-from numpy.random.mtrand import ranf as ranf
-from numpy.random.mtrand import rayleigh as rayleigh
-from numpy.random.mtrand import sample as sample
-from numpy.random.mtrand import seed as seed
-from numpy.random.mtrand import set_state as set_state
-from numpy.random.mtrand import shuffle as shuffle
-from numpy.random.mtrand import standard_cauchy as standard_cauchy
-from numpy.random.mtrand import standard_exponential as standard_exponential
-from numpy.random.mtrand import standard_gamma as standard_gamma
-from numpy.random.mtrand import standard_normal as standard_normal
-from numpy.random.mtrand import standard_t as standard_t
-from numpy.random.mtrand import triangular as triangular
-from numpy.random.mtrand import uniform as uniform
-from numpy.random.mtrand import vonmises as vonmises
-from numpy.random.mtrand import wald as wald
-from numpy.random.mtrand import weibull as weibull
-from numpy.random.mtrand import zipf as zipf
+from numpy.random.mtrand import (
+    RandomState as RandomState,
+    beta as beta,
+    binomial as binomial,
+    bytes as bytes,
+    chisquare as chisquare,
+    choice as choice,
+    dirichlet as dirichlet,
+    exponential as exponential,
+    f as f,
+    gamma as gamma,
+    geometric as geometric,
+    get_state as get_state,
+    gumbel as gumbel,
+    hypergeometric as hypergeometric,
+    laplace as laplace,
+    logistic as logistic,
+    lognormal as lognormal,
+    logseries as logseries,
+    multinomial as multinomial,
+    multivariate_normal as multivariate_normal,
+    negative_binomial as negative_binomial,
+    noncentral_chisquare as noncentral_chisquare,
+    noncentral_f as noncentral_f,
+    normal as normal,
+    pareto as pareto,
+    permutation as permutation,
+    poisson as poisson,
+    power as power,
+    rand as rand,
+    randint as randint,
+    randn as randn,
+    random as random,
+    random_integers as random_integers,
+    random_sample as random_sample,
+    ranf as ranf,
+    rayleigh as rayleigh,
+    sample as sample,
+    seed as seed,
+    set_state as set_state,
+    shuffle as shuffle,
+    standard_cauchy as standard_cauchy,
+    standard_exponential as standard_exponential,
+    standard_gamma as standard_gamma,
+    standard_normal as standard_normal,
+    standard_t as standard_t,
+    triangular as triangular,
+    uniform as uniform,
+    vonmises as vonmises,
+    wald as wald,
+    weibull as weibull,
+    zipf as zipf,
+)
 
 __all__: List[str]
