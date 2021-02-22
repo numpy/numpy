85284f55a7f6260971ad669a91934b056ef0c3b6
Rebwar Ali Omer Bajallan <rebwar@Rebwars-MacBook-Pro.local>
2021-02-22 16:56:30 +0100

    ADD: coverage data struct

---
+++ tools/branches_covered.py
@@ -0,0 +1,50 @@
+import json
+
+# Manual Coverage Tool
+
+# Branches covered dictionary
+branches_covered = dict()
+
+# Polydiv
+branches_covered['Polydiv'] = {
+                    'branch1': False, 
+                    'branch2': False,
+                    'branch3': False,
+                    'branch4': False,
+}
+
+# Polyder 
+branches_covered['Polyder'] = {
+                    'branch1': False, 
+                    'branch2': False,
+                    'branch3': False,
+                    'branch4': False,
+                    'branch5': False,
+}
+
+#Polyint
+branches_covered['Polyint'] = {
+                    'branch1': False, 
+                    'branch2': False,
+                    'branch3': False,
+                    'branch4': False,
+                    'branch5': False,
+                    'branch6': False, 
+                    'branch7': False,
+                    'branch8': False,
+                    'branch9': False,
+}
+#Matrix_power
+branches_covered['Matrix_power'] = {
+                    'branch1': False, 
+                    'branch2': False,
+                    'branch3': False,
+                    'branch4': False,
+                    'branch5': False,
+                    'branch6': False, 
+                    'branch7': False,
+                    'branch8': False,
+                    'branch9': False,
+                    'branch10': False,
+                    'branch11': False,
+}
