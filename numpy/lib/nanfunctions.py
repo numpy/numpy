BUG: fix nanpercentile returns incorrect shape when input empty array

numpy/lib/nanfunctions.py
@@ -1363,7 +1363,8 @@ def _nanquantile_unchecked(a, q, axis=None, out=None, overwrite_input=False,
     # apply_along_axis in _nanpercentile doesn't handle empty arrays well,
     # so deal them upfront
     if a.size == 0:
-        return np.nanmean(a, axis, out=out, keepdims=keepdims)
+        r = np.nanmean(a, axis, out=out, keepdims=keepdims)
+        return np.broadcast_to(r, q.shape + r.shape)
 
     r, k = function_base._ureduce(
         a, func=_nanquantile_ureduce_func, q=q, axis=axis, out=out,