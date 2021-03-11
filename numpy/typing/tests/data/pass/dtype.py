2fad7d21fa0d38368f345e5483f7e4594bff9723
Bas van Beek <b.f.van.beek@vu.nl>
2021-03-11 13:08:35 +0100

    MAINT: Add annotations for `dtype.__getitem__`, `__mul__` and `names`

--- numpy/typing/tests/data/pass/dtype.py
+++ numpy/typing/tests/data/pass/dtype.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 dtype_obj = np.dtype(np.str_)
+void_dtype_obj = np.dtype([("f0", np.float64), ("f1", np.float32)])
 
 np.dtype(dtype=np.int64)
 np.dtype(int)
@@ -41,3 +42,16 @@ dtype_obj.base
 dtype_obj.subdtype
 dtype_obj.newbyteorder()
 dtype_obj.type
+dtype_obj.name
+dtype_obj.names
+
+dtype_obj * 0
+dtype_obj * 2
+
+0 * dtype_obj
+2 * dtype_obj
+
+void_dtype_obj["f0"]
+void_dtype_obj[0]
+void_dtype_obj[["f0", "f1"]]
+void_dtype_obj[["f0"]]
