36471b2aed8f5fb8933a06389b135f90baff4d0c
donno2048 <just4now666666@gmail.com>
2021-04-13 06:22:54 +0300

    Reimport _set_madvise_hugepage

--- numpy/core/multiarray.py
+++ numpy/core/multiarray.py
@@ -15,7 +15,7 @@ from ._multiarray_umath import *  # noqa: F403
 # _get_ndarray_c_version is semi-public, on purpose not added to __all__
 from ._multiarray_umath import (
     _fastCopyAndTranspose, _flagdict, _insert, _reconstruct, _vec_string,
-    _ARRAY_API, _monotonicity, _get_ndarray_c_version,
+    _ARRAY_API, _monotonicity, _get_ndarray_c_version, _set_madvise_hugepage,
     )
 
 __all__ = [
