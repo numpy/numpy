71dfd592080ba6542b36c2c61a11aa7f5850565e
KoningR <r.m.koning@student.tudelft.nl>
2021-04-21 11:50:53 +0200

    MAINT: Change doctest array print.

--- numpy/lib/shape_base.py
+++ numpy/lib/shape_base.py
@@ -763,13 +763,12 @@ def array_split(ary, indices_or_sections, axis=0):
     [array([0.,  1.,  2.]), array([3.,  4.]), array([5.,  6.])]
     >>> x = np.reshape(np.arange(16), (4, 4))
     >>> np.array_split(x, [3, 3], (0, 1))
-    array([
-        [array([[0, 1], [4, 5]]),
-         array([[2], [6]]),
-         array([[3], [7]])],
-        [array([[8, 9]]), array([[10]]), array([[11]])],
-        [array([[12, 13]]), array([[14]]), array([[15]])]
-    ], dtype=object)
+    array([[array([[0, 1],
+                   [4, 5]]), array([[2],
+                                    [6]]), array([[3],
+                                                  [7]])],
+           [array([[8, 9]]), array([[10]]), array([[11]])],
+           [array([[12, 13]]), array([[14]]), array([[15]])]], dtype=object)
     """
     # If axis is a tuple, assume an N-dimensional split.
     if isinstance(axis, tuple):
@@ -928,12 +927,10 @@ def split(ary, indices_or_sections, axis=0):
      array([], dtype=float64)]
     >>> x = np.reshape(np.arange(16), (4, 4))
     >>> np.split(x, [4, 2], (0, 1))
-    array([
-        [array([[0, 1]]), array([[2, 3]])],
-        [array([[4, 5]]), array([[6, 7]])],
-        [array([[8, 9]]), array([[10, 11]])],
-        [array([[12, 13]]), array([[14, 15]])]
-    ], dtype=object)
+    array([[array([[0, 1]]), array([[2, 3]])],
+           [array([[4, 5]]), array([[6, 7]])],
+           [array([[8, 9]]), array([[10, 11]])],
+           [array([[12, 13]]), array([[14, 15]])]], dtype=object)
     """
 
     def verify_equal_division(indices_or_sections, axis):
