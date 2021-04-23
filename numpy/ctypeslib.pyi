a4ae295828c73480882a0385822a20a296393a30
Bas van Beek <b.f.van.beek@vu.nl>
2021-04-23 13:56:41 +0200

    ENH: Add improved placeholder annotations for `np.ctypeslib`

--- numpy/ctypeslib.pyi
+++ numpy/ctypeslib.pyi
@@ -1,10 +1,14 @@
-from typing import Any, List
+from typing import List, Type
+from ctypes import _SimpleCData
 
 __all__: List[str]
 
-load_library: Any
-ndpointer: Any
-c_intp: Any
-as_ctypes: Any
-as_array: Any
-as_ctypes_type: Any
+# TODO: Update the `npt.mypy_plugin` such that it substitutes `c_intp` for
+# a specific `_SimpleCData[int]` subclass (e.g. `ctypes.c_long`)
+c_intp: Type[_SimpleCData[int]]
+
+def load_library(libname, loader_path): ...
+def ndpointer(dtype=..., ndim=..., shape=..., flags=...): ...
+def as_ctypes(obj): ...
+def as_array(obj, shape=...): ...
+def as_ctypes_type(dtype): ...
