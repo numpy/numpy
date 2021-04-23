2e60be21905d2fd53d712ffd90daa94cf073e3a7
Bas van Beek <b.f.van.beek@vu.nl>
2021-04-23 14:07:45 +0200

    ENH: Add improved placeholder annotations for `np.f2py`

--- numpy/f2py/__init__.pyi
+++ numpy/f2py/__init__.pyi
@@ -1,7 +1,18 @@
 from typing import Any, List
 
+from numpy.f2py import (
+    f2py_testing as f2py_testing,
+)
+
 __all__: List[str]
 
-run_main: Any
-compile: Any
-f2py_testing: Any
+def run_main(comline_list): ...
+def compile(
+    source,
+    modulename=...,
+    extra_args=...,
+    verbose=...,
+    source_fn=...,
+    extension=...,
+    full_output=...,
+): ...
