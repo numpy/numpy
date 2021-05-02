99991be3a21f8293fcf4b16663522475b15535a4
Bas van Beek <b.f.van.beek@vu.nl>
2021-05-03 01:38:51 +0200

    TST: Fix a typing failure for python 3.7
    Mypy 0.790 does not yet contain annotations for `typing_extensions.SupportsIndex`, so we'll have to define them ourselves

--- numpy/typing/_shape.py
+++ numpy/typing/_shape.py
@@ -1,15 +1,15 @@
 import sys
-from typing import Sequence, Tuple, Union
+from typing import Sequence, Tuple, Union, TYPE_CHECKING
 
-if sys.version_info >= (3, 8):
-    from typing import SupportsIndex
-else:
-    try:
-        from typing_extensions import SupportsIndex
-    except ImportError:
-        SupportsIndex = NotImplemented
+if TYPE_CHECKING:
+    if sys.version_info >= (3, 8):
+        from typing import SupportsIndex
+    else:
+        from typing_extensions import Protocol
+        class SupportsIndex(Protocol):
+            def __index__(self) -> int: ...
 
 _Shape = Tuple[int, ...]
 
 # Anything that can be coerced to a shape tuple
-_ShapeLike = Union[SupportsIndex, Sequence[SupportsIndex]]
+_ShapeLike = Union["SupportsIndex", Sequence["SupportsIndex"]]
