BUG: fixed ma.average ignoring masked weights
Closes #10438

Co-authored-by: mecopur <mecopur@outlook.com>

numpy/ma/extras.py
@@ -614,10 +614,10 @@ def average(a, axis=None, weights=None, returned=False):
                     "Length of weights not compatible with specified axis.")
 
             # setup wgt to broadcast along axis
-            wgt = np.broadcast_to(wgt, (a.ndim-1)*(1,) + wgt.shape)
+            wgt = np.broadcast_to(wgt, (a.ndim-1)*(1,) + wgt.shape, subok=True)
             wgt = wgt.swapaxes(-1, axis)
 
-        if m is not nomask:
+        if m is nomask:
             wgt = wgt*(~a.mask)
 
         scl = wgt.sum(axis=axis, dtype=result_dtype)