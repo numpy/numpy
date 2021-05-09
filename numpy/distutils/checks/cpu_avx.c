244ee0188f1b0cb264e6f52aac73a71a7118c4c3
Sayed Adel <seiko@imavr.com>
2021-05-09 21:33:37 +0200

    MAINT, SIMD: Hardened the AVX compile-time tests
      To avoid optimizing it out by the compiler so we make
      sure that the assembler is getting involved.

--- numpy/distutils/checks/cpu_avx.c
+++ numpy/distutils/checks/cpu_avx.c
@@ -1,7 +1,7 @@
 #include <immintrin.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    __m256 a = _mm256_add_ps(_mm256_setzero_ps(), _mm256_setzero_ps());
+    __m256 a = _mm256_add_ps(_mm256_loadu_ps((const float*)argv[argc-1]), _mm256_loadu_ps((const float*)argv[1]));
     return (int)_mm_cvtss_f32(_mm256_castps256_ps128(a));
 }
--- numpy/distutils/checks/cpu_fma3.c
+++ numpy/distutils/checks/cpu_fma3.c
@@ -1,8 +1,9 @@
 #include <xmmintrin.h>
 #include <immintrin.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    __m256 a = _mm256_fmadd_ps(_mm256_setzero_ps(), _mm256_setzero_ps(), _mm256_setzero_ps());
+    __m256 a = _mm256_loadu_ps((const float*)argv[argc-1]);
+           a = _mm256_fmadd_ps(a, a, a);
     return (int)_mm_cvtss_f32(_mm256_castps256_ps128(a));
 }
