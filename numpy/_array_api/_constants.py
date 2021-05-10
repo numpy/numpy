4817784c6e1050034faabb1b3d04382fe8997b41
Aaron Meurer <asmeurer@gmail.com>
2021-05-10 15:35:40 -0600

    Make the array API constants Python floats

--- numpy/_array_api/_constants.py
+++ numpy/_array_api/_constants.py
@@ -1,9 +1,6 @@
-from ._array_object import ndarray
-from ._dtypes import float64
-
 import numpy as np
 
-e = ndarray._new(np.array(np.e, dtype=float64))
-inf = ndarray._new(np.array(np.inf, dtype=float64))
-nan = ndarray._new(np.array(np.nan, dtype=float64))
-pi = ndarray._new(np.array(np.pi, dtype=float64))
+e = np.e
+inf = np.inf
+nan = np.nan
+pi = np.pi
