2bb9cedac51778e2dcadcd2f26e2d532d306c7c5
Marco Aurelio da Costa <costa@gamic.com>
2021-05-13 13:41:51 -0300

    Clarify reason for calling  PyArray_ITER_GOTO1D in comment line

--- numpy/core/src/multiarray/iterators.c
+++ numpy/core/src/multiarray/iterators.c
@@ -1772,7 +1772,7 @@ PyArray_NeighborhoodIterNew(PyArrayIterObject *x, const npy_intp *bounds,
      */
     x->contiguous = 0;
 
-    /* update the coordinates */
+    /* update the coordinates if x was contiguous before */
     PyArray_ITER_GOTO1D(x, x->index);
 
     PyArrayNeighborhoodIter_Reset(ret);
