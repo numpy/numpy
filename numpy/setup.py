4e92e1a40814df762ec9a88437b436fb0d7a67ee
Giulio Procopio <61784957+Giuppox@users.noreply.github.com>
2021-05-15 20:32:57 +0200

    MAINT: Refactored numpy/setup.py
    Removed redundant specifications of `add_subpackage`, `add_data_dir` and `add_data_files` by mapping over lists

--- numpy/setup.py
+++ numpy/setup.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 
-def configuration(parent_package='',top_path=None):
+def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('numpy', parent_package, top_path)
 
-    config.add_subpackage('compat')
-    config.add_subpackage('core')
-    config.add_subpackage('distutils')
-    config.add_subpackage('doc')
-    config.add_subpackage('f2py')
-    config.add_subpackage('fft')
-    config.add_subpackage('lib')
-    config.add_subpackage('linalg')
-    config.add_subpackage('ma')
-    config.add_subpackage('matrixlib')
-    config.add_subpackage('polynomial')
-    config.add_subpackage('random')
-    config.add_subpackage('testing')
-    config.add_subpackage('typing')
-    config.add_data_dir('doc')
-    config.add_data_files('py.typed')
-    config.add_data_files('*.pyi')
-    config.add_subpackage('tests')
+    for subpackage in ('compat', 'core', 'distutils', 'doc', 'f2py', 'fft', 'lib',
+        'linalg', 'ma', 'matrixlib', 'polynomial', 'random', 'testing', 'typing'):
+        
+    subpackages = ['compat', 'core', 'distutils', 'doc', 'f2py', 'fft', 'lib',
+                   'linalg', 'ma', 'matrixlib', 'polynomial', 'random', 'testing', 
+                   'typing', 'tests']
+    data_dirs = ['doc']
+    data_files = ['py.typed', '*.pyi']
+    
+    map(config.add_subpackage, subpackages)
+    map(config.add_data_dir, data_dirs)
+    map(config.add_data_files, data_files)
+        
     config.make_config_py() # installs __config__.py
     return config
 
