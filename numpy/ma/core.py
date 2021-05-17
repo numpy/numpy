cdb75f300817efec1c8a8f01970faebc08c9c756
Pax <13646646+paxcodes@users.noreply.github.com>
2021-05-17 08:46:01 -0700

    DOC: replace return type for np.ma.zeros docstring
    Relevant issue: numpy#16751

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -8094,25 +8094,59 @@ class _convert2ma:
     """
     __doc__ = None
 
-    def __init__(self, funcname, params=None):
+    def __init__(self, funcname, params=None, np_ret=None, np_ma_ret=None):
         self._func = getattr(np, funcname)
-        self.__doc__ = self.getdoc()
+        self.__doc__ = self.getdoc(np_ret, np_ma_ret)
         self._extras = params or {}
 
-    def getdoc(self):
+    def getdoc(self, np_ret, np_ma_ret):
         "Return the doc of the function (from the doc of the method)."
         doc = getattr(self._func, '__doc__', None)
         sig = get_object_signature(self._func)
         if doc:
+            doc = self._replace_return_type(doc, np_ret, np_ma_ret)
             # Add the signature of the function at the beginning of the doc
             if sig:
                 sig = "%s%s\n" % (self._func.__name__, sig)
-            note_for_return_type = (
-                "\nNote: If input value is ``np.ma.MaskedArray``, "
-                "return value is also an ``np.ma.MaskedArray``."
-            )
-            doc = sig + doc + note_for_return_type
+            doc = sig + doc
         return doc
+
+    def _replace_return_type(self, doc, np_ret, np_ma_ret):
+        """
+        Replace documentation of ``np`` function's return type.
+
+        Replaces it with the proper type for the ``np.ma`` function.
+
+        Parameters
+        ----------
+        doc : str
+            The documentation of the ``np`` method.
+        np_ret : Optional[str]
+            The return type string of the ``np`` method that we want to
+            replace. (e.g. "out : ndarray")
+        np_ma_ret : Optional[str]
+            The return type string of the ``np.ma`` method.
+            (e.g. "out : ndarray or MaskedArray")
+        """
+        if np_ret is None:
+            return doc
+
+        if np_ret not in doc:
+            raise RuntimeError(
+                f"Failed to replace `{np_ret}` with `{np_ma_ret}`. "
+                f"The documentation string for return type, {np_ret}, is not "
+                f"found in the docstring for `np.{self._func.__name__}`. "
+                f"Fix the docstring for `np.{self._func.__name__}` or "
+                "update the expected string for return type."
+            )
+
+        if np_ma_ret is None:
+            raise RuntimeError(
+                "You must also specify the string that will "
+                f"replace `{np_ret}`"
+            )
+
+        return doc.replace(np_ret, np_ma_ret)
 
     def __call__(self, *args, **params):
         # Find the common parameters to the call and the definition
@@ -8142,7 +8176,12 @@ indices = np.indices
 ones = _convert2ma('ones', params=dict(fill_value=None, hardmask=False))
 ones_like = np.ones_like
 squeeze = np.squeeze
-zeros = _convert2ma('zeros', params=dict(fill_value=None, hardmask=False))
+zeros = _convert2ma(
+    'zeros',
+    params=dict(fill_value=None, hardmask=False),
+    np_ret='out : ndarray',
+    np_ma_ret='out : MaskedArray',
+)
 zeros_like = np.zeros_like
 
 
