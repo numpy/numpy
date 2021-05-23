52d5fe1ede45083d0783c3e2bbaee5c44df9d553
Carl Michal <michal@physics.ubc.ca>
2021-05-23 08:24:52 -0700

    Change fix of cpu_popcnt.c to use _mm_popcnt_u64/_mm_popcnt_u32 on GCC
    _builtin_popcount is always available, so the compile-time check always
    succeeds.

--- numpy/distutils/checks/cpu_popcnt.c
+++ numpy/distutils/checks/cpu_popcnt.c
@@ -4,26 +4,16 @@
     #include <popcntintrin.h>
 #endif
 
-#include <stdlib.h>
-
-int main(void)
+int main(int argc, char **argv)
 {
-    long long a = 0;
-    int b;
-    
-    a = random();
-    b = random();
-      
-#ifdef _MSC_VER
-    #ifdef _M_X64
+    // To make sure popcnt instructions are generated
+    // and been tested against the assembler
+    unsigned long long a = *((unsigned long long*)argv[argc-1]);
+    unsigned int b = *((unsigned int*)argv[argc-2]);
+
+#if defined(_M_X64) || defined(__x86_64__)    
     a = _mm_popcnt_u64(a);
-    #endif
-    b = _mm_popcnt_u32(b);
-#else
-    #ifdef __x86_64__
-    a = __builtin_popcountll(a);
-    #endif
-    b = __builtin_popcount(b);
 #endif
+    b = _mm_popcnt_u32(b);
     return (int)a + b;
 }
