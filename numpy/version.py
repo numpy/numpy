264cc43729acb08913f6c4759c325e07bc9facff
Jérôme Kieffer <jerome.kieffer@terre-adelie.org>
2021-05-31 15:46:54 +0200

    expose `short_version` as previously in version.py

--- numpy/version.py
+++ numpy/version.py
@@ -5,6 +5,7 @@ __ALL__ = ['version', 'full_version', 'git_revision', 'release']
 vinfo = get_versions()
 version: str = vinfo["version"]
 full_version: str = vinfo['version']
+short_version: str = vinfo['version']
 git_revision: str = vinfo['full-revisionid']
 release = 'dev0' not in version and '+' not in version
 
