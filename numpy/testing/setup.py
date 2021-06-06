22412b3f46451177286f8fc58509a69bb2d95731
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2021-06-06 17:05:48 +0200

    BLD: Add support for *.pyi data-files to `np.testing._private`

--- numpy/testing/setup.py
+++ numpy/testing/setup.py
@@ -7,6 +7,7 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('_private')
     config.add_subpackage('tests')
     config.add_data_files('*.pyi')
+    config.add_data_files('_private/*.pyi')
     return config
 
 if __name__ == '__main__':
