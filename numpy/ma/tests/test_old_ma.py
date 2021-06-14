b92df509ca6767f65891b1b0eac6e64ee7621ee4
iameskild <eskild@doublee.io>
2021-06-14 08:54:40 -0700

    Fix lint E225 complaint

--- numpy/ma/tests/test_old_ma.py
+++ numpy/ma/tests/test_old_ma.py
@@ -700,7 +700,7 @@ class TestMa:
     def test_assignment_by_condition(self):
         # Test for gh-18951
         a = array([1, 2, 3, 4], mask=[1, 0, 1, 0])
-        c = a>=3
+        c = a >= 3
         a[c] = 5
         assert_(a[2] is masked)
 
