6345920701a16c83a561f45edd7caf701a130a4d
Bas van Beek <b.f.van.beek@vu.nl>
2021-06-25 09:55:29 +0200

    TST: Update the typing tests

--- numpy/typing/tests/data/reveal/constants.py
+++ numpy/typing/tests/data/reveal/constants.py
@@ -18,27 +18,27 @@ reveal_type(np.pi)  # E: float
 reveal_type(np.ALLOW_THREADS)  # E: int
 reveal_type(np.BUFSIZE)  # E: Literal[8192]
 reveal_type(np.CLIP)  # E: Literal[0]
-reveal_type(np.ERR_CALL)  # E: int
-reveal_type(np.ERR_DEFAULT)  # E: int
-reveal_type(np.ERR_IGNORE)  # E: int
-reveal_type(np.ERR_LOG)  # E: int
-reveal_type(np.ERR_PRINT)  # E: int
-reveal_type(np.ERR_RAISE)  # E: int
-reveal_type(np.ERR_WARN)  # E: int
-reveal_type(np.FLOATING_POINT_SUPPORT)  # E: int
-reveal_type(np.FPE_DIVIDEBYZERO)  # E: int
-reveal_type(np.FPE_INVALID)  # E: int
-reveal_type(np.FPE_OVERFLOW)  # E: int
-reveal_type(np.FPE_UNDERFLOW)  # E: int
+reveal_type(np.ERR_CALL)  # E: Literal[3]
+reveal_type(np.ERR_DEFAULT)  # E: Literal[521]
+reveal_type(np.ERR_IGNORE)  # E: Literal[0]
+reveal_type(np.ERR_LOG)  # E: Literal[5]
+reveal_type(np.ERR_PRINT)  # E: Literal[4]
+reveal_type(np.ERR_RAISE)  # E: Literal[2]
+reveal_type(np.ERR_WARN)  # E: Literal[1]
+reveal_type(np.FLOATING_POINT_SUPPORT)  # E: Literal[1]
+reveal_type(np.FPE_DIVIDEBYZERO)  # E: Literal[1]
+reveal_type(np.FPE_INVALID)  # E: Literal[8]
+reveal_type(np.FPE_OVERFLOW)  # E: Literal[2]
+reveal_type(np.FPE_UNDERFLOW)  # E: Literal[4]
 reveal_type(np.MAXDIMS)  # E: Literal[32]
 reveal_type(np.MAY_SHARE_BOUNDS)  # E: Literal[0]
 reveal_type(np.MAY_SHARE_EXACT)  # E: Literal[-1]
 reveal_type(np.RAISE)  # E: Literal[2]
-reveal_type(np.SHIFT_DIVIDEBYZERO)  # E: int
-reveal_type(np.SHIFT_INVALID)  # E: int
-reveal_type(np.SHIFT_OVERFLOW)  # E: int
-reveal_type(np.SHIFT_UNDERFLOW)  # E: int
-reveal_type(np.UFUNC_BUFSIZE_DEFAULT)  # E: int
+reveal_type(np.SHIFT_DIVIDEBYZERO)  # E: Literal[0]
+reveal_type(np.SHIFT_INVALID)  # E: Literal[9]
+reveal_type(np.SHIFT_OVERFLOW)  # E: Literal[3]
+reveal_type(np.SHIFT_UNDERFLOW)  # E: Literal[6]
+reveal_type(np.UFUNC_BUFSIZE_DEFAULT)  # E: Literal[8192]
 reveal_type(np.WRAP)  # E: Literal[1]
 reveal_type(np.tracemalloc_domain)  # E: Literal[389047]
 
@@ -46,7 +46,7 @@ reveal_type(np.little_endian)  # E: bool
 reveal_type(np.True_)  # E: numpy.bool_
 reveal_type(np.False_)  # E: numpy.bool_
 
-reveal_type(np.UFUNC_PYVALS_NAME)  # E: str
+reveal_type(np.UFUNC_PYVALS_NAME)  # E: Literal['UFUNC_PYVALS']
 
 reveal_type(np.sctypeDict)  # E: dict
 reveal_type(np.sctypes)  # E: TypedDict
