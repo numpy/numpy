29974fba6810e1be7e8a2ba8322bd8c78a9012d0
Aaron Meurer <asmeurer@gmail.com>
2021-07-09 16:32:42 -0600

    Use tuples for internal type lists in the array API
    These are easier for type checkers to handle.

--- numpy/_array_api/_dtypes.py
+++ numpy/_array_api/_dtypes.py
@@ -15,10 +15,10 @@ float64 = np.dtype('float64')
 # Note: This name is changed
 bool = np.dtype('bool')
 
-_all_dtypes = [int8, int16, int32, int64, uint8, uint16, uint32, uint64,
-               float32, float64, bool]
-_boolean_dtypes = [bool]
-_floating_dtypes = [float32, float64]
-_integer_dtypes = [int8, int16, int32, int64, uint8, uint16, uint32, uint64]
-_integer_or_boolean_dtypes = [bool, int8, int16, int32, int64, uint8, uint16, uint32, uint64]
-_numeric_dtypes = [float32, float64, int8, int16, int32, int64, uint8, uint16, uint32, uint64]
+_all_dtypes = (int8, int16, int32, int64, uint8, uint16, uint32, uint64,
+               float32, float64, bool)
+_boolean_dtypes = (bool)
+_floating_dtypes = (float32, float64)
+_integer_dtypes = (int8, int16, int32, int64, uint8, uint16, uint32, uint64)
+_integer_or_boolean_dtypes = (bool, int8, int16, int32, int64, uint8, uint16, uint32, uint64)
+_numeric_dtypes = (float32, float64, int8, int16, int32, int64, uint8, uint16, uint32, uint64)
