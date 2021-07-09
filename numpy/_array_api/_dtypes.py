6765494edee2b90f239ae622abb5f3a7d218aa84
Aaron Meurer <asmeurer@gmail.com>
2021-07-09 17:58:39 -0600

    Fix typo

--- numpy/_array_api/_dtypes.py
+++ numpy/_array_api/_dtypes.py
@@ -17,7 +17,7 @@ bool = np.dtype('bool')
 
 _all_dtypes = (int8, int16, int32, int64, uint8, uint16, uint32, uint64,
                float32, float64, bool)
-_boolean_dtypes = (bool)
+_boolean_dtypes = (bool,)
 _floating_dtypes = (float32, float64)
 _integer_dtypes = (int8, int16, int32, int64, uint8, uint16, uint32, uint64)
 _integer_or_boolean_dtypes = (bool, int8, int16, int32, int64, uint8, uint16, uint32, uint64)
