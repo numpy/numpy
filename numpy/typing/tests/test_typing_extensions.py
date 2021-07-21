ee9855823c41c0f5d4d2aa81e1965e898a375fbe
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2021-07-21 11:43:10 +0200

    TST: Remodel the typing-extensions test after `test_full_reimport`
    Co-Authored-By: Sebastian Berg <sebastian@sipsolutions.net>

--- numpy/typing/tests/test_typing_extensions.py
+++ numpy/typing/tests/test_typing_extensions.py
@@ -1,35 +1,20 @@
 """Tests for the optional typing-extensions dependency."""
 
 import sys
-import types
-import inspect
-import importlib
+import textwrap
+import subprocess
 
-import typing_extensions
-import numpy.typing as npt
+CODE = textwrap.dedent(r"""
+    import sys
+    import importlib
 
+    assert "typing_extensions" not in sys.modules
+    assert "numpy.typing" not in sys.modules
 
-def _is_sub_module(obj: object) -> bool:
-    """Check if `obj` is a `numpy.typing` submodule."""
-    return inspect.ismodule(obj) and obj.__name__.startswith("numpy.typing")
-
-
-def _is_dunder(name: str) -> bool:
-    """Check whether `name` is a dunder."""
-    return name.startswith("__") and name.endswith("__")
-
-
-def _clear_attr(module: types.ModuleType) -> None:
-    """Clear all (non-dunder) module-level attributes."""
-    del_names = [name for name in vars(module) if not _is_dunder(name)]
-    for name in del_names:
-        delattr(module, name)
-
-
-MODULES = {"numpy.typing": npt}
-MODULES.update({
-    f"numpy.typing.{k}": v for k, v in vars(npt).items() if _is_sub_module(v)
-})
+    # Importing `typing_extensions` will now raise an `ImportError`
+    sys.modules["typing_extensions"] = None
+    assert importlib.import_module("numpy.typing")
+""")
 
 
 def test_no_typing_extensions() -> None:
@@ -42,15 +27,9 @@ def test_no_typing_extensions() -> None:
     impossible as it is an indirect hard dependency of pytest.
 
     """
-    assert "typing_extensions" in sys.modules
+    p = subprocess.run([sys.executable, '-c', CODE], capture_output=True)
+    if p.returncode:
+        raise AssertionError(
+            f"Non-zero return code: {p.returncode!r}\n\n{p.stderr.decode()}"
+        )
 
-    try:
-        sys.modules["typing_extensions"] = None
-        for name, module in MODULES.items():
-            _clear_attr(module)
-            assert importlib.reload(module), name
-    finally:
-        sys.modules["typing_extensions"] = typing_extensions
-        for module in MODULES.values():
-            _clear_attr(module)
-            importlib.reload(module)
