27f3b03dcc4fa2570825f9233f2e593c3c320673
Sebastian Berg <sebastian@sipsolutions.net>
2021-09-07 17:40:13 -0500

    TST: Add a test covering logical ufuncs for custom DTypes
    In particular, this covers a casting error that currently cannot
    be hit for normal ufuncs, because they already check casting during
    the legacy dtype resolution (which is called first).

--- numpy/core/tests/test_custom_dtypes.py
+++ numpy/core/tests/test_custom_dtypes.py
@@ -161,3 +161,23 @@ class TestSFloat:
         # Check that casting the output fails also (done by the ufunc here)
         with pytest.raises(TypeError):
             np.add(a, a, out=c, casting="safe")
+
+    @pytest.mark.parametrize("ufunc",
+            [np.logical_and, np.logical_or, np.logical_xor])
+    def test_logical_ufuncs_casts_to_bool(self, ufunc):
+        a = self._get_array(2.)
+        a[0] = 0.  # make sure first element is considered False.
+
+        float_equiv = a.astype(float)
+        expected = ufunc(float_equiv, float_equiv)
+        res = ufunc(a, a)
+        assert_array_equal(res, expected)
+
+        # also check that the same works for reductions:
+        expected = ufunc.reduce(float_equiv)
+        res = ufunc.reduce(a)
+        assert_array_equal(res, expected)
+
+        # The output casting does not match the bool, bool -> bool loop:
+        with pytest.raises(TypeError):
+            ufunc(a, a, out=np.empty(a.shape, dtype=int), casting="equiv")
