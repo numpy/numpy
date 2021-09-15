0baeeb163e008a829d195fba48f750f7b517ec61
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2021-09-15 16:35:26 +0200

    MAINT: Directly use `np.ndarray.__class_getitem__` for the definition of `npt.NDArray`

--- numpy/typing/_generic_alias.py
+++ numpy/typing/_generic_alias.py
@@ -205,12 +205,9 @@ else:
 
 ScalarType = TypeVar("ScalarType", bound=np.generic, covariant=True)
 
-if TYPE_CHECKING:
+if TYPE_CHECKING or sys.version_info >= (3, 9):
     _DType = np.dtype[ScalarType]
     NDArray = np.ndarray[Any, np.dtype[ScalarType]]
-elif sys.version_info >= (3, 9):
-    _DType = types.GenericAlias(np.dtype, (ScalarType,))
-    NDArray = types.GenericAlias(np.ndarray, (Any, _DType))
 else:
     _DType = _GenericAlias(np.dtype, (ScalarType,))
     NDArray = _GenericAlias(np.ndarray, (Any, _DType))
