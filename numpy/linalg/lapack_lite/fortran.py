a12e0d14eefae4b0f0d0de41205fc6d047b231b3
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-09-21 15:21:18 +0200

    REV: 30f8391
    This is a Python 2 script, we need to preserve Python 2 compatibility.

--- numpy/linalg/lapack_lite/fortran.py
+++ numpy/linalg/lapack_lite/fortran.py
@@ -45,6 +45,8 @@ class LineIterator:
         line = line.rstrip()
         return line
 
+    next = __next__
+
 
 class PushbackIterator:
     """PushbackIterator(iterable)
@@ -69,6 +71,8 @@ class PushbackIterator:
 
     def pushback(self, item):
         self.buffer.append(item)
+
+    next = __next__
 
 
 def fortranSourceLines(fo):
