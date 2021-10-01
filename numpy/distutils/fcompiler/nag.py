acb0e9eb57674288ef18bf39908026bc4f42c4a4
Dima Pasechnik <dima@pasechnik.info>
2021-10-01 11:43:21 +0100

    MAINT: correct linker flags for NAG Fortran compiler (#20010)
    This reverts commit 1ac3cbea412c7d9ff467a8e599609621887c6cdd
    and replaces #19992 with documented nagfor option -unsharedrts, not
    -unsharedf95, as pointed out by ThemosTsikas

--- numpy/distutils/fcompiler/nag.py
+++ numpy/distutils/fcompiler/nag.py
@@ -15,9 +15,6 @@ class BaseNAGFCompiler(FCompiler):
             return None
 
     def get_flags_linker_so(self):
-        if sys.platform == 'darwin':
-            return ['-unsharedf95', 
-                    '-Wl,-bundle,-flat_namespace,-undefined,suppress']
         return ["-Wl,-shared"]
     def get_flags_opt(self):
         return ['-O4']
@@ -39,6 +36,10 @@ class NAGFCompiler(BaseNAGFCompiler):
         'ranlib'       : ["ranlib"]
         }
 
+    def get_flags_linker_so(self):
+        if sys.platform == 'darwin':
+            return ['-unsharedf95', '-Wl,-bundle,-flat_namespace,-undefined,suppress']
+        return BaseNAGFCompiler.get_flags_linker_so(self)
     def get_flags_arch(self):
         version = self.get_version()
         if version and version < '5.1':
@@ -63,6 +64,11 @@ class NAGFORCompiler(BaseNAGFCompiler):
         'ranlib'       : ["ranlib"]
         }
 
+    def get_flags_linker_so(self):
+        if sys.platform == 'darwin':
+            return ['-unsharedrts',
+                    '-Wl,-bundle,-flat_namespace,-undefined,suppress']
+        return BaseNAGFCompiler.get_flags_linker_so(self)
     def get_flags_debug(self):
         version = self.get_version()
         if version and version > '6.1':
