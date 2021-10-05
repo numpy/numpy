173a7bbcd575f0c4c80540ed54b9dd055ded0ff3
Rohit Goswami <rog32@hi.is>
2021-10-05 01:55:26 +0000

    ENH: Add more options for the new front-end

--- numpy/f2py/f2pyarg.py
+++ numpy/f2py/f2pyarg.py
@@ -17,6 +17,28 @@ implementation in terms of subparsers
 
 import argparse
 import logging
+import pathlib
+
+###########
+# Helpers #
+###########
+
+
+def check_fortran(fname):
+    fpname = pathlib.Path(fname)
+    if fpname.suffix.lower() in [".f90", ".f", ".f77"]:
+        return fpname
+    else:
+        logger.warning(
+            """Does not look like a standard fortran file ending in *.f90, *.f or
+            *.f77, continuing against better judgement"""
+        )
+        return fpname
+
+
+##########
+# Parser #
+##########
 
 
 parser = argparse.ArgumentParser(
@@ -25,7 +47,11 @@ parser = argparse.ArgumentParser(
     (<modulename>module.c) that contains wrappers for given fortran
     functions so that they can be called from Python. With the -c
     option the corresponding extension modules are built.""",
-    add_help=False,
+    add_help=False,  # Since -h is taken...
+    # Format to look like f2py
+    formatter_class=lambda prog: argparse.HelpFormatter(
+        prog, max_help_position=100, width=80
+    ),
 )
 
 build_helpers = parser.add_argument_group("build helpers")
@@ -34,16 +60,36 @@ generate_wrappers = parser.add_argument_group("wrappers and signature files")
 # Common #
 ##########
 
+# --help is still free
+parser.add_argument("--help", action="store_true", help="Print the help")
+
+# TODO: Remove?
+parser.add_argument(
+    "--2d-numpy",
+    type=bool,
+    default=True,
+    help="Use f2py with Numeric support [DEFAULT]",
+)
+
+parser.add_argument(
+    "Fortran Functions",
+    metavar="<fortran functions>",
+    type=str,
+    nargs="*",
+    default="ALL",
+    help="""Names of fortran routines for which Python C/API
+                   functions will be generated. Default is all that are found
+                   in <fortran files>.""",
+)
+
 parser.add_argument(
     "Fortran Files",
     metavar="<fortran files>",
-    type=str,
-    nargs="*",
+    type=check_fortran,  # Returns a pathlib.Path
+    nargs="*",  # As many as present, 0 OK
     help="""Paths to fortran/signature files that will be scanned for
                    <fortran functions> in order to determine their signatures.""",
 )
-
-parser.add_argument("--2d-numpy", default=True, help="Use f2py with Numeric support")
 
 # Wrappers/Signatures #
 #######################
@@ -52,15 +98,14 @@ generate_wrappers.add_argument(
     # TODO: Seriously consider scrapping this naming convention
     "-h",
     "--hint-signature",
-    type=str,
+    type=pathlib.Path,
     nargs=1,
-    help="""Write signatures of the fortran routines to file <filename> and
-    exit. You can then edit <filename> and use it instead of
-    <fortran files>. If <filename>==stdout then the signatures
-    are printed to stdout.""",
+    help=r"""
+    Write signatures of the fortran routines to file <filename> and exit. You
+    can then edit <filename> and use it instead of <fortran files>. If
+    <filename>==stdout then the signatures are printed to stdout.
+    """,
 )
-
-parser.add_argument("--help", action="store_true", help="Print the help")
 
 
 def process_args(args):
