25fb2de8512bc066961fa566c1cc84477dfbb579
Bas van Beek <b.f.van.beek@vu.nl>
2021-10-09 13:03:44 +0200

    ENH: Add annotations for `np.matrix`

--- numpy/matrixlib/__init__.pyi
+++ numpy/matrixlib/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Any, List
+from typing import List
 
 from numpy._pytesttester import PytestTester
 
@@ -6,10 +6,12 @@ from numpy import (
     matrix as matrix,
 )
 
+from numpy.matrixlib.defmatrix import (
+    bmat as bmat,
+    mat as mat,
+    asmatrix as asmatrix,
+)
+
 __all__: List[str]
 __path__: List[str]
 test: PytestTester
-
-def bmat(obj, ldict=..., gdict=...): ...
-def asmatrix(data, dtype=...): ...
-mat = asmatrix
