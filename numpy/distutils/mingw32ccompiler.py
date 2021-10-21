efda582a548d7d620d942f7049a202115f426e1d
Matthew Brett <matthew.brett@gmail.com>
2021-10-21 17:24:04 +0100

    Patch to allow for 2 as a minor version (!)
    The float representation of e.g. 14.2 is not exact, leading to a minor
    version number of int(1.999999) == 1 in the original code.

--- numpy/distutils/mingw32ccompiler.py
+++ numpy/distutils/mingw32ccompiler.py
@@ -647,11 +647,9 @@ def generate_manifest(config):
     if msver is not None:
         if msver >= 8:
             check_embedded_msvcr_match_linked(msver)
-            ma = int(msver)
-            mi = int((msver - ma) * 10)
+            ma_str, mi_str = str(msver).split('.')
             # Write the manifest file
-            manxml = msvc_manifest_xml(ma, mi)
-            man = open(manifest_name(config), "w")
-            config.temp_files.append(manifest_name(config))
-            man.write(manxml)
-            man.close()
+            manxml = msvc_manifest_xml(int(ma_str), int(mi_str))
+            with open(manifest_name(config), "w") as man:
+                config.temp_files.append(manifest_name(config))
+                man.write(manxml)
