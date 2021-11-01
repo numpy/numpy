e3720c8c0be849f1e68577bdec87166dd19c140d
vinith2 <85550536+vinith2@users.noreply.github.com>
2021-11-01 15:39:45 +0100

    Adding Examples to numpy.roll()
    Added exampled where we use tuple in for axis and shifts separately. This shows how the  mulitple shift examples works

--- numpy/core/numeric.py
+++ numpy/core/numeric.py
@@ -1206,6 +1206,12 @@ def roll(a, shift, axis=None):
     >>> np.roll(x2, -1, axis=1)
     array([[1, 2, 3, 4, 0],
            [6, 7, 8, 9, 5]])
+    >>> np.roll(x2, (1,1), axis=(1,0))
+    array([[9, 5, 6, 7, 8],
+           [4, 0, 1, 2, 3]])
+    >>> np.roll(x2, (2,1), axis=(1,0))
+    array([[8, 9, 5, 6, 7],
+           [3, 4, 0, 1, 2]])
 
     """
     a = asanyarray(a)
