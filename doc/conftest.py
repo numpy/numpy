ac8e7d43d912ce802a1a26d233468492e71d77ad
Pierre de Buyl <pdebuyl@pdebuyl.be>
2021-11-15 10:20:15 +0100

    remove duplicate pytest.ini, use agg backend

--- doc/conftest.py
+++ doc/conftest.py
@@ -3,6 +3,9 @@ Pytest configuration and fixtures for the Numpy test suite.
 """
 import pytest
 import numpy
+import matplotlib
+
+matplotlib.use('agg', force=True)
 
 @pytest.fixture(autouse=True)
 def add_np(doctest_namespace):
