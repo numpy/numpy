6af83ae81d4ad15f178d9f6d3256bd4535613f6b
Rohit Goswami <rog32@hi.is>
2021-11-28 14:05:28 +0000

    STY: F2PY tests shouldn't print

--- numpy/f2py/tests/test_string.py
+++ numpy/f2py/tests/test_string.py
@@ -34,19 +34,10 @@ C FILE: STRING.F
       CHARACTER*(*) C,D
 Cf2py intent(in) a,c
 Cf2py intent(inout) b,d
-      PRINT*, "A=",A
-      PRINT*, "B=",B
-      PRINT*, "C=",C
-      PRINT*, "D=",D
-      PRINT*, "CHANGE A,B,C,D"
       A(1:1) = 'A'
       B(1:1) = 'B'
       C(1:1) = 'C'
       D(1:1) = 'D'
-      PRINT*, "A=",A
-      PRINT*, "B=",B
-      PRINT*, "C=",C
-      PRINT*, "D=",D
       END
 C END OF FILE STRING.F
         """
