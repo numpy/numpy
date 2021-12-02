5a15a1c45c7a209e968a038d67721a00c0f0a336
Thomas Green <tomgreen66@hotmail.com>
2021-12-02 23:18:00 +0000

    Update armccompiler.py

--- numpy/distutils/armccompiler.py
+++ numpy/distutils/armccompiler.py
@@ -12,12 +12,17 @@ class ArmCCompiler(UnixCCompiler):
     cc_exe = 'armclang'
     cxx_exe = 'armclang++'
 
-    def __init__ (self, verbose=0, dry_run=0, force=0):                        
-        UnixCCompiler.__init__ (self, verbose, dry_run, force)                 
+    def __init__(self, verbose=0, dry_run=0, force=0):
+        UnixCCompiler.__init__(self, verbose, dry_run, force)
         cc_compiler = self.cc_exe
         cxx_compiler = self.cxx_exe
-        self.set_executables(compiler=cc_compiler + ' -mcpu=native -O3 -fPIC',       
-                             compiler_so=cc_compiler + ' -mcpu=native -O3 -fPIC',    
-                             compiler_cxx=cxx_compiler + ' -mcpu=native -O3 -fPIC',  
-                             linker_exe=cc_compiler + ' -lamath',              
-                             linker_so=cc_compiler + ' -lamath -shared')
+        self.set_executables(compiler=cc_compiler +
+                                      ' -mcpu=native -O3 -fPIC',
+                             compiler_so=cc_compiler +
+                                         ' -mcpu=native -O3 -fPIC',
+                             compiler_cxx=cxx_compiler +
+                                          ' -mcpu=native -O3 -fPIC',
+                             linker_exe=cc_compiler +
+                                        ' -lamath',
+                             linker_so=cc_compiler +
+                                       ' -lamath -shared')
