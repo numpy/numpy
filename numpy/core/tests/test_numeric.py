878ad3db8e6da68a112ba3d09f0a58109f7cb143
Omar Ali <Omarh90@gmail.com>
2021-12-25 00:29:27 -0800

    BUG: Added dtype conversions w/ test. Closes #19138.

--- numpy/core/tests/test_numeric.py
+++ numpy/core/tests/test_numeric.py
@@ -3383,6 +3383,14 @@ class TestCross:
         for axisc in range(-2, 2):
             assert_equal(np.cross(u, u, axisc=axisc).shape, (3, 4))
 
+    def test_nested_diff_types(self):        
+        #gh-19138
+        u=np.array([[195,8,9]],np.uint8)
+        v=np.array([250,166,68],np.int)
+        z=np.array([[950, 11010, -30370]], dtype=np.int32)
+        assert_equal(np.cross(v,u), z)
+        assert_equal(np.cross(u,v), -z)
+
 
 def test_outer_out_param():
     arr1 = np.ones((5,))
