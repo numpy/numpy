b5f179b1e969f13283d22c9ce56f66f40df661b6
Bas van Beek <43369155+BvB93@users.noreply.github.com>
2021-12-26 13:38:13 +0100

    TYP,TST: Bump mypy to 0.930

--- numpy/typing/tests/data/fail/scalars.pyi
+++ numpy/typing/tests/data/fail/scalars.pyi
@@ -31,7 +31,7 @@ np.complex64([])  # E: incompatible type
 np.complex64(1, 2)  # E: Too many arguments
 # TODO: protocols (can't check for non-existent protocols w/ __getattr__)
 
-np.datetime64(0)  # E: non-matching overload
+np.datetime64(0)  # E: No overload variant
 
 class A:
     def __float__(self):
