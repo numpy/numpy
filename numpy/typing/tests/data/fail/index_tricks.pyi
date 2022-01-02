2fbc8fa2424d0c2e4403f259732fbb674db0e229
Bas van Beek <b.f.van.beek@vu.nl>
2022-01-02 14:57:30 +0100

    TYP,MAINT: Allow `ndindex` to accept integer tuples

--- numpy/typing/tests/data/fail/index_tricks.pyi
+++ numpy/typing/tests/data/fail/index_tricks.pyi
@@ -4,6 +4,7 @@ import numpy as np
 AR_LIKE_i: List[int]
 AR_LIKE_f: List[float]
 
+np.ndindex([1, 2, 3])  # E: No overload variant
 np.unravel_index(AR_LIKE_f, (1, 2, 3))  # E: incompatible type
 np.ravel_multi_index(AR_LIKE_i, (1, 2, 3), mode="bob")  # E: No overload variant
 np.mgrid[1]  # E: Invalid index type
