c62e1dbc1c038606d2466f0816611b593f19a56d
Charles Harris <charlesr.harris@gmail.com>
2022-01-06 16:51:20 -0700

    MAINT: Raise RuntimeError if setuptools version is too recent.
    NumPy does not build with setuptools versions greater than '60.0.0'.
    Check the version when setuptools is imported in setup.py and raise
    a RuntimeError if the version is too recent. That way we avoid people
    opening issues when their build fails for that reason.
    
    Closes #20692.

--- setup.py
+++ setup.py
@@ -85,6 +85,10 @@ if os.path.exists('MANIFEST'):
 # so that it is in sys.modules
 import numpy.distutils.command.sdist
 import setuptools
+if int(setuptools.__version__.split('.')[0]) >= 60:
+    raise RuntimeError(
+        "Setuptools version is '{}', version < '60.0.0' is required. "
+        "See pyproject.toml".format(setuptools.__version__))
 
 # Initialize cmdclass from versioneer
 from numpy.distutils.core import numpy_cmdclass
