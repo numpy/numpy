d5ea58a9ad04e56f442d6dcb0f4b8d482d51f206
Matthias Bussonnier <bussonniermatthias@gmail.com>
2022-01-12 16:43:32 +0100

    ENH: Add a numeric __version_info__ to numpy.
    This makes it easier and less error prone to write conditional code that
    depends on numpy version.
    
    We limit version tuple to be numeric only, and only major/minor/patch.
    
    Dependencies can now check numpy version simply with:
    
    >>> import numpy
    ... assert numpy.version.__version_info__ > (1, 22)
    
    Without relying on more complex dependencies like
    `packaging.version.parse`, or distutils' LooseVersion (which is now
    deprecated).
    
    This also prevent roll-out of custom version parsing code that might be
    error prone, or worse, comparison of strings.
    
    We do not go as far as Matplotlib that use a named tuple and include
    a/b/rc in the fourth component of the name tuple, but this could always
    be added latter.
    
    Co-authored-by: Bas van Beek <43369155+BvB93@users.noreply.github.com>
    Co-authored-by: Clément Robert <cr52@protonmail.com>

--- numpy/version.py
+++ numpy/version.py
@@ -20,4 +20,34 @@ release = 'dev0' not in version and '+' not in version
 full_version = version
 short_version = version.split("+")[0]
 
-del get_versions, vinfo
+
+def make_version_info() -> tuple[int, int, int]:
+    """
+    We want to expose a numeric version tuple to make it easier for
+    dependencies to have conditional code without having to parse our version
+    string.
+
+    It can be tricky as 1) you don't want to compare string. 2) it can be
+    tempting to just split on dot and map convert to int, but that will fail on
+    rc, and others.
+
+    It is ok to drop all the non-numeric items as conditional code is unlikely
+    to rely on those values. We also don't add the non-numeric elements at the
+    end, as strings should anyway not be compared.
+
+    Matplotlib goes a bit further and make that a named tuple and include the
+    a/b/rc/... in the fourth place in the tuple, but this is generally
+    unnecessary. Though it could be added in a backward compatible manner.
+    """
+    import re
+
+    str_major, str_minor, str_patch_extra = short_version.split(".")[:3]
+    major = int(str_major)
+    minor = int(str_minor)
+    patch = int(re.findall(r"\d+", str_patch_extra)[0])
+    return (major, minor, patch)
+
+
+__version_info__ = make_version_info()
+
+del get_versions, vinfo, make_version_info
