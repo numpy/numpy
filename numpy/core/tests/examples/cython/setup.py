1edd6407d008fcaa282a6058ae355025c26517fd
Leo Singer <leo.singer@ligo.org>
2022-01-13 20:47:50 -0500

    TST: Split example package, skip limited API test for debug

--- numpy/core/tests/examples/cython/setup.py
+++ numpy/core/tests/examples/cython/setup.py
@@ -9,24 +9,16 @@ from Cython.Build import cythonize
 from setuptools.extension import Extension
 import os
 
-include_dirs = [np.get_include()]
 macros = [("NPY_NO_DEPRECATED_API", 0)]
 
 checks = Extension(
     "checks",
     sources=[os.path.join('.', "checks.pyx")],
-    include_dirs=include_dirs,
+    include_dirs=[np.get_include()],
     define_macros=macros,
 )
 
-example_limited_api = Extension(
-    "example_limited_api",
-    sources=[os.path.join('.', "example_limited_api.c")],
-    include_dirs=include_dirs,
-    define_macros=macros,
-)
-
-extensions = [checks, example_limited_api]
+extensions = [checks]
 
 setup(
     ext_modules=cythonize(extensions)
