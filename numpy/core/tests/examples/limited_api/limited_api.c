593b1b28b685a79838f88ff9932a0cfb1859a910
Leo Singer <leo.singer@ligo.org>
2022-01-13 20:47:50 -0500

    TST: Split example package, skip limited API test for debug

--- numpy/core/tests/examples/limited_api/limited_api.c
+++ numpy/core/tests/examples/limited_api/limited_api.c
@@ -1,8 +1,3 @@
-/*
- * Test that third-party extensions that use the Numpy C API can be built with
- * the limited Python C API (see https://docs.python.org/3/c-api/stable.html).
- */
-
 #define Py_LIMITED_API 0x03060000
 
 #include <Python.h>
@@ -11,10 +6,10 @@
 
 static PyModuleDef moduledef = {
     .m_base = PyModuleDef_HEAD_INIT,
-    .m_name = "example_limited_api"
+    .m_name = "limited_api"
 };
 
-PyMODINIT_FUNC PyInit_example_limited_api(void)
+PyMODINIT_FUNC PyInit_limited_api(void)
 {
     import_array();
     import_umath();
