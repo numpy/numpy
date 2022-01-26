172c40943ac53fb350f20dc69bf17476e71ff41f
Sebastian Berg <sebastian@sipsolutions.net>
2022-01-26 13:44:52 -0600

    MAINT: Mark strides as `const` for the `get_loop` function
    They should have been marked as const to begin with, this just
    adds the quality.

--- numpy/core/src/multiarray/array_method.h
+++ numpy/core/src/multiarray/array_method.h
@@ -77,7 +77,7 @@ typedef NPY_CASTING (resolve_descriptors_function)(
 typedef int (get_loop_function)(
         PyArrayMethod_Context *context,
         int aligned, int move_references,
-        npy_intp *strides,
+        const npy_intp *strides,
         PyArrayMethod_StridedLoop **out_loop,
         NpyAuxData **out_transferdata,
         NPY_ARRAYMETHOD_FLAGS *flags);
@@ -224,7 +224,7 @@ extern NPY_NO_EXPORT PyTypeObject PyBoundArrayMethod_Type;
 NPY_NO_EXPORT int
 npy_default_get_strided_loop(
         PyArrayMethod_Context *context,
-        int aligned, int NPY_UNUSED(move_references), npy_intp *strides,
+        int aligned, int NPY_UNUSED(move_references), const npy_intp *strides,
         PyArrayMethod_StridedLoop **out_loop, NpyAuxData **out_transferdata,
         NPY_ARRAYMETHOD_FLAGS *flags);
 
