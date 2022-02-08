dc00b126ca86b917f29fdee2b897e9fd0960684f
Tyler Reddy <tyler.je.reddy@gmail.com>
2022-02-08 10:00:48 -0700

    DOC: fix SciPy intersphinx link.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -298,7 +298,7 @@ texinfo_documents = [
 intersphinx_mapping = {
     'neps': ('https://numpy.org/neps', None),
     'python': ('https://docs.python.org/3', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy/', None),
     'matplotlib': ('https://matplotlib.org/stable', None),
     'imageio': ('https://imageio.readthedocs.io/en/stable', None),
     'skimage': ('https://scikit-image.org/docs/stable', None),
