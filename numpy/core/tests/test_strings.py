e8c473796644d98bf3c542f7aa20eedf0cba6a57
Sebastian Berg <sebastian@sipsolutions.net>
2022-02-14 13:16:57 -0600

    TST: Add test for empty string comparisons

--- numpy/core/tests/test_strings.py
+++ numpy/core/tests/test_strings.py
@@ -70,3 +70,16 @@ def test_string_comparisons(op, ufunc, sym, dtypes, aligned):
     assert_array_equal(op(arr2, arr), expected)
     assert_array_equal(ufunc(arr2, arr), expected)
     assert_array_equal(np.compare_chararrays(arr2, arr, sym, False), expected)
+
+
+@pytest.mark.parametrize(["op", "ufunc", "sym"], COMPARISONS)
+@pytest.mark.parametrize("dtypes", [
+        ("S2", "S2"), ("S2", "S10"), ("<U1", "<U1"), ("<U1", ">U10")])
+def test_string_comparisons_empty(op, ufunc, sym, dtypes):
+    arr = np.empty((1, 0, 1, 5), dtype=dtypes[0])
+    arr2 = np.empty((100, 1, 0, 1), dtype=dtypes[1])
+
+    expected = np.empty(np.broadcast_shapes(arr.shape, arr2.shape), dtype=bool)
+    assert_array_equal(op(arr, arr2), expected)
+    assert_array_equal(ufunc(arr, arr2), expected)
+    assert_array_equal(np.compare_chararrays(arr, arr2, sym, False), expected)
