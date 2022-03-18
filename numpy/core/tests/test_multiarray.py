928afc616644cee22a77bdf7d9bca31d6bb3fabb
Sebastian Berg <sebastian@sipsolutions.net>
2022-03-18 13:56:03 -0700

    BUG: Catch error if array-priority is not float compatible
    This is a bit cleaner than before.  This shouldn't lead to serious
    issues, normally just a SystemError, because the error is not cleared.
    But maybe it is a bit cleaner.

--- numpy/core/tests/test_multiarray.py
+++ numpy/core/tests/test_multiarray.py
@@ -3694,6 +3694,22 @@ class TestBinop:
         check(make_obj(np.ndarray, array_ufunc=None), True, False, False,
               check_scalar=False)
 
+    def test_ufunc_binop_bad_array_priority(self):
+        # Mainly checks that this does not crash.  The second array has a lower
+        # priority than -1 ("error value").  If the __radd__ actually exists,
+        # bad things can happen (I think via the scalar paths).
+        class BadPriority():
+            __array_priority__ = None
+
+            def __radd__(self, other):
+                return "result"
+
+        class LowPriority(np.ndarray):
+            __array_priority__ = -1000
+
+        with pytest.raises(TypeError):
+            np.arange(3).view(LowPriority) + BadPriority()
+
     def test_ufunc_override_normalize_signature(self):
         # gh-5674
         class SomeClass:
