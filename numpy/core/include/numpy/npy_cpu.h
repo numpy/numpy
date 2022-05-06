ff7dda4078166bb363cd9a9ace81777c817917b2
Zhang Na <zhangna@loongson.cn>
2022-05-06 15:07:26 +0800

    BLD: Add LoongArch support

--- numpy/core/include/numpy/npy_cpu.h
+++ numpy/core/include/numpy/npy_cpu.h
@@ -18,6 +18,7 @@
  *              NPY_CPU_ARCEL
  *              NPY_CPU_ARCEB
  *              NPY_CPU_RISCV64
+ *              NPY_CPU_LOONGARCH
  *              NPY_CPU_WASM
  */
 #ifndef _NPY_CPUARCH_H_
@@ -102,6 +103,8 @@
     #define NPY_CPU_ARCEB
 #elif defined(__riscv) && defined(__riscv_xlen) && __riscv_xlen == 64
     #define NPY_CPU_RISCV64
+#elif defined(__loongarch__)
+    #define NPY_CPU_LOONGARCH
 #elif defined(__EMSCRIPTEN__)
     /* __EMSCRIPTEN__ is defined by emscripten: an LLVM-to-Web compiler */
     #define NPY_CPU_WASM
