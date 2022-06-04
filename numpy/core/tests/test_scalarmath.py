d8925f530755a2a26d4a11000e42828a041cbe1b
Meekail Zain <zainmeekail@gmail.com>
2022-06-03 23:24:44 -0400

    Linting

--- numpy/core/tests/test_scalarmath.py
+++ numpy/core/tests/test_scalarmath.py
@@ -898,7 +898,8 @@ def test_scalar_integer_operation_overflow(dtype, operation):
 @pytest.mark.parametrize("operation", [
         lambda min, neg_1: abs(min),
         lambda min, neg_1: min * neg_1,
-        pytest.param(lambda min, neg_1: min // neg_1, marks=pytest.mark.xfail)], ids=["abs", "*", "//"])
+        pytest.param(lambda min, neg_1: min // neg_1,
+            marks=pytest.mark.xfail)], ids=["abs", "*", "//"])
 def test_scalar_signed_integer_overflow(dtype, operation):
     # The minimum signed integer can "overflow" for some additional operations
     st = np.dtype(dtype).type
