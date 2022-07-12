019c0e8078aa45a68b4e4e078f181831bf687016
Namami Shanker <namami2011@gmail.com>
2022-07-12 20:04:32 +0530

    ENH: Use context manager for build_dir management

--- numpy/f2py/utils.py
+++ numpy/f2py/utils.py
@@ -1,7 +1,29 @@
 """Global f2py utilities."""
 
 import os
+import contextlib
+import tempfile
+import pathlib
+import shutil
 
 def get_f2py_dir():
 	"""Return the directory where f2py is installed."""
 	return os.path.dirname(os.path.abspath(__file__))
+
+@contextlib.contextmanager
+def open_build_dir(build_dir: list or None, compile: bool):
+	remove_build_dir: bool = False
+	if(type(build_dir) is list and build_dir[0] is not None):
+		build_dir = build_dir[0]
+	try:
+		if build_dir is None:
+			if(compile):
+				remove_build_dir = True
+				build_dir = tempfile.mkdtemp()
+			else:
+				build_dir = pathlib.Path.cwd()
+		else:
+			build_dir = pathlib.Path(build_dir).absolute()
+		yield build_dir
+	finally:
+		shutil.rmtree(build_dir) if remove_build_dir else None
