6b8d55e66e532d66e1701ad039b4cda306839b3f
Brigitta Sipőcz <bsipocz@gmail.com>
2022-07-17 05:58:53 -0700

    BUG: Fix masked median multiple masked arrays (#21999)
    Fixed issue that occurs when trying to take the median of a list of masked arrays.
    Added a check to see if the input is a list then converts to a masked array.
    See issue #10757 for more information.
    
    Co-authored-by: jsclose <jsclose@umich.edu>

--- numpy/ma/extras.py
+++ numpy/ma/extras.py
@@ -723,7 +723,10 @@ def median(a, axis=None, out=None, overwrite_input=False, keepdims=False):
            fill_value=1e+20)
 
     """
-    if not hasattr(a, 'mask'):
+
+    a = np.ma.asarray(a)
+
+    if a.mask is np.ma.nomask:
         m = np.median(getdata(a, subok=True), axis=axis,
                       out=out, overwrite_input=overwrite_input,
                       keepdims=keepdims)
