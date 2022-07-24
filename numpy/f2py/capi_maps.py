7fb2af14da4be3c68b7db1bc13a906f150b4ed35
Namami Shanker <namami2011@gmail.com>
2022-07-24 15:13:33 +0530

    FIX: Replace os.path with pathlib.Path

--- numpy/f2py/capi_maps.py
+++ numpy/f2py/capi_maps.py
@@ -16,7 +16,7 @@ f2py_version = __version__.version
 
 import copy
 import re
-import os
+from pathlib import Path
 from .crackfortran import markoutercomma
 from . import cb_rules
 
@@ -141,8 +141,8 @@ def load_f2cmap_file(f2cmap_file):
 
     if f2cmap_file is None:
         # Default value
-        f2cmap_file = '.f2py_f2cmap'
-    if not os.path.isfile(f2cmap_file):
+        f2cmap_file = Path.cwd() / Path('.f2py_f2cmap')
+    if not f2cmap_file.is_file():
         return
 
     # User defined additions to f2cmap_all.
@@ -245,7 +245,7 @@ def getctype(var):
                     except KeyError:
                         errmess('getctype: "%s(kind=%s)" is mapped to C "%s" (to override define dict(%s = dict(%s="<C typespec>")) in %s/.f2py_f2cmap file).\n'
                                 % (typespec, var['kindselector']['kind'], ctype,
-                                   typespec, var['kindselector']['kind'], os.getcwd()))
+                                   typespec, var['kindselector']['kind'], Path.cwd()))
     else:
         if not isexternal(var):
             errmess('getctype: No C-type found in "%s", assuming void.\n' % var)
