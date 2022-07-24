b93e05442b744d00e85d255e77dce9baf50d44d8
Namami Shanker <namami2011@gmail.com>
2022-07-24 16:23:19 +0530

    MAINT: No need to use try..finally

--- numpy/f2py/utils.py
+++ numpy/f2py/utils.py
@@ -16,15 +16,13 @@ def open_build_dir(build_dir: Optional[list[Path]], compile: bool):
 	remove_build_dir: bool = False
 	if(isinstance(build_dir, list)):
 		build_dir = build_dir[0] if build_dir else None
-	try:
-		if build_dir is None:
-			if(compile):
-				remove_build_dir = True
-				build_dir = tempfile.mkdtemp()
-			else:
-				build_dir = Path.cwd()
+	if build_dir is None:
+		if(compile):
+			remove_build_dir = True
+			build_dir = tempfile.mkdtemp()
 		else:
-			build_dir = Path(build_dir).absolute()
-		yield build_dir
-	finally:
-		shutil.rmtree(build_dir) if remove_build_dir else None
+			build_dir = Path.cwd()
+	else:
+		build_dir = Path(build_dir).absolute()
+	yield build_dir
+	shutil.rmtree(build_dir) if remove_build_dir else None
