ac164c7514eb05961a5fe9f7da08bd7746cda10d
Namami Shanker <namami2011@gmail.com>
2022-08-07 20:12:48 +0530

    ENH: Add --backend flag for meson integration

--- numpy/f2py/f2pyarg.py
+++ numpy/f2py/f2pyarg.py
@@ -20,6 +20,7 @@ from __future__ import annotations
 import argparse
 import logging
 import pathlib
+import enum
 
 from numpy.version import version as __version__
 
@@ -158,6 +159,36 @@ class ProcessMacros(argparse.Action):
                 items.append((value, None))
         setattr(namespace, self.dest, items)
 
+class EnumAction(argparse.Action):
+    """
+    Argparse action for handling Enums
+    """
+    def __init__(self, **kwargs):
+        # Pop off the type value
+        enum_type = kwargs.pop("type", None)
+
+        # Ensure an Enum subclass is provided
+        if enum_type is None:
+            raise ValueError("type must be assigned an Enum when using EnumAction")
+        if not issubclass(enum_type, enum.Enum):
+            raise TypeError("type must be an Enum when using EnumAction")
+
+        # Generate choices from the Enum
+        kwargs.setdefault("choices", tuple(e.value for e in enum_type))
+
+        super(EnumAction, self).__init__(**kwargs)
+
+        self._enum = enum_type
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Convert value back into an Enum
+        value = self._enum(values)
+        setattr(namespace, self.dest, value)
+
+
+class Backends(enum.Enum):
+    Meson = "meson"
+    Distutils = "distutils"
 
 ##########
 # Parser #
@@ -583,6 +614,10 @@ build_helpers.add_argument(
     help="The link helpers for numpy distutils"
 )
 
+parser.add_argument('--backend',
+                    type=Backends,
+                    default="distutils",
+                    action=EnumAction)
 
 # The rest, only works for files, since we expect:
 #   <filename>.o <filename>.so <filename>.a
