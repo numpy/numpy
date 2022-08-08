fbdf0d30d4acc577306db20a33183b0b93f8afec
GavinZhang <zhanggan@cn.ibm.com>
2022-08-08 12:43:58 +0800

    DEV: Make the numpy.distutils support os400 similar as aix.[ci skip]

--- numpy/distutils/ccompiler.py
+++ numpy/distutils/ccompiler.py
@@ -686,7 +686,7 @@ def CCompiler_cxx_compiler(self):
     cxx.compiler_cxx = cxx.compiler_cxx
     cxx.compiler_so = [cxx.compiler_cxx[0]] + \
                       sanitize_cxx_flags(cxx.compiler_so[1:])
-    if sys.platform.startswith('aix') and 'ld_so_aix' in cxx.linker_so[0]:
+    if sys.platform.startswith(('aix','os400')) and 'ld_so_aix' in cxx.linker_so[0]:
         # AIX needs the ld_so_aix script included with Python
         cxx.linker_so = [cxx.linker_so[0], cxx.compiler_cxx[0]] \
                         + cxx.linker_so[2:]
