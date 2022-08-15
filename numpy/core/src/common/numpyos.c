759e0cdbc2642211ad2f0adce3ef247428a1e3b3
mattip <matti.picus@gmail.com>
2022-08-15 12:47:03 +0300

    strtoull and strtoll are mandatory

--- numpy/core/src/common/numpyos.c
+++ numpy/core/src/common/numpyos.c
@@ -770,27 +770,12 @@ NumPyOS_ascii_ftoLf(FILE *fp, long double *value)
 NPY_NO_EXPORT npy_longlong
 NumPyOS_strtoll(const char *str, char **endptr, int base)
 {
-#if defined HAVE_STRTOLL
     return strtoll(str, endptr, base);
-#elif defined _MSC_VER
-    return _strtoi64(str, endptr, base);
-#else
-    /* ok on 64 bit posix */
-    return PyOS_strtol(str, endptr, base);
-#endif
 }
 
 NPY_NO_EXPORT npy_ulonglong
 NumPyOS_strtoull(const char *str, char **endptr, int base)
 {
-#if defined HAVE_STRTOULL
     return strtoull(str, endptr, base);
-#elif defined _MSC_VER
-    return _strtoui64(str, endptr, base);
-#else
-    /* ok on 64 bit posix */
-    return PyOS_strtoul(str, endptr, base);
-#endif
 }
-
 
