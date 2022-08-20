198895cb66ade5f134968d91290add1658db703f
Namami Shanker <namami2011@gmail.com>
2022-08-20 11:52:30 +0530

    ENH: Update f2pyarg with full meson backend params

--- numpy/f2py/f2pyarg.py
+++ numpy/f2py/f2pyarg.py
@@ -864,7 +864,7 @@ def process_args(args: argparse.Namespace, rem: list[str]) -> None:
             # Step 8: Generate wrapper or signature file if compile flag is not given
             c_wrapper = generate_files(f77_files + f90_files, module_name, sign_file)
             if c_wrapper and args.c:
-                backend: Backend = backends.get(args.backend.value)(module_name=module_name, include_dirs=args.include_dirs, include_path=args.include_paths, external_resources=args.link_resource, debug=args.debug)
+                backend: Backend = backends.get(args.backend.value)(module_name=module_name, include_dirs=args.include_dirs, include_paths=args.include_paths, external_resources=args.link_resource, debug=args.debug, arch_flags=args.arch, opt_flags=args.opt, f77_flags=args.f77flags, f90_flags=args.f90flags, linker_libpath=args.library_path, linker_libname=args.library_name, define_macros=args.define_macros, undef_macros=args.undef_macros)
                 backend.compile(f77_files + f90_files, c_wrapper, build_dir)
 
 def main():
