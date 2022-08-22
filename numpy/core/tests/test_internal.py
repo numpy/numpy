f5ebaf6cf83c7ca071c881ccf9b29d1b016575bc
Pieter Eendebak <pieter.eendebak@gmail.com>
2022-08-22 15:54:46 +0200

    pylint

--- numpy/core/tests/test_internal.py
+++ numpy/core/tests/test_internal.py
@@ -6,5 +6,6 @@ import numpy.testing
 class TestInternal:
 
     def test_IS_PYPY(self):
-        assert IS_PYPY == numpy.testing.IS_PYPY, 'fast check on PYPY is unequal to full check'
+        assert IS_PYPY == numpy.testing.IS_PYPY, \
+                'fast check on PYPY is unequal to full check'
         
