3947391d1a2225d8c4498f518c03097d2e02ee70
BvB93 <43369155+BvB93@users.noreply.github.com>
2022-09-06 17:25:15 +0200

    TST: Explicitly test `ndarray.__class_getitem__` for subclasses with variable length tuples

--- numpy/core/tests/test_arraymethod.py
+++ numpy/core/tests/test_arraymethod.py
@@ -3,9 +3,11 @@ This file tests the generic aspects of ArrayMethod.  At the time of writing
 this is private API, but when added, public API may be added here.
 """
 
+from __future__ import annotations
+
 import sys
 import types
-from typing import Any, Type
+from typing import Any
 
 import pytest
 
@@ -63,20 +65,20 @@ class TestSimpleStridedCall:
 
 
 @pytest.mark.skipif(sys.version_info < (3, 9), reason="Requires python 3.9")
+@pytest.mark.parametrize(
+    "cls", [np.ndarray, np.recarray, np.chararray, np.matrix, np.memmap]
+)
 class TestClassGetItem:
-    @pytest.mark.parametrize(
-        "cls", [np.ndarray, np.recarray, np.chararray, np.matrix, np.memmap]
-    )
-    def test_class_getitem(self, cls: Type[np.ndarray]) -> None:
+    def test_class_getitem(self, cls: type[np.ndarray]) -> None:
         """Test `ndarray.__class_getitem__`."""
         alias = cls[Any, Any]
         assert isinstance(alias, types.GenericAlias)
         assert alias.__origin__ is cls
 
     @pytest.mark.parametrize("arg_len", range(4))
-    def test_subscript_tuple(self, arg_len: int) -> None:
+    def test_subscript_tup(self, cls: type[np.ndarray], arg_len: int) -> None:
         arg_tup = (Any,) * arg_len
-        assert np.ndarray[arg_tup]
+        assert cls[arg_tup]
 
 
 @pytest.mark.skipif(sys.version_info >= (3, 9), reason="Requires python 3.8")
