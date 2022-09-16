45fb7f0dc86e2d085d7110134bcdb94e0b2ae6db
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-09-15 21:31:04 -0700

    Test cov

--- numpy/polynomial/tests/test_hermite.py
+++ numpy/polynomial/tests/test_hermite.py
@@ -468,6 +468,21 @@ class TestFitting:
         coef2 = herm.hermfit(x, y, [0, 2, 4])
         assert_almost_equal(herm.hermval(x, coef2), y)
         assert_almost_equal(coef1, coef2)
+        # test cov
+        n = 100
+        x = np.linspace(-1,1,n)
+        y = x**2
+        p, cov = herm.hermfit(x,y,2,cov=True)
+        assert_almost_equal(cov,np.zeros((3,3)))
+        #
+        x = np.linspace(-1000,1000,n)
+        s = 0.1
+        r = np.random.randn(n)
+        yA = (4*x**2 - 2)*(1+s*r)
+        yB = (x**2)*(1+s*r)
+        _, covA = herm.hermfit(x,yA,2,cov=True)
+        _, covB = np.polyfit(x,yB,2,cov=True)
+        assert_almost_equal(covA[2,2],covB[0,0])
 
 
 class TestCompanion:
