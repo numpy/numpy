2d3903a1c34a73538bec1046a601101fd874cd06
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-09-16 09:46:29 -0700

    Make lint happy

--- numpy/polynomial/hermite_e.py
+++ numpy/polynomial/hermite_e.py
@@ -1328,11 +1328,11 @@ def hermefit(x, y, deg, rcond=None, full=False, w=None, cov=False):
         For more details, see `numpy.linalg.lstsq`.
         
     V : ndarray, shape (M,M) or (M,M,K)
-        Present only if ``full == False`` and ``cov == True``.  The covariance
-        matrix of the polynomial coefficient estimates.  The diagonal of
-        this matrix are the variance estimates for each coefficient.  If y
-        is a 2-D array, then the covariance matrix for the k-th data set
-        are in ``V[:,:,k]``
+        Present only if ``full == False`` and ``cov == True``.  The
+        covariance matrix of the polynomial coefficient estimates.  The
+        diagonal of this matrix are the variance estimates for each 
+        coefficient. If y is a 2-D array, then the covariance matrix for
+        the k-th data set are in ``V[:,:,k]``.
 
     Warns
     -----
