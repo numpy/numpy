888a16f7d9858b872d2ae26baa81c98ebe495567
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-09-15 19:37:56 -0700

    Allow returning cov
    See https://github.com/numpy/numpy/pull/7880

--- numpy/polynomial/hermite_e.py
+++ numpy/polynomial/hermite_e.py
@@ -1263,7 +1263,7 @@ def hermevander3d(x, y, z, deg):
     return pu._vander_nd_flat((hermevander, hermevander, hermevander), (x, y, z), deg)
 
 
-def hermefit(x, y, deg, rcond=None, full=False, w=None):
+def hermefit(x, y, deg, rcond=None, full=False, w=None, cov=False):
     """
     Least squares fit of Hermite series to data.
 
@@ -1306,7 +1306,10 @@ def hermefit(x, y, deg, rcond=None, full=False, w=None):
         chosen so that the errors of the products ``w[i]*y[i]`` all have the
         same variance.  When using inverse-variance weighting, use
         ``w[i] = 1/sigma(y[i])``.  The default value is None.
-
+    cov : bool, optional
+        Return the estimate and the covariance matrix of the estimate
+        If full is True, then cov is not returned.
+        
     Returns
     -------
     coef : ndarray, shape (M,) or (M, K)
@@ -1321,6 +1324,13 @@ def hermefit(x, y, deg, rcond=None, full=False, w=None):
         - rank -- the numerical rank of the scaled Vandermonde matrix
         - singular_values -- singular values of the scaled Vandermonde matrix
         - rcond -- value of `rcond`.
+        
+    V : ndarray, shape (M,M) or (M,M,K)
+        Present only if `full` = False and `cov`=True.  The covariance
+        matrix of the polynomial coefficient estimates.  The diagonal of
+        this matrix are the variance estimates for each coefficient.  If y
+        is a 2-D array, then the covariance matrix for the `k`-th data set
+        are in ``V[:,:,k]``
 
         For more details, see `numpy.linalg.lstsq`.
 
@@ -1393,7 +1403,7 @@ def hermefit(x, y, deg, rcond=None, full=False, w=None):
     array([ 1.01690445,  1.99951418,  2.99948696]) # may vary
 
     """
-    return pu._fit(hermevander, x, y, deg, rcond, full, w)
+    return pu._fit(hermevander, x, y, deg, rcond, full, w, cov)
 
 
 def hermecompanion(c):
