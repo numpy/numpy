9e76e5ccf6e75d7e8632c5930bc2ec6f2e05df62
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-09-16 00:27:34 -0700

    Update hermite.py

--- numpy/polynomial/hermite.py
+++ numpy/polynomial/hermite.py
@@ -1315,7 +1315,7 @@ def hermfit(x, y, deg, rcond=None, full=False, w=None, cov=False):
         chosen so that the errors of the products ``w[i]*y[i]`` all have the
         same variance.  When using inverse-variance weighting, use
         ``w[i] = 1/sigma(y[i])``.  The default value is None.
-   cov : bool, optional
+    cov : bool, optional
         Return the estimate and the covariance matrix of the estimate
         If full is True, then cov is not returned.
      
@@ -1333,14 +1333,14 @@ def hermfit(x, y, deg, rcond=None, full=False, w=None, cov=False):
         - rank -- the numerical rank of the scaled Vandermonde matrix
         - singular_values -- singular values of the scaled Vandermonde matrix
         - rcond -- value of `rcond`.
-    V : ndarray, shape (M,M) or (M,M,K)
-          Present only if `full` = False and `cov`=True.  The covariance
-          matrix of the polynomial coefficient estimates.  The diagonal of
-          this matrix are the variance estimates for each coefficient.  If y
-          is a 2-D array, then the covariance matrix for the `k`-th data set
-          are in ``V[:,:,k]``
-   
         For more details, see `numpy.linalg.lstsq`.
+        
+    V : ndarray, shape (M,M) or (M,M,K)
+        Present only if `full` = False and `cov`=True.  The covariance
+        matrix of the polynomial coefficient estimates.  The diagonal of
+        this matrix are the variance estimates for each coefficient.  If y
+        is a 2-D array, then the covariance matrix for the `k`-th data set
+        are in ``V[:,:,k]``
 
     Warns
     -----
