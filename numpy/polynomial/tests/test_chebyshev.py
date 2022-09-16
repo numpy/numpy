72fdc31882d55bbfef12fc501de8954caa7e0e27
eliottrosenberg <61400172+eliottrosenberg@users.noreply.github.com>
2022-09-15 23:04:06 -0700

    Make lint happy

--- numpy/polynomial/tests/test_chebyshev.py
+++ numpy/polynomial/tests/test_chebyshev.py
@@ -481,19 +481,19 @@ class TestFitting:
         assert_almost_equal(cheb.chebval(x, coef2), y)
         assert_almost_equal(coef1, coef2)
         # test cov
-        x = np.linspace(-1,1,100)
+        x = np.linspace(-1, 1, 100)
         y = x**2
-        _, cov = cheb.chebfit(x,y,2,cov=True)
-        assert_almost_equal(cov,np.zeros((3,3)))
+        _, cov = cheb.chebfit(x, y, 2, cov=True)
+        assert_almost_equal(cov, np.zeros((3, 3)))
         #
-        x = np.linspace(-1000,1000,n)
+        x = np.linspace(-1000, 1000, n)
         s = 0.1
         r = np.random.randn(n)
         yA = (2*x**2 - 1)*(1+s*r)
         yB = (x**2)*(1+s*r)
-        _, covA = cheb.chebfit(x,yA,2,cov=True)
-        _, covB = np.polyfit(x,yB,2,cov=True)
-        assert_almost_equal(covA[2,2],covB[0,0])
+        _, covA = cheb.chebfit(x, yA, 2, cov=True)
+        _, covB = np.polyfit(x, yB, 2, cov=True)
+        assert_almost_equal(covA[2, 2], covB[0, 0])
 
 
 class TestInterpolate:
