7a98e6c18c2985dceca0667693f570864e85c159
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-10-24 19:23:15 +0200

    MAINT: remove code specific to Python 2

--- numpy/_globals.py
+++ numpy/_globals.py
@@ -83,10 +83,6 @@ class _NoValueType:
             cls.__instance = super().__new__(cls)
         return cls.__instance
 
-    # needed for python 2 to preserve identity through a pickle
-    def __reduce__(self):
-        return (self.__class__, ())
-
     def __repr__(self):
         return "<no value>"
 
