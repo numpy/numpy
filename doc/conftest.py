b89417d67fe3c8b1aef0a156b9e228cb2c149128
Aleksa <fankerman11@gmail.com>
2022-11-04 16:24:06 +0100

    MAINT: Linting fixes

--- doc/conftest.py
+++ doc/conftest.py
@@ -6,14 +6,17 @@ import numpy
 import matplotlib
 import doctest
 
+
 matplotlib.use('agg', force=True)
 
 # Ignore matplotlib output such as `<matplotlib.image.AxesImage at
-# 0x7f956908c280>`. doctest monkeypatching inspired by
+# 0x7f956908c280>`. doctest monkey-patching inspired by
 # https://github.com/wooyek/pytest-doctest-ellipsis-markers (MIT license)
 OutputChecker = doctest.OutputChecker
 
 empty_line_markers = ['<matplotlib.', '<mpl_toolkits.mplot3d.']
+
+
 class SkipMatplotlibOutputChecker(doctest.OutputChecker):
     def check_output(self, want, got, optionflags):
         for marker in empty_line_markers:
@@ -25,8 +28,8 @@ class SkipMatplotlibOutputChecker(doctest.OutputChecker):
 
 doctest.OutputChecker = SkipMatplotlibOutputChecker
 
+
 @pytest.fixture(autouse=True)
 def add_np(doctest_namespace):
     numpy.random.seed(1)
     doctest_namespace['np'] = numpy
-
