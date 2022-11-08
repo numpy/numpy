ab7add7f56cafc4464d6573f19f63ad7b7b054c2
Richie Cotton <richierocks@gmail.com>
2022-11-08 18:55:47 +0000

    DOC: #22266 Add examples for tri[lu]_indices_from()

--- numpy/lib/twodim_base.py
+++ numpy/lib/twodim_base.py
@@ -1146,6 +1146,38 @@ def triu_indices_from(arr, k=0):
     triu_indices_from : tuple, shape(2) of ndarray, shape(N)
         Indices for the upper-triangle of `arr`.
 
+    Examples
+    --------
+
+    Create a 4 by 4 array.
+
+    >>> a = np.arange(16).reshape(4, 4)
+    >>> a
+    array([[ 0,  1,  2,  3],
+           [ 4,  5,  6,  7],
+           [ 8,  9, 10, 11],
+           [12, 13, 14, 15]])
+
+    Pass the array to get the incides of the upper triangualr elements. 
+
+    >>> triui = np.triu_indices_from(a)
+    >>> triui
+    (array([0, 0, 0, 0, 1, 1, 1, 2, 2, 3]), array([0, 1, 2, 3, 1, 2, 3, 2, 3, 3]))
+
+    >>> a[triui]
+    array([ 0,  1,  2,  3,  5,  6,  7, 10, 11, 15])
+
+    This is syntactic sugar for triu_indices().
+
+    >>> np.triu_indices(a.shape[0])
+    (array([0, 0, 0, 0, 1, 1, 1, 2, 2, 3]), array([0, 1, 2, 3, 1, 2, 3, 2, 3, 3]))
+
+    Negative k results in part of the lower triangular area being returned.
+
+    >>> triuim1 = np.triu_indices_from(a, 1)
+    >>> a[triuim1]
+    array([ 0,  1,  2,  3,  4,  5,  6,  7,  9, 10, 11, 14, 15])
+
     See Also
     --------
     triu_indices, triu
