eb31a6aab27028d4a65d5965e8a1339809547488
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2022-11-09 14:50:38 +0100

    Fixed lint issue with multiple spaces after = sign

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -713,7 +713,7 @@ def getdata(a, subok=True):
     try:
         data = a._data
         if not issubclass(type(data), np.ndarray):
-            data =  np.array(a, copy=False, subok=subok)
+            data = np.array(a, copy=False, subok=subok)
     except AttributeError:
         data = np.array(a, copy=False, subok=subok)
     if not subok:
