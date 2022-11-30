a031a84979d1deefa5cba78c1fdb564ed74cd674
mattip <matti.picus@gmail.com>
2022-11-30 13:08:48 +0200

    BUILD: fix cirrus wheel upload triggers

--- tools/wheels/upload_wheels.sh
+++ tools/wheels/upload_wheels.sh
@@ -1,22 +1,13 @@
 set_travis_vars() {
     # Set env vars
     echo "TRAVIS_EVENT_TYPE is $TRAVIS_EVENT_TYPE"
-    echo CIRRUS_TASK_NAME is "$CIRRUS_TASK_NAME"
     echo "TRAVIS_TAG is $TRAVIS_TAG"
-    echo "CIRRUS_TAG is $CIRRUS_TAG"
-    echo "CIRRUS_API_CREATED is $CIRRUS_API_CREATED"
-    echo "CIRRUS_PR is $CIRRUS_PR"
     if [[ "$TRAVIS_EVENT_TYPE" == "push" && "$TRAVIS_TAG" == v* ]]; then
-      IS_PUSH="true"
-    elif [[ "$CIRRUS_PR" == "" && "$CIRRUS_TAG" == v* ]]; then
       IS_PUSH="true"
     else
       IS_PUSH="false"
     fi
     if [[ "$TRAVIS_EVENT_TYPE" == "cron"  || -v CIRRUS_CRON ]]; then
-      IS_SCHEDULE_DISPATCH="true"
-    elif [[ "$TRAVIS_EVENT_TYPE" == "api"  || "$CIRRUS_API_CREATED" == "true" ]]; then
-      # Manual CI run, so upload
       IS_SCHEDULE_DISPATCH="true"
     else
       IS_SCHEDULE_DISPATCH="false"
