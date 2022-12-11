6a7a231e083cf0cb54986317d67e1b624e82001f
ganesh-k13 <ganesh3597@gmail.com>
2022-12-11 12:16:17 +0530

    DOC: Improved `bitwise_count` documentation
    * Added return type for integer inputs
    * Refined release note
    * Refined external references for popcount

--- numpy/core/code_generators/ufunc_docstrings.py
+++ numpy/core/code_generators/ufunc_docstrings.py
@@ -4230,14 +4230,17 @@ add_newdoc('numpy.core.umath', 'bitwise_count',
     -------
     y : ndarray
         The corresponding number of 1-bits in the input.
+        Returns uint8 for all integer types
         $OUT_SCALAR_1
 
     References
     ----------
-    .. [1] https://stackoverflow.com/a/109025/5671364
+    .. [1] https://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
 
     .. [2] Wikipedia, "Hamming weight",
            https://en.wikipedia.org/wiki/Hamming_weight
+
+    .. [3] http://aggregate.ee.engr.uky.edu/MAGIC/#Population%20Count%20(Ones%20Count)
 
     Examples
     --------
