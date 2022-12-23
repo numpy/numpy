71485c179893e401a149a14132dcf2a460db5d43
Matthew Muresan <mmuresan@miovision.com>
2022-12-23 13:15:12 -0500

    DOC: Add a note to the documentation of the rot90
    The note added indicates that rotation is counter clockwise with the default argumemnts.

--- numpy/lib/function_base.py
+++ numpy/lib/function_base.py
@@ -193,6 +193,8 @@ def rot90(m, k=1, axes=(0, 1)):
     ``rot90(m, k=1, axes=(1,0))`` is equivalent to
     ``rot90(m, k=-1, axes=(0,1))``
 
+    ``rot90(m, k=1, axes=(0,1))`` will rotate the array by 90 degrees in the counter-clockwise direction
+
     Examples
     --------
     >>> m = np.array([[1,2],[3,4]], int)
