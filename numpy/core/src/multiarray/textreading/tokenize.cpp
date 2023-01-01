6d474f2dfe5e4b7ea2a6e7423a638316fa186227
dmbelov <dmbelov@gmail.com>
2023-01-01 07:38:57 -0500

    BUG: np.loadtxt cannot load text file with quoted fields separated by whitespace (#22906)
    Fix issue with `delimiter=None` and quote character not working properly (not using whitespace delimiter mode).
    
    Closes gh-22899

--- numpy/core/src/multiarray/textreading/tokenize.cpp
+++ numpy/core/src/multiarray/textreading/tokenize.cpp
@@ -223,7 +223,8 @@ tokenizer_core(tokenizer_state *ts, parser_config *const config)
             }
             else {
                 /* continue parsing as if unquoted */
-                ts->state = TOKENIZE_UNQUOTED;
+                /* Set to TOKENIZE_UNQUOTED or TOKENIZE_UNQUOTED_WHITESPACE */
+                ts->state = ts->unquoted_state;
             }
             break;
 
