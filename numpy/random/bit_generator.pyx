969c748cf6539e5b31b52ad13c7a814976735719
LeonaTaric <numpy@huawei.com>
2023-01-09 19:02:05 +0800

    去掉note

--- numpy/random/bit_generator.pyx
+++ numpy/random/bit_generator.pyx
@@ -260,7 +260,7 @@ cdef class SeedSequence():
     ----------
     entropy : {None, int, sequence[int]}, optional
         The entropy for creating a `SeedSequence`.
-        note: all integer values must be non-negative.
+        All integer values must be non-negative.
     spawn_key : {(), sequence[int]}, optional
         An additional source of entropy based on the position of this
         `SeedSequence` in the tree of such objects created with the
@@ -491,8 +491,7 @@ cdef class BitGenerator():
         ``array_like[ints]`` is passed, then it will be passed to
         `~numpy.random.SeedSequence` to derive the initial `BitGenerator` state.
         One may also pass in a `SeedSequence` instance.
-        
-        note: all integer values must be non-negative.
+        All integer values must be non-negative.
 
     Attributes
     ----------
