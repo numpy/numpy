7a9d56a66a382bd333c950d59e08d65b3c180c32
gaudyb <85708998+gaudyb@users.noreply.github.com>
2023-01-17 01:14:05 +0000

    minimum instead of maximum for auto strategy

--- numpy/lib/histograms.py
+++ numpy/lib/histograms.py
@@ -494,7 +494,7 @@ def histogram_bin_edges(a, bins=10, range=None, weights=None):
         supported for automated bin size selection.
 
         'auto'
-            Maximum of the 'sturges' and 'fd' estimators. Provides good
+            Minimum of the 'sturges' and 'fd' estimators. Provides good
             all around performance.
 
         'fd' (Freedman Diaconis Estimator)
