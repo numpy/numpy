467f6e9e2a45be0011c01ef8c2de111f72f445af
Noé Rubinstein <nrubinstein@scortex.io>
2023-01-26 17:52:37 +0100

    Raise EOFError instead when trying to load past the end of a `.npy` file
    Currently, the following code:
    ```
    import numpy as np
    with open('foo.npy', 'wb') as f:
        for i in range(np.random.randint(10)):
            np.save(f, 1)
    
    with open('foo.npy', 'rb') as f:
        while True:
            np.load(f)
    ```
    Will raise:
    ```
    ValueError: Cannot load file containing pickled data when allow_pickle=False
    ```
    While there is no pickled data in the file.

--- numpy/lib/npyio.py
+++ numpy/lib/npyio.py
@@ -327,6 +327,9 @@ def load(file, mmap_mode=None, allow_pickle=False, fix_imports=True,
         If ``allow_pickle=True``, but the file cannot be loaded as a pickle.
     ValueError
         The file contains an object array, but ``allow_pickle=False`` given.
+    EOFError
+        If the input file is empty or, when calling `np.load` multiple times
+        on the same file, if all data has already been read
 
     See Also
     --------
@@ -413,7 +416,9 @@ def load(file, mmap_mode=None, allow_pickle=False, fix_imports=True,
         # If the file size is less than N, we need to make sure not
         # to seek past the beginning of the file
         fid.seek(-min(N, len(magic)), 1)  # back-up
-        if magic.startswith(_ZIP_PREFIX) or magic.startswith(_ZIP_SUFFIX):
+        if not magic:
+            raise EOFError()
+        elif magic.startswith(_ZIP_PREFIX) or magic.startswith(_ZIP_SUFFIX):
             # zip-file (assume .npz)
             # Potentially transfer file ownership to NpzFile
             stack.pop_all()
