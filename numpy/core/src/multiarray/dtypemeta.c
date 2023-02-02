96389f69e298729a33c2c6ad6bf994d338638d60
Sebastian Berg <sebastianb@nvidia.com>
2023-02-02 01:54:27 +0100

    ENH: Allow trivial pickling of user DType (classes)
    This also adds a `_legac` attribute, I am happy to rename it, but
    I suspect having something like it is useful.
    
    Arguably, the best solution would be to give our DTypes a working
    module+name, but given that we are not there, this seems easy.
    We could probably check for `__reduce__`, but I am not certain that
    wouldn't pre-empt successfully already, so this just restores the
    default for now.

--- numpy/core/src/multiarray/dtypemeta.c
+++ numpy/core/src/multiarray/dtypemeta.c
@@ -918,6 +918,11 @@ dtypemeta_get_abstract(PyArray_DTypeMeta *self) {
 }
 
 static PyObject *
+dtypemeta_get_legacy(PyArray_DTypeMeta *self) {
+    return PyBool_FromLong(NPY_DT_is_legacy(self));
+}
+
+static PyObject *
 dtypemeta_get_parametric(PyArray_DTypeMeta *self) {
     return PyBool_FromLong(NPY_DT_is_parametric(self));
 }
@@ -927,6 +932,7 @@ dtypemeta_get_parametric(PyArray_DTypeMeta *self) {
  */
 static PyGetSetDef dtypemeta_getset[] = {
         {"_abstract", (getter)dtypemeta_get_abstract, NULL, NULL, NULL},
+        {"_legacy", (getter)dtypemeta_get_legacy, NULL, NULL, NULL},
         {"_parametric", (getter)dtypemeta_get_parametric, NULL, NULL, NULL},
         {NULL, NULL, NULL, NULL, NULL}
 };
