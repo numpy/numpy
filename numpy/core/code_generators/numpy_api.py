c335e7131994f12ecd84f83bfe8ffa46b1db14fc
Brock <jbrockmendel@gmail.com>
2023-03-13 16:37:22 -0700

    whitespace fixup

--- numpy/core/code_generators/numpy_api.py
+++ numpy/core/code_generators/numpy_api.py
@@ -370,11 +370,11 @@ multiarray_funcs_api = {
     'PyDataMem_SetHandler':                 (304,),
     'PyDataMem_GetHandler':                 (305,),
     # End 1.22 API
-    'NpyDatetime_ConvertDatetime64ToDatetimeStruct':   (307,),  # GH#21199
+    'NpyDatetime_ConvertDatetime64ToDatetimeStruct':   (307,),
     'NpyDatetime_ConvertDatetimeStructToDatetime64':   (308,),
-    'NpyDatetime_ConvertPydatetimeToDatetimeStruct': (309,),
-    'NpyDatetime_GetDatetimeISO8601StrLen':         (310,),
-    'NpyDatetime_MakeISO8601Datetime':               (311,),
+    'NpyDatetime_ConvertPydatetimeToDatetimeStruct':   (309,),
+    'NpyDatetime_GetDatetimeISO8601StrLen':            (310,),
+    'NpyDatetime_MakeISO8601Datetime':                 (311,),
 }
 
 ufunc_types_api = {
