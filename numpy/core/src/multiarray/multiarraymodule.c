1c10ca575f6c73e6a59f90911deaec132a18dab5
Pedro Lameiras <pedrolameiras@tecnico.ulisboa.pt>
2023-03-26 19:03:02 +0100

    BUG: bug fix
    Added the decrease of the ref count of ap1 and ap2.

--- numpy/core/src/multiarray/multiarraymodule.c
+++ numpy/core/src/multiarray/multiarraymodule.c
@@ -1049,9 +1049,13 @@ PyArray_MatrixProduct2(PyObject *op1, PyObject *op2, PyArrayObject* out)
             if (PyArray_AssignArray(out, (PyArrayObject *)result,
                         NULL, NPY_DEFAULT_ASSIGN_CASTING) < 0) {
                 Py_DECREF(result);
+                Py_DECREF(ap1);
+                Py_DECREF(ap2);
                 return NULL;
             }
             Py_DECREF(result);
+            Py_DECREF(ap1);
+            Py_DECREF(ap2);
             Py_INCREF(out);
             return (PyObject *)out;
         }
