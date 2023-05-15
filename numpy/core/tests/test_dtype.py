981d3a71daf36a14e77e8d37694937c8836510ae
Sebastian Berg <sebastianb@nvidia.com>
2023-05-15 16:44:52 +0200

    TST: fixup test for time promotion change

--- numpy/core/tests/test_dtype.py
+++ numpy/core/tests/test_dtype.py
@@ -1892,5 +1892,5 @@ def test_result_type_integers_and_unitless_timedelta64():
     # would cause a seg. fault.
     # This was allowed initially, but times should not promote with ints.
     td = np.timedelta64(4)
-    result = np.result_type(0, td)
-    assert_dtype_equal(result, td.dtype)
+    with pytest.raises(TypeError):
+        np.result_type(0, td)
