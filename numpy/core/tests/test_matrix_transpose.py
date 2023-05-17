95da479f33974dbacbcb148babf427114d9e20ac
Kai Striega <kaistriega@gmail.com>
2023-05-17 12:20:01 +0800

    TST: Avoid using hypothesis in test case
    Hypothesis seems to be overkill for the provided situation. Use explicitly
    defined values instead.

--- numpy/core/tests/test_matrix_transpose.py
+++ numpy/core/tests/test_matrix_transpose.py
@@ -1,6 +1,4 @@
 import pytest
-from hypothesis import given
-from hypothesis.extra.numpy import array_shapes
 
 import numpy as np
 from numpy.testing import assert_array_equal
@@ -16,7 +14,15 @@ def test_matrix_transpose_equals_transpose_2d():
     assert_array_equal(arr.T, arr.mT)
 
 
-@given(shape=array_shapes(max_dims=7))
+ARRAY_SHAPES_TO_TEST = (
+    (5,),
+    (5, 2),
+    (5, 2, 3),
+    (5, 2, 3, 4),
+)
+
+
+@pytest.mark.parametrize("shape", ARRAY_SHAPES_TO_TEST)
 def test_matrix_transpose_equals_swapaxes(shape):
     num_of_axes = len(shape)
     vec = np.arange(shape[-1])
