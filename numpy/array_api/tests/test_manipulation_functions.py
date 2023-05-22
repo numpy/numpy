f0a84d171506933782da8f6330a3597c2b5c97c6
Brian Walshe <walshe.brian@gmail.com>
2023-05-22 11:43:44 +0100

    BUG: add copy parameter for api.reshape function
    This adds a parameter to api.reshape to specify if data
    should be copied. This parameter is required so that
    api.reshape conforms to the standard. See #23410

---
+++ numpy/array_api/tests/test_manipulation_functions.py
@@ -0,0 +1,30 @@
+from numpy.testing import assert_raises
+import numpy as np
+
+from .. import all
+from .._creation_functions import asarray
+from .._dtypes import float64, int8
+from .._manipulation_functions import (
+        concat,
+        reshape,
+        stack
+)
+
+
+def test_concat_errors():
+    assert_raises(TypeError, lambda: concat((1, 1), axis=None))
+    assert_raises(TypeError, lambda: concat([asarray([1], dtype=int8), asarray([1], dtype=float64)]))
+
+
+def test_stack_errors():
+    assert_raises(TypeError, lambda: stack([asarray([1, 1], dtype=int8), asarray([2, 2], dtype=float64)]))
+
+
+def test_reshape_copy():
+    a = asarray([1])
+    b = reshape(a, (1, 1), copy=True)
+    a[0] = 0
+    assert all(b[0, 0] == 1)
+    assert all(a[0] == 0)
+    assert_raises(NotImplementedError, lambda: reshape(a, (1, 1), copy=False))
+
