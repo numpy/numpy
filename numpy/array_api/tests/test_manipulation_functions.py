289f24d37c96ee8e6bc9bf83a5c7d3688a915e0d
Brian Walshe <walshe.brian@gmail.com>
2023-05-25 12:50:35 +0100

    fixup: more linter issues

--- numpy/array_api/tests/test_manipulation_functions.py
+++ numpy/array_api/tests/test_manipulation_functions.py
@@ -23,15 +23,15 @@ def test_stack_errors():
 
 
 def test_reshape_copy():
-    a = asarray(np.ones((2,3)))
+    a = asarray(np.ones((2, 3)))
     b = reshape(a, (3, 2), copy=True)
     assert not np.shares_memory(a._array, b._array)
     
-    a = asarray(np.ones((2,3)))
+    a = asarray(np.ones((2, 3)))
     b = reshape(a, (3, 2), copy=False)
     assert np.shares_memory(a._array, b._array)
 
-    a = asarray(np.ones((2,3)).T)
+    a = asarray(np.ones((2, 3)).T)
     b = reshape(a, (3, 2), copy=True)
     assert_raises(AttributeError, lambda: reshape(a, (2, 3), copy=False))
 
