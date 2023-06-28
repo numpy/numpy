a907d36ef4484594b6258eb3d16d096df104ceec
Mohit Kumar <120097428+MohitKumar020291@users.noreply.github.com>
2023-06-28 14:35:06 +0530

    Update numpy/core/multiarray.py
    Co-authored-by: Mukulika <60316606+Mukulikaa@users.noreply.github.com>

--- numpy/core/multiarray.py
+++ numpy/core/multiarray.py
@@ -173,7 +173,7 @@ def concatenate(arrays, axis=None, out=None, *, dtype=None, casting=None):
 
     casting : {'no', 'equiv', 'safe', 'same_kind', 'unsafe'}, optional
         Controls what kind of data casting may occur. Defaults to 'same_kind'.
-        For more information
+        For a description of the options, please see :term:`casting`.
         `<https://numpy.org/devdocs/glossary.html#glossary>`.
         
         .. versionadded:: 1.20.0
