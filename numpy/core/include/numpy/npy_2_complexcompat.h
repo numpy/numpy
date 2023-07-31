2d25d2ae63966ed046d17eba274d4afce9418a07
Lysandros Nikolaou <lisandrosnik@gmail.com>
2023-07-31 14:35:07 +0200

    Improve comment in compat header
    Co-authored-by: mattip <matti.picus@gmail.com>

--- numpy/core/include/numpy/npy_2_complexcompat.h
+++ numpy/core/include/numpy/npy_2_complexcompat.h
@@ -1,6 +1,6 @@
 /* This header is designed to be copy-pasted into downstream packages, since it provides
    a compatibility layer between the old C struct complex types and the new native C99
-   complex types. */
+   complex types. The new macros are in numpy/npy_math.h, which is why it is included here. */
 #ifndef NUMPY_CORE_INCLUDE_NUMPY_NPY_2_COMPLEXCOMPAT_H_
 #define NUMPY_CORE_INCLUDE_NUMPY_NPY_2_COMPLEXCOMPAT_H_
 
