aace8f850382541d19be4c85cbc1a95deae6ad1d
Priyank Patel <priyankpatel1207@gmail.com>
2023-08-01 07:47:22 -0700

    Update function_base.py

--- numpy/lib/function_base.py
+++ numpy/lib/function_base.py
@@ -758,7 +758,7 @@ def piecewise(x, condlist, funclist, *args, **kw):
             vals = x[cond]
             if vals.size > 0:
 
-                func_val =  func(vals, *args, **kw)
+                func_val = func(vals, *args, **kw)
                 y = y.astype(asarray(func_val).dtype)
                 y[cond] = func_val
 
