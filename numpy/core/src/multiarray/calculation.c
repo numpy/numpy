5fd8169346e0d0d5c18fa21a0e674bbfe5e2ce67
Sebastian Berg <sebastianb@nvidia.com>
2023-08-01 13:04:04 +0200

    MAINT: Remove also now unnecessary `GenericBinaryOutFunction`

--- numpy/core/src/multiarray/calculation.c
+++ numpy/core/src/multiarray/calculation.c
@@ -783,38 +783,6 @@ PyArray_All(PyArrayObject *self, int axis, PyArrayObject *out)
 }
 
 
-static PyObject *
-_GenericBinaryOutFunction(PyArrayObject *m1, PyObject *m2, PyArrayObject *out,
-                          PyObject *op)
-{
-    if (out == NULL) {
-        return PyObject_CallFunction(op, "OO", m1, m2);
-    }
-    else {
-        PyObject *args, *ret;
-        static PyObject *kw = NULL;
-
-        if (kw == NULL) {
-            kw = Py_BuildValue("{s:s}", "casting", "unsafe");
-            if (kw == NULL) {
-                return NULL;
-            }
-        }
-
-        args = Py_BuildValue("OOO", m1, m2, out);
-        if (args == NULL) {
-            return NULL;
-        }
-
-        ret = PyObject_Call(op, args, kw);
-
-        Py_DECREF(args);
-
-        return ret;
-    }
-}
-
-
 /*NUMPY_API
  * Clip
  */
