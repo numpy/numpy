75132eef6b94913ee3ab31ff1e27bbfcf8b037d9
Stefan van der Walt <stefanv@berkeley.edu>
2023-08-09 22:16:02 -0700

    git hash is optional in, e.g., sdist build

--- numpy/tests/test_numpy_version.py
+++ numpy/tests/test_numpy_version.py
@@ -25,7 +25,7 @@ def test_valid_numpy_version():
     # Verify that the numpy version is a valid one (no .post suffix or other
     # nonsense).  See gh-6431 for an issue caused by an invalid version.
     version_pattern = r"^[0-9]+\.[0-9]+\.[0-9]+(a[0-9]|b[0-9]|rc[0-9])?"
-    dev_suffix = r"(\.dev[0-9]+\+git[0-9]+\.[0-9a-f]+)?"
+    dev_suffix = r"(\.dev[0-9]+(\+git[0-9]+\.[0-9a-f]+)?)?"
     res = re.match(version_pattern + dev_suffix + '$', np.__version__)
 
     assert_(res is not None, np.__version__)
