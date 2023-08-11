6aff0bc423523b01bfff1ef30e89d40c0cb886a8
Ralf Gommers <ralf.gommers@gmail.com>
2023-08-11 22:05:53 +0200

    MAINT: revert change to setup.py for sdist/wheel versioning
    [skip azp] [skip cirrus] [skip circle] [skip travis]

--- setup.py
+++ setup.py
@@ -519,7 +519,7 @@ def setup_package():
         classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
         platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
         test_suite='pytest',
-        version=FULLVERSION,
+        version=VERSION,
         cmdclass=cmdclass,
         python_requires='>=3.9',
         zip_safe=False,
