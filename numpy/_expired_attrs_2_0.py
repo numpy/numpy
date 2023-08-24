e6af3a12429ba7c757c1fb69e317f3c843776604
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2023-08-24 13:51:24 -0600

    MAINT: copyediting for migration notes

--- numpy/_expired_attrs_2_0.py
+++ numpy/_expired_attrs_2_0.py
@@ -4,12 +4,12 @@ Each item is associated with a migration note.
 """
 
 __expired_attributes__ = {
-    "geterrobj": "Use context manager `with np.errstate()` instead.",
-    "seterrobj": "Use context manager `with np.errstate()` instead.",
+    "geterrobj": "Use the np.errstate context manager instead.",
+    "seterrobj": "Use the np.errstate context manager instead.",
     "cast": "Use `np.asarray(arr, dtype=dtype)` instead.",
     "source": "Use `inspect.getsource` instead.",
     "lookfor":  "Search NumPy's documentation directly.",
-    "who": "Use IDE variable explorer or `locals()` instead.",
+    "who": "Use an IDE variable explorer or `locals()` instead.",
     "fastCopyAndTranspose": "Use `arr.T.copy()` instead.",
     "set_numeric_ops": 
         "For the general case, use `PyUFunc_ReplaceLoopBySignature`. "
@@ -20,11 +20,11 @@ __expired_attributes__ = {
     "NZERO": "Use `-0.0` instead.",
     "PZERO": "Use `0.0` instead.",
     "add_newdoc": 
-        "It's and internal function and doesn't have a replacement.",
+        "It's an internal function and doesn't have a replacement.",
     "add_docstring": 
-        "It's and internal function and doesn't have a replacement.",
+        "It's an internal function and doesn't have a replacement.",
     "add_newdoc_ufunc": 
-        "It's and internal function and doesn't have a replacement.",
+        "It's an internal function and doesn't have a replacement.",
     "compat": "There's no replacement, as Python 2 is no longer supported.",
     "safe_eval": "Use `ast.literal_eval` instead."
 }
