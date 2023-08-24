451e80ae41b6e882bb56bd052869119a2099e5a9
Mateusz Sokół <mat646@gmail.com>
2023-08-24 23:29:41 +0200

    Add changelog and expired attributes

--- numpy/typing/tests/data/reveal/numerictypes.pyi
+++ numpy/typing/tests/data/reveal/numerictypes.pyi
@@ -4,7 +4,7 @@ reveal_type(np.cast[int])  # E: _CastFunc
 reveal_type(np.cast["i8"])  # E: _CastFunc
 reveal_type(np.cast[np.int64])  # E: _CastFunc
 
-reveal_type(np.ScalarType)  # E: Tuple
+reveal_type(np.ScalarType)  # E: tuple
 reveal_type(np.ScalarType[0])  # E: Type[builtins.int]
 reveal_type(np.ScalarType[3])  # E: Type[builtins.bool]
 reveal_type(np.ScalarType[8])  # E: Type[{csingle}]
