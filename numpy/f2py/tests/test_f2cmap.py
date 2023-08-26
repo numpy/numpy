a34d2bca3e3c53ca1ba2b4f01174bf5fcfa463f0
Rohit Goswami <rog32@hi.is>
2023-08-26 12:44:16 +0000

    TST: Add a test for gh-24553

--- numpy/f2py/tests/test_f2cmap.py
+++ numpy/f2py/tests/test_f2cmap.py
@@ -13,3 +13,14 @@ class TestF2Cmap(util.F2PyTest):
         out = self.module.func1(inp)
         exp_out = 3
         assert out == exp_out
+
+class TestISOCmap(util.F2PyTest):
+    sources = [
+        util.getpath("tests", "src", "f2cmap", "iso_c_oddity.f90"),
+    ]
+
+    # gh-24553
+    def test_c_double(self):
+        out = self.module.coddity.c_add(1, 2)
+        exp_out = 3
+        assert  out == exp_out
