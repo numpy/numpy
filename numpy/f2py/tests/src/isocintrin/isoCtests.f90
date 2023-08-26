3755a4b6496c06bbc5c785df5cb8034b81ab6f63
Rohit Goswami <rog32@hi.is>
2023-08-26 20:09:21 +0000

    TST: Add more tests for iso_c_binding
    Functions and subroutines

--- numpy/f2py/tests/src/isocintrin/isoCtests.f90
+++ numpy/f2py/tests/src/isocintrin/isoCtests.f90
@@ -1,5 +1,5 @@
   module coddity
-    use iso_c_binding, only: c_double
+    use iso_c_binding, only: c_double, c_int
     implicit none
     contains
       subroutine c_add(a, b, c) bind(c, name="c_add")
@@ -7,4 +7,11 @@
         real(c_double), intent(out) :: c
         c = a + b
       end subroutine c_add
+      ! gh-9693
+      function wat(x, y) result(z) bind(c, name='wat')
+          integer(c_int), intent(in) :: x, y
+          integer(c_int) :: z
+
+          z = x + 7
+      end function wat
   end module coddity
