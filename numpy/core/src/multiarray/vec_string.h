ab5f16fe8cbece60aea5b94e7de525515b294aa0
Lysandros Nikolaou <lisandrosnik@gmail.com>
2023-09-15 17:56:53 +0200

    Move vec_string related stuff to new file

---
+++ numpy/core/src/multiarray/vec_string.h
@@ -0,0 +1,7 @@
+#ifndef NUMPY_CORE_SRC_MULTIARRAY_VEC_STRING_H_
+#define NUMPY_CORE_SRC_MULTIARRAY_VEC_STRING_H_
+
+NPY_NO_EXPORT PyObject *
+_vec_string(PyObject *, PyObject *, PyObject *);
+
+#endif
