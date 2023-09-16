7068ab304d8dd54da3110628a403df99c522c338
Krunal6767 <106827625+Krunal6767@users.noreply.github.com>
2023-09-15 20:00:29 -0400

    Update _sorting_functions.py

--- numpy/array_api/_sorting_functions.py
+++ numpy/array_api/_sorting_functions.py
@@ -1,54 +1,72 @@
 from __future__ import annotations
+import numpy as np
 
 from ._array_object import Array
 from ._dtypes import _real_numeric_dtypes
 
-import numpy as np
-
-
-# Note: the descending keyword argument is new in this function
-def argsort(
-    x: Array, /, *, axis: int = -1, descending: bool = False, stable: bool = True
-) -> Array:
+def argsort(x: Array, /, *, axis: int = -1, descending: bool = False, stable: bool = True) -> Array:
     """
-    Array API compatible wrapper for :py:func:`np.argsort <numpy.argsort>`.
+    Argsorts the input Array along the specified axis.
 
-    See its docstring for more information.
+    Args:
+        x (Array): The input Array to be sorted.
+        axis (int, optional): The axis along which to sort. Default is -1.
+        descending (bool, optional): If True, sort in descending order. Default is False.
+        stable (bool, optional): If True, use stable sorting algorithm. Default is True.
+
+    Returns:
+        Array: A new sorted Array.
+
+    Raises:
+        TypeError: If the input Array's dtype is not a real numeric dtype.
     """
     if x.dtype not in _real_numeric_dtypes:
         raise TypeError("Only real numeric dtypes are allowed in argsort")
-    # Note: this keyword argument is different, and the default is different.
+    
     kind = "stable" if stable else "quicksort"
-    if not descending:
-        res = np.argsort(x._array, axis=axis, kind=kind)
-    else:
-        # As NumPy has no native descending sort, we imitate it here. Note that
-        # simply flipping the results of np.argsort(x._array, ...) would not
-        # respect the relative order like it would in native descending sorts.
-        res = np.flip(
-            np.argsort(np.flip(x._array, axis=axis), axis=axis, kind=kind),
-            axis=axis,
-        )
-        # Rely on flip()/argsort() to validate axis
-        normalised_axis = axis if axis >= 0 else x.ndim + axis
-        max_i = x.shape[normalised_axis] - 1
-        res = max_i - res
+    res = np.argsort(x._array, axis=axis, kind=kind)
+    
+    if descending:
+        res = invert_sort(res, axis)
+    
     return Array._new(res)
 
-# Note: the descending keyword argument is new in this function
-def sort(
-    x: Array, /, *, axis: int = -1, descending: bool = False, stable: bool = True
-) -> Array:
+def sort(x: Array, /, *, axis: int = -1, descending: bool = False, stable: bool = True) -> Array:
     """
-    Array API compatible wrapper for :py:func:`np.sort <numpy.sort>`.
+    Sorts the input Array along the specified axis.
 
-    See its docstring for more information.
+    Args:
+        x (Array): The input Array to be sorted.
+        axis (int, optional): The axis along which to sort. Default is -1.
+        descending (bool, optional): If True, sort in descending order. Default is False.
+        stable (bool, optional): If True, use stable sorting algorithm. Default is True.
+
+    Returns:
+        Array: A new sorted Array.
+
+    Raises:
+        TypeError: If the input Array's dtype is not a real numeric dtype.
     """
     if x.dtype not in _real_numeric_dtypes:
         raise TypeError("Only real numeric dtypes are allowed in sort")
-    # Note: this keyword argument is different, and the default is different.
+    
     kind = "stable" if stable else "quicksort"
     res = np.sort(x._array, axis=axis, kind=kind)
+    
     if descending:
-        res = np.flip(res, axis=axis)
+        res = invert_sort(res, axis)
+    
     return Array._new(res)
+
+def invert_sort(arr, axis):
+    """
+    Inverts the sorting order of an array along the specified axis.
+
+    Args:
+        arr (np.ndarray): The input array to be inverted.
+        axis (int): The axis along which to invert the sorting order.
+
+    Returns:
+        np.ndarray: The array with inverted sorting order along the specified axis.
+    """
+    return np.flip(arr, axis=axis)
