cb1759dd1c9e27cd652d805facd28424398cef36
Lysandros Nikolaou <lisandrosnik@gmail.com>
2023-09-18 16:56:30 +0200

    Start working on infrastructure to allow specialization of funcs

--- numpy/core/src/multiarray/vec_string.c
+++ numpy/core/src/multiarray/vec_string.c
@@ -3,9 +3,67 @@
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "numpy/arrayobject.h"
 #include "dtypemeta.h"
+
+static PyObject *
+_vec_string_is_alpha(PyArrayIterObject *in_iter)
+{
+    char *data = in_iter->dataptr;
+    for (int i = 0, l = strlen(data); i < l; i++) {
+        if (!isalpha(data[i])) {
+            Py_RETURN_FALSE;
+        }
+    }
+    Py_RETURN_TRUE;
+}
+
+typedef PyObject * (*_vec_string_fast_op)(PyArrayIterObject *);
+
+typedef struct {
+    const char *name;
+    _vec_string_fast_op func;
+    int with_args;
+} _vec_string_named_fast_op;
+
+static _vec_string_named_fast_op *SUPPORTED_FAST_OPS[] = {
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {{NULL, NULL, -1}},
+    (_vec_string_named_fast_op[]) {
+        {"isalpha", _vec_string_is_alpha, 0},
+    },
+};
+
+static int N_FAST_OP_LISTS = 8;
+
+int
+get_fast_op(PyObject *method_name, _vec_string_fast_op *method, int *with_args)
+{
+    const char *method_name_str = PyUnicode_AsUTF8(method_name);
+    const int method_name_len = strlen(method_name_str);
+
+    if (method_name_len >= N_FAST_OP_LISTS) {
+        return 0;
+    }
+
+    for (_vec_string_named_fast_op *f = SUPPORTED_FAST_OPS[method_name_len]; f->name != NULL; f++) {
+        if (strncmp(method_name_str, f->name, method_name_len) == 0) {
+            *method = f->func;
+            *with_args = f->with_args;
+            return 1;
+        }
+    }
+
+    return 0;
+}
 
 /*
  * returns 1 if array is a user-defined string dtype, sets an error and
@@ -200,6 +258,64 @@ _vec_string_no_args(PyArrayObject* char_array,
     return 0;
 }
 
+static PyObject *
+_vec_string_fast_op_no_args(PyArrayObject *char_array, PyArray_Descr *type, _vec_string_fast_op method)
+{
+    /*
+     * This is a faster version of _vec_string_args to use when there
+     * are no additional arguments to the string method.  This doesn't
+     * require a broadcast iterator (and broadcast iterators don't work
+     * with 1 argument anyway).
+     */
+    PyArrayIterObject* in_iter = NULL;
+    PyArrayObject* result = NULL;
+    PyArrayIterObject* out_iter = NULL;
+
+    in_iter = (PyArrayIterObject*)PyArray_IterNew((PyObject*)char_array);
+    if (in_iter == NULL) {
+        Py_DECREF(type);
+        goto err;
+    }
+
+    result = (PyArrayObject*)PyArray_SimpleNewFromDescr(
+            PyArray_NDIM(char_array), PyArray_DIMS(char_array), type);
+    if (result == NULL) {
+        goto err;
+    }
+
+    out_iter = (PyArrayIterObject*)PyArray_IterNew((PyObject*)result);
+    if (out_iter == NULL) {
+        goto err;
+    }
+
+    while (PyArray_ITER_NOTDONE(in_iter)) {
+        PyObject* item_result = method(in_iter);
+
+        if (PyArray_SETITEM(result, PyArray_ITER_DATA(out_iter), item_result)) {
+            Py_DECREF(item_result);
+            PyErr_SetString( PyExc_TypeError,
+                "result array type does not match underlying function");
+            goto err;
+        }
+        Py_DECREF(item_result);
+
+        PyArray_ITER_NEXT(in_iter);
+        PyArray_ITER_NEXT(out_iter);
+    }
+
+    Py_DECREF(in_iter);
+    Py_DECREF(out_iter);
+
+    return (PyObject*)result;
+
+ err:
+    Py_XDECREF(in_iter);
+    Py_XDECREF(out_iter);
+    Py_XDECREF(result);
+
+    return 0;
+}
+
 NPY_NO_EXPORT PyObject *
 _vec_string(PyObject *NPY_UNUSED(dummy), PyObject *args, PyObject *NPY_UNUSED(kwds))
 {
@@ -208,6 +324,9 @@ _vec_string(PyObject *NPY_UNUSED(dummy), PyObject *args, PyObject *NPY_UNUSED(kw
     PyObject* method_name;
     PyObject* args_seq = NULL;
 
+    int fast_method_found = 0;
+    int fast_method_with_args = 0;
+    _vec_string_fast_op fast_method = NULL;
     PyObject* method = NULL;
     PyObject* result = NULL;
 
@@ -218,47 +337,56 @@ _vec_string(PyObject *NPY_UNUSED(dummy), PyObject *args, PyObject *NPY_UNUSED(kw
         goto err;
     }
 
-    if (PyArray_TYPE(char_array) == NPY_STRING) {
-        method = PyObject_GetAttr((PyObject *)&PyBytes_Type, method_name);
-    }
-    else if (PyArray_TYPE(char_array) == NPY_UNICODE) {
-        method = PyObject_GetAttr((PyObject *)&PyUnicode_Type, method_name);
-    }
-    else {
-        if (_is_user_defined_string_array(char_array)) {
-            PyTypeObject* scalar_type =
-                NPY_DTYPE(PyArray_DESCR(char_array))->scalar_type;
-            method = PyObject_GetAttr((PyObject*)scalar_type, method_name);
+    fast_method_found = get_fast_op(method_name, &fast_method, &fast_method_with_args);
+    if (fast_method_found) {
+        // if (fast_method_with_args) {
+        //     result = _vec_string_fast_op_with_args(char_array, type, fast_method, args_seq);
+        // } else {
+            result = _vec_string_fast_op_no_args(char_array, type, fast_method);
+        // }
+    } else {
+        if (PyArray_TYPE(char_array) == NPY_STRING) {
+            method = PyObject_GetAttr((PyObject *)&PyBytes_Type, method_name);
+        }
+        else if (PyArray_TYPE(char_array) == NPY_UNICODE) {
+            method = PyObject_GetAttr((PyObject *)&PyUnicode_Type, method_name);
         }
         else {
+            if (_is_user_defined_string_array(char_array)) {
+                PyTypeObject* scalar_type =
+                    NPY_DTYPE(PyArray_DESCR(char_array))->scalar_type;
+                method = PyObject_GetAttr((PyObject*)scalar_type, method_name);
+            }
+            else {
+                Py_DECREF(type);
+                goto err;
+            }
+        }
+        if (method == NULL) {
             Py_DECREF(type);
             goto err;
         }
-    }
-    if (method == NULL) {
-        Py_DECREF(type);
-        goto err;
-    }
 
-    if (args_seq == NULL
-            || (PySequence_Check(args_seq) && PySequence_Size(args_seq) == 0)) {
-        result = _vec_string_no_args(char_array, type, method);
-    }
-    else if (PySequence_Check(args_seq)) {
-        result = _vec_string_with_args(char_array, type, method, args_seq);
-    }
-    else {
-        Py_DECREF(type);
-        PyErr_SetString(PyExc_TypeError,
-                "'args' must be a sequence of arguments");
-        goto err;
+        if (args_seq == NULL
+                || (PySequence_Check(args_seq) && PySequence_Size(args_seq) == 0)) {
+            result = _vec_string_no_args(char_array, type, method);
+        }
+        else if (PySequence_Check(args_seq)) {
+            result = _vec_string_with_args(char_array, type, method, args_seq);
+        }
+        else {
+            Py_DECREF(type);
+            PyErr_SetString(PyExc_TypeError,
+                    "'args' must be a sequence of arguments");
+            goto err;
+        }
     }
     if (result == NULL) {
         goto err;
     }
 
     Py_DECREF(char_array);
-    Py_DECREF(method);
+    Py_XDECREF(method);
 
     return (PyObject*)result;
 
