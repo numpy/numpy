5c5ab19fa127123661ef06e0484f3b6f5bfd3290
Algorithmist_Girl <singh_deep_narain@yahoo.com>
2023-09-26 15:08:19 -0400

    new example for select

--- numpy/lib/_function_base_impl.py
+++ numpy/lib/_function_base_impl.py
@@ -802,7 +802,8 @@ def select(condlist, choicelist, default=0):
     >>> np.select(condlist, choicelist, 55)
     array([ 0,  1,  2,  3,  4, 25])
 
-    >>> condlist = [x<=4, x>7]
+    Test case showing when the default value is used:
+    >>> condlist = [x<=4, x<=0]
     >>> choicelist = [x, x**2]
     >>> np.select(condlist, choicelist, 70)
     array([ 0,  1,  2,  3,  4, 70])
