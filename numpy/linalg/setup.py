891fb2213ca7919f48e7e7705ad1be0b681df330
Ralf Gommers <ralf.gommers@gmail.com>
2023-10-11 11:59:03 +0200

    BLD: update setup.py build for changes to ILP64 build config
    [skip circle] [skip cirrus]

--- numpy/linalg/setup.py
+++ numpy/linalg/setup.py
@@ -51,7 +51,8 @@ def configuration(parent_package='', top_path=None):
                     # but use the "64_" convention here.
                     info['define_macros'] = [
                         ('HAVE_BLAS_ILP64', None),
-                        ('BLAS_SYMBOL_SUFFIX', '64_')
+                        ('BLAS_SYMBOL_SUFFIX', '64_'),
+                        ('OPENBLAS_ILP64_NAMING_SCHEME', None),
                     ]
                 self.set_info(**info)
 
