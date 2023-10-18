4c1e4a79d8922803dafe92aea06b4abecc2c69cc
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2023-10-17 20:47:28 -0700

    Revert "Dont include arg methods on 32-bit platforms"
    This reverts commit 76d55349130b9510750f17b26eed6c80e3d4f570.

--- numpy/_core/src/npysort/simd_qsort.dispatch.cpp
+++ numpy/_core/src/npysort/simd_qsort.dispatch.cpp
@@ -11,10 +11,7 @@
 #if defined(NPY_HAVE_AVX512_SKX)
     #include "x86-simd-sort/src/avx512-32bit-qsort.hpp"
     #include "x86-simd-sort/src/avx512-64bit-qsort.hpp"
-/* arg methods are only intended to 64-bit platforms */
-#ifdef NPY_CPU_AMD64
     #include "x86-simd-sort/src/avx512-64bit-argsort.hpp"
-#endif
 #endif
 
 namespace np { namespace qsort_simd {
@@ -22,39 +19,27 @@ namespace np { namespace qsort_simd {
 #if defined(NPY_HAVE_AVX512_SKX)
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(int32_t *arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(uint32_t *arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(int64_t*arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(uint64_t*arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(float *arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSelect)(double *arr, npy_intp* arg, npy_intp num, npy_intp kth)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argselect(arr, reinterpret_cast<int64_t*>(arg), kth, num);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int32_t *arr, npy_intp num, npy_intp kth)
 {
@@ -106,39 +91,27 @@ template<> void NPY_CPU_DISPATCH_CURFX(QSort)(double *arr, intptr_t size)
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(int32_t *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(uint32_t *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(int64_t *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(uint64_t *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(float *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(ArgQSort)(double *arr, npy_intp *arg, npy_intp size)
 {
-#ifdef NPY_CPU_AMD64
     avx512_argsort(arr, reinterpret_cast<int64_t*>(arg), size);
-#endif
 }
 #endif  // NPY_HAVE_AVX512_SKX
 
