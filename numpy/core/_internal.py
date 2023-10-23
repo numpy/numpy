2ead7ca391c4f89b782e07b4e6fed536cdb29767
Mateusz Sokół <mat646@gmail.com>
2023-10-23 10:46:10 +0200

    MAINT: Disable warnings for items imported by pybind11

--- numpy/core/_internal.py
+++ numpy/core/_internal.py
@@ -1,3 +1,5 @@
+from numpy._core import _internal
+
 # Build a new array from the information in a pickle.
 # Note that the name numpy.core._internal._reconstruct is embedded in
 # pickles of ndarrays made with NumPy before release 1.0
@@ -7,6 +9,10 @@ def _reconstruct(subtype, shape, dtype):
     from numpy import ndarray
     return ndarray.__new__(subtype, shape, dtype)
 
+
+# Pybind11 (in versions <= 2.11.1) imports _dtype_from_pep3118 from the
+# _internal submodule, therefore it must be importable without a warning.
+_dtype_from_pep3118 = _internal._dtype_from_pep3118
 
 def __getattr__(attr_name):
     from numpy._core import _internal
