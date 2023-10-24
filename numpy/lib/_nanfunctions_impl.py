b5c8398ec77fbba93226e677974650514704183e
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2023-10-24 12:05:50 -0600

    MAINT: appease linter

--- numpy/lib/_nanfunctions_impl.py
+++ numpy/lib/_nanfunctions_impl.py
@@ -1376,7 +1376,8 @@ def nanpercentile(
         raise TypeError("a must be an array of real numbers")
 
     q = np.true_divide(q, a.dtype.type(100) if a.dtype.kind == "f" else 100)
-    q = np.asanyarray(q)  # undo any decay that the ufunc performed (see gh-13105)
+    # undo any decay that the ufunc performed (see gh-13105)
+    q = np.asanyarray(q)
     if not fnb._quantile_is_valid(q):
         raise ValueError("Percentiles must be in the range [0, 100]")
     return _nanquantile_unchecked(
