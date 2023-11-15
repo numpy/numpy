d6840f54bf8e390686045537a0a9f08be2ecf47e
Sebastian Berg <sebastianb@nvidia.com>
2023-11-15 16:06:48 +0100

    TST: "fix" failing memoryview with too many dimensions test
    Memoryview actually also only supports 65 dimensions currently,
    so skip the test when the memoryview creation fails.
    If it were to work, the test should kick in and work fine.

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -8204,19 +8204,22 @@ class TestNewBufferProtocol:
                 t = dim * t
             return t
 
-        # construct a memoryview with 33 dimensions
-        c_u8_33d = make_ctype((1,)*33, ctypes.c_uint8)
-        m = memoryview(c_u8_33d())
-        assert_equal(m.ndim, 33)
+        # Try constructing a memory with too many dimensions:
+        c_u8_65d = make_ctype((1,)*65, ctypes.c_uint8)
+        try:
+            m = memoryview(c_u8_65d())
+        except ValueError:
+            pytest.skip("memoryview doesn't support more dimensions")
 
-        assert_raises_regex(
-            RuntimeError, "ndim",
-            np.array, m)
+        assert_equal(m.ndim, 65)
+
+        with pytest.raises(RuntimeError, match=".*ndim"):
+            np.array(m)
 
         # The above seems to create some deep cycles, clean them up for
         # easier reference count debugging:
-        del c_u8_33d, m
-        for i in range(33):
+        del c_u8_65d, m
+        for i in range(65):
             if gc.collect() == 0:
                 break
 
