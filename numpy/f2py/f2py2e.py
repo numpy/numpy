fbf0ea0512b19f552a0b643384ae8e6076145a19
Rohit Goswami <rog32@hi.is>
2023-11-19 01:40:24 +0000

    MAINT: Fix implementation for gh22819 [f2py]

--- numpy/f2py/f2py2e.py
+++ numpy/f2py/f2py2e.py
@@ -705,14 +705,15 @@ def run_compile():
 def validate_modulename(pyf_files, modulename='untitled'):
     if len(pyf_files) > 1:
         raise ValueError("Only one .pyf file per call")
-    pyff = pyf_files[0]
-    pyf_modname = auxfuncs.get_f2py_modulename(pyff)
-    if modulename != pyf_modname:
-        outmess(
-            f"Ignoring -m {modulename}.\n"
-            f"{pyff} defines {pyf_modname} to be the modulename.\n"
-        )
-        modulename = pyf_modname
+    if pyf_files:
+        pyff = pyf_files[0]
+        pyf_modname = auxfuncs.get_f2py_modulename(pyff)
+        if modulename != pyf_modname:
+            outmess(
+                f"Ignoring -m {modulename}.\n"
+                f"{pyff} defines {pyf_modname} to be the modulename.\n"
+            )
+            modulename = pyf_modname
     return modulename
 
 def main():
