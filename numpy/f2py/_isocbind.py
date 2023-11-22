732b155d4f9ed4181857e25b7183e6d4097abf25
Rohit Goswami <rog32@hi.is>
2023-11-22 19:49:25 +0000

    ENH: Populate all maps for iso_c bindings

--- numpy/f2py/_isocbind.py
+++ numpy/f2py/_isocbind.py
@@ -51,6 +51,70 @@ iso_c_binding_map = {
     }
 }
 
+isoc_c2pycode_map = {
+    'int': 'i',  # int
+    'short int': 'h',  # short int
+    'long': 'l',  # long int
+    'long long': 'q',  # long long int
+    'signed char': 'b',  # signed char
+    'size_t': 'I',  # size_t (approx unsigned int)
+    'int8_t': 'b',  # int8_t
+    'int16_t': 'h',  # int16_t
+    'int32_t': 'i',  # int32_t
+    'int64_t': 'q',  # int64_t
+    'int_least8_t': 'b',  # int_least8_t
+    'int_least16_t': 'h',  # int_least16_t
+    'int_least32_t': 'i',  # int_least32_t
+    'int_least64_t': 'q',  # int_least64_t
+    'int_fast8_t': 'b',  # int_fast8_t
+    'int_fast16_t': 'h',  # int_fast16_t
+    'int_fast32_t': 'i',  # int_fast32_t
+    'int_fast64_t': 'q',  # int_fast64_t
+    'intmax_t': 'q',  # intmax_t (approx long long)
+    'intptr_t': 'q',  # intptr_t (approx long long)
+    'ptrdiff_t': 'q',  # intptr_t (approx long long)
+    'float': 'f',  # float
+    'double': 'd',  # double
+    'long double': 'g',  # long double
+    'float _Complex': 'F',  # float  _Complex
+    'double _Complex': 'D',  # double  _Complex
+    'long double _Complex': 'G',  # long double  _Complex
+    '_Bool': '?',  #  Bool
+    'char': 'c',   # char
+}
+
+iso_c2py_map = {
+    'int': 'int',
+    'short int': 'int',                 # forced casting
+    'long': 'int',
+    'long long': 'long',
+    'signed char': 'int',           # forced casting
+    'size_t': 'int',                # approx Python int
+    'int8_t': 'int',                # forced casting
+    'int16_t': 'int',               # forced casting
+    'int32_t': 'int',
+    'int64_t': 'long',
+    'int_least8_t': 'int',          # forced casting
+    'int_least16_t': 'int',         # forced casting
+    'int_least32_t': 'int',
+    'int_least64_t': 'long',
+    'int_fast8_t': 'int',           # forced casting
+    'int_fast16_t': 'int',          # forced casting
+    'int_fast32_t': 'int',
+    'int_fast64_t': 'long',
+    'intmax_t': 'long',
+    'intptr_t': 'long',
+    'ptrdiff_t': 'long',
+    'float': 'float',
+    'double': 'float',              # forced casting
+    'long double': 'float',         # forced casting
+    'float _Complex': 'complex',     # forced casting
+    'double _Complex': 'complex',
+    'long double _Complex': 'complex', # forced casting
+    '_Bool': 'bool',
+    'char': 'bytes',                  # approx Python bytes
+}
+
 isoc_kindmap = {}
 for fortran_type, c_type_dict in iso_c_binding_map.items():
     for c_type in c_type_dict.keys():
