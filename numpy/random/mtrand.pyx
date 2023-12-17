5a5f40fb6a2a707e29371d476236565c004ebc8d
linus-md <linus.sommer@protonmail.com>
2023-12-17 19:55:13 +0100

    Update

--- numpy/random/mtrand.pyx
+++ numpy/random/mtrand.pyx
@@ -2462,7 +2462,7 @@ cdef class RandomState:
         density function:
 
         >>> import matplotlib.pyplot as plt
-        >>> count, bins, ignored = plt.hist(s, 100, density=True)
+        >>> count, bins, _ = plt.hist(s, 100, density=True)
         >>> fit = a*m**a / bins**(a+1)
         >>> plt.plot(bins, max(count)*fit/max(fit), linewidth=2, color='r')
         >>> plt.show()
