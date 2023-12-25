67515cc03ab3254536786728eacc484b96cf329f
Rohit Goswami <rog32@hi.is>
2023-12-25 10:46:32 +0530

    TST: Rework callback tests [f2py]

--- numpy/f2py/tests/test_callback.py
+++ numpy/f2py/tests/test_callback.py
@@ -11,234 +11,260 @@ from numpy.testing import IS_PYPY
 from . import util
 
 
-@pytest.mark.usefixtures("build_module")
-class TestF77Callback(util.F2PyTest):
+@pytest.fixture(scope="module")
+def f77_callback_spec():
     spec = util.F2PyModuleSpec(
         test_class_name="TestF77Callback",
-        sources = [util.getpath("tests", "src", "callback", "foo.f")]
+        sources=[util.getpath("tests", "src", "callback", "foo.f")],
     )
-
-    @pytest.mark.parametrize("name", "t,t2".split(","))
-    @pytest.mark.slow
-    def test_all(self, name):
-        self.check_function(name)
-
-    @pytest.mark.xfail(IS_PYPY,
-                       reason="PyPy cannot modify tp_doc after PyType_Ready")
-    def test_docstring(self):
-        expected = textwrap.dedent("""\
-        a = t(fun,[fun_extra_args])
-
-        Wrapper for ``t``.
-
-        Parameters
-        ----------
-        fun : call-back function
-
-        Other Parameters
-        ----------------
-        fun_extra_args : input tuple, optional
-            Default: ()
-
-        Returns
-        -------
-        a : int
-
-        Notes
-        -----
-        Call-back functions::
-
-            def fun(): return a
-            Return objects:
-                a : int
-        """)
-        assert self.module.t.__doc__ == expected
-
-    def check_function(self, name):
-        t = getattr(self.module, name)
-        r = t(lambda: 4)
-        assert r == 4
-        r = t(lambda a: 5, fun_extra_args=(6, ))
-        assert r == 5
-        r = t(lambda a: a, fun_extra_args=(6, ))
-        assert r == 6
-        r = t(lambda a: 5 + a, fun_extra_args=(7, ))
-        assert r == 12
-        r = t(lambda a: math.degrees(a), fun_extra_args=(math.pi, ))
-        assert r == 180
-        r = t(math.degrees, fun_extra_args=(math.pi, ))
-        assert r == 180
-
-        r = t(self.module.func, fun_extra_args=(6, ))
-        assert r == 17
-        r = t(self.module.func0)
-        assert r == 11
-        r = t(self.module.func0._cpointer)
-        assert r == 11
-
-        class A:
-            def __call__(self):
-                return 7
-
-            def mth(self):
-                return 9
-
-        a = A()
-        r = t(a)
-        assert r == 7
-        r = t(a.mth)
-        assert r == 9
-
-    @pytest.mark.skipif(sys.platform == 'win32',
-                        reason='Fails with MinGW64 Gfortran (Issue #9673)')
-    def test_string_callback(self):
-        def callback(code):
-            if code == "r":
-                return 0
-            else:
-                return 1
-
-        f = getattr(self.module, "string_callback")
-        r = f(callback)
-        assert r == 0
-
-    @pytest.mark.skipif(sys.platform == 'win32',
-                        reason='Fails with MinGW64 Gfortran (Issue #9673)')
-    def test_string_callback_array(self):
-        # See gh-10027
-        cu1 = np.zeros((1, ), "S8")
-        cu2 = np.zeros((1, 8), "c")
-        cu3 = np.array([""], "S8")
-
-        def callback(cu, lencu):
-            if cu.shape != (lencu,):
-                return 1
-            if cu.dtype != "S8":
-                return 2
-            if not np.all(cu == b""):
-                return 3
-            return 0
-
-        f = getattr(self.module, "string_callback_array")
-        for cu in [cu1, cu2, cu3]:
-            res = f(callback, cu, cu.size)
-            assert res == 0
-
-    def test_threadsafety(self):
-        # Segfaults if the callback handling is not threadsafe
-
-        errors = []
-
-        def cb():
-            # Sleep here to make it more likely for another thread
-            # to call their callback at the same time.
-            time.sleep(1e-3)
-
-            # Check reentrancy
-            r = self.module.t(lambda: 123)
-            assert r == 123
-
-            return 42
-
-        def runner(name):
-            try:
-                for j in range(50):
-                    r = self.module.t(cb)
-                    assert r == 42
-                    self.check_function(name)
-            except Exception:
-                errors.append(traceback.format_exc())
-
-        threads = [
-            threading.Thread(target=runner, args=(arg, ))
-            for arg in ("t", "t2") for n in range(20)
-        ]
-
-        for t in threads:
-            t.start()
-
-        for t in threads:
-            t.join()
-
-        errors = "\n\n".join(errors)
-        if errors:
-            raise AssertionError(errors)
-
-    def test_hidden_callback(self):
-        try:
-            self.module.hidden_callback(2)
-        except Exception as msg:
-            assert str(msg).startswith("Callback global_f not defined")
-
-        try:
-            self.module.hidden_callback2(2)
-        except Exception as msg:
-            assert str(msg).startswith("cb: Callback global_f not defined")
-
-        self.module.global_f = lambda x: x + 1
-        r = self.module.hidden_callback(2)
-        assert r == 3
-
-        self.module.global_f = lambda x: x + 2
-        r = self.module.hidden_callback(2)
-        assert r == 4
-
-        del self.module.global_f
-        try:
-            self.module.hidden_callback(2)
-        except Exception as msg:
-            assert str(msg).startswith("Callback global_f not defined")
-
-        self.module.global_f = lambda x=0: x + 3
-        r = self.module.hidden_callback(2)
-        assert r == 5
-
-        # reproducer of gh18341
-        r = self.module.hidden_callback2(2)
-        assert r == 3
+    return spec
 
 
-@pytest.mark.usefixtures("build_module")
-class TestF90Callback(util.F2PyTest):
-    sources = [util.getpath("tests", "src", "callback", "gh17797.f90")]
-
-    @pytest.mark.slow
-    def test_gh17797(self):
-        def incr(x):
-            return x + 123
-
-        y = np.array([1, 2, 3], dtype=np.int64)
-        r = self.module.gh17797(incr, y)
-        assert r == 123 + 1 + 2 + 3
+@pytest.fixture(scope="module")
+def f90_callback_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestF90Callback",
+        sources=[util.getpath("tests", "src", "callback", "gh17797.f90")],
+    )
+    return spec
 
 
-@pytest.mark.usefixtures("build_module")
-class TestGH18335(util.F2PyTest):
-    """The reproduction of the reported issue requires specific input that
-    extensions may break the issue conditions, so the reproducer is
-    implemented as a separate test class. Do not extend this test with
-    other tests!
+@pytest.fixture(scope="module")
+def gh18335_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestGH18335",
+        sources=[util.getpath("tests", "src", "callback", "gh18335.f90")],
+    )
+    return spec
+
+
+@pytest.fixture(scope="module")
+def gh25211_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestGH25211",
+        sources=[
+            util.getpath("tests", "src", "callback", "gh25211.f"),
+            util.getpath("tests", "src", "callback", "gh25211.pyf"),
+        ],
+        module_name="callback2",
+    )
+    return spec
+
+
+# TestF77Callback
+
+
+def check_function(mod, name):
+    t = getattr(mod, name)
+    r = t(lambda: 4)
+    assert r == 4
+    r = t(lambda a: 5, fun_extra_args=(6,))
+    assert r == 5
+    r = t(lambda a: a, fun_extra_args=(6,))
+    assert r == 6
+    r = t(lambda a: 5 + a, fun_extra_args=(7,))
+    assert r == 12
+    r = t(lambda a: math.degrees(a), fun_extra_args=(math.pi,))
+    assert r == 180
+    r = t(math.degrees, fun_extra_args=(math.pi,))
+    assert r == 180
+
+    r = t(mod.func, fun_extra_args=(6,))
+    assert r == 17
+    r = t(mod.func0)
+    assert r == 11
+    r = t(mod.func0._cpointer)
+    assert r == 11
+
+
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+@pytest.mark.parametrize("name", ["t", "t2"])
+def test_all(_mod, name):
+    check_function(_mod, name)
+
+
+@pytest.mark.xfail(IS_PYPY, reason="PyPy cannot modify tp_doc after PyType_Ready")
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+def test_docstring(_mod):
+    expected = textwrap.dedent(
+        """\
+    a = t(fun,[fun_extra_args])
+
+    Wrapper for ``t``.
+
+    Parameters
+    ----------
+    fun : call-back function
+
+    Other Parameters
+    ----------------
+    fun_extra_args : input tuple, optional
+        Default: ()
+
+    Returns
+    -------
+    a : int
+
+    Notes
+    -----
+    Call-back functions::
+
+        def fun(): return a
+        Return objects:
+            a : int
     """
-    sources = [util.getpath("tests", "src", "callback", "gh18335.f90")]
-
-    @pytest.mark.slow
-    def test_gh18335(self):
-        def foo(x):
-            x[0] += 1
-
-        r = self.module.gh18335(foo)
-        assert r == 123 + 1
+    )
+    assert _mod.t.__doc__ == expected
 
 
-@pytest.mark.usefixtures("build_module")
-class TestGH25211(util.F2PyTest):
-    sources = [util.getpath("tests", "src", "callback", "gh25211.f"),
-               util.getpath("tests", "src", "callback", "gh25211.pyf")]
-    module_name = "callback2"
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Fails with MinGW64 Gfortran (Issue #9673)"
+)
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+def test_string_callback(_mod):
+    def callback(code):
+        if code == "r":
+            return 0
+        else:
+            return 1
 
-    def test_gh25211(self):
-        def bar(x):
-            return x*x
+    f = getattr(_mod, "string_callback")
+    r = f(callback)
+    assert r == 0
 
-        res = self.module.foo(bar)
-        assert res == 110
+
+@pytest.mark.skipif(
+    sys.platform == "win32", reason="Fails with MinGW64 Gfortran (Issue #9673)"
+)
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+def test_string_callback_array(_mod):
+    # See gh-10027
+    cu1 = np.zeros((1,), "S8")
+    cu2 = np.zeros((1, 8), "c")
+    cu3 = np.array([""], "S8")
+
+    def callback(cu, lencu):
+        if cu.shape != (lencu,):
+            return 1
+        if cu.dtype != "S8":
+            return 2
+        if not np.all(cu == b""):
+            return 3
+        return 0
+
+    f = getattr(_mod, "string_callback_array")
+    for cu in [cu1, cu2, cu3]:
+        res = f(callback, cu, cu.size)
+        assert res == 0
+
+
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+def test_threadsafety(_mod):
+    # Segfaults if the callback handling is not threadsafe
+
+    errors = []
+
+    def cb():
+        # Sleep here to make it more likely for another thread
+        # to call their callback at the same time.
+        time.sleep(1e-3)
+
+        # Check reentrancy
+        r = _mod.t(lambda: 123)
+        assert r == 123
+
+        return 42
+
+    def runner(name):
+        try:
+            for j in range(50):
+                r = _mod.t(cb)
+                assert r == 42
+                check_function(_mod, name)
+        except Exception:
+            errors.append(traceback.format_exc())
+
+    threads = [
+        threading.Thread(target=runner, args=(arg,))
+        for arg in ("t", "t2")
+        for n in range(20)
+    ]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    errors = "\n\n".join(errors)
+    if errors:
+        raise AssertionError(errors)
+
+
+@pytest.mark.parametrize("_mod", ["f77_callback_spec"], indirect=True)
+def test_hidden_callback(_mod):
+    try:
+        _mod.hidden_callback(2)
+    except Exception as msg:
+        assert str(msg).startswith("Callback global_f not defined")
+
+    try:
+        _mod.hidden_callback2(2)
+    except Exception as msg:
+        assert str(msg).startswith("cb: Callback global_f not defined")
+
+    _mod.global_f = lambda x: x + 1
+    r = _mod.hidden_callback(2)
+    assert r == 3
+
+    _mod.global_f = lambda x: x + 2
+    r = _mod.hidden_callback(2)
+    assert r == 4
+
+    del _mod.global_f
+    try:
+        _mod.hidden_callback(2)
+    except Exception as msg:
+        assert str(msg).startswith("Callback global_f not defined")
+
+    _mod.global_f = lambda x=0: x + 3
+    r = _mod.hidden_callback(2)
+    assert r == 5
+
+    # reproducer of gh18341
+    r = _mod.hidden_callback2(2)
+    assert r == 3
+
+
+# TestF90Callback
+
+
+@pytest.mark.parametrize("_mod", ["f90_callback_spec"], indirect=True)
+def test_gh17797(_mod):
+    def incr(x):
+        return x + 123
+
+    y = np.array([1, 2, 3], dtype=np.int64)
+    r = _mod.gh17797(incr, y)
+    assert r == 123 + 1 + 2 + 3
+
+
+# TestGH18335
+
+
+@pytest.mark.parametrize("_mod", ["gh18335_spec"], indirect=True)
+def test_gh18335(_mod):
+    def foo(x):
+        x[0] += 1
+
+    r = _mod.gh18335(foo)
+    assert r == 123 + 1
+
+
+@pytest.mark.parametrize("_mod", ["gh25211_spec"], indirect=True)
+def test_gh25211(_mod):
+    def bar(x):
+        return x * x
+
+    res = _mod.foo(bar)
+    assert res == 110
