4729064c6ccaf9253072ad1c2d73917b89744256
Rohit Goswami <rog32@hi.is>
2024-01-01 01:38:49 +0530

    TST: test_docs and test_f2cmap

--- numpy/f2py/tests/test_docs.py
+++ numpy/f2py/tests/test_docs.py
@@ -19,34 +19,40 @@ pytestmark = pytest.mark.skipif(
 def _path(*args):
     return get_docdir().joinpath(*args)
 
-@pytest.mark.slow
-@pytest.mark.usefixtures("build_module")
-class TestDocAdvanced(util.F2PyTest):
-    # options = ['--debug-capi', '--build-dir', '/tmp/build-f2py']
-    sources = [_path('asterisk1.f90'), _path('asterisk2.f90'),
-               _path('ftype.f')]
+@pytest.fixture(scope="module")
+def doc_advanced_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestDocAdvanced",
+        sources=[_path("asterisk1.f90"),
+                 _path("asterisk2.f90"),
+                 _path("ftype.f")],
+    )
+    return spec
 
-    def test_asterisk1(self):
-        foo = getattr(self.module, 'foo1')
-        assert_equal(foo(), b'123456789A12')
+@pytest.mark.parametrize("_mod", ["doc_advanced_spec"], indirect=True)
+def test_asterisk1(_mod):
+    foo = getattr(_mod, 'foo1')
+    assert_equal(foo(), b'123456789A12')
 
-    def test_asterisk2(self):
-        foo = getattr(self.module, 'foo2')
-        assert_equal(foo(2), b'12')
-        assert_equal(foo(12), b'123456789A12')
-        assert_equal(foo(20), b'123456789A123456789B')
+@pytest.mark.parametrize("_mod", ["doc_advanced_spec"], indirect=True)
+def test_asterisk2(_mod):
+    foo = getattr(_mod, 'foo2')
+    assert_equal(foo(2), b'12')
+    assert_equal(foo(12), b'123456789A12')
+    assert_equal(foo(20), b'123456789A123456789B')
 
-    def test_ftype(self):
-        ftype = self.module
-        ftype.foo()
-        assert_equal(ftype.data.a, 0)
-        ftype.data.a = 3
-        ftype.data.x = [1, 2, 3]
-        assert_equal(ftype.data.a, 3)
-        assert_array_equal(ftype.data.x,
-                           np.array([1, 2, 3], dtype=np.float32))
-        ftype.data.x[1] = 45
-        assert_array_equal(ftype.data.x,
-                           np.array([1, 45, 3], dtype=np.float32))
+@pytest.mark.parametrize("_mod", ["doc_advanced_spec"], indirect=True)
+def test_ftype(_mod):
+    ftype = _mod
+    ftype.foo()
+    assert_equal(ftype.data.a, 0)
+    ftype.data.a = 3
+    ftype.data.x = [1, 2, 3]
+    assert_equal(ftype.data.a, 3)
+    assert_array_equal(ftype.data.x,
+                       np.array([1, 2, 3], dtype=np.float32))
+    ftype.data.x[1] = 45
+    assert_array_equal(ftype.data.x,
+                       np.array([1, 45, 3], dtype=np.float32))
 
-    # TODO: implement test methods for other example Fortran codes
+# TODO: implement test methods for other example Fortran codes
--- numpy/f2py/tests/test_f2cmap.py
+++ numpy/f2py/tests/test_f2cmap.py
@@ -2,16 +2,22 @@ from . import util
 import pytest
 import numpy as np
 
-@pytest.mark.usefixtures("build_module")
-class TestF2Cmap(util.F2PyTest):
-    sources = [
-        util.getpath("tests", "src", "f2cmap", "isoFortranEnvMap.f90"),
-        util.getpath("tests", "src", "f2cmap", ".f2py_f2cmap")
-    ]
 
-    # gh-15095
-    def test_gh15095(self):
-        inp = np.ones(3)
-        out = self.module.func1(inp)
-        exp_out = 3
-        assert out == exp_out
+@pytest.fixture(scope="module")
+def f2cmap_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestF2Cmap",
+        sources=[
+            util.getpath("tests", "src", "f2cmap", "isoFortranEnvMap.f90"),
+            util.getpath("tests", "src", "f2cmap", ".f2py_f2cmap"),
+        ],
+    )
+    return spec
+
+
+@pytest.mark.parametrize("_mod", ["f2cmap_spec"], indirect=True)
+def test_gh15095(_mod):
+    inp = np.ones(3)
+    out = _mod.func1(inp)
+    exp_out = 3
+    assert out == exp_out
