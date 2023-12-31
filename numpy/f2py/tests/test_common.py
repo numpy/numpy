0cb439dd5da30b7f5db6a7acd8df9a32a4f065c0
Rohit Goswami <rog32@hi.is>
2023-12-31 20:22:24 +0530

    TST: Rework test_common

--- numpy/f2py/tests/test_common.py
+++ numpy/f2py/tests/test_common.py
@@ -2,21 +2,30 @@ import pytest
 import numpy as np
 from . import util
 
-@pytest.mark.slow
-@pytest.mark.usefixtures("build_module")
-class TestCommonBlock(util.F2PyTest):
-    sources = [util.getpath("tests", "src", "common", "block.f")]
+@pytest.fixture(scope="module")
+def common_block_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestCommonBlock",
+        sources = [util.getpath("tests", "src", "common", "block.f")]
+    )
+    return spec
 
-    def test_common_block(self):
-        self.module.initcb()
-        assert self.module.block.long_bn == np.array(1.0, dtype=np.float64)
-        assert self.module.block.string_bn == np.array("2", dtype="|S1")
-        assert self.module.block.ok == np.array(3, dtype=np.int32)
+@pytest.mark.parametrize("_mod", ["common_block_spec"], indirect=True)
+def test_common_block(_mod):
+    _mod.initcb()
+    assert _mod.block.long_bn == np.array(1.0, dtype=np.float64)
+    assert _mod.block.string_bn == np.array("2", dtype="|S1")
+    assert _mod.block.ok == np.array(3, dtype=np.int32)
 
 
-@pytest.mark.usefixtures("build_module")
-class TestCommonWithUse(util.F2PyTest):
-    sources = [util.getpath("tests", "src", "common", "gh19161.f90")]
+@pytest.fixture(scope="module")
+def common_use_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestCommonWithUse",
+        sources = [util.getpath("tests", "src", "common", "gh19161.f90")]
+    )
+    return spec
 
-    def test_common_gh19161(self):
-        assert self.module.data.x == 0
+@pytest.mark.parametrize("_mod", ["common_use_spec"], indirect=True)
+def test_common_gh19161(_mod):
+    assert _mod.data.x == 0
