82464a5396d2dc4e726b38fa62aeece83d47a9fb
Rohit Goswami <rog32@hi.is>
2024-01-04 22:28:01 +0530

    TST: Convert test_mixed

--- numpy/f2py/tests/test_mixed.py
+++ numpy/f2py/tests/test_mixed.py
@@ -6,30 +6,38 @@ from numpy.testing import IS_PYPY
 from . import util
 
 
-@pytest.mark.usefixtures("build_module")
-class TestMixed(util.F2PyTest):
-    sources = [
-        util.getpath("tests", "src", "mixed", "foo.f"),
-        util.getpath("tests", "src", "mixed", "foo_fixed.f90"),
-        util.getpath("tests", "src", "mixed", "foo_free.f90"),
-    ]
+@pytest.fixture(scope="module")
+def mixed_spec():
+    spec = util.F2PyModuleSpec(
+        test_class_name="TestMixed",
+        sources=[
+            util.getpath("tests", "src", "mixed", "foo.f"),
+            util.getpath("tests", "src", "mixed", "foo_fixed.f90"),
+            util.getpath("tests", "src", "mixed", "foo_free.f90"),
+        ],
+    )
+    return spec
 
-    @pytest.mark.slow
-    def test_all(self):
-        assert self.module.bar11() == 11
-        assert self.module.foo_fixed.bar12() == 12
-        assert self.module.foo_free.bar13() == 13
 
-    @pytest.mark.xfail(IS_PYPY,
-                       reason="PyPy cannot modify tp_doc after PyType_Ready")
-    def test_docstring(self):
-        expected = textwrap.dedent("""\
-        a = bar11()
+@pytest.mark.parametrize("_mod", ["mixed_spec"], indirect=True)
+def test_all(_mod):
+    assert _mod.bar11() == 11
+    assert _mod.foo_fixed.bar12() == 12
+    assert _mod.foo_free.bar13() == 13
 
-        Wrapper for ``bar11``.
 
-        Returns
-        -------
-        a : int
-        """)
-        assert self.module.bar11.__doc__ == expected
+@pytest.mark.xfail(IS_PYPY, reason="PyPy cannot modify tp_doc after PyType_Ready")
+@pytest.mark.parametrize("_mod", ["mixed_spec"], indirect=True)
+def test_docstring(_mod):
+    expected = textwrap.dedent(
+        """\
+    a = bar11()
+
+    Wrapper for ``bar11``.
+
+    Returns
+    -------
+    a : int
+    """
+    )
+    assert _mod.bar11.__doc__ == expected
