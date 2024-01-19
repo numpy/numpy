0ac6bdef2791cb7509bd6ed571e1c37c5ca2e987
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2024-01-19 11:09:33 -0500

    MAINT: mark pocketfft interface visibility hidden
    These changes make no sense for upstream.

--- numpy/fft/pocketfft/pocketfft.h
+++ numpy/fft/pocketfft/pocketfft.h
@@ -14,21 +14,22 @@
 #define POCKETFFT_H
 
 #include <stdlib.h>
+#include "numpy/numpyconfig.h"  // for NPY_VISIBILITY_HIDDEN
 
-struct cfft_plan_i;
+NPY_VISIBILITY_HIDDEN struct cfft_plan_i;
 typedef struct cfft_plan_i * cfft_plan;
-cfft_plan make_cfft_plan (size_t length);
-void destroy_cfft_plan (cfft_plan plan);
-int cfft_backward(cfft_plan plan, double c[], double fct);
-int cfft_forward(cfft_plan plan, double c[], double fct);
-size_t cfft_length(cfft_plan plan);
+NPY_VISIBILITY_HIDDEN cfft_plan make_cfft_plan (size_t length);
+NPY_VISIBILITY_HIDDEN void destroy_cfft_plan (cfft_plan plan);
+NPY_VISIBILITY_HIDDEN int cfft_backward(cfft_plan plan, double c[], double fct);
+NPY_VISIBILITY_HIDDEN int cfft_forward(cfft_plan plan, double c[], double fct);
+NPY_VISIBILITY_HIDDEN size_t cfft_length(cfft_plan plan);
 
-struct rfft_plan_i;
+NPY_VISIBILITY_HIDDEN struct rfft_plan_i;
 typedef struct rfft_plan_i * rfft_plan;
-rfft_plan make_rfft_plan (size_t length);
-void destroy_rfft_plan (rfft_plan plan);
-int rfft_backward(rfft_plan plan, double c[], double fct);
-int rfft_forward(rfft_plan plan, double c[], double fct);
-size_t rfft_length(rfft_plan plan);
+NPY_VISIBILITY_HIDDEN rfft_plan make_rfft_plan (size_t length);
+NPY_VISIBILITY_HIDDEN void destroy_rfft_plan (rfft_plan plan);
+NPY_VISIBILITY_HIDDEN int rfft_backward(rfft_plan plan, double c[], double fct);
+NPY_VISIBILITY_HIDDEN int rfft_forward(rfft_plan plan, double c[], double fct);
+NPY_VISIBILITY_HIDDEN size_t rfft_length(rfft_plan plan);
 
 #endif
