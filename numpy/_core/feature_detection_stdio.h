5eb2d910aebef011fca8800d9de56133693607fb
Ola x Nilsson <olani@axis.com>
2024-01-19 10:26:41 +0100

    BUG: Use large file fallocate on 32 bit linux platforms
    Using local prototypes instead of the header files meant that the
    redirects triggered by -D_FILE_OFFSET_BITS=64 were not triggered for
    fallocate.
    
    The prototypes in feature_detection_stdio.h are not used since the
    switch to meason, and might trigger false positives in the feature
    discovery code.

--- numpy/_core/feature_detection_stdio.h
+++ numpy/_core/feature_detection_stdio.h
@@ -1,6 +1,3 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <fcntl.h>
-
-off_t ftello(FILE *stream);
-int fseeko(FILE *stream, off_t offset, int whence);
-int fallocate(int, int, off_t, off_t);
