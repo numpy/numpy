46fe99746cc1b3a19067b19d335d4c9a03a92d9b
Sebastian Berg <sebastianb@nvidia.com>
2024-01-21 17:46:50 +0100

    Try to use new logic for quantiles (not perfect yet, but better maybe?)

--- numpy/lib/_function_base_impl.py
+++ numpy/lib/_function_base_impl.py
@@ -4268,13 +4268,22 @@ def percentile(a,
         method = _check_interpolation_as_method(
             method, interpolation, "percentile")
 
-    a = np.asanyarray(a)
+    conv = _array_converter(a, q)
+    a, q_arr = conv.as_arrays(pyscalars="convert")
+
     if a.dtype.kind == "c":
         raise TypeError("a must be an array of real numbers")
 
     # Use dtype of array if possible (e.g., if q is a python int or float)
     # by making the divisor have the dtype of the data array.
     q = np.true_divide(q, a.dtype.type(100) if a.dtype.kind == "f" else 100, out=...)
+
+    # TODO(seberg): from rebase, maybe the following was an improvement?
+    # if isinstance(q, (int, float)) and a.dtype.kind == "f":
+    #     q = np.true_divide(q, np.array(100, dtype=a.dtype))
+    # else:
+    #     q = q_arr / 100
+
     if not _quantile_is_valid(q):
         raise ValueError("Percentiles must be in the range [0, 100]")
 
@@ -4289,8 +4298,10 @@ def percentile(a,
         if np.any(weights < 0):
             raise ValueError("Weights must be non-negative.")
 
-    return _quantile_unchecked(
+    result = _quantile_unchecked(
         a, q, axis, out, overwrite_input, method, keepdims, weights)
+    # If no broadcasting happened, and q was a scalar, return a scalar:
+    return conv.wrap(result, to_scalar=conv.scalar_input[1])
 
 
 def _quantile_dispatcher(a, q, axis=None, out=None, overwrite_input=None,
@@ -4529,15 +4540,16 @@ def quantile(a,
         method = _check_interpolation_as_method(
             method, interpolation, "quantile")
 
-    a = np.asanyarray(a)
+    conv = _array_converter(a, q)
+    a, q_arr = conv.as_arrays(pyscalars="convert")
     if a.dtype.kind == "c":
         raise TypeError("a must be an array of real numbers")
 
     # Use dtype of array if possible (e.g., if q is a python int or float).
     if isinstance(q, (int, float)) and a.dtype.kind == "f":
-        q = np.asanyarray(q, dtype=a.dtype)
+        q = np.asarray(q, dtype=a.dtype)
     else:
-        q = np.asanyarray(q)
+        q = q_arr
 
     if not _quantile_is_valid(q):
         raise ValueError("Quantiles must be in the range [0, 1]")
@@ -4553,8 +4565,10 @@ def quantile(a,
         if np.any(weights < 0):
             raise ValueError("Weights must be non-negative.")
 
-    return _quantile_unchecked(
+    res = _quantile_unchecked(
         a, q, axis, out, overwrite_input, method, keepdims, weights)
+    # If no broadcasting happened, and q was a scalar, return a scalar:
+    return conv.wrap(res, to_scalar=conv.scalar_input[1])
 
 
 def _quantile_unchecked(a,
@@ -4669,12 +4683,9 @@ def _lerp(a, b, t, out=None):
         Output array.
     """
     diff_b_a = subtract(b, a)
-    # asanyarray is a stop-gap until gh-13105
-    lerp_interpolation = asanyarray(add(a, diff_b_a * t, out=out))
-    subtract(b, diff_b_a * (1 - t), out=lerp_interpolation, where=t >= 0.5,
-             casting='unsafe', dtype=type(lerp_interpolation.dtype))
-    if lerp_interpolation.ndim == 0 and out is None:
-        lerp_interpolation = lerp_interpolation[()]  # unpack 0d arrays
+    lerp_interpolation = add(a, diff_b_a * t, out=out)
+    subtract(b, diff_b_a * (1 - t), out=lerp_interpolation, where=t >= 0.5)
+
     return lerp_interpolation
 
 
