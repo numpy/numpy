ff9cedf9c540e9e4acff57772d075f4112e5e2dc
mattip <matti.picus@gmail.com>
2024-01-29 21:06:29 +0200

    BLD: mv all requirement files to a subdirectory

--- tools/ci/run_32_bit_linux_docker.sh
+++ tools/ci/run_32_bit_linux_docker.sh
@@ -4,8 +4,8 @@ git config --global --add safe.directory /numpy
 cd /numpy
 /opt/python/cp39-cp39/bin/python -mvenv venv
 source venv/bin/activate
-pip install -r ci_requirements.txt
-python3 -m pip install -r test_requirements.txt
+pip install -r requirements/ci_requirements.txt
+python3 -m pip install -r requirements/test_requirements.txt
 echo CFLAGS \$CFLAGS
 spin config-openblas --with-scipy-openblas=32
 export PKG_CONFIG_PATH=/numpy/.openblas
