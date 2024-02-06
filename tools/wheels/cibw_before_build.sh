ddf97f9e75f1a774eeba24b09d6a9e42e25eba83
Matti Picus <matti.picus@gmail.com>
2024-02-07 00:27:23 +0200

    use env variable for windows PKG_CONFIG_PATH, replace slashes, split cirrus jobs [wheel build]

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -25,11 +25,12 @@ fi
 
 # Install Openblas from scipy-openblas64
 if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
+    echo PKG_CONFIG_PATH $PKG_CONFIG_PATH
     PKG_CONFIG_PATH=$PROJECT_DIR/.openblas
     rm -rf $PKG_CONFIG_PATH
     mkdir -p $PKG_CONFIG_PATH
     python -m pip install scipy-openblas64
-    python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config())" > $PKG_CONFIG_PATH/scipy-openblas.pc
+    python -c "import scipy_openblas64; print(scipy_openblas64.get_pkg_config().replace('\\\\', '/'))" > $PKG_CONFIG_PATH/scipy-openblas.pc
     # Copy the shared objects to a path under $PKG_CONFIG_PATH, the build
     # will point $LD_LIBRARY_PATH there and then auditwheel/delocate-wheel will
     # pull these into the wheel. Use python to avoid windows/posix problems
