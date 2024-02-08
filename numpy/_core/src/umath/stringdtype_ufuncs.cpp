e61af601696081a7437021154bf225401ae7561d
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-02-07 19:12:22 -0700

    ENH: implement index and rindex for stringdtype

--- numpy/_core/src/umath/stringdtype_ufuncs.cpp
+++ numpy/_core/src/umath/stringdtype_ufuncs.cpp
@@ -1235,7 +1235,7 @@ fail:
 }
 
 static int
-string_find_rfind_count_promoter(PyUFuncObject *NPY_UNUSED(ufunc),
+string_findlike_promoter(PyUFuncObject *NPY_UNUSED(ufunc),
         PyArray_DTypeMeta *op_dtypes[], PyArray_DTypeMeta *signature[],
         PyArray_DTypeMeta *new_op_dtypes[])
 {
@@ -1248,7 +1248,7 @@ string_find_rfind_count_promoter(PyUFuncObject *NPY_UNUSED(ufunc),
 }
 
 static NPY_CASTING
-string_find_rfind_count_resolve_descriptors(
+string_findlike_resolve_descriptors(
         struct PyArrayMethodObject_tag *NPY_UNUSED(method),
         PyArray_DTypeMeta *NPY_UNUSED(dtypes[]),
         PyArray_Descr *given_descrs[],
@@ -1353,6 +1353,7 @@ string_startswith_endswith_resolve_descriptors(
     return NPY_NO_CASTING;
 }
 
+template <bool raise_error>
 static int
 string_find_strided_loop(PyArrayMethod_Context *context, char *const data[],
                          npy_intp const dimensions[],
@@ -1403,6 +1404,10 @@ string_find_strided_loop(PyArrayMethod_Context *context, char *const data[],
         Buffer<ENCODING::UTF8> buf2((char *)s2.buf, s2.size);
 
         npy_intp pos = string_find(buf1, buf2, start, end);
+        if (raise_error && pos == -1) {
+            npy_gil_error(PyExc_ValueError, "substring not found");
+            goto fail;
+        }
         *(npy_intp *)out = pos;
 
         in1 += strides[0];
@@ -1422,6 +1427,7 @@ fail:
     return -1;
 }
 
+template <bool raise_error>
 static int
 string_rfind_strided_loop(PyArrayMethod_Context *context, char *const data[],
                           npy_intp const dimensions[],
@@ -1473,6 +1479,10 @@ string_rfind_strided_loop(PyArrayMethod_Context *context, char *const data[],
         Buffer<ENCODING::UTF8> buf2((char *)s2.buf, s2.size);
 
         npy_intp pos = string_rfind(buf1, buf2, start, end);
+        if (raise_error && pos == -1) {
+            npy_gil_error(PyExc_ValueError, "substring not found");
+            goto fail;
+        }
         *(npy_intp *)out = pos;
 
         in1 += strides[0];
@@ -2880,40 +2890,42 @@ init_stringdtype_ufuncs(PyObject *umath)
         return -1;
     }
 
-    PyArray_DTypeMeta *find_rfind_count_dtypes[] = {
+    PyArray_DTypeMeta *findlike_dtypes[] = {
         &PyArray_StringDType, &PyArray_StringDType,
         &PyArray_Int64DType, &PyArray_Int64DType,
         &PyArray_DefaultIntDType,
     };
 
-    static PyArrayMethod_StridedLoop *find_rfind_count_strided_loops[] = {
-        &string_find_strided_loop,
-        &string_rfind_strided_loop,
+    static PyArrayMethod_StridedLoop *findlike_strided_loops[] = {
+        &string_find_strided_loop<false>,
+        &string_rfind_strided_loop<false>,
+        &string_find_strided_loop<true>,
+        &string_rfind_strided_loop<true>,
         &string_count_strided_loop,
     };
 
-    const char* find_rfind_count_names[] = {
-        "find", "rfind", "count",
+    const char* findlike_names[] = {
+        "find", "rfind", "index", "rindex", "count",
     };
 
-    PyArray_DTypeMeta *find_rfind_count_promoter_dtypes[] = {
+    PyArray_DTypeMeta *findlike_promoter_dtypes[] = {
         &PyArray_StringDType, &PyArray_UnicodeDType,
         &PyArray_PyIntAbstractDType, &PyArray_PyIntAbstractDType,
         &PyArray_DefaultIntDType,
     };
 
-    for (int i=0; i<3; i++) {
-        if (init_ufunc(umath, find_rfind_count_names[i], find_rfind_count_dtypes,
-                       &string_find_rfind_count_resolve_descriptors,
-                       find_rfind_count_strided_loops[i], 4, 1, NPY_NO_CASTING,
+    for (int i=0; i<5; i++) {
+        if (init_ufunc(umath, findlike_names[i], findlike_dtypes,
+                       &string_findlike_resolve_descriptors,
+                       findlike_strided_loops[i], 4, 1, NPY_NO_CASTING,
                        (NPY_ARRAYMETHOD_FLAGS) 0) < 0) {
             return -1;
         }
 
 
-        if (add_promoter(umath, find_rfind_count_names[i],
-                         find_rfind_count_promoter_dtypes,
-                         5, string_find_rfind_count_promoter) < 0) {
+        if (add_promoter(umath, findlike_names[i],
+                         findlike_promoter_dtypes,
+                         5, string_findlike_promoter) < 0) {
             return -1;
         }
     }
