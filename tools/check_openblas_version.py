f746a5856aefcad6645966afea93897a2baf6f0f
mattip <matti.picus@gmail.com>
2024-02-22 16:58:03 +0200

    ENH: add name to fields checked [skip azp][skip cirrus][skip circle]

--- tools/check_openblas_version.py
+++ tools/check_openblas_version.py
@@ -1,6 +1,8 @@
 """
-Check the blas version is blas and is higher than min_version
 usage: check_openblas_version.py <min_version>
+
+Check the blas version is blas from scipy-openblas and is higher than
+min_version
 example: check_openblas_version.py 0.3.26
 """
 
@@ -13,5 +15,5 @@ deps = numpy.show_config('dicts')['Build Dependencies']
 assert "blas" in deps
 print("Build Dependencies: blas")
 pprint.pprint(deps["blas"])
-print(f"Checking verions >= {version}")
 assert deps["blas"]["version"].split(".") >= version.split(".")
+assert deps["blas"]["name"] == "scipy-openblas"
