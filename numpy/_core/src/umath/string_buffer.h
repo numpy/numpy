567b57d751fb1f7e1625520a58c8d0c0df05f4df
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-02-26 21:39:07 +0100

    ENH: Implement multiply ufunc for unicode & bytes (#25864)
    * ENH: Implement multiply ufunc for unicode & bytes
    
    * Do binary op for stringdtype and fix tests for type errors
    
    * Address feedback; assert(false) in memset & fix type resolution for ints
    
    * Fix nan handling for stringdtype multiply
    
    * Fix doctests and broadcasting of input arrays to multiply
    
    * Address feedback; delegate overflow checking to stringdtype loops
    
    * Address feedback; fix comments and add backwards-compatibility wrapper
    
    * Address feedback; skip overflow checking
    
    * Add improved overflow check back to np.strings.multiply
    
    * Remove unused import from test_strings [skip ci]

--- numpy/_core/src/umath/string_buffer.h
+++ numpy/_core/src/umath/string_buffer.h
@@ -405,6 +405,31 @@ struct Buffer {
     }
 
     inline void
+    buffer_memset(npy_ucs4 fill_char, size_t n_chars)
+    {
+        if (n_chars == 0) {
+            return;
+        }
+        switch (enc) {
+            case ENCODING::ASCII:
+                memset(buf, fill_char, n_chars);
+                break;
+            case ENCODING::UTF32:
+            {
+                char *tmp = buf;
+                for (size_t i = 0; i < n_chars; i++) {
+                    *(npy_ucs4 *)tmp = fill_char;
+                    tmp += sizeof(npy_ucs4);
+                }
+                break;
+            }
+            case ENCODING::UTF8:
+                assert(false);  // buffer_memset not used by stringdtype
+                break;
+        }
+    }
+
+    inline void
     buffer_fill_with_zeros_after_index(size_t start_index)
     {
         Buffer<enc> offset = *this + start_index;
