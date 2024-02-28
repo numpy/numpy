9b94cce22f99e7a2b8ba6c127e075bf8a93bbcda
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-02-28 12:54:23 +0100

    Add test_expandtabs with unicode characters

--- numpy/_core/tests/test_strings.py
+++ numpy/_core/tests/test_strings.py
@@ -639,8 +639,6 @@ class TestMethods:
         ("abc", "", "-", 0, "abc"),
         ("abc", "ab", "--", 0, "abc"),
         ("abc", "xy", "--", -1, "abc"),
-        ("Ae¢☃€ 😊" * 2, "A", "B", -1, "Be¢☃€ 😊Be¢☃€ 😊"),
-        ("Ae¢☃€ 😊" * 2, "😊", "B", -1, "Ae¢☃€ BAe¢☃€ B"),
         (["abbc", "abbd"], "b", "z", [1, 2], ["azbc", "azzd"]),
     ])
     def test_replace(self, buf, old, new, count, res, dt):
@@ -751,10 +749,17 @@ class TestMethodsWithUnicode:
         buf = np.array(in_, dtype=dt)
         assert_array_equal(np.strings.isnumeric(buf), out)
 
-    def test_replace_unicode(self, dt):
-        buf = np.array("...\u043c......<", dtype=dt)
-        assert_array_equal(np.strings.replace(buf,  "<", "&lt;"),
-                           "...\u043c......&lt;")
+    @pytest.mark.parametrize("buf,old,new,count,res", [
+        ("...\u043c......<", "<", "&lt;", -1, "...\u043c......&lt;"),
+        ("Ae¢☃€ 😊" * 2, "A", "B", -1, "Be¢☃€ 😊Be¢☃€ 😊"),
+        ("Ae¢☃€ 😊" * 2, "😊", "B", -1, "Ae¢☃€ BAe¢☃€ B"),
+    ])
+    def test_replace_unicode(self, buf, old, new, count, res, dt):
+        buf = np.array(buf, dtype=dt)
+        old = np.array(old, dtype=dt)
+        new = np.array(new, dtype=dt)
+        res = np.array(res, dtype=dt)
+        assert_array_equal(np.strings.replace(buf, old, new, count), res)
 
     @pytest.mark.parametrize("in_", [
         '\U00010401',
@@ -841,6 +846,16 @@ class TestMethodsWithUnicode:
     def test_index_raises_unicode(self, dt):
         with pytest.raises(ValueError, match="substring not found"):
             np.strings.index("Ae¢☃€ 😊", "😀")
+
+    @pytest.mark.parametrize("buf,res", [
+        ("Ae¢☃€ \t 😊", "Ae¢☃€    😊"),
+        ("\t\U0001044E", "        \U0001044E"),
+    ])
+    def test_expandtabs(self, buf, res, dt):
+        buf = np.array(buf, dtype=dt)
+        res = np.array(res, dtype=dt)
+        assert_array_equal(np.strings.expandtabs(buf), res)
+
 
 
 class TestUnicodeOnlyMethodsRaiseWithBytes:
