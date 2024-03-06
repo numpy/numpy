064b55cfdd5bb47297d80d5d1b9010af93799e9a
Matti Picus <matti.picus@gmail.com>
2024-03-06 20:13:43 +0200

    Update numpy/linalg/_linalg.py

--- numpy/linalg/_linalg.py
+++ numpy/linalg/_linalg.py
@@ -333,7 +333,7 @@ def solve(a, b):
     Returns
     -------
     x : {(..., M,), (..., M, K)} ndarray
-        Solution to the system a x = b.  Returned shape is (..., M) is b is
+        Solution to the system a x = b.  Returned shape is (..., M) if b is
         shape (M,) and (..., M, K) if b is (..., M, K), where the "..." part is
         broadcasted between a and b.
 
