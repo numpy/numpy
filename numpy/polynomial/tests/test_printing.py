baa4bc50fd0b274826b84042ab4b4ebdc340871d
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-03-07 15:54:24 +0100

    refactor

--- numpy/polynomial/tests/test_printing.py
+++ numpy/polynomial/tests/test_printing.py
@@ -175,7 +175,7 @@ class TestStrAscii:
         assert_equal(res, tgt)
 
         res = str(poly.Polynomial([0, 1], domain=[1, 2]))
-        tgt = '0.0 + 1.0 (-3.0 + 2.0*x)'
+        tgt = '0.0 + 1.0 (-3.0 + 2.0x)'
         assert_equal(res, tgt)
 
 class TestLinebreaking:
