BUG: fix UTF-8 heap overflow in ljust/rjust/center

numpy/_core/tests/test_stringdtype.py
@@ -1205,32 +1205,22 @@ def test_binary(string_array, unicode_array, function_name, args):
         assert 0
 
 
-def test_strip(string_array, unicode_array):
-    print("rjs")
-
+def test_strip_ljust_rjust_consistency(string_array, unicode_array):
     rjs = np.char.rjust(string_array, 1000)
     rju = np.char.rjust(unicode_array, 1000)
 
-    print("ljs")
-
     ljs = np.char.ljust(string_array, 1000)
     lju = np.char.ljust(unicode_array, 1000)
-
-    print("lstrip")
 
     assert_array_equal(
         np.char.lstrip(rjs),
         np.char.lstrip(rju).astype(StringDType()),
     )
 
-    print("rstrip")
-
     assert_array_equal(
         np.char.rstrip(ljs),
         np.char.rstrip(lju).astype(StringDType()),
     )
-
-    print("strip")
 
     assert_array_equal(
         np.char.strip(ljs),