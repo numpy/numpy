1b5b3ae1decaf4a937682b6149777677e7c556b2
Raquel Braunschweig <raquel.braunschweig@tecnico.ulisboa.pt>
2024-03-09 21:02:10 +0000

    BUG: update if condition to allow empty indices

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -3506,6 +3506,7 @@ class TestMethods:
         # array is empty
         empty_array = np.asarray(list())
         assert_raises(IndexError, np.put, empty_array, 1, 1, mode="wrap")
+        assert_raises(IndexError, np.put, empty_array, 1, 1, mode="clip")
 
     def test_ravel(self):
         a = np.array([[0, 1], [2, 3]])
