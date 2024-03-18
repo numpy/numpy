c42e3506b7daae0ff0b2f73e1ceb26b0a9f9af02
Sebastian Berg <sebastianb@nvidia.com>
2024-03-18 12:25:31 +0100

    MAINT: Add some const qualifiers to new DType related API
    These are mostly for the places where we pass arrays.

--- numpy/_core/src/multiarray/dtypemeta.h
+++ numpy/_core/src/multiarray/dtypemeta.h
@@ -261,7 +261,7 @@ extern PyArray_DTypeMeta PyArray_StringDType;
 
 /* Internal version see dtypmeta.c for more information. */
 static inline PyArray_ArrFuncs *
-PyDataType_GetArrFuncs(PyArray_Descr *descr)
+PyDataType_GetArrFuncs(const PyArray_Descr *descr)
 {
     return &NPY_DT_SLOTS(NPY_DTYPE(descr))->f;
 }
