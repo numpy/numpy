c1cf2dac9831df11f462f84f7edc2ee548376128
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2024-04-02 14:53:52 -0700

    Use the new static methods API from x86-simd-sort

--- numpy/_core/src/npysort/x86_simd_qsort.dispatch.cpp
+++ numpy/_core/src/npysort/x86_simd_qsort.dispatch.cpp
@@ -1,89 +1,57 @@
 #include "x86_simd_qsort.hpp"
 #ifndef __CYGWIN__
 
-#if defined(NPY_HAVE_AVX512_SKX)
-    #include "x86-simd-sort/src/avx512-32bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx512-64bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx512-64bit-argsort.hpp"
-#elif defined(NPY_HAVE_AVX2)
-    #include "x86-simd-sort/src/avx2-32bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx2-64bit-qsort.hpp"
-#endif
-
-namespace {
-template<typename T>
-void x86_qsort(T* arr, npy_intp num)
-{
-#if defined(NPY_HAVE_AVX512_SKX)
-    avx512_qsort(arr, num, true);
-#elif defined(NPY_HAVE_AVX2)
-    avx2_qsort(arr, num, true);
-#endif
-}
-
-template<typename T>
-void x86_qselect(T* arr, npy_intp num, npy_intp kth)
-{
-#if defined(NPY_HAVE_AVX512_SKX)
-    avx512_qselect(arr, kth, num, true);
-#elif defined(NPY_HAVE_AVX2)
-    avx2_qselect(arr, kth, num, true);
-#endif
-}
-} // anonymous
+#include "x86-simd-sort/src/x86simdsort-static-incl.h"
 
 namespace np { namespace qsort_simd {
-#if defined(NPY_HAVE_AVX512_SKX) || defined(NPY_HAVE_AVX2)
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int32_t *arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(uint32_t *arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int64_t*arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(uint64_t*arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(float *arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num, true);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(double *arr, npy_intp num, npy_intp kth)
 {
-    x86_qselect(arr, num, kth);
+    x86simdsortStatic::qselect(arr, kth, num, true);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(int32_t *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(uint32_t *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(int64_t *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(uint64_t *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(float *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num, true);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(double *arr, npy_intp num)
 {
-    x86_qsort(arr, num);
+    x86simdsortStatic::qsort(arr, num, true);
 }
-#endif // NPY_HAVE_AVX512_SKX || NPY_HAVE_AVX2
-
 }} // namespace np::qsort_simd
 
 #endif // __CYGWIN__
