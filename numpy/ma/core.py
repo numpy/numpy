BUG: masked array division should ignore all FPEs in mask calculation (#26135)
* BUG: masked array division broken with np.seterr(under=raise) #25810

* STY: Remove unnecessary brackets

Closes gh-25810

---------

Co-authored-by: Sebastian Berg <sebastian@sipsolutions.net>

numpy/ma/core.py
@@ -877,7 +877,7 @@ class _DomainSafeDivide:
             self.tolerance = np.finfo(float).tiny
         # don't call ma ufuncs from __array_wrap__ which would fail for scalars
         a, b = np.asarray(a), np.asarray(b)
-        with np.errstate(invalid='ignore'):
+        with np.errstate(all='ignore'):
             return umath.absolute(a) * self.tolerance >= umath.absolute(b)
 
 