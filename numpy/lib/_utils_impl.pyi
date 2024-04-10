58853840757ca02eacc2318c9f982317cff6b123
ganesh-k13 <ganesh3597@gmail.com>
2024-04-10 19:26:21 +0530

    TYP: Added typing for modified `show_runtime`

--- numpy/lib/_utils_impl.pyi
+++ numpy/lib/_utils_impl.pyi
@@ -2,11 +2,12 @@ from _typeshed import SupportsWrite
 from typing import LiteralString
 
 import numpy as np
+from numpy._utils._config_helpers import ConfigDisplayModes
 
 __all__ = ["get_include", "info", "show_runtime"]
 
 def get_include() -> LiteralString: ...
-def show_runtime() -> None: ...
+def show_runtime(mode: ConfigDisplayModes) -> None | dict: ...
 def info(
     object: object = None, maxwidth: int = 76, output: SupportsWrite[str] | None = None, toplevel: str = "numpy"
 ) -> None: ...
