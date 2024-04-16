44ba7ca07984557f2006f9a6916adb8e3ecfca61
Kevin Sheppard <bashtage@users.noreply.github.com>
2024-04-16 11:10:15 +0100

    BUG: Ensure seed sequences are restored through pickling (#26260)
    Explicity store and restore seed sequence
    
    closes #26234
    
    ---
    
    * BUG: Ensure seed sequences are restored through pickling
    
    Explicity store and restore seed sequence
    
    closes #26234
    
    * CLN: Simplify refactor
    
    Make more use of set and getstate to avoid changes in the pickling functions
    
    * BUG: Correct behavior for legacy pickles
    
    Add test for legacy pickles
    Include pickles for tests
    
    * MAINT: Correct types for pickle related functions
    
    * REF: Switch from string to type
    
    * REF: Swtich to returning bit generators
    
    Explicitly return bit generator rather than ctor

--- numpy/random/bit_generator.pyx
+++ numpy/random/bit_generator.pyx
@@ -537,14 +537,27 @@ cdef class BitGenerator():
 
     # Pickling support:
     def __getstate__(self):
-        return self.state
+        return self.state, self._seed_seq
 
-    def __setstate__(self, state):
-        self.state = state
+    def __setstate__(self, state_seed_seq):
+
+        if isinstance(state_seed_seq, dict):
+            # Legacy path
+            # Prior to 2.0.x only the state of the underlying bit generator
+            # was preserved and any seed sequence information was lost
+            self.state = state_seed_seq
+        else:
+            self._seed_seq = state_seed_seq[1]
+            self.state = state_seed_seq[0]
 
     def __reduce__(self):
         from ._pickle import __bit_generator_ctor
-        return __bit_generator_ctor, (self.state['bit_generator'],), self.state
+
+        return (
+            __bit_generator_ctor,
+            (type(self), ),
+            (self.state, self._seed_seq)
+        )
 
     @property
     def state(self):
--- numpy/random/mtrand.pyx
+++ numpy/random/mtrand.pyx
@@ -205,10 +205,13 @@ cdef class RandomState:
         self.set_state(state)
 
     def __reduce__(self):
-        ctor, name_tpl, _ = self._bit_generator.__reduce__()
-
         from ._pickle import __randomstate_ctor
-        return __randomstate_ctor, (name_tpl[0], ctor), self.get_state(legacy=False)
+        # The third argument containing the state is required here since
+        # RandomState contains state information in addition to the state
+        # contained in the bit generator that described the gaussian
+        # generator. This argument is passed to __setstate__ after the
+        # Generator is created.
+        return __randomstate_ctor, (self._bit_generator, ), self.get_state(legacy=False)
 
     cdef _initialize_bit_generator(self, bit_generator):
         self._bit_generator = bit_generator
