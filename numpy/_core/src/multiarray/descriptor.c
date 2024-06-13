90f952ad7efd32622231fd8db2ec80664b292cbb
Charles Harris <charlesr.harris@gmail.com>
2024-06-12 19:41:48 -0600

    MAINT: Add comment lost in previous PR.
    The line appears to have been lost when merging main into the PR.
    
    [skip ci]

--- numpy/_core/src/multiarray/descriptor.c
+++ numpy/_core/src/multiarray/descriptor.c
@@ -2038,6 +2038,7 @@ arraydescr_dealloc(PyArray_Descr *self)
 {
     Py_XDECREF(self->typeobj);
     if (!PyDataType_ISLEGACY(self)) {
+        /* non legacy dtypes must not have fields, etc. */
         Py_TYPE(self)->tp_free((PyObject *)self);
         return;
     }
