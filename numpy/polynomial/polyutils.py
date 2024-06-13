95d633b0b0a98c57911f5326643321b96a84f550
Pieter Eendebak <unknow@example.com>
2024-06-13 12:17:57 +0200

    review comments

--- numpy/polynomial/polyutils.py
+++ numpy/polynomial/polyutils.py
@@ -346,7 +346,7 @@ def mapdomain(x, old, new):
     array([-1.0+1.j , -0.6+0.6j, -0.2+0.2j,  0.2-0.2j,  0.6-0.6j,  1.0-1.j ]) # may vary
 
     """
-    if not np.isscalar(x):
+    if type(x) not in (int, float, complex, bool):
         x = np.asanyarray(x)
     off, scl = mapparms(old, new)
     return off + scl*x
