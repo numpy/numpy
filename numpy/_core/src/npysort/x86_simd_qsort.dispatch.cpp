ba7dfa0ef0cd184bf5ccd0b6cfd0922a55f0c7e5
Charles Harris <charlesr.harris@gmail.com>
2024-07-04 14:23:23 -0600

    BLD: Fix x86-simd-sort build failure on openBSD (#26821)
    * BLD: Fix x86-simd-sort build failure on openBSD
    
    * MAINT: Update highway to 1dbb1180.
    
    * MAINT: Update various sort files from main
    
    - Update highway-qsort.dispatch.cpp.
    - Update x86_simd_argsort.dispatch.cpp
    - Update x86_simd_qsort.dispatch.cpp
    - Update x86_simd_qsort_16bit.dispatch.cpp
    
    * MAINT: Update vendored-meson from main
    
    * MAINT: Update "numpy/_core/meson.build" from #26273
    
    ---------
    
    Co-authored-by: Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>

--- numpy/_core/src/npysort/x86_simd_qsort.dispatch.cpp
+++ numpy/_core/src/npysort/x86_simd_qsort.dispatch.cpp
@@ -1,89 +1,25 @@
 #include "x86_simd_qsort.hpp"
 #ifndef __CYGWIN__
 
-#if defined(NPY_HAVE_AVX512_SKX)
-    #include "x86-simd-sort/src/avx512-32bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx512-64bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx512-64bit-argsort.hpp"
-#elif defined(NPY_HAVE_AVX2)
-    #include "x86-simd-sort/src/avx2-32bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx2-64bit-qsort.hpp"
-#endif
+#include "x86-simd-sort/src/x86simdsort-static-incl.h"
 
-namespace {
-template<typename T>
-void x86_qsort(T* arr, npy_intp num)
-{
-#if defined(NPY_HAVE_AVX512_SKX)
-    avx512_qsort(arr, num, true);
-#elif defined(NPY_HAVE_AVX2)
-    avx2_qsort(arr, num, true);
-#endif
-}
-
-template<typename T>
-void x86_qselect(T* arr, npy_intp num, npy_intp kth)
-{
-#if defined(NPY_HAVE_AVX512_SKX)
-    avx512_qselect(arr, kth, num, true);
-#elif defined(NPY_HAVE_AVX2)
-    avx2_qselect(arr, kth, num, true);
-#endif
-}
-} // anonymous
+#define DISPATCH_SORT_METHODS(TYPE) \
+template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(TYPE *arr, npy_intp num, npy_intp kth) \
+{ \
+    x86simdsortStatic::qselect(arr, kth, num, true); \
+} \
+template<> void NPY_CPU_DISPATCH_CURFX(QSort)(TYPE *arr, npy_intp num) \
+{ \
+    x86simdsortStatic::qsort(arr, num, true); \
+} \
 
 namespace np { namespace qsort_simd {
-#if defined(NPY_HAVE_AVX512_SKX) || defined(NPY_HAVE_AVX2)
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int32_t *arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(uint32_t *arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int64_t*arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(uint64_t*arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(float *arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(double *arr, npy_intp num, npy_intp kth)
-{
-    x86_qselect(arr, num, kth);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(int32_t *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(uint32_t *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(int64_t *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(uint64_t *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(float *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-template<> void NPY_CPU_DISPATCH_CURFX(QSort)(double *arr, npy_intp num)
-{
-    x86_qsort(arr, num);
-}
-#endif // NPY_HAVE_AVX512_SKX || NPY_HAVE_AVX2
-
+    DISPATCH_SORT_METHODS(uint32_t)
+    DISPATCH_SORT_METHODS(int32_t)
+    DISPATCH_SORT_METHODS(float)
+    DISPATCH_SORT_METHODS(uint64_t)
+    DISPATCH_SORT_METHODS(int64_t)
+    DISPATCH_SORT_METHODS(double)
 }} // namespace np::qsort_simd
 
 #endif // __CYGWIN__
--- numpy/_core/src/npysort/x86_simd_qsort_16bit.dispatch.cpp
+++ numpy/_core/src/npysort/x86_simd_qsort_16bit.dispatch.cpp
@@ -1,11 +1,13 @@
 #include "x86_simd_qsort.hpp"
 #ifndef __CYGWIN__
 
-#if defined(NPY_HAVE_AVX512_SPR)
-    #include "x86-simd-sort/src/avx512fp16-16bit-qsort.hpp"
-    #include "x86-simd-sort/src/avx512-16bit-qsort.hpp"
-#elif defined(NPY_HAVE_AVX512_ICL)
-    #include "x86-simd-sort/src/avx512-16bit-qsort.hpp"
+#include "x86-simd-sort/src/x86simdsort-static-incl.h"
+/*
+ * MSVC doesn't set the macro __AVX512VBMI2__ which is required for the 16-bit
+ * functions and therefore we need to manually include this file here
+ */
+#ifdef _MSC_VER
+#include "x86-simd-sort/src/avx512-16bit-qsort.hpp"
 #endif
 
 namespace np { namespace qsort_simd {
@@ -13,24 +15,23 @@ namespace np { namespace qsort_simd {
 /*
  * QSelect dispatch functions:
  */
-#if defined(NPY_HAVE_AVX512_ICL) || defined(NPY_HAVE_AVX512_SPR)
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(Half *arr, npy_intp num, npy_intp kth)
 {
 #if defined(NPY_HAVE_AVX512_SPR)
-    avx512_qselect(reinterpret_cast<_Float16*>(arr), kth, num, true);
+    x86simdsortStatic::qselect(reinterpret_cast<_Float16*>(arr), kth, num, true);
 #else
-    avx512_qselect_fp16(reinterpret_cast<uint16_t*>(arr), kth, num, true);
+    avx512_qselect_fp16(reinterpret_cast<uint16_t*>(arr), kth, num, true, false);
 #endif
 }
 
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(uint16_t *arr, npy_intp num, npy_intp kth)
 {
-    avx512_qselect(arr, kth, num);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 
 template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int16_t *arr, npy_intp num, npy_intp kth)
 {
-    avx512_qselect(arr, kth, num);
+    x86simdsortStatic::qselect(arr, kth, num);
 }
 
 /*
@@ -39,20 +40,19 @@ template<> void NPY_CPU_DISPATCH_CURFX(QSelect)(int16_t *arr, npy_intp num, npy_
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(Half *arr, npy_intp size)
 {
 #if defined(NPY_HAVE_AVX512_SPR)
-    avx512_qsort(reinterpret_cast<_Float16*>(arr), size, true);
+    x86simdsortStatic::qsort(reinterpret_cast<_Float16*>(arr), size, true);
 #else
-    avx512_qsort_fp16(reinterpret_cast<uint16_t*>(arr), size, true);
+    avx512_qsort_fp16(reinterpret_cast<uint16_t*>(arr), size, true, false);
 #endif
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(uint16_t *arr, npy_intp size)
 {
-    avx512_qsort(arr, size);
+    x86simdsortStatic::qsort(arr, size);
 }
 template<> void NPY_CPU_DISPATCH_CURFX(QSort)(int16_t *arr, npy_intp size)
 {
-    avx512_qsort(arr, size);
+    x86simdsortStatic::qsort(arr, size);
 }
-#endif // NPY_HAVE_AVX512_ICL || SPR
 
 }} // namespace np::qsort_simd
 
