fc471a2eba61226a025c24954fa2b3429c4e0d48
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-07-05 17:53:05 +0200

    increase performance for scalars

--- numpy/polynomial/polyutils.py
+++ numpy/polynomial/polyutils.py
@@ -346,7 +346,7 @@ def mapdomain(x, old, new):
     array([-1.0+1.j , -0.6+0.6j, -0.2+0.2j,  0.2-0.2j,  0.6-0.6j,  1.0-1.j ]) # may vary
 
     """
-    if type(x) not in (int, float, complex):
+    if type(x) not in (int, float, complex) or not isinstance(x, np.generic):
         x = np.asanyarray(x)
     off, scl = mapparms(old, new)
     return off + scl*x
