bb87341d242fd9c7b9107bdce7ead1df7e3cf86d
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-07-06 14:16:54 +0200

    MAINT: apply ruff/Pycodestyle rule E712
    E712 Avoid inequality comparisons to `True`; use `if not cond:` for false checks

--- numpy/_core/tests/test_simd.py
+++ numpy/_core/tests/test_simd.py
@@ -587,7 +587,7 @@ class _SIMD_FP(_Test_Utility):
             v = self.setall(d)
             clear_floatstatus()
             intrin(v)
-            assert check_floatstatus(invalid=True) == False
+            assert check_floatstatus(invalid=True) is False
 
     @pytest.mark.parametrize('py_comp,np_comp', [
         (operator.lt, "cmplt"),
