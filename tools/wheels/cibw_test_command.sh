1ee327c42efc90e54d576583dca9bd2f7c700875
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-07-15 13:19:16 -0600

    MAINT: re-add environment variable, add import test

--- tools/wheels/cibw_test_command.sh
+++ tools/wheels/cibw_test_command.sh
@@ -34,6 +34,20 @@ if [[ $FREE_THREADED_BUILD == "True" ]]; then
     # with a released version of cython
     python -m pip uninstall -y cython
     python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple cython
+
+    # Manually check that importing NumPy does not re-enable the GIL.
+    # Afterwards, force the GIL to always be disabled so it does not get
+    # re-enabled during the tests.
+    #
+    # TODO: delete when f2py grows the ability to define extensions that declare
+    # they can run without the gil or when we can work around the fact the f2py
+    # tests import modules that don't declare gil-disabled support.
+    if [[ $(python -c "import numpy" 2>&1) == "*The global interpreter lock (GIL) has been enabled*" ]]; then
+        echo "Error: Importing NumPy re-enables the GIL in the free-threaded build"
+        exit 1
+    fi
+
+    export PYTHON_GIL=0
 fi
 
 # Run full tests with -n=auto. This makes pytest-xdist distribute tests across
