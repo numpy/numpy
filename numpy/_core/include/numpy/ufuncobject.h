ee89a1fb27cd9866bab71f0d34187dcf459eaf47
Warren Weckesser <warren.weckesser@gmail.com>
2024-07-15 16:28:35 -0400

    DOC: Copy-edit PyUFunc_ProcessCoreDimsFunc comment.

--- numpy/_core/include/numpy/ufuncobject.h
+++ numpy/_core/include/numpy/ufuncobject.h
@@ -67,26 +67,30 @@ typedef int (PyUFunc_TypeResolutionFunc)(
 
 /*
  * This is the signature for the functions that may be assigned to the
- * `process_core_dims_func` field of the PyUFuncObject structure.  This
- * function is only used by generalized ufuncs (i.e. those with the field
- * `core_enabled` set to 1).  The function is called during the processing
- * of the arguments when a gufunc is called. The function can check the
- * core dimensions of the input and output arrays and set an exception if
- * any requirements are not satisfied. If the caller of the gufunc didn't
- * provide output arrays, the core dimensions associated with the output
- * arrays (i.e. those that are not also used in input arrays) will have
- * the value -1 in `core_dim_sizes`.  This function can replace any output
- * core dimensions that are -1 with a value that is appropriate for the
- * gufunc.
+ * `process_core_dims_func` field of the PyUFuncObject structure.
+ * Implementation of this function is optional.  This function is only used
+ * by generalized ufuncs (i.e. those with the field `core_enabled` set to 1).
+ * The function is called by the ufunc during the processing of the arguments
+ * of a call of the ufunc. The function can check the core dimensions of the
+ * input and output arrays and return -1 with an exception set if any
+ * requirements are not satisfied. If the caller of the ufunc didn't provide
+ * output arrays, the core dimensions associated with the output arrays (i.e.
+ * those that are not also used in input arrays) will have the value -1 in
+ * `core_dim_sizes`.  This function can replace any output core dimensions
+ * that are -1 with a value that is appropriate for the ufunc.
  *
  * Parameter       Description
  * --------------- ------------------------------------------------------
  * ufunc           The ufunc object
  * core_dim_sizes  An array with length `ufunc->core_num_dim_ix`.
- *                 The core dimensions of the arrays passed to the gufunc
- *                 will have been set.  If the caller didn't provide the
- *                 output arrays, the output-only core dimensions will have
- *                 the value -1.
+ *                 The core dimensions of the arrays passed to the ufunc
+ *                 will have been set.  If the caller of the ufunc didn't
+ *                 provide the output array(s), the output-only core
+ *                 dimensions will have the value -1.
+ *
+ * The function must not change any element in `core_dim_sizes` that is
+ * not -1 on input. Doing so will result in incorrect output from the
+ * ufunc, and could result in a crash of the Python interpreter.
  *
  * The function must return 0 on success, -1 on failure (with an exception
  * set).
