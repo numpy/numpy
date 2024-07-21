415c919aa591621fd5d1796c118277be7f0987bc
Charles Harris <charlesr.harris@gmail.com>
2024-07-21 08:09:25 -0600

    MAINT: prepare 2.0.x for further development

--- pavement.py
+++ pavement.py
@@ -38,7 +38,7 @@ from paver.easy import Bunch, options, task, sh
 #-----------------------------------
 
 # Path to the release notes
-RELEASE_NOTES = 'doc/source/release/2.0.1-notes.rst'
+RELEASE_NOTES = 'doc/source/release/2.0.2-notes.rst'
 
 
 #-------------------------------------------------------
