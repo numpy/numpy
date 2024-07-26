2502063dd3e543292bc7f5de9ea9f6639ce6655d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2024-07-26 07:36:48 +0000

    TST:  Add regression test for gh-26920

--- numpy/f2py/tests/test_modules.py
+++ numpy/f2py/tests/test_modules.py
@@ -6,6 +6,21 @@ from numpy.testing import IS_PYPY
 
 
 @pytest.mark.slow
+class TestModuleWithoutPublicEntities(util.F2PyTest):
+    sources = [
+        util.getpath(
+            "tests", "src", "modules", "gh26920",
+            "two_mods_with_one_public_routine.f90"
+        )
+    ]
+    only = ["mod1_func1", ]
+
+    def test_gh26920(self):
+        # if it compiles and can be loaded, things are fine
+        pass
+
+
+@pytest.mark.slow
 class TestModuleDocString(util.F2PyTest):
     sources = [util.getpath("tests", "src", "modules", "module_data_docstring.f90")]
 
