3ed4c86dcbbba4e624b0af507d5eec81cf395ce7
GUAN MING <guanmingchiu@gmail.com>
2024-08-05 17:29:47 +0800

    TST, DOC: update transpose axes with negative indices

--- numpy/_core/tests/test_numeric.py
+++ numpy/_core/tests/test_numeric.py
@@ -343,6 +343,7 @@ class TestNonarrayArgs:
         arr = [[1, 2], [3, 4], [5, 6]]
         tgt = [[1, 3, 5], [2, 4, 6]]
         assert_equal(np.transpose(arr, (1, 0)), tgt)
+        assert_equal(np.transpose(arr, (-1, -2)), tgt)
         assert_equal(np.matrix_transpose(arr), tgt)
 
     def test_var(self):
