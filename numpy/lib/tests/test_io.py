dde1fa8089736196b304e098d9eadf955d045943
Stefan van der Walt <stefanv@berkeley.edu>
2024-09-05 15:34:57 -0700

    Test allow_pickle with savez

--- numpy/lib/tests/test_io.py
+++ numpy/lib/tests/test_io.py
@@ -2790,6 +2790,7 @@ def test_load_refcount():
         x = np.loadtxt(TextIO("0 1 2 3"), dtype=dt)
         assert_equal(x, np.array([((0, 1), (2, 3))], dtype=dt))
 
+
 def test_load_multiple_arrays_until_eof():
     f = BytesIO()
     np.save(f, 1)
@@ -2799,3 +2800,17 @@ def test_load_multiple_arrays_until_eof():
     assert np.load(f) == 2
     with pytest.raises(EOFError):
         np.load(f)
+
+
+def test_savez_nopickle():
+    obj_array = np.array([1, 'hello'], dtype=object)
+    with NamedTemporaryFile(suffix='.npz') as tmp:
+        np.savez(tmp.name, obj_array)
+
+        with pytest.raises(ValueError, match="Object arrays cannot be saved when.*"):
+            np.savez(tmp.name, obj_array, allow_pickle=False)
+
+        np.savez_compressed(tmp.name, obj_array)
+
+        with pytest.raises(ValueError, match="Object arrays cannot be saved when.*"):
+            np.savez_compressed(tmp.name, obj_array, allow_pickle=False)
