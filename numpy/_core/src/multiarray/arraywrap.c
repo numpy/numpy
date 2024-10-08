315a9d03e3c99441f778cc57ec6853b2e34f7558
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-10-08 11:02:29 +0200

    refactor

--- numpy/_core/src/multiarray/arraywrap.c
+++ numpy/_core/src/multiarray/arraywrap.c
@@ -34,7 +34,7 @@ npy_find_array_wrap(
     PyObject *wrap = NULL;
     PyObject *wrap_type = NULL;
 
-    double priority = NPY_PRIORITY;  /* silence uninitialized warning */
+    double priority = -DBL_MAX;  /* silence uninitialized warning */
 
     /*
      * Iterate through all inputs taking the first one with an __array_wrap__
