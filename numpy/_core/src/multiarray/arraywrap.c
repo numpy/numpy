1febdf105a5e43fcd1bc7e78a3dfa12845b8dcf1
Pieter Eendebak <pieter.eendebak@gmail.com>
2024-10-11 21:33:27 +0200

    review comments

--- numpy/_core/src/multiarray/arraywrap.c
+++ numpy/_core/src/multiarray/arraywrap.c
@@ -33,7 +33,7 @@ npy_find_array_wrap(
     PyObject *wrap = NULL;
     PyObject *wrap_type = NULL;
 
-    double priority = -NPY_INFINITY;  /* silence uninitialized warning */
+    double priority = -NPY_INFINITY;
 
     /*
      * Iterate through all inputs taking the first one with an __array_wrap__
