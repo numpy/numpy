0f20e127021b5ca8500e97e42ee77af6b2723411
fengluo <fengluoqiuwu@gmail.com>
2024-10-19 09:46:06 +0800

    delete some test file

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -694,7 +694,6 @@ def get_masked_subclass(*arrays):
         for cls in arrcls[1:]:
             if issubclass(cls, rcls):
                 rcls = cls
-    test = rcls
     # Don't return MaskedConstant as result: revert to MaskedArray
     if rcls.__name__ == 'MaskedConstant':
         return MaskedArray
