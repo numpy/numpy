9821df95917422906f118a57491ab585e64ee9d4
fengluo <fengluoqiuwu@gmail.com>
2024-10-19 11:20:42 +0800

    fix code line too long

--- numpy/ma/tests/test_core.py
+++ numpy/ma/tests/test_core.py
@@ -1467,33 +1467,38 @@ class TestMaskedArrayArithmetic:
         # check we don't get array([False], dtype=bool)
         assert_equal(np.true_divide(m, 5).mask.shape, ())
 
-    def test_binary_ufunc_nomask(self):
-        # Check masked array methods have same bihaviours as ndarray when nomask
-        # See issue 27258
+    def test_binops_nomask(self):
+        # Check masked array methods have same bihaviours as ndarray
+        # when nomask. See issue 27258.
         # https://github.com/numpy/numpy/issues/27258
         floating_data = 0.123456789_123456789_123456789
 
         a = 1.0
-        b = np.array([floating_data+i for i in range(6)], dtype=np.float32).reshape(2, 3)
-        c = np.array([complex(floating_data+i, floating_data-i) for i in range(6)],
-                     dtype=np.complex64).reshape(2,3)
+        b = np.array([floating_data+i for i in range(6)],
+                     dtype=np.float32).reshape(2, 3)
+        c = np.array([complex(floating_data+i, floating_data-i)
+                      for i in range(6)], dtype=np.complex64).reshape(2,3)
 
-        operations = [lambda arr1, arr2: arr1 + arr2, lambda arr1, arr2: arr1 - arr2,
-                      lambda arr1, arr2: arr1 * arr2, lambda arr1, arr2: arr1 / arr2,
-                      lambda arr1, arr2: arr1 // arr2, lambda arr1, arr2: arr1 % arr2,
+        operations = [lambda arr1, arr2: arr1 + arr2,
+                      lambda arr1, arr2: arr1 - arr2,
+                      lambda arr1, arr2: arr1 * arr2,
+                      lambda arr1, arr2: arr1 / arr2,
+                      lambda arr1, arr2: arr1 // arr2,
+                      lambda arr1, arr2: arr1 % arr2,
                       lambda arr1, arr2: arr1 ** arr2]
 
         for i in range(len(operations)):
-            scalar_op_result = operations[i](b, a)
-            scalar_ma_op_result = operations[i](masked_where(None, b, True), a)
-            assert_equal(scalar_op_result, scalar_ma_op_result)
+            op_result = operations[i](b, a)
+            ma_op_result = (operations[i]
+                            (masked_where(None, b, True), a))
+            assert_equal(op_result, ma_op_result)
 
             if i not in (4, 5):
-                scalar_op_result = operations[i](b, c)
-                scalar_ma_op_result = operations[i](
+                op_result = operations[i](b, c)
+                ma_op_result = operations[i](
                     masked_where(None, b, True),
                     masked_where(None, c, True))
-                assert_equal(scalar_op_result, scalar_ma_op_result)
+                assert_equal(op_result, ma_op_result)
 
     def test_noshink_on_creation(self):
         # Check that the mask is not shrunk on array creation when not wanted
