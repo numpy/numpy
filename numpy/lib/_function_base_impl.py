2d740bbbb775aad475b6892c67f652668670a6c3
SF <sf2718@proton.me>
2024-10-24 19:27:15 +0300

    DOC: extend numpy.lib.unwrap doc

--- numpy/lib/_function_base_impl.py
+++ numpy/lib/_function_base_impl.py
@@ -1713,6 +1713,27 @@ def unwrap(p, discont=None, axis=-1, *, period=2*pi):
     difference from their predecessor of more than ``max(discont, period/2)``
     to their `period`-complementary values.
 
+    Output `q` satisfies:
+
+     1. ``q[0] == p[0]``.
+
+     2. :math:`\forall i` ``(p[i] - q[i]) % period == 0``.
+
+     3. :math:`\forall i>0`
+      ``abs(p[i] - p[i-1]) < discont`` -> ``q[i] - q[i-1] == p[i] - p[i-1]``.
+
+     4. :math:`\forall i>0`
+
+      ``abs(p[i] - p[i-1]) >= discont`` ->
+
+       1. ``abs(q[i] - q[i-1]) <= abs(period/2)``.
+       2. ``(p[i] - p[i-1]) % period == (period/2)`` ->
+        a. ``period > 0`` -> ``q[i] > q[i-1]`` iff ``p[i] > p[i-1]``.
+        b. ``period < 0`` -< ``q[i] < q[i-1]`` iff ``p[i] > p[i-1]``.
+
+     (things should be understood w.r.t. axis, and
+     comparisons are only approximately true in non-integral case)
+
     For the default case where `period` is :math:`2\pi` and `discont` is
     :math:`\pi`, this unwraps a radian phase `p` such that adjacent differences
     are never greater than :math:`\pi` by adding :math:`2k\pi` for some
