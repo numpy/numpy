6272f01349af16aa451315a4474beb06d579bc9d
yuanx749 <yuanx749@gmail.com>
2024-11-07 22:05:52 +0800

    DOC: fix vector_norm docstring

--- numpy/linalg/_linalg.py
+++ numpy/linalg/_linalg.py
@@ -3498,7 +3498,7 @@ def vector_norm(x, /, *, axis=None, keepdims=False, ord=2):
     keepdims : bool, optional
         If this is set to True, the axes which are normed over are left in
         the result as dimensions with size one. Default: False.
-    ord : {1, -1, 2, -2, inf, -inf, 'fro', 'nuc'}, optional
+    ord : {int, inf, -inf}, optional
         The order of the norm. For details see the table under ``Notes``
         in `numpy.linalg.norm`.
 
