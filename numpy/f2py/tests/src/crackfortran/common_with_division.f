0aed7e02bf29bca83ba6c91d9bfcd7adc82ff5aa
Nicholas Christensen <njchris2@illinois.edu>
2025-02-27 19:05:54 -0600

    fix typos

--- numpy/f2py/tests/src/crackfortran/common_with_division.f
+++ numpy/f2py/tests/src/crackfortran/common_with_division.f
@@ -3,13 +3,13 @@
       integer lmu,lb,lub,lpmin
       parameter (lmu=1)
       parameter (lb=20)
-c     crackpython fails to parse this  
+c     crackfortran fails to parse this  
       parameter (lub=(lb-1)*lmu+1)
-c     crackpython can successfully parse this though
+c     crackfortran can successfully parse this though
 c     parameter (lub=lb*lmu-lmu+1)
       parameter (lpmin=1)
 
-c     crackpython fails to parse this correctly 
+c     crackfortran fails to parse this correctly 
       common /mortmp/ ctmp((lub*(lub+1)*(lub+1))/lpmin+1)
       return
       end
