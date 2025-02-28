8add0cbf787fa715a4890ec33ded695d804767f2
Nicholas Christensen <njchris2@illinois.edu>
2025-02-27 19:01:20 -0600

    Add test for more complicated array sizing expression

---
+++ numpy/f2py/tests/src/crackfortran/common_with_division.f
@@ -0,0 +1,15 @@
+      subroutine test
+      implicit none
+      integer lmu,lb,lub,lpmin
+      parameter (lmu=1)
+      parameter (lb=20)
+c     crackpython fails to parse this  
+      parameter (lub=(lb-1)*lmu+1)
+c     crackpython can successfully parse this though
+c     parameter (lub=lb*lmu-lmu+1)
+      parameter (lpmin=1)
+
+c     crackpython fails to parse this correctly 
+      common /mortmp/ ctmp((lub*(lub+1)*(lub+1))/lpmin+1)
+      return
+      end
