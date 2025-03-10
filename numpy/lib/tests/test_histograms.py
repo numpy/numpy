986338fe474405133c83b0a508f770e1fb28dd5d
Sebastian Berg <sebastian@sipsolutions.net>
2025-03-10 14:05:07 +0100

    Apply suggestions from code review

--- numpy/lib/tests/test_histograms.py
+++ numpy/lib/tests/test_histograms.py
@@ -517,7 +517,7 @@ class TestHistogramOptimBinNums:
 
         edges_auto = histogram_bin_edges(lim_var_data, 'auto')
         assert_equal(edges_auto[0], 0)
-        assert_equal(edges_auto[-1], 1)
+        assert_equal(edges_auto[-1], 100.)
         assert_equal(len(edges_auto) < 20)
 
         edges_fd = histogram_bin_edges(lim_var_data, 'fd')
