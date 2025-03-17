53f4cc5eb526b51cc01e25ca47bca68fff1e4b8f
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-03-17 08:39:32 +0100

    BUG: Limit the maximal number of bins for automatic histogram binning (#28426)
    * Limit the maximal number of bins for automatic histogram binning
    
    * BUG: Limit the maximal number of bins for automatic histogram binning
    
    * fix test
    
    * fix test import
    
    * Update numpy/lib/_histograms_impl.py
    
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>
    
    * lint
    
    * lint
    
    * add release note
    
    * fix issues with overflow
    
    * review comments
    
    * remove unused import
    
    * remove unused import
    
    * typo
    
    * use continuous bin approximation
    
    * update test
    
    * Apply suggestions from code review
    
    Co-authored-by: Sebastian Berg <sebastian@sipsolutions.net>
    
    * Apply suggestions from code review
    
    * Update numpy/lib/tests/test_histograms.py
    
    * fix test
    
    * Update numpy/lib/_histograms_impl.py
    
    Co-authored-by: Sebastian Berg <sebastian@sipsolutions.net>
    
    ---------
    
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>
    Co-authored-by: Sebastian Berg <sebastian@sipsolutions.net>

--- numpy/lib/tests/test_histograms.py
+++ numpy/lib/tests/test_histograms.py
@@ -416,6 +416,13 @@ class TestHistogram:
         expected_hist = np.array([1, 0])
         assert_array_equal(hist, expected_hist)
 
+    def test_gh_28400(self):
+        e = 1 + 1e-12
+        Z = [0, 1, 1, 1, 1, 1, e, e, e, e, e, e, 2]
+        counts, edges = np.histogram(Z, bins="auto")
+        assert len(counts) < 10
+        assert edges[0] == Z[0]
+        assert edges[-1] == Z[-1]
 
 class TestHistogramOptimBinNums:
     """
@@ -502,15 +509,16 @@ class TestHistogramOptimBinNums:
 
     def test_limited_variance(self):
         """
-        Check when IQR is 0, but variance exists, we return the sturges value
-        and not the fd value.
+        Check when IQR is 0, but variance exists, we return a reasonable value.
         """
         lim_var_data = np.ones(1000)
         lim_var_data[:3] = 0
         lim_var_data[-4:] = 100
 
         edges_auto = histogram_bin_edges(lim_var_data, 'auto')
-        assert_equal(edges_auto, np.linspace(0, 100, 12))
+        assert_equal(edges_auto[0], 0)
+        assert_equal(edges_auto[-1], 100.)
+        assert len(edges_auto) < 100
 
         edges_fd = histogram_bin_edges(lim_var_data, 'fd')
         assert_equal(edges_fd, np.array([0, 100]))
