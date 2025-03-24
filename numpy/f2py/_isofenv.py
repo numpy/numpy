95570b972ac78e5444908d09eada6e9cdd3bd077
DWesl <22566757+DWesl@users.noreply.github.com>
2025-03-24 12:39:20 -0400

    TYP: Add type annotations to used variables
    Also mark private variables and delete unused variables.

--- numpy/f2py/_isofenv.py
+++ numpy/f2py/_isofenv.py
@@ -1,7 +1,7 @@
 """
 ISO_FORTRAN_ENV maps for f2py2e
 """
-iso_fortran_env_map = {
+iso_fortran_env_map: dict[str, dict[str, str]] = {
     'integer': {
         'int8': 'signed_char',
         'int16': 'short',
@@ -14,10 +14,7 @@ iso_fortran_env_map = {
     }
 }
 
-isof_f2pycode_map = {}
-iso_c2py_map = {}
-
-isof_kindmap = {}
-for fortran_type, std_type_dict in iso_fortran_env_map.items():
-    for std_type in std_type_dict.keys():
-        isof_kindmap[std_type] = fortran_type
+isof_kindmap: dict[str, str] = {}
+for _fortran_type, _std_type_dict in iso_fortran_env_map.items():
+    for _std_type in _std_type_dict.keys():
+        isof_kindmap[_std_type] = _fortran_type
