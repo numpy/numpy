8fecded72a9c45d1ad4298149b6c3d0218ee2247
Maanas Arora <maanasarora23@gmail.com>
2025-03-26 13:53:20 -0400

    ENH: Add dtype slots for sorting and begin integration

--- numpy/_core/include/numpy/dtype_api.h
+++ numpy/_core/include/numpy/dtype_api.h
@@ -364,9 +364,12 @@ typedef int (PyArrayMethod_PromoterFunction)(PyObject *ufunc,
 #define NPY_DT_ensure_canonical 6
 #define NPY_DT_setitem 7
 #define NPY_DT_getitem 8
-#define NPY_DT_get_clear_loop 9
-#define NPY_DT_get_fill_zero_loop 10
-#define NPY_DT_finalize_descr 11
+#define NPY_DT_get_sort_function 9
+#define NPY_DT_get_argsort_function 10
+#define NPY_DT_sort_compare 11
+#define NPY_DT_get_clear_loop 12
+#define NPY_DT_get_fill_zero_loop 13
+#define NPY_DT_finalize_descr 14
 
 // These PyArray_ArrFunc slots will be deprecated and replaced eventually
 // getitem and setitem can be defined as a performance optimization;
@@ -476,5 +479,10 @@ typedef PyArray_Descr *(PyArrayDTypeMeta_FinalizeDescriptor)(PyArray_Descr *dtyp
  */
 typedef int(PyArrayDTypeMeta_SetItem)(PyArray_Descr *, PyObject *, char *);
 typedef PyObject *(PyArrayDTypeMeta_GetItem)(PyArray_Descr *, char *);
+
+typedef int *(PyArrayDTypeMeta_GetSortFunction)(PyArray_Descr *, 
+        npy_intp, PyArray_SortFunc **);
+typedef int *(PyArrayDTypeMeta_GetArgSortFunction)(PyArray_Descr *, 
+        npy_intp, PyArray_ArgSortFunc **);
 
 #endif  /* NUMPY_CORE_INCLUDE_NUMPY___DTYPE_API_H_ */
