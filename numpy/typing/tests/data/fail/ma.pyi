2c8a512b785e480e42854125169ba00145f2f56a
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-03-31 19:48:41 +0100

    TYP: Type ma.max and ma.ptp

--- numpy/typing/tests/data/fail/ma.pyi
+++ numpy/typing/tests/data/fail/ma.pyi
@@ -8,7 +8,17 @@ m: np.ma.MaskedArray[tuple[int], np.dtype[np.float64]]
 m.shape = (3, 1)  # E: Incompatible types in assignment
 m.dtype = np.bool  # E: Incompatible types in assignment
 
-np.amin(m, axis=1.0)  # E: No overload variant
-np.amin(m, keepdims=1.0)  # E: No overload variant
-np.amin(m, out=1.0)  # E: No overload variant
-np.amin(m, fill_value=lambda x: 27)  # E: No overload variant
+np.ma.min(m, axis=1.0)  # E: No overload variant
+np.ma.min(m, keepdims=1.0)  # E: No overload variant
+np.ma.min(m, out=1.0)  # E: No overload variant
+np.ma.min(m, fill_value=lambda x: 27)  # E: No overload variant
+
+np.ma.max(m, axis=1.0)  # E: No overload variant
+np.ma.max(m, keepdims=1.0)  # E: No overload variant
+np.ma.max(m, out=1.0)  # E: No overload variant
+np.ma.max(m, fill_value=lambda x: 27)  # E: No overload variant
+
+np.ma.ptp(m, axis=1.0)  # E: No overload variant
+np.ma.ptp(m, keepdims=1.0)  # E: No overload variant
+np.ma.ptp(m, out=1.0)  # E: No overload variant
+np.ma.ptp(m, fill_value=lambda x: 27)  # E: No overload variant
