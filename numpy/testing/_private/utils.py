56ca6ccbb4e8ff3ebe242c451ff466185a76db38
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-04-02 10:28:44 -0600

    MAINT: bind variable for complicated f-string

--- numpy/testing/_private/utils.py
+++ numpy/testing/_private/utils.py
@@ -2653,7 +2653,9 @@ def _parse_size(size_str):
                 'kb': 1000, 'mb': 1000**2, 'gb': 1000**3, 'tb': 1000**4,
                 'kib': 1024, 'mib': 1024**2, 'gib': 1024**3, 'tib': 1024**4}
 
-    size_re = re.compile(fr'^\s*(\d+|\d+\.\d+)\s*({"|".join(suffixes.keys())})\s*$', re.I)
+    pipe_suffixes = "|".join(suffixes.keys())
+
+    size_re = re.compile(fr'^\s*(\d+|\d+\.\d+)\s*({pipe_suffixes})\s*$', re.I)
 
     m = size_re.match(size_str.lower())
     if not m or m.group(2) not in suffixes:
