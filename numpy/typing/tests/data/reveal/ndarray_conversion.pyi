0a7f8190512f8ce640c1818f7954539482c5d4ca
jorenham <jhammudoglu@gmail.com>
2025-04-04 03:36:43 +0200

    TYP: fix `ndarray.tolist()` and `.item()` for unknown dtype

--- numpy/typing/tests/data/reveal/ndarray_conversion.pyi
+++ numpy/typing/tests/data/reveal/ndarray_conversion.pyi
@@ -30,8 +30,15 @@ assert_type(b1_0d.tolist(), bool)
 assert_type(u2_1d.tolist(), list[int])
 assert_type(i4_2d.tolist(), list[list[int]])
 assert_type(f8_3d.tolist(), list[list[list[float]]])
-assert_type(cG_4d.tolist(), complex | list[complex] | list[list[complex]] | list[list[list[Any]]])
-assert_type(i0_nd.tolist(), int | list[int] | list[list[int]] | list[list[list[Any]]])
+assert_type(cG_4d.tolist(), Any)
+assert_type(i0_nd.tolist(), Any)
+
+# regression tests for numpy/numpy#27944
+any_dtype: np.ndarray[Any, Any]
+any_sctype: np.ndarray[Any, Any]
+assert_type(any_dtype.tolist(), Any)
+assert_type(any_sctype.tolist(), Any)
+
 
 # itemset does not return a value
 # tostring is pretty simple
