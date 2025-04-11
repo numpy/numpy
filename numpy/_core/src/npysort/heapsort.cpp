50988ba6a5d5fbe5f4a3484c33ae4e4d5b4aecc6
Maanas Arora <maanasarora23@gmail.com>
2025-04-11 17:17:02 -0400

    ENH: Add new sort func implementations and use in stringdtype

--- numpy/_core/src/npysort/heapsort.cpp
+++ numpy/_core/src/npysort/heapsort.cpp
@@ -50,13 +50,31 @@
  */
 
 NPY_NO_EXPORT int
+npy_heapsort_with_context(void *start, npy_intp num,
+                          PyArrayMethod_Context *context, NpyAuxData *auxdata,
+                          NpyAuxData **out_auxdata)
+{
+    return handle_npysort_with_context(start, num, context, auxdata,
+                                       out_auxdata, &npy_heapsort);
+}
+
+NPY_NO_EXPORT int
+npy_aheapsort_with_context(void *vv, npy_intp *tosort, npy_intp num,
+                           PyArrayMethod_Context *context, NpyAuxData *auxdata,
+                           NpyAuxData **out_auxdata)
+{
+    return handle_npyasort_with_context(vv, tosort, num, context, auxdata,
+                                        out_auxdata, &npy_aheapsort);
+}
+
+NPY_NO_EXPORT int
 npy_heapsort(void *start, npy_intp num, void *varr)
 {
     void *arr;
     npy_intp elsize;
     PyArray_CompareFunc *cmp;
     fill_sort_data_from_arr_or_descr(varr, &arr, &elsize, &cmp);
-    
+
     if (elsize == 0) {
         return 0;  /* no need for sorting elements of no size */
     }
