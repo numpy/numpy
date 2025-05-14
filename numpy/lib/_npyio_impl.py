40440a3fdf72096bb3531495c3247a95abf7038e
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-05-14 14:45:52 +0200

    Consistent quoting
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>

--- numpy/lib/_npyio_impl.py
+++ numpy/lib/_npyio_impl.py
@@ -949,7 +949,7 @@ def _read(fname, *, delimiter=',', comment='#', quote='"',
 
     read_dtype_via_object_chunks = None
     if dtype.kind in 'SUM' and dtype in {
-            np.dtype("S0"), np.dtype("U0"), np.dtype("M8"), np.dtype('m8')}:
+            np.dtype("S0"), np.dtype("U0"), np.dtype("M8"), np.dtype("m8")}:
         # This is a legacy "flexible" dtype.  We do not truly support
         # parametric dtypes currently (no dtype discovery step in the core),
         # but have to support these for backward compatibility.
