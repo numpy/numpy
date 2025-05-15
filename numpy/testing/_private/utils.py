54be49e32e1cb15fb68bfd4deaf5c04fb2b0a127
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-05-15 11:59:22 +0200

    STY: Apply ruff/Perflint rule PERF102
    PERF102 When using only the values of a dict use the `values()` method
    PERF102 When using only the keys of a dict use the `keys()` method

--- numpy/testing/_private/utils.py
+++ numpy/testing/_private/utils.py
@@ -356,13 +356,13 @@ def assert_equal(actual, desired, err_msg='', verbose=True, *, strict=False):
         for k, i in desired.items():
             if k not in actual:
                 raise AssertionError(repr(k))
-            assert_equal(actual[k], desired[k], f'key={k!r}\n{err_msg}',
+            assert_equal(actual[k], i, f'key={k!r}\n{err_msg}',
                          verbose)
         return
     if isinstance(desired, (list, tuple)) and isinstance(actual, (list, tuple)):
         assert_equal(len(actual), len(desired), err_msg, verbose)
-        for k in range(len(desired)):
-            assert_equal(actual[k], desired[k], f'item={k!r}\n{err_msg}',
+        for k, i in enumerate(desired):
+            assert_equal(actual[k], i, f'item={k!r}\n{err_msg}',
                          verbose)
         return
     from numpy import imag, iscomplexobj, real
