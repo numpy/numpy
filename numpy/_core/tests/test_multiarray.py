bce4d2290d2d753487c70b8abd9858fffef36ba0
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-05-18 22:15:10 +0200

    ci

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -8339,8 +8339,8 @@ class TestNewBufferProtocol:
         self._check_roundtrip(x3)
 
     @pytest.mark.valgrind_error(reason="leaks buffer info cache temporarily.")
-    def test_relaxed_strides(self, c=stride_tricks.as_strided(
-                                              np.ones((1, 10, 10), dtype='i8'), # noqa: B008
+    def test_relaxed_strides(self, c=stride_tricks.as_strided(  # noqa: B008
+                                              np.ones((1, 10, 10), dtype='i8'),  # noqa: B008
                                               strides=(-1, 80, 8)
                                               )
                                   ):
