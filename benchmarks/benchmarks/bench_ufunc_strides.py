74a4d8455c84feb3e7d3f51c45ecd682b7de92f8
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2025-05-29 08:57:55 -0700

    BENCH: Increase array sizes for ufunc and sort benchmarks

--- benchmarks/benchmarks/bench_ufunc_strides.py
+++ benchmarks/benchmarks/bench_ufunc_strides.py
@@ -10,7 +10,7 @@ class _AbstractBinary(Benchmark):
     params = []
     param_names = ['ufunc', 'stride_in0', 'stride_in1', 'stride_out', 'dtype']
     timeout = 10
-    arrlen = 10000
+    arrlen = 1000000
     data_finite = True
     data_denormal = False
     data_zeros = False
@@ -63,7 +63,7 @@ class _AbstractUnary(Benchmark):
     params = []
     param_names = ['ufunc', 'stride_in', 'stride_out', 'dtype']
     timeout = 10
-    arrlen = 10000
+    arrlen = 1000000
     data_finite = True
     data_denormal = False
     data_zeros = False
