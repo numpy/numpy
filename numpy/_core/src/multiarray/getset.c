d8c22e5dc95f31310265c773b44f526cadb78b46
Sebastian Berg <sebastian@sipsolutions.net>
2025-05-30 11:28:49 +0200

    Update numpy/_core/src/multiarray/getset.c

--- numpy/_core/src/multiarray/getset.c
+++ numpy/_core/src/multiarray/getset.c
@@ -122,8 +122,8 @@ array_strides_set(PyArrayObject *self, PyObject *obj, void *NPY_UNUSED(ignored))
         return -1;
     }
 
-    /* Deprecated NumPy 2.3, 2025-05-11 */
-    if( DEPRECATE("Setting the strides on a NumPy array has been deprecated in NumPy 2.3.\n"
+    /* Deprecated NumPy 2.4, 2025-05-11 */
+    if( DEPRECATE("Setting the strides on a NumPy array has been deprecated in NumPy 2.4.\n"
                   "As an alternative, you can create a new view using np.lib.stride_tricks.as_strided."
                  ) < 0 ) {
         return -1;
