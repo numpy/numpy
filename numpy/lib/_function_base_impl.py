23c9e7c6874e31c2c1283e0fd0a9a48bc081a696
Charles Harris <charlesr.harris@gmail.com>
2025-06-13 09:00:53 -0600

    BUG: Revert `np.vectorize` casting to legacy behavior (#29196) (#29205)
    Revert use of no `dtype=object` to ensure correct cast behavior when the output dtype is discovered.
    
    Co-authored-by: François Rozet <francois.rozet@outlook.com>
    Co-authored-by: Sebastian Berg <sebastian@sipsolutions.net>

--- numpy/lib/_function_base_impl.py
+++ numpy/lib/_function_base_impl.py
@@ -2573,6 +2573,7 @@ class vectorize:
             # the subsequent call when the ufunc is evaluated.
             # Assumes that ufunc first evaluates the 0th elements in the input
             # arrays (the input values are not checked to ensure this)
+            args = [asarray(a) for a in args]
             if builtins.any(arg.size == 0 for arg in args):
                 raise ValueError('cannot call `vectorize` on size 0 inputs '
                                  'unless `otypes` is set')
@@ -2618,8 +2619,9 @@ class vectorize:
         elif not args:
             res = func()
         else:
-            args = [asanyarray(a, dtype=object) for a in args]
             ufunc, otypes = self._get_ufunc_and_otypes(func=func, args=args)
+            # gh-29196: `dtype=object` should eventually be removed
+            args = [asanyarray(a, dtype=object) for a in args]
             outputs = ufunc(*args, out=...)
 
             if ufunc.nout == 1:
