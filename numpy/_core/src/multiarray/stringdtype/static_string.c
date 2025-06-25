44607862efdb4d4bbda337766b77e41bc5e5dc95
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-06-25 11:39:05 -0600

    BUG: fix fencepost error in StringDType internals (#29269)
    This makes all the comparisons with NPY_MEDIUM_STRING_MAX_SIZE use <= consistently:

--- numpy/_core/src/multiarray/stringdtype/static_string.c
+++ numpy/_core/src/multiarray/stringdtype/static_string.c
@@ -478,7 +478,7 @@ heap_or_arena_allocate(npy_string_allocator *allocator,
         if (*flags == 0) {
             // string isn't previously allocated, so add to existing arena allocation
             char *ret = arena_malloc(arena, allocator->realloc, sizeof(char) * size);
-            if (size < NPY_MEDIUM_STRING_MAX_SIZE) {
+            if (size <= NPY_MEDIUM_STRING_MAX_SIZE) {
                 *flags = NPY_STRING_INITIALIZED;
             }
             else {
