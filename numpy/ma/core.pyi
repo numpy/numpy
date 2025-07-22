35368a12b4511e1a2184392242b3fc4590e6de9f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-22 17:22:06 +0100

    TYP: Type ``MaskedArray.__deepcopy__`` and ``MaskedArray.argsort``

--- numpy/ma/core.pyi
+++ numpy/ma/core.pyi
@@ -1398,7 +1398,16 @@ class MaskedArray(ndarray[_ShapeT_co, _DTypeT_co]):
     @overload
     def round(self, /, decimals: SupportsIndex = 0, *, out: _ArrayT) -> _ArrayT: ...
 
-    def argsort(self, axis=..., kind=..., order=..., endwith=..., fill_value=..., *, stable=...): ...
+    def argsort(
+        self,
+        axis: SupportsIndex | _NoValueType = ...,
+        kind: _SortKind | None = None,
+        order: str | Sequence[str] | None = None,
+        endwith: bool = True,
+        fill_value: _ScalarLike_co | None = None,
+        *,
+        stable: bool | None = None,
+    ) -> _MaskedArray[Any]: ...
 
     # Keep in-sync with np.ma.argmin
     @overload  # type: ignore[override]
@@ -1703,7 +1712,7 @@ class MaskedArray(ndarray[_ShapeT_co, _DTypeT_co]):
 
     #
     def __reduce__(self): ...
-    def __deepcopy__(self, memo=...): ...
+    def __deepcopy__(self, memo: dict[int, Any] | None = None) -> Self: ...
 
     # Keep `dtype` at the bottom to avoid name conflicts with `np.dtype`
     @property
