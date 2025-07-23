436662ee6cd73e959d5eff375f3e8eec5e0be8a2
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-23 13:57:43 +0100

    TYP: Type ``MaskedArray.__deepcopy__`` and ``MaskedArray.argsort`` (#29418)

--- numpy/typing/tests/data/reveal/ma.pyi
+++ numpy/typing/tests/data/reveal/ma.pyi
@@ -405,6 +405,12 @@ assert_type(MAR_f8.cumprod(out=MAR_subclass), MaskedArraySubclass)
 assert_type(MAR_f8.cumsum(), MaskedArray[Any])
 assert_type(MAR_f8.cumsum(out=MAR_subclass), MaskedArraySubclass)
 
+assert_type(MAR_f8.__deepcopy__(), MaskedArray[np.float64])
+
+assert_type(MAR_f8.argsort(), MaskedArray[np.intp])
+assert_type(MAR_f8.argsort(axis=0, kind='heap', order=('x', 'y')), MaskedArray[np.intp])
+assert_type(MAR_f8.argsort(endwith=True, fill_value=1.5, stable=False), MaskedArray[np.intp])
+
 def invalid_resize() -> None:
     assert_type(MAR_f8.resize((1,1)), NoReturn)  # type: ignore[arg-type]
 
