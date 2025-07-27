3203001d84ad387253697f1ba4af805093dc338e
Charles Harris <charlesr.harris@gmail.com>
2025-07-27 15:39:25 -0600

    MAINT: Replace pavement.py
    Replace `pavement.py` functionality with a `write_release.py` script.
    This gets rid of a paver dependency and simplifies the release process.
    
    - Add tools/write_release.py
    - Remove pavement.py
    
    [skip azp] [skip cirrus] [skip actions]

---
+++ tools/write_release.py
@@ -0,0 +1,121 @@
+"""
+Standalone script for writing release doc::
+
+    python tools/write_release <version>
+
+Example::
+
+    python tools/write_release.py 1.7.0
+
+Needs to be run from the root of the repository and assumes
+that the output is in `release` and wheels and sdist in
+`release/installers`.
+
+Translation from rst to md markdown requires Pandoc, you 
+will need to rely on your distribution to provide that.
+
+"""
+import os
+import subprocess
+import textwrap
+import argparse
+from hashlib import md5
+from hashlib import sha256
+from pathlib import Path
+
+# Name of the notes directory
+NOTES_DIR = "doc/source/release"
+# Name of the output directory
+OUTPUT_DIR = "release"
+# Output base name, `.rst` or `.md` will be appended
+OUTPUT_FILE = "README"
+
+def compute_hash(wheel_dir, hash_func):
+    """
+    Compute hashes of files in wheel_dir.
+
+    Parameters
+    ----------
+    wheel_dir: str
+        Path to wheel directory from repo root.
+    hash_func: function
+        Hash function, i.e., md5, sha256, etc.
+
+    Returns
+    -------
+    list_of_strings: list
+        List of of strings. Each string is the hash
+        followed by the file basename.
+
+    """
+    released = os.listdir(wheel_dir)
+    checksums = []
+    for fn in sorted(released):
+        fn_path = Path(f"{wheel_dir}/{fn}") 
+        with open(fn_path, 'rb') as f:
+            m = hash_func(f.read())
+        checksums.append(f"{m.hexdigest()}  {fn}")
+    return checksums
+
+
+def write_release(version):
+    """
+    Copy the <version>-notes.rst file to the OUTPUT_DIR, append
+    the md5 and sha256 hashes of the wheels and sdist, and produce
+    README.rst and README.md files.
+
+    Parameters
+    ----------
+    version: str
+       Release version, e.g., '2.3.2', etc.
+
+    Returns
+    -------
+    None.
+
+    """
+    notes = Path(f"{NOTES_DIR}/{version}-notes.rst")
+    wheel_dir = Path(f"{OUTPUT_DIR}/installers")
+    target_md = Path(f"{OUTPUT_DIR}/{OUTPUT_FILE}.md")
+    target_rst = Path(f"{OUTPUT_DIR}/{OUTPUT_FILE}.rst")
+    
+    os.system(f"cp {notes} {target_rst}")
+
+    with open(str(target_rst), 'a') as f:
+        f.writelines(textwrap.dedent(
+            """
+            Checksums
+            =========
+
+            MD5
+            ---
+            ::
+
+            """))
+        f.writelines([f'    {c}\n' for c in compute_hash(wheel_dir, md5)])
+
+        f.writelines(textwrap.dedent(
+            """
+            SHA256
+            ------
+            ::
+
+            """))
+        f.writelines([f'    {c}\n' for c in compute_hash(wheel_dir, sha256)])
+
+    # translate README.rst to md for posting on GitHub
+    rst_to_md = subprocess.Popen(
+        ["pandoc", "-s", "-o", f"{target_md}", f"{target_rst}", "--wrap=preserve"],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = rst_to_md.communicate()
+    if not rst_to_md.returncode == 0:
+        raise RuntimeError(f"{error} failed")
+ 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "version",
+        help="NumPy version of the release, e.g. 2.3.2, etc.")
+
+    args = parser.parse_args()
+    write_release(args.version)
