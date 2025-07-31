0fbd49aad6d941008ccbb32c919186770b153483
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-07-31 16:25:36 +0100

    TYP: Type ``MaskedArray.__setitem__`` (#29478)

--- numpy/typing/tests/data/pass/ma.py
+++ numpy/typing/tests/data/pass/ma.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from typing import Any, TypeAlias, TypeVar, cast
 
 import numpy as np
@@ -15,11 +16,16 @@ MAR_i: MaskedArray[np.int64] = np.ma.MaskedArray([1])
 MAR_f: MaskedArray[np.float64] = np.ma.MaskedArray([1.0])
 MAR_c: MaskedArray[np.complex128] = np.ma.MaskedArray([1j])
 MAR_td64: MaskedArray[np.timedelta64] = np.ma.MaskedArray([np.timedelta64(1, "D")])
-MAR_M_dt64: MaskedArray[np.datetime64] = np.ma.MaskedArray([np.datetime64(1, "D")])
+MAR_dt64: MaskedArray[np.datetime64] = np.ma.MaskedArray([np.datetime64(1, "D")])
 MAR_S: MaskedArray[np.bytes_] = np.ma.MaskedArray([b'foo'], dtype=np.bytes_)
 MAR_U: MaskedArray[np.str_] = np.ma.MaskedArray(['foo'], dtype=np.str_)
 MAR_T = cast(np.ma.MaskedArray[Any, np.dtypes.StringDType],
              np.ma.MaskedArray(["a"], dtype="T"))
+MAR_V: MaskedArray[np.void] = np.ma.MaskedArray(
+    [(1, 1)],
+    mask=[(False, False)],
+    dtype=[('a', int), ('b', int)]
+)
 
 AR_b: npt.NDArray[np.bool] = np.array([True, False, True])
 
@@ -37,6 +43,14 @@ MAR_f.mask = np.False_
 MAR_i.fill_value = 0
 
 MAR_f.flat = [9]
+
+MAR_b[MAR_i > 0] = False
+MAR_i[:] = 1
+MAR_f[[0]] = AR_LIKE_f
+MAR_c[[0, 0]] = [3, 4 + 3j]
+MAR_td64[0] = dt.timedelta(1)
+MAR_dt64[0] = dt.datetime(2020, 1, 1)
+MAR_V['a'] = [2]
 
 # Inplace addition
 
@@ -65,10 +79,10 @@ MAR_td64 += AR_LIKE_b
 MAR_td64 += AR_LIKE_u
 MAR_td64 += AR_LIKE_i
 MAR_td64 += AR_LIKE_m
-MAR_M_dt64 += AR_LIKE_b
-MAR_M_dt64 += AR_LIKE_u
-MAR_M_dt64 += AR_LIKE_i
-MAR_M_dt64 += AR_LIKE_m
+MAR_dt64 += AR_LIKE_b
+MAR_dt64 += AR_LIKE_u
+MAR_dt64 += AR_LIKE_i
+MAR_dt64 += AR_LIKE_m
 
 MAR_S += b'snakes'
 MAR_U += 'snakes'
@@ -97,10 +111,10 @@ MAR_td64 -= AR_LIKE_b
 MAR_td64 -= AR_LIKE_u
 MAR_td64 -= AR_LIKE_i
 MAR_td64 -= AR_LIKE_m
-MAR_M_dt64 -= AR_LIKE_b
-MAR_M_dt64 -= AR_LIKE_u
-MAR_M_dt64 -= AR_LIKE_i
-MAR_M_dt64 -= AR_LIKE_m
+MAR_dt64 -= AR_LIKE_b
+MAR_dt64 -= AR_LIKE_u
+MAR_dt64 -= AR_LIKE_i
+MAR_dt64 -= AR_LIKE_m
 
 # Inplace floor division
 
