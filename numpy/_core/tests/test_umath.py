9af1e8317b6fe59c6f28bb10e9b83a7aaee7e52a
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-07-31 23:12:16 +0200

    more conversions

--- numpy/_core/tests/test_umath.py
+++ numpy/_core/tests/test_umath.py
@@ -4818,18 +4818,20 @@ def test_outer_bad_subclass():
         def __array_finalize__(self, obj):
             # The outer call reshapes to 3 dims, try to do a bad reshape.
             if self.ndim == 3:
-                self.shape = self.shape + (1,)
+                with pytest.warns(DeprecationWarning):
+                    self.shape = self.shape + (1,)
 
     class BadArr2(np.ndarray):
         def __array_finalize__(self, obj):
             if isinstance(obj, BadArr2):
                 # outer inserts 1-sized dims. In that case disturb them.
                 if self.shape[-1] == 1:
-                    self.shape = self.shape[::-1]
+                    with pytest.warns(DeprecationWarning):
+                        self.shape = self.shape[::-1]
 
     for cls in [BadArr1, BadArr2]:
         arr = np.ones((2, 3)).view(cls)
-        with assert_raises(TypeError) as a:
+        with assert_raises(TypeError) as _:
             # The first array gets reshaped (not the second one)
             np.add.outer(arr, [1, 2])
 
