fce87004a0dc93ccadaa030b7b9c103ff133b655
abhishek-fujitsu <abhishek.r.kumar@fujitsu.com>
2025-08-18 16:27:34 +0530

    remove baseline HWY dispatch

--- numpy/_core/src/umath/loops_arithmetic.dispatch.cpp
+++ numpy/_core/src/umath/loops_arithmetic.dispatch.cpp
@@ -3,9 +3,6 @@
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
 
 #include "npy_cpu_dispatch.h"
-#if NPY__CPU_TARGET_INDEX == 0
-#define HWY_COMPILE_ONLY_SCALAR 1
-#endif
 
 #include "numpy/npy_common.h"
 #include "numpy/npy_math.h"
