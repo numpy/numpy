c497f21fd3e9ec8fa319f1fc92c9c1db268a8804
Britney Whittington <britneywhittington@gmail.com>
2025-09-29 19:55:38 -0400

    STY: More changes for lint

--- numpy/f2py/tests/conftest.py
+++ numpy/f2py/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
 
+
 def pytest_itemcollected(item):
     item.add_marker(pytest.mark.thread_unsafe(reason="f2py is thread-unsafe"))
--- numpy/f2py/tests/test_f2cmap.py
+++ numpy/f2py/tests/test_f2cmap.py
@@ -1,5 +1,3 @@
-import pytest
-
 import numpy as np
 
 from . import util
