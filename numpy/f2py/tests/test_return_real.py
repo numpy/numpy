f56b76f5f6f1f268845e7973591370da6d859d83
jorenham <jhammudoglu@gmail.com>
2025-10-09 14:35:53 +0200

    TST: expect more specific f2py exceptions, resolving ``B017``

--- numpy/f2py/tests/test_return_real.py
+++ numpy/f2py/tests/test_return_real.py
@@ -39,8 +39,8 @@ class TestReturnReal(util.F2PyTest):
         pytest.raises(IndexError, t, [])
         pytest.raises(IndexError, t, ())
 
-        pytest.raises(Exception, t, t)  # noqa: B017
-        pytest.raises(Exception, t, {})  # noqa: B017
+        pytest.raises(TypeError, t, t)
+        pytest.raises(TypeError, t, {})
 
         try:
             r = t(10**400)
