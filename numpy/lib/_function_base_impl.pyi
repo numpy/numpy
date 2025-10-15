9269f5ea030dfd514ac3caa9509da2fa3af4669a
jorenham <jhammudoglu@gmail.com>
2025-10-15 05:56:21 +0200

    TYP: ``trim_zeros``: missing ``axis`` parameter

--- numpy/lib/_function_base_impl.pyi
+++ numpy/lib/_function_base_impl.pyi
@@ -415,6 +415,7 @@ def sort_complex(a: ArrayLike) -> NDArray[complexfloating]: ...
 def trim_zeros(
     filt: _TrimZerosSequence[_T],
     trim: L["f", "b", "fb", "bf"] = "fb",
+    axis: _ShapeLike | None = None,
 ) -> _T: ...
 
 @overload
