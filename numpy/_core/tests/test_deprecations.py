eab9ca31af5d6fa141ffdd5462413fdcb78ad164
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-10-30 19:30:29 +0100

    Update numpy/_core/tests/test_deprecations.py

--- numpy/_core/tests/test_deprecations.py
+++ numpy/_core/tests/test_deprecations.py
@@ -11,7 +11,7 @@ import pytest
 import numpy as np
 import numpy._core._struct_ufunc_tests as struct_ufunc
 from numpy._core._multiarray_tests import fromstring_null_term_c_api  # noqa: F401
-from numpy.testing import IS_PYPY, assert_raises, temppath
+from numpy.testing import IS_PYPY, assert_raises
 
 
 class _DeprecationTestCase:
