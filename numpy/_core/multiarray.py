0b70601057e967f70adffae76db625c9bffca6e6
Antareep Sarkar <sarkar.antareep.9035@gmail.com>
2025-11-04 17:18:16 +0530

    add example for out in concatenate

--- numpy/_core/multiarray.py
+++ numpy/_core/multiarray.py
@@ -256,7 +256,12 @@ def concatenate(arrays, axis=None, out=None, *, dtype=None, casting=None):
     >>> import numpy as np
     >>> a = np.array([[1, 2], [3, 4]])
     >>> b = np.array([[5, 6]])
-    >>> np.concatenate((a, b), axis=0)
+    >>> c = np.array([[7, 8], [9, 10], [11, 12]])
+    >>> np.concatenate((a, b), axis=0, out=c)
+    array([[1, 2],
+           [3, 4],
+           [5, 6]])
+    >>> c
     array([[1, 2],
            [3, 4],
            [5, 6]])
