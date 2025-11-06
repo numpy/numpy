ab169f2c52b4c2bfa928bc4698b7fda1bea5050c
jorenham <jhammudoglu@gmail.com>
2025-11-06 17:19:13 +0100

    MAINT: some ``numpy.polynomial.*`` namespace pollution cleanup

--- numpy/polynomial/hermite_e.py
+++ numpy/polynomial/hermite_e.py
@@ -76,8 +76,6 @@ See also
 
 """
 import numpy as np
-import numpy.linalg as la
-from numpy.lib.array_utils import normalize_axis_index
 
 from . import polyutils as pu
 from ._polybase import ABCPolyBase
@@ -653,7 +651,7 @@ def hermeder(c, m=1, scl=1, axis=0):
     iaxis = pu._as_int(axis, "the axis")
     if cnt < 0:
         raise ValueError("The order of derivation must be non-negative")
-    iaxis = normalize_axis_index(iaxis, c.ndim)
+    iaxis = np.lib.array_utils.normalize_axis_index(iaxis, c.ndim)
 
     if cnt == 0:
         return c
@@ -770,7 +768,7 @@ def hermeint(c, m=1, k=[], lbnd=0, scl=1, axis=0):
         raise ValueError("lbnd must be a scalar.")
     if np.ndim(scl) != 0:
         raise ValueError("scl must be a scalar.")
-    iaxis = normalize_axis_index(iaxis, c.ndim)
+    iaxis = np.lib.array_utils.normalize_axis_index(iaxis, c.ndim)
 
     if cnt == 0:
         return c
@@ -1461,7 +1459,7 @@ def hermeroots(c):
 
     # rotated companion matrix reduces error
     m = hermecompanion(c)[::-1, ::-1]
-    r = la.eigvals(m)
+    r = np.linalg.eigvals(m)
     r.sort()
     return r
 
@@ -1548,7 +1546,7 @@ def hermegauss(deg):
     # matrix is symmetric in this case in order to obtain better zeros.
     c = np.array([0] * deg + [1])
     m = hermecompanion(c)
-    x = la.eigvalsh(m)
+    x = np.linalg.eigvalsh(m)
 
     # improve roots by one application of Newton
     dy = _normed_hermite_e_n(x, ideg)
