1d14aa18389f65c397bd84e600e1228a5ed6f9bd
DWesl <22566757+DWesl@users.noreply.github.com>
2025-11-10 19:57:39 -0500

    BUG: Allow failure to copy due to source being dest
    f2py failed when the source directory was the build directory, due to a failure to copy the source files to the build directory.  This commit accepts this state as resulting in the source files being present in the build directory.
    
    Suggested in #29762 and #24874

--- numpy/f2py/_backends/_meson.py
+++ numpy/f2py/_backends/_meson.py
@@ -204,7 +204,10 @@ def _prepare_sources(mname, sources, bdir):
     # Copy sources
     for source in sources:
         if Path(source).exists() and Path(source).is_file():
-            shutil.copy(source, bdir)
+            try:
+                shutil.copy(source, bdir)
+            except shutil.SameFileError:
+                pass
     generated_sources = [
         Path(f"{mname}module.c"),
         Path(f"{mname}-f2pywrappers2.f90"),
