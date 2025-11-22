e6d0ebe68ace2a70af948c647f1e7d7b73e688ce
Kumar Aditya <kumaraditya@python.org>
2025-11-22 10:32:13 +0530

    better assertions

--- numpy/_core/src/common/npy_rwlock.c
+++ numpy/_core/src/common/npy_rwlock.c
@@ -3,6 +3,11 @@
 
 #ifdef Py_GIL_DISABLED
 
+// PyMutex_IsLocked is available only in Python 3.14+
+#ifndef PyMutex_IsLocked
+#define PyMutex_IsLocked(m) (1)
+#endif
+
 NPY_NO_EXPORT void
 PyRWMutex_Lock(PyRWMutex *rwmutex)
 {
@@ -11,6 +16,7 @@ PyRWMutex_Lock(PyRWMutex *rwmutex)
     if (writer_id == thread_id) {
         // If the current thread already holds the write lock, allow recursive write lock
         rwmutex->level++;
+        assert(PyMutex_IsLocked(&rwmutex->writer_lock));
         return;
     }
     PyMutex_Lock(&rwmutex->writer_lock);
@@ -21,8 +27,9 @@ PyRWMutex_Lock(PyRWMutex *rwmutex)
 NPY_NO_EXPORT void
 PyRWMutex_Unlock(PyRWMutex *rwmutex)
 {
+    assert(PyMutex_IsLocked(&rwmutex->writer_lock));
+    assert(rwmutex->writer_id == PyThread_get_thread_ident());
     if (rwmutex->level > 0) {
-        assert(rwmutex->writer_id == PyThread_get_thread_ident());
         rwmutex->level--;
         return;
     }
@@ -33,14 +40,22 @@ PyRWMutex_Unlock(PyRWMutex *rwmutex)
 NPY_NO_EXPORT void
 PyRWMutex_RLock(PyRWMutex *rwmutex)
 {
+    unsigned long thread_id = PyThread_get_thread_ident();
+    unsigned long writer_id = npy_atomic_load_ulong(&rwmutex->writer_id);
+    // If current thread holds the write lock, allow recursive read lock
+    if (writer_id == thread_id) {
+        rwmutex->level++;
+        assert(PyMutex_IsLocked(&rwmutex->writer_lock));
+        return;
+    }
+
     PyMutex_Lock(&rwmutex->reader_lock);
     rwmutex->reader_count++;
     if (rwmutex->reader_count == 1) {
         // First reader acquires the write lock to block writers
         PyMutex_Lock(&rwmutex->writer_lock);
-        // zero means locked by reader
-        npy_atomic_store_ulong(&rwmutex->writer_id, 0);
     }
+    assert(PyMutex_IsLocked(&rwmutex->writer_lock));
     assert(rwmutex->writer_id == 0 || rwmutex->writer_id == PyThread_get_thread_ident());
     PyMutex_Unlock(&rwmutex->reader_lock);
 }
@@ -48,8 +63,14 @@ PyRWMutex_RLock(PyRWMutex *rwmutex)
 NPY_NO_EXPORT void
 PyRWMutex_RUnlock(PyRWMutex *rwmutex)
 {
-    PyMutex_Lock(&rwmutex->reader_lock);
+    assert(PyMutex_IsLocked(&rwmutex->writer_lock));
     assert(rwmutex->writer_id == 0 || rwmutex->writer_id == PyThread_get_thread_ident());
+    if (rwmutex->level > 0) {
+        assert(rwmutex->writer_id == PyThread_get_thread_ident());
+        rwmutex->level--;
+        return;
+    }
+    PyMutex_Lock(&rwmutex->reader_lock);
     rwmutex->reader_count--;
     if (rwmutex->reader_count == 0) {
         // Last reader releases the writer lock
