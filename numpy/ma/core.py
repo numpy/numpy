fix:recarray, masked_array and datetime break fill value calculation

numpy/ma/core.py
@@ -20,13 +20,13 @@ Released for unlimited redistribution.
 
 """
 import builtins
+import datetime
 import functools
 import inspect
 import operator
 import re
 import textwrap
 import warnings
-import datetime
 
 import numpy as np
 import numpy._core.numerictypes as ntypes
@@ -245,7 +245,7 @@ def _recursive_fill_value(dtype, f):
                 if isinstance(val, (datetime.date, datetime.datetime)):
                     val = np.datetime64(val)
             vals.append(np.array(val))
-        return np.array(vals, dtype=dtype)[()]  # decay to void scalar from 0d
+        return np.array(tuple(vals), dtype=dtype)[()]  # decay to void scalar from 0d
     elif dtype.subdtype:
         subtype, shape = dtype.subdtype
         subval = _recursive_fill_value(subtype, f)

numpy/ma/tests/test_core.py
@@ -4980,9 +4980,10 @@ class TestMaskedArrayFunctions:
         bools = [True, False]
         dtypes = [MaskType, float]
         msgformat = 'copy=%s, shrink=%s, dtype=%s'
-        for cpy, shr, dt in itertools.product(bools, bools, dtypes):
-            res = make_mask(nomask, copy=cpy, shrink=shr, dtype=dt)
-            assert_(res is nomask, msgformat % (cpy, shr, dt))
+        # renamed 'dt' to 'dtype'
+        for cpy, shr, dtype in itertools.product(bools, bools, dtypes):
+            res = make_mask(nomask, copy=cpy, shrink=shr, dtype=dtype)
+            assert_(res is nomask, msgformat % (cpy, shr, dtype))
 
     def test_mask_or(self):
         # Initialize
@@ -5313,9 +5314,10 @@ class TestMaskedObjectArray:
 
     def test_getitem(self):
         arr = np.ma.array([None, None])
-        for dt in [float, object]:
-            a0 = np.eye(2).astype(dt)
-            a1 = np.eye(3).astype(dt)
+        # renamed 'dt' to 'dtype'
+        for dtype in [float, object]:
+            a0 = np.eye(2).astype(dtype)
+            a1 = np.eye(3).astype(dtype)
             arr[0] = a0
             arr[1] = a1
 