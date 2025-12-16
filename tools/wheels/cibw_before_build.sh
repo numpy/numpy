ad3433b89f9030bf8551c3458ffb8f4b0d5e501f
Malcolm Smith <smith@chaquo.com>
2025-12-16 18:03:36 +0000

    Build with OpenBLAS

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -19,31 +19,55 @@ if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     # On 32-bit platforms and on win-arm64, use scipy-openblas32
     OPENBLAS=openblas64
     # Possible values for RUNNER_ARCH in GitHub Actions are: X86, X64, ARM, or ARM64
-    if [[ $RUNNER_ARCH == "X86" || $RUNNER_ARCH == "ARM" ]] ; then
+    if echo "${CIBW_HOST_TRIPLET:-}" | grep -q android; then
+        OPENBLAS=android
+    elif [[ $RUNNER_ARCH == "X86" || $RUNNER_ARCH == "ARM" ]] ; then
         OPENBLAS=openblas32
     elif [[ $RUNNER_ARCH == "ARM64" && $RUNNER_OS == "Windows" ]] ; then
         OPENBLAS=openblas32
     fi
 
-    # The PKG_CONFIG_PATH environment variable will be pointed to this path in
-    # pyproject.toml and .github/workflows/wheels.yml. Note that
-    # `pkgconf_path` here is only a bash variable local to this file.
-    pkgconf_path=$PROJECT_DIR/.openblas
-    echo pkgconf_path is $pkgconf_path, OPENBLAS is ${OPENBLAS}
-    rm -rf $pkgconf_path
-    mkdir -p $pkgconf_path
-    python -m pip install -r $PROJECT_DIR/requirements/ci_requirements.txt
-    python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $pkgconf_path/scipy-openblas.pc
+    if [[ $OPENBLAS == "android" ]]; then
+        openblas_version=0.2.20
+        pip install \
+            --target $PKG_CONFIG_PATH \
+            --extra-index-url https://chaquo.com/pypi-13.1/ \
+            chaquopy-openblas==$openblas_version
+        mv $PKG_CONFIG_PATH/chaquopy/* $PKG_CONFIG_PATH
+        rmdir $PKG_CONFIG_PATH/chaquopy
 
-    # Copy scipy-openblas DLL's to a fixed location so we can point delvewheel
-    # at it in `repair_windows.sh` (needed only on Windows because of the lack
-    # of RPATH support).
-    if [[ $RUNNER_OS == "Windows" ]]; then
-        python <<EOF
+        # There is a pkgconfig file, but it's not usable because it doesn't use the
+        # `prefix` variable, so overwrite it.
+        cat <<EOF > $PKG_CONFIG_PATH/lib/pkgconfig/openblas.pc
+prefix=/usr
+Name: openblas
+Description: OpenBLAS
+Version: ${openblas_version}
+Cflags: -I\${prefix}/include
+Libs: -L\${prefix}/lib -lopenblas
+EOF
+
+    else
+        # The PKG_CONFIG_PATH environment variable will be pointed to this path in
+        # pyproject.toml and .github/workflows/wheels.yml. Note that
+        # `pkgconf_path` here is only a bash variable local to this file.
+        pkgconf_path=$PROJECT_DIR/.openblas
+        echo pkgconf_path is $pkgconf_path, OPENBLAS is ${OPENBLAS}
+        rm -rf $pkgconf_path
+        mkdir -p $pkgconf_path
+        python -m pip install -r $PROJECT_DIR/requirements/ci_requirements.txt
+        python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $pkgconf_path/scipy-openblas.pc
+
+        # Copy scipy-openblas DLL's to a fixed location so we can point delvewheel
+        # at it in `repair_windows.sh` (needed only on Windows because of the lack
+        # of RPATH support).
+        if [[ $RUNNER_OS == "Windows" ]]; then
+            python <<EOF
 import os, scipy_${OPENBLAS}, shutil
 srcdir = os.path.join(os.path.dirname(scipy_${OPENBLAS}.__file__), "lib")
 shutil.copytree(srcdir, os.path.join("$pkgconf_path", "lib"))
 EOF
+        fi
     fi
 fi
 
