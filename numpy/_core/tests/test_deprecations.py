fd5f19d39ee9c297d85b3b7361bf9ad6a783dc3a
jorenham <jhammudoglu@gmail.com>
2025-12-17 21:03:33 +0100

    DEP: remove ``_core.numerictypes.maximum_sctype``

--- numpy/_core/tests/test_deprecations.py
+++ numpy/_core/tests/test_deprecations.py
@@ -275,7 +275,6 @@ class TestLibImports(_DeprecationTestCase):
     # Deprecated in Numpy 1.26.0, 2023-09
     def test_lib_functions_deprecation_call(self):
         from numpy import row_stack
-        from numpy._core.numerictypes import maximum_sctype
         from numpy.lib._npyio_impl import recfromcsv, recfromtxt
         from numpy.lib._shape_base_impl import get_array_wrap
         from numpy.lib._utils_impl import safe_eval
@@ -289,7 +288,6 @@ class TestLibImports(_DeprecationTestCase):
         self.assert_deprecated(lambda: recfromtxt(data_gen(), **kwargs))
 
         self.assert_deprecated(get_array_wrap)
-        self.assert_deprecated(lambda: maximum_sctype(int))
 
         self.assert_deprecated(lambda: row_stack([[]]))
         self.assert_deprecated(lambda: np.chararray)
