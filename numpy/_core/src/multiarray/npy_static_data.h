2192904d11674bf06e1082adb86d2fddd7b26735
Othman-vram <othmanelidrissi234@gmail.com>
2025-12-29 20:39:53 +0900

    DOC: correct a/an usage in documentation

--- numpy/_core/src/multiarray/npy_static_data.h
+++ numpy/_core/src/multiarray/npy_static_data.h
@@ -92,7 +92,7 @@ typedef struct npy_static_pydata_struct {
     PyObject *zero_obj;
 
     /*
-     * Reference to an np.array(0, dtype=np.long) instance
+     * Reference to a np.array(0, dtype=np.long) instance
      */
     PyObject *zero_pyint_like_arr;
 
