2a94668d377a6ab1d1fc5dde5a6f2f8571430a3e
Alejandro Candioti <amcandio@gmail.com>
2026-01-05 13:37:01 -0800

    Adding sorter benchmarks

--- benchmarks/benchmarks/bench_searchsorted.py
+++ benchmarks/benchmarks/bench_searchsorted.py
@@ -4,28 +4,32 @@ from .common import Benchmark
 
 
 class SearchSortedInt64(Benchmark):
-    # Benchmark for np.searchsorted with int64 arrays
     params = [
-        # 1B u64 is 8gb
-        [100, 10_000, 1_000_000, 1_000_000_000],       # array sizes
-        [1, 2, 100, 100_000],   # number of query elements
-        ['ordered', 'random'],        # query order
-        [42, 18122022],   # seed
+        [100, 10_000, 1_000_000, 100_000_000],  # array sizes
+        [1, 10, 100_000],                       # number of query elements
+        ['ordered', 'random'],                  # query order
+        [False, True],                          # use sorted
+        [42, 18122022],                         # seed
     ]
-    param_names = ['array_size', 'n_queries', 'query_order', 'seed']
+    param_names = ['array_size', 'n_queries', 'query_order', 'use_sorter', 'seed']
 
-    def setup(self, array_size, n_queries, query_order, seed):
-        self.arr = np.arange(array_size, dtype=np.int64)
+    def setup(self, array_size, n_queries, query_order, use_sorter, seed):
+        self.arr = np.arange(array_size, dtype=np.int32)
 
         rng = np.random.default_rng(seed)
 
         low = -array_size // 10
         high = array_size + array_size // 10
-        self.queries = rng.integers(low, high, size=n_queries, dtype=np.int64)
 
-        # Generate queries
+        self.queries = rng.integers(low, high, size=n_queries, dtype=np.int32)
         if query_order == 'ordered':
             self.queries.sort()
 
-    def time_searchsorted(self, array_size, n_queries, query_order, seed):
-        np.searchsorted(self.arr, self.queries)
+        if use_sorter:
+            rng.shuffle(self.arr)
+            self.sorter = self.arr.argsort()
+        else:
+            self.sorter = None
+
+    def time_searchsorted(self, array_size, n_queries, query_order, use_sorter, seed):
+        np.searchsorted(self.arr, self.queries, sorter=self.sorter)
