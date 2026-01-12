e13d9d51695ca04b3631dfd82fe6a9032ed277bb
吴小白 <296015668@qq.com>
2026-01-12 13:11:34 +0800

    use clang instead of gcc
    Signed-off-by: 吴小白 <296015668@qq.com>

---
+++ tools/install-static-clang.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Stop at any error, show all commands
+set -exuo pipefail
+
+TOOLCHAIN_PATH=/opt/clang
+
+# Download static-clang
+DEFAULT_ARCH="$(uname -m)"
+if [ "${STATIC_CLANG_ARCH:-}" == "" ]; then
+	STATIC_CLANG_ARCH="${RUNNER_ARCH:-${DEFAULT_ARCH}}"
+fi
+case "${STATIC_CLANG_ARCH}" in
+	ARM64|aarch64|arm64|arm64/*) GO_ARCH=arm64;;
+	ARM|armv7l|armv8l|arm|arm/v7) GO_ARCH=arm;;  # assume arm/v7 for arm
+	X64|x86_64|amd64|amd64/*) GO_ARCH=amd64;;
+	X86|i686|386) GO_ARCH=386;;
+	loongarch64) GO_ARCH=loong64;;
+	ppc64le) GO_ARCH=ppc64le;;
+	riscv64) GO_ARCH=riscv64;;
+	s390x) GO_ARCH=s390x;;
+	*) echo "No static-clang toolchain for ${CLANG_ARCH}">2; exit 1;;
+esac
+STATIC_CLANG_VERSION=21.1.6.0
+STATIC_CLANG_FILENAME="static-clang-linux-${GO_ARCH}.tar.xz"
+STATIC_CLANG_URL="https://github.com/mayeut/static-clang-images/releases/download/v${STATIC_CLANG_VERSION}/${STATIC_CLANG_FILENAME}"
+pushd /tmp
+cat<<'EOF' | grep "${STATIC_CLANG_FILENAME}" > "${STATIC_CLANG_FILENAME}.sha256"
+3f92a131d27ca606dae8230550236a0c897a7f5990d61a293814e0abea8d0e1f  static-clang-linux-386.tar.xz
+3fc6a3500cb9514b2c3af6d4a95676842769c301f872b6cea8c15576a64e756c  static-clang-linux-amd64.tar.xz
+82ea0c148ec75f72a2f6f61cc877561efe9675c6e59a1a2c4d130f088f9dc868  static-clang-linux-arm.tar.xz
+9b5ad28877b6d56aff530164f7f88590e5d3441a1fddd7a73370539783056120  static-clang-linux-arm64.tar.xz
+2adccbcad99d033222c8a63872739919375a7aef2339ce2e8ab7dcfc938502b1  static-clang-linux-loong64.tar.xz
+5f551911ad73ecbbcf278e6d05a04bc68bd0dc4918a6a145352072f7734959c6  static-clang-linux-ppc64le.tar.xz
+90f5beda1004bec124607df1f9fc0a70c2b9f382b82ab1db2703ebd131c920ef  static-clang-linux-riscv64.tar.xz
+e4047765a5e64bace4be36f6aae4d859e96bc1298d3ff5ba6b7d6100ea7d23f7  static-clang-linux-s390x.tar.xz
+EOF
+curl -fsSLO "${STATIC_CLANG_URL}"
+sha256sum -c "${STATIC_CLANG_FILENAME}.sha256"
+tar -C /opt -xf "${STATIC_CLANG_FILENAME}"
+popd
+
+# configure target triple
+case "${AUDITWHEEL_POLICY}-${AUDITWHEEL_ARCH}" in
+	manylinux*-armv7l) TARGET_TRIPLE=armv7-unknown-linux-gnueabihf;;
+	musllinux*-armv7l) TARGET_TRIPLE=armv7-alpine-linux-musleabihf;;
+	manylinux*-ppc64le) TARGET_TRIPLE=powerpc64le-unknown-linux-gnu;;
+	musllinux*-ppc64le) TARGET_TRIPLE=powerpc64le-alpine-linux-musl;;
+	manylinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-unknown-linux-gnu;;
+	musllinux*-*) TARGET_TRIPLE=${AUDITWHEEL_ARCH}-alpine-linux-musl;;
+esac
+case "${AUDITWHEEL_POLICY}-${AUDITWHEEL_ARCH}" in
+	*-riscv64) M_ARCH="-march=rv64gc";;
+	*-x86_64) M_ARCH="-march=x86-64";;
+	*-armv7l) M_ARCH="-march=armv7a";;
+	manylinux*-i686) M_ARCH="-march=k8 -mtune=generic";;  # same as gcc manylinux2014 / manylinux_2_28
+	musllinux*-i686) M_ARCH="-march=pentium-m -mtune=generic";;  # same as gcc musllinux_1_2
+esac
+GCC_TRIPLE=$(gcc -dumpmachine)
+
+cat<<EOF >"${TOOLCHAIN_PATH}/bin/${AUDITWHEEL_PLAT}.cfg"
+	-target ${TARGET_TRIPLE}
+	${M_ARCH:-}
+	--gcc-toolchain=${DEVTOOLSET_ROOTPATH:-}/usr
+	--gcc-triple=${GCC_TRIPLE}
+EOF
+
+cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang.cfg"
+	@${AUDITWHEEL_PLAT}.cfg
+EOF
+
+cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang++.cfg"
+	@${AUDITWHEEL_PLAT}.cfg
+EOF
+
+cat<<EOF >"${TOOLCHAIN_PATH}/bin/clang-cpp.cfg"
+	@${AUDITWHEEL_PLAT}.cfg
+EOF
+
+# override entrypoint to add the toolchain to PATH
+mv /usr/local/bin/manylinux-entrypoint /usr/local/bin/manylinux-entrypoint-org
+cat<<EOF >/usr/local/bin/manylinux-entrypoint
+#!/bin/bash
+
+set -eu
+
+export PATH="${TOOLCHAIN_PATH}/bin:\${PATH}"
+exec /usr/local/bin/manylinux-entrypoint-org "\$@"
+EOF
+
+chmod +x /usr/local/bin/manylinux-entrypoint
