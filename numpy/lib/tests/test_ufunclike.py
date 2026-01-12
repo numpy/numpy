4420e94a8f60715f5ab2829f16c04b875143f15c
skyvanguard <skyvanguard@gmail.com>
2026-01-12 19:35:07 -0300

    DEP: Deprecate numpy.fix in favor of numpy.trunc
    Deprecate `numpy.fix` as it provides identical functionality to
    `numpy.trunc` but is slower and is not part of the Array API standard.
    
    - Add DeprecationWarning to numpy.fix()
    - Update docstring with .. deprecated:: directive
    - Add tests to verify deprecation warning is emitted
    - Update existing tests to suppress deprecation warnings
    - Add release note about deprecation
    
    Closes #30096

--- numpy/lib/tests/test_ufunclike.py
+++ numpy/lib/tests/test_ufunclike.py
@@ -1,3 +1,7 @@
+import warnings
+
+import pytest
+
 import numpy as np
 from numpy import fix, isneginf, isposinf
 from numpy.testing import assert_, assert_array_equal, assert_equal, assert_raises
@@ -40,12 +44,14 @@ class TestUfunclike:
         out = np.zeros(a.shape, float)
         tgt = np.array([[1., 1., 1., 1.], [-1., -1., -1., -1.]])
 
-        res = fix(a)
-        assert_equal(res, tgt)
-        res = fix(a, out)
-        assert_equal(res, tgt)
-        assert_equal(out, tgt)
-        assert_equal(fix(3.14), 3)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", DeprecationWarning)
+            res = fix(a)
+            assert_equal(res, tgt)
+            res = fix(a, out)
+            assert_equal(res, tgt)
+            assert_equal(out, tgt)
+            assert_equal(fix(3.14), 3)
 
     def test_fix_with_subclass(self):
         class MyArray(np.ndarray):
@@ -67,17 +73,19 @@ class TestUfunclike:
 
         a = np.array([1.1, -1.1])
         m = MyArray(a, metadata='foo')
-        f = fix(m)
-        assert_array_equal(f, np.array([1, -1]))
-        assert_(isinstance(f, MyArray))
-        assert_equal(f.metadata, 'foo')
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", DeprecationWarning)
+            f = fix(m)
+            assert_array_equal(f, np.array([1, -1]))
+            assert_(isinstance(f, MyArray))
+            assert_equal(f.metadata, 'foo')
 
-        # check 0d arrays don't decay to scalars
-        m0d = m[0, ...]
-        m0d.metadata = 'bar'
-        f0d = fix(m0d)
-        assert_(isinstance(f0d, MyArray))
-        assert_equal(f0d.metadata, 'bar')
+            # check 0d arrays don't decay to scalars
+            m0d = m[0, ...]
+            m0d.metadata = 'bar'
+            f0d = fix(m0d)
+            assert_(isinstance(f0d, MyArray))
+            assert_equal(f0d.metadata, 'bar')
 
     def test_scalar(self):
         x = np.inf
@@ -87,11 +95,32 @@ class TestUfunclike:
         assert_equal(type(actual), type(expected))
 
         x = -3.4
-        actual = np.fix(x)
-        expected = np.float64(-3.0)
-        assert_equal(actual, expected)
-        assert_equal(type(actual), type(expected))
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", DeprecationWarning)
+            actual = np.fix(x)
+            expected = np.float64(-3.0)
+            assert_equal(actual, expected)
+            assert_equal(type(actual), type(expected))
 
-        out = np.array(0.0)
-        actual = np.fix(x, out=out)
-        assert_(actual is out)
+            out = np.array(0.0)
+            actual = np.fix(x, out=out)
+            assert_(actual is out)
+
+
+class TestFixDeprecation:
+    """Test that numpy.fix emits a DeprecationWarning."""
+
+    def test_fix_emits_deprecation_warning(self):
+        a = np.array([1.5, 2.7, -1.5, -2.7])
+        with pytest.warns(DeprecationWarning, match="numpy.fix is deprecated"):
+            fix(a)
+
+    def test_fix_scalar_emits_deprecation_warning(self):
+        with pytest.warns(DeprecationWarning, match="numpy.fix is deprecated"):
+            fix(3.14)
+
+    def test_fix_with_out_emits_deprecation_warning(self):
+        a = np.array([1.5, 2.7])
+        out = np.zeros(a.shape)
+        with pytest.warns(DeprecationWarning, match="numpy.fix is deprecated"):
+            fix(a, out=out)
