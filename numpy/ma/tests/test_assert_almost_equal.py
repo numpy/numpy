2f65219d52852e31645c057d7345aff402b19644
Gamila <gamilawisam1710@gmail.com>
2026-01-22 17:26:21 +0200

    Fix import order and blank line is test

--- numpy/ma/tests/test_assert_almost_equal.py
+++ numpy/ma/tests/test_assert_almost_equal.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 from numpy.ma.testutils import assert_almost_equal
 
+
 def test_assert_almost_equal_decimal_6_fails():
     actual = np.array([0.0])
     desired = np.array([7.7e-7])
