80649360570a65e57d1c867f513790301a914f95
Gamila <gamilawisam1710@gmail.com>
2026-01-22 16:27:14 +0200

    Fix assert_almost_equal tolerance for arrays

---
+++ numpy/ma/tests/test_assert_almost_equal.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pytest
+from numpy.ma.testutils import assert_almost_equal
+
+def test_assert_almost_equal_decimal_6_fails():
+    actual = np.array([0.0])
+    desired = np.array([7.7e-7])
+    with pytest.raises(AssertionError):
+        assert_almost_equal(actual, desired, decimal=6)
