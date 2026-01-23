53edb1d4c335424fde88a6b077b6e606143b5716
Gamila <gamilawisam1710@gmail.com>
2026-01-23 14:53:18 +0200

    BUG: ensure MaskedRecords.view preserves mask for ndarray subclasses

--- numpy/ma/core.py
+++ numpy/ma/core.py
@@ -3276,6 +3276,16 @@ class MaskedArray(ndarray):
         # also make the mask be a view (so attr changes to the view's
         # mask do no affect original object's mask)
         # (especially important to avoid affecting np.masked singleton)
+        # ensure subclass views preserve mask and fill_value
+        
+        if not hasattr(output, "_mask"):
+            output._mask = self._mask
+        if not hasattr(output, "_fill_value"):
+            output._fill_value = self._fill_value
+
+        # also make the mask be a view
+        # (so attr changes to the view's mask do no affect original object's mask)
+
         if getmask(output) is not nomask:
             output._mask = output._mask.view()
 
