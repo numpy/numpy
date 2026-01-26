d8fe753ca0cd5fd2df247e5095ec60e2d92ba417
Matti Picus <matti.picus@gmail.com>
2026-01-26 03:42:56 +0200

    reflow from comments
    Co-authored-by: Joren Hammudoglu <jhammudoglu@gmail.com>

--- numpy/_core/tests/test_umath.py
+++ numpy/_core/tests/test_umath.py
@@ -1961,8 +1961,8 @@ class TestSpecialFloats:
 
 class TestFPClass:
     @pytest.mark.parametrize(
-            "stride",
-            [-5, -4, -3, -2, -1, 1, 2, 4, 5, 6, 7, 8, 9, 10],
+        "stride",
+        [-5, -4, -3, -2, -1, 1, 2, 4, 5, 6, 7, 8, 9, 10],
     )
     def test_fpclass(self, stride):
         arr_f64 = np.array(
@@ -1995,16 +1995,16 @@ class TestFPClass:
             ],
             dtype="f",
         )
-        nan     = np.array(  # noqa: E221
+        nan = np.array(
             [True, True, False, False, False, False, False, False, False, False],
         )
-        inf     = np.array(  # noqa: E221
+        inf = np.array(
             [False, False, True, True, False, False, False, False, False, False],
         )
-        sign    = np.array(  # noqa: E221
+        sign = np.array(
             [False, True, False, True, True, False, True, False, False, True],
         )
-        finite  = np.array(  # noqa: E221
+        finite = np.array(
             [False, False, False, False, True, True, True, True, True, True],
         )
         assert_equal(np.isnan(arr_f32[::stride]), nan[::stride])
