82369166d366e99c8cb372321934dd114dd0c56a
Akshay R <157674473+Akshay-Ramesh-VITC@users.noreply.github.com>
2026-02-05 11:38:18 +0530

    Update __all__ to exclude matrix_inv_mod
    Removed 'matrix_inv_mod' from the exported symbols.

--- numpy/matrixlib/__init__.pyi
+++ numpy/matrixlib/__init__.pyi
@@ -2,4 +2,4 @@ from .defmatrix import asmatrix, bmat, matrix
 
 def matrix_inv_mod(matrix, m): ...
 
-__all__ = ["matrix", "bmat", "asmatrix", "matrix_inv_mod"]
+__all__ = ["matrix", "bmat", "asmatrix"]
