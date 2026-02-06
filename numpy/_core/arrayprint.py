6b1d095a56ee617d3d3802e924df09e39a085dc3
Noxaster <208382900+Noxaster@users.noreply.github.com>
2026-02-06 22:30:01 +0100

    MAINT: Use intermediate variable to be more readable

--- numpy/_core/arrayprint.py
+++ numpy/_core/arrayprint.py
@@ -1415,10 +1415,11 @@ class DatetimeFormat(_TimelikeFormat):
         return super().__call__(x)
 
     def _format_non_nat(self, x):
-        return f"'{datetime_as_string(x,
-                                      unit=self.unit,
-                                      timezone=self.timezone,
-                                      casting=self.casting)}'"
+        datetime_str = datetime_as_string(x,
+                                          unit=self.unit,
+                                          timezone=self.timezone,
+                                          casting=self.casting)
+        return f"'{datetime_str}'"
 
 
 class TimedeltaFormat(_TimelikeFormat):
