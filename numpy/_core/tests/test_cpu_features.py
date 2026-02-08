0576d7c45d3ec766b91f14889da1a7ebfd72376c
Harshith J <abslock128w@gmail.com>
2026-02-08 16:26:26 +0530

    fix POWER VSX test feature mapping

--- numpy/_core/tests/test_cpu_features.py
+++ numpy/_core/tests/test_cpu_features.py
@@ -387,7 +387,7 @@ is_power = re.match(r"^(powerpc|ppc)64", machine, re.IGNORECASE)
 @pytest.mark.skipif(not is_linux or not is_power, reason="Only for Linux and Power")
 class Test_POWER_Features(AbstractTest):
     features = ["VSX", "VSX2", "VSX3", "VSX4"]
-    features_map = {"VSX2": "ARCH_2_07", "VSX3": "ARCH_3_00", "VSX4": "ARCH_3_1"}
+    features_map = { "VSX":  "ARCH_2_06", "VSX2": "ARCH_2_07", "VSX3": "ARCH_3_00", "VSX4": "ARCH_3_1"}
 
     def load_flags(self):
         self.load_flags_auxv()
