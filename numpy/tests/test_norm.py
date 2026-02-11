8a9f9f55f14e65382aea791f209041652308562a
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-02-11 12:19:28 +0100

    Remove license comment

--- numpy/tests/test_norm.py
+++ numpy/tests/test_norm.py
@@ -1,11 +1,3 @@
-# Copyright (c) 2023 - 2026 Chair for Design Automation, TUM
-# Copyright (c) 2025 - 2026 Munich Quantum Software Company GmbH
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
-
 from pathlib import Path
 
 import numpy as np
