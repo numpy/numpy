0cc3e3732876d77e13fcbeb922055a695626555f
Kumar Aditya <kumaraditya@python.org>
2026-02-12 19:50:24 +0530

    enh: make PyDataMem_DefaultHandler immortal in free-threading to reduce refcounting contention

--- numpy/_core/src/multiarray/multiarraymodule.c
+++ numpy/_core/src/multiarray/multiarraymodule.c
@@ -5170,7 +5170,11 @@ _multiarray_umath_exec(PyObject *m) {
     if (PyDataMem_DefaultHandler == NULL) {
         return -1;
     }
-
+#ifdef Py_GIL_DISABLED
+    int rc = PyUnstable_SetImmortal(PyDataMem_DefaultHandler);
+    assert(rc == 1);
+    (void)rc;
+#endif
     /*
      * Initialize the context-local current handler
      * with the default PyDataMem_Handler capsule.
