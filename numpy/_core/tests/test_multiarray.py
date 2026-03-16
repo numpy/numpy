f2c50360bf9f7bb67a91c81e99bec99880dc2472
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-16 13:10:29 -0600

    skip test using subprocesses on wasm

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -6379,6 +6379,7 @@ class TestResize:
         y = x
         assert_raises(ValueError, x.resize, (5, 1))
 
+    @pytest.mark.skipif(IS_WASM, reason="Cannot start subprocess")
     def test_check_reference_module_scope(self):
         code = textwrap.dedent("""
             import numpy as np
