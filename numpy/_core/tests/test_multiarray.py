feeaad6bfc632602bfdac1061cc606f963650187
riku-sakamoto <46015196+riku-sakamoto@users.noreply.github.com>
2026-03-29 19:37:03 +0900

    DEP: Deprecate 'generic' unit in np.timedelta64 (#29619)
    This PR deprecates the generic unit in np.timedelta64. Using this unit can lead to unexpected behavior in some cases. Using generic unit now raises a `DeprecationWarning`.
    
    This also affects some integer/float and timedelta mixing code paths (although that could be changed).
    
    Co-authored-by: Sebastian Berg <sebastianb@nvidia.com>

--- numpy/_core/tests/test_multiarray.py
+++ numpy/_core/tests/test_multiarray.py
@@ -2833,6 +2833,8 @@ class TestMethods:
         for dt in types:
             if dt == 'M':
                 dt = 'M8[D]'
+            if dt == 'm':
+                dt = 'm8[s]'
             if dt == '?':
                 a = np.arange(2, dtype=dt)
                 out = np.arange(2)
@@ -2933,6 +2935,8 @@ class TestMethods:
         for dt in types:
             if dt == 'M':
                 dt = 'M8[D]'
+            if dt == 'm':
+                dt = 'm8[s]'
             if dt == '?':
                 a = np.array([1, 0], dtype=dt)
                 # We want the sorter array to be of a type that is different
