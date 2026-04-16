05c73f91d6b57279c8f8e5d8a7b1839659d27cf0
Kumar Aditya <kumaraditya@python.org>
2026-04-16 10:04:58 +0530

    revert unnecessary change

--- numpy/_core/code_generators/generate_numpy_api.py
+++ numpy/_core/code_generators/generate_numpy_api.py
@@ -58,7 +58,7 @@ static int PyArray_RUNTIME_VERSION = 0;
 #include "numpy/_public_dtype_api_table.h"
 
 #if !defined(NO_IMPORT_ARRAY) && !defined(NO_IMPORT)
-static inline int
+static int
 _import_array(void)
 {
   int st;
