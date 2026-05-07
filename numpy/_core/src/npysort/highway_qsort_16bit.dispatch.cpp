3f695ff3af3a0f21a54c790f85e27d9695ee835e
Maanas Arora <maanasarora23@gmail.com>
2026-05-07 06:34:40 -0400

    Fix missing argument (descending)

--- numpy/_core/src/npysort/highway_qsort_16bit.dispatch.cpp
+++ numpy/_core/src/npysort/highway_qsort_16bit.dispatch.cpp
@@ -7,27 +7,31 @@
 
 namespace np::highway::qsort_simd {
 template <typename T>
-void NPY_CPU_DISPATCH_CURFX(QSort)(T *arr, npy_intp size)
+void NPY_CPU_DISPATCH_CURFX(QSort)(T *arr, npy_intp size, bool descending)
 {
 #if VQSORT_ENABLED
     using THwy = std::conditional_t<std::is_same_v<T, Half>, hwy::float16_t, T>;
-    hwy::HWY_NAMESPACE::VQSortStatic(reinterpret_cast<THwy*>(arr), size, hwy::SortAscending());
+    if (descending) {
+        hwy::HWY_NAMESPACE::VQSortStatic(reinterpret_cast<THwy*>(arr), size, hwy::SortDescending());
+    } else {
+        hwy::HWY_NAMESPACE::VQSortStatic(reinterpret_cast<THwy*>(arr), size, hwy::SortAscending());
+    }
 #else
     sort::Quick(arr, size);
 #endif
 }
 #if !HWY_HAVE_FLOAT16
 template <>
-void NPY_CPU_DISPATCH_CURFX(QSort)<Half>(Half *arr, npy_intp size)
+void NPY_CPU_DISPATCH_CURFX(QSort)<Half>(Half *arr, npy_intp size, bool descending)
 {
     sort::Quick(arr, size);
 }
 #endif // !HWY_HAVE_FLOAT16
 
-template void NPY_CPU_DISPATCH_CURFX(QSort)<int16_t>(int16_t*, npy_intp);
-template void NPY_CPU_DISPATCH_CURFX(QSort)<uint16_t>(uint16_t*, npy_intp);
+template void NPY_CPU_DISPATCH_CURFX(QSort)<int16_t>(int16_t*, npy_intp, bool);
+template void NPY_CPU_DISPATCH_CURFX(QSort)<uint16_t>(uint16_t*, npy_intp, bool);
 #if HWY_HAVE_FLOAT16
-template void NPY_CPU_DISPATCH_CURFX(QSort)<Half>(Half*, npy_intp);
+template void NPY_CPU_DISPATCH_CURFX(QSort)<Half>(Half*, npy_intp, bool);
 #endif
 
 } // np::highway::qsort_simd
