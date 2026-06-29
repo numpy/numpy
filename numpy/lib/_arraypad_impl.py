2d59c157b788157bc1c3f2b4b232cbf2a37ac6da
Kevin Turcios <turcioskevinr@gmail.com>
2026-06-29 09:29:10 -0500

    MAINT: Avoid repeated zero pad-width check

--- numpy/lib/_arraypad_impl.py
+++ numpy/lib/_arraypad_impl.py
@@ -837,12 +837,13 @@ def pad(array, pad_width, mode='constant', **kwargs):
     stat_functions = {"maximum": np.amax, "minimum": np.amin,
                       "mean": np.mean, "median": np.median}
 
-    if mode == "linear_ramp" and not np.any(pad_width):
+    zero_pad_width = not np.any(pad_width)
+    if mode == "linear_ramp" and zero_pad_width:
         _as_pairs(kwargs.get("end_values", 0), array.ndim)
         order = 'F' if array.flags.fnc else 'C'
         return array.copy(order=order)
 
-    if mode in stat_functions and not np.any(pad_width):
+    if mode in stat_functions and zero_pad_width:
         length = _as_pairs(
             kwargs.get("stat_length"), array.ndim, as_index=True)
         has_zero_length = array.size and any(
