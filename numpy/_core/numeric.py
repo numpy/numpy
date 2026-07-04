f32e58cd164d00c4b6e8b041e842317f90e0add1
Iason Krommydas <iason.krom@gmail.com>
2026-07-04 00:26:13 -0500

    MNT: a.astype(np.bool) should work not for count_nonzero
    Signed-off-by: Iason Krommydas <iason.krom@gmail.com>

--- numpy/_core/numeric.py
+++ numpy/_core/numeric.py
@@ -535,13 +535,7 @@ def count_nonzero(a, axis=None, *, keepdims=False):
         return multiarray.count_nonzero(a)
 
     a = asanyarray(a)
-
-    # TODO: this works around .astype(bool) not working properly (gh-9847)
-    if np.issubdtype(a.dtype, np.character):
-        a_bool = a != a.dtype.type()
-    else:
-        a_bool = a.astype(np.bool, copy=False)
-
+    a_bool = a.astype(np.bool, copy=False)
     return a_bool.sum(axis=axis, dtype=np.intp, keepdims=keepdims)
 
 
