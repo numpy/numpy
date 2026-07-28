BUG: avoid possible stack overflow in arraydescr_dealloc

numpy/_core/tests/test_dtype.py
@@ -5,6 +5,7 @@ import operator
 import os
 import pickle
 import sys
+import textwrap
 import types
 import warnings
 from itertools import permutations
@@ -20,13 +21,14 @@ from numpy._core._multiarray_tests import create_custom_field_dtype
 from numpy._core._rational_tests import rational, rational2
 from numpy.testing import (
     HAS_REFCOUNT,
+    HAS_SUBPROCESSES,
     IS_64BIT,
     assert_,
     assert_array_equal,
     assert_equal,
     assert_raises,
 )
-from numpy.testing._private.utils import requires_deep_recursion
+from numpy.testing._private.utils import requires_deep_recursion, run_subprocess
 
 
 def assert_dtype_equal(a, b):
@@ -888,10 +890,6 @@ class TestMonsterType:
             np.dtype(l)
 
     @requires_deep_recursion
-    @pytest.mark.skipif(
-        sys.platform.startswith("darwin"),
-        reason="test now segfaults on some mac setups",
-    )
     def test_tuple_recursion(self):
         d = np.int32
         for i in range(100000):
@@ -900,6 +898,29 @@ class TestMonsterType:
         # see gh-30370 and cpython issue #142253
         with contextlib.suppress(RecursionError):
             np.dtype(d)
+
+    @pytest.mark.skipif(not HAS_SUBPROCESSES,
+                        reason="platform cannot start subprocesses")
+    def test_deep_subarray_dtype_dealloc(self):
+        script = textwrap.dedent("""
+            import threading
+
+            import numpy as np
+
+            def build_and_drop():
+                d = np.dtype(np.int32)
+                for _ in range(200000):
+                    d = np.dtype((d, (1,)))
+                held = d.base
+                del d
+                del held
+
+            threading.stack_size(1024 * 1024)
+            t = threading.Thread(target=build_and_drop)
+            t.start()
+            t.join()
+        """)
+        run_subprocess([sys.executable, "-c", script], timeout=180)
 
     @requires_deep_recursion
     def test_dict_recursion(self):