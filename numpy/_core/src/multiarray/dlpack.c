0b86fcd29a34ef2d563ffad0fd9a96499a702c5a
Maanas Arora <maanasarora23@gmail.com>
2026-07-31 17:08:45 -0400

    Adopt suggestion from Nathan to assert scalars are exported versioned
    Co-authored-by: Nathan Goldbaum <nathan.goldbaum@gmail.com>

--- numpy/_core/src/multiarray/dlpack.c
+++ numpy/_core/src/multiarray/dlpack.c
@@ -420,6 +420,9 @@ create_dlpack_capsule(PyObject *scalar, PyArrayObject *array,
     int ndim = array == NULL ? 0 : PyArray_NDIM(array);
     PyObject *self = array == NULL ? scalar : (PyObject *)array;
 
+    /* Scalars are exported read-only, which only versioned capsules can signal. */
+    assert(array != NULL || versioned);
+ 
     /*
      * We align shape and strides at the end but need to align them, offset
      * gives the offset of the shape (and strides) including the struct size.
