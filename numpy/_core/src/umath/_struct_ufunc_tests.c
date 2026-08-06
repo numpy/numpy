246ebad017ee5ae642f39917ec040e33fe3ec34d
prathamhole14 <prathamhole@gmail.com>
2026-08-06 14:39:35 +0530

    ENH: Add limited API support for `_struct_ufunc_tests` module

--- numpy/_core/src/umath/_struct_ufunc_tests.c
+++ numpy/_core/src/umath/_struct_ufunc_tests.c
@@ -2,6 +2,15 @@
 #include <Python.h>
 
 #define NPY_NO_DEPRECATED_API NPY_API_VERSION
+
+/* Build as a downstream module: the internal `PyArray_DTypeMeta` is built on
+ * `PyHeapTypeObject`, which is not in the limited API.  `NPY_TARGET_VERSION`
+ * keeps the API target that `NPY_INTERNAL_BUILD` implied. */
+#if defined(NPY_INTERNAL_BUILD)
+#undef NPY_INTERNAL_BUILD
+#endif
+#define NPY_TARGET_VERSION NPY_API_VERSION
+
 #include "numpy/ndarraytypes.h"
 #include "numpy/ufuncobject.h"
 #include "numpy/npy_3kcompat.h"
@@ -157,7 +166,7 @@ PyMODINIT_FUNC PyInit__struct_ufunc_tests(void)
     PyDict_SetItemString(d, "add_triplet", add_triplet);
     Py_DECREF(add_triplet);
 
-#ifdef Py_GIL_DISABLED
+#if defined(Py_GIL_DISABLED) && !defined(Py_LIMITED_API)
     // signal this module supports running with the GIL disabled
     PyUnstable_Module_SetGIL(m, Py_MOD_GIL_NOT_USED);
 #endif
