5e449365f20117f94b4888ae3c919c3e95d34f20
hariomloharapps <hariomlohar.new@gmail.com>
2026-08-07 21:53:59 +0530

    FIX: Handle overflow in i0 for large inputs like 713.0 Using math log this overflow problem is solved  issue #32209

--- numpy/lib/_function_base_impl.py
+++ numpy/lib/_function_base_impl.py
@@ -43,6 +43,7 @@ from numpy._core.umath import (
     arctan2,
     cos,
     exp,
+    log,
     floor,
     frompyfunc,
     less_equal,
@@ -3562,7 +3563,7 @@ def _i0_1(x):
 
 
 def _i0_2(x):
-    return exp(x) * _chbevl(32.0 / x - 2.0, _i0B) / sqrt(x)
+    return exp(x + log(_chbevl(32.0 / x - 2.0, _i0B)) - 0.5 * log(x))
 
 
 def _i0_dispatcher(x):
