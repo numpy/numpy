d86651dceed2baf2768442365a043f19b016177c
Iason Krommydas <iason.krom@gmail.com>
2026-09-10 23:01:22 +0200

    MNT: fallback in ptp
    Signed-off-by: Iason Krommydas <iason.krom@gmail.com>

--- numpy/_core/_methods.py
+++ numpy/_core/_methods.py
@@ -230,9 +230,13 @@ def _std(a, axis=None, dtype=None, out=None, ddof=0, keepdims=False, *,
     return ret
 
 def _ptp(a, axis=None, out=None, keepdims=False):
-    min_value, max_value = umr_minimummaximum(
-        a, axis, None, (None, out), keepdims
-    )
+    try:
+        min_value, max_value = umr_minimummaximum(
+            a, axis, None, (None, out), keepdims
+        )
+    except TypeError:
+        min_value = umr_minimum(a, axis, None, None, keepdims)
+        max_value = umr_maximum(a, axis, None, out, keepdims)
     return um.subtract(max_value, min_value, out)
 
 def _dump(self, file, protocol=2):
