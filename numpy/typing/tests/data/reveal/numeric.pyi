ed85518fdac457349837d68fe00d456e951fed9e
jorenham <jhammudoglu@gmail.com>
2026-09-10 16:38:20 +0200

    TYP: ``count_nonzero`` shape-typing

--- numpy/typing/tests/data/reveal/numeric.pyi
+++ numpy/typing/tests/data/reveal/numeric.pyi
@@ -17,6 +17,11 @@ i8: np.int64
 AR_b: npt.NDArray[np.bool]
 AR_u8: npt.NDArray[np.uint64]
 AR_i8: npt.NDArray[np.int64]
+AR_i8_0d: np.ndarray[tuple[()], np.dtype[np.int64]]
+AR_i8_1d: np.ndarray[tuple[int], np.dtype[np.int64]]
+AR_i8_2d: np.ndarray[tuple[int, int], np.dtype[np.int64]]
+AR_i8_3d: np.ndarray[tuple[int, int, int], np.dtype[np.int64]]
+AR_i8_4d: np.ndarray[tuple[int, int, int, int], np.dtype[np.int64]]
 AR_f8: npt.NDArray[np.float64]
 AR_c16: npt.NDArray[np.complex128]
 AR_m: npt.NDArray[np.timedelta64]
@@ -34,9 +39,20 @@ _to_1d_complex: list[complex]
 
 assert_type(np.count_nonzero(i8), np.intp)
 assert_type(np.count_nonzero(AR_i8), np.intp)
-assert_type(np.count_nonzero(_to_1d_int), np.intp)
+assert_type(np.count_nonzero(AR_i8, axis=0), npt.NDArray[np.intp] | Any)
+assert_type(np.count_nonzero(AR_i8_1d, axis=0), np.intp)
+assert_type(np.count_nonzero(AR_i8_2d, axis=0), np.ndarray[tuple[int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(AR_i8_3d, axis=(0,)), np.ndarray[tuple[int, int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(AR_i8_4d, axis=1), np.ndarray[tuple[int, int, int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(AR_i8_0d, keepdims=True), np.intp)
 assert_type(np.count_nonzero(AR_i8, keepdims=True), npt.NDArray[np.intp])
-assert_type(np.count_nonzero(AR_i8, axis=0), Any)
+assert_type(np.count_nonzero(AR_i8_2d, keepdims=True), np.ndarray[tuple[int, int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(AR_i8_2d, axis=(0, 1)), npt.NDArray[np.intp] | Any)
+assert_type(np.count_nonzero(_to_1d_int), np.intp)
+assert_type(np.count_nonzero(_to_1d_int, axis=0), np.intp)
+assert_type(np.count_nonzero(_to_2d_float, axis=0), np.ndarray[tuple[int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(_to_1d_int, keepdims=True), np.ndarray[tuple[int], np.dtype[np.intp]])
+assert_type(np.count_nonzero(_to_2d_float, keepdims=True), np.ndarray[tuple[int, int], np.dtype[np.intp]])
 
 assert_type(np.isfortran(i8), bool)
 assert_type(np.isfortran(AR_i8), bool)
