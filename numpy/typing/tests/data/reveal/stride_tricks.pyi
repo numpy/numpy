e49390c87f8194d54cd5f708eb15c946e6dfcfa2
Joren Hammudoglu <jhammudoglu@gmail.com>
2026-09-14 19:30:19 +0200

    TYP: ``broadcast_arrays`` improved shape-typing (#32612)

--- numpy/typing/tests/data/reveal/stride_tricks.pyi
+++ numpy/typing/tests/data/reveal/stride_tricks.pyi
@@ -3,8 +3,18 @@ from typing import Any, assert_type
 import numpy as np
 import numpy.typing as npt
 
+type _Array1D[ScalarT: np.generic] = np.ndarray[tuple[int], np.dtype[ScalarT]]
+type _Array2D[ScalarT: np.generic] = np.ndarray[tuple[int, int], np.dtype[ScalarT]]
+type _Array3D[ScalarT: np.generic] = np.ndarray[tuple[int, int, int], np.dtype[ScalarT]]
+
 AR_f8: npt.NDArray[np.float64]
 AR_LIKE_f: list[float]
+AR_f8_2d: np.ndarray[tuple[int, int], np.dtype[np.float64]]
+AR_i8_1d: np.ndarray[tuple[int], np.dtype[np.int64]]
+AR_i8_2d: np.ndarray[tuple[int, int], np.dtype[np.int64]]
+AR_i8_3d: np.ndarray[tuple[int, int, int], np.dtype[np.int64]]
+AR_i8_4d: np.ndarray[tuple[int, int, int, int], np.dtype[np.int64]]
+f8: np.float64
 interface_dict: dict[str, Any]
 
 assert_type(np.lib.stride_tricks.as_strided(AR_f8), npt.NDArray[np.float64])
@@ -32,5 +42,21 @@ assert_type(np.broadcast_to(AR_LIKE_f, [1, 2]), npt.NDArray[Any])
 assert_type(np.broadcast_shapes((1, 2), [3, 1], (3, 2)), tuple[Any, ...])
 assert_type(np.broadcast_shapes((6, 7), (5, 6, 1), 7, (5, 1, 7)), tuple[Any, ...])
 
-assert_type(np.broadcast_arrays(AR_f8, AR_f8), tuple[npt.NDArray[Any], ...])
-assert_type(np.broadcast_arrays(AR_f8, AR_LIKE_f), tuple[npt.NDArray[Any], ...])
+assert_type(np.broadcast_arrays(), tuple[()])
+assert_type(np.broadcast_arrays(AR_f8_2d), tuple[_Array2D[np.float64]])
+assert_type(np.broadcast_arrays(AR_LIKE_f), tuple[npt.NDArray[Any]])
+assert_type(np.broadcast_arrays(AR_f8, AR_i8_2d), tuple[npt.NDArray[np.float64], npt.NDArray[np.int64]])
+assert_type(np.broadcast_arrays(AR_i8_2d, AR_f8), tuple[npt.NDArray[np.int64], npt.NDArray[np.float64]])
+assert_type(np.broadcast_arrays(AR_i8_1d, AR_i8_1d), tuple[_Array1D[np.int64], _Array1D[np.int64]])
+assert_type(np.broadcast_arrays(AR_i8_1d, AR_f8_2d), tuple[_Array2D[np.int64], _Array2D[np.float64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, AR_i8_1d), tuple[_Array2D[np.float64], _Array2D[np.int64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, AR_i8_3d), tuple[_Array3D[np.float64], _Array3D[np.int64]])
+assert_type(np.broadcast_arrays(AR_i8_3d, AR_f8_2d), tuple[_Array3D[np.int64], _Array3D[np.float64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, f8), tuple[_Array2D[np.float64], _Array2D[np.float64]])
+assert_type(np.broadcast_arrays(f8, AR_i8_2d), tuple[_Array2D[np.float64], _Array2D[np.int64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, 2.0), tuple[_Array2D[np.float64], _Array2D[Any]])
+assert_type(np.broadcast_arrays(2.0, AR_i8_2d), tuple[_Array2D[Any], _Array2D[np.int64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, AR_i8_4d), tuple[npt.NDArray[np.float64], npt.NDArray[np.int64]])
+assert_type(np.broadcast_arrays(AR_f8_2d, AR_LIKE_f), tuple[npt.NDArray[Any], npt.NDArray[Any]])
+assert_type(np.broadcast_arrays(AR_f8, AR_f8, AR_f8), tuple[npt.NDArray[np.float64], ...])
+assert_type(np.broadcast_arrays(AR_f8, AR_LIKE_f, AR_f8), tuple[npt.NDArray[Any], ...])
