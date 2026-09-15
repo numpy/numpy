c4a911ba9b5b36d93ed28e4b9d38210c16df9324
Joren Hammudoglu <jhammudoglu@gmail.com>
2026-09-15 16:02:24 +0200

    TYP: ``diagonal`` improved shape-typing (#32628)

--- numpy/typing/tests/data/reveal/fromnumeric.pyi
+++ numpy/typing/tests/data/reveal/fromnumeric.pyi
@@ -254,6 +254,7 @@ assert_type(np.diagonal(AR_b), npt.NDArray[np.bool])
 assert_type(np.diagonal(AR_f4), npt.NDArray[np.float32])
 assert_type(np.diagonal(AR_f4_2d), np.ndarray[tuple[int], np.dtype[np.float32]])
 assert_type(np.diagonal(AR_f4_3d), np.ndarray[tuple[int, int], np.dtype[np.float32]])
+assert_type(np.diagonal(AR_f4_4d), _Array3D[np.float32])
 
 assert_type(np.trace(AR_f4), npt.NDArray[np.float32] | Any)
 assert_type(np.trace(AR_i8), npt.NDArray[np.int_] | Any)
