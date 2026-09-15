5ad19469fb2c0c3cd477dc7f941bf49cdac8f7b9
jorenham <jhammudoglu@gmail.com>
2026-09-15 14:41:55 +0200

    TYP: ``require`` improved shape-typing

--- numpy/_core/_asarray.pyi
+++ numpy/_core/_asarray.pyi
@@ -1,7 +1,8 @@
 from collections.abc import Iterable
 from typing import Any, Literal, overload
 
-from numpy._typing import DTypeLike, NDArray, _SupportsArrayFunc
+import numpy as np
+from numpy._typing import DTypeLike, NDArray, _DTypeLike, _Shape, _SupportsArrayFunc
 
 __all__ = ["require"]
 
@@ -10,32 +11,58 @@ type _Requirements = Literal[
     "F", "F_CONTIGUOUS", "FORTRAN",
     "A", "ALIGNED",
     "W", "WRITEABLE",
-    "O", "OWNDATA"
-]
+    "O", "OWNDATA",
+]  # fmt: skip
 type _E = Literal["E", "ENSUREARRAY"]
 type _RequirementsWithE = _Requirements | _E
 
-@overload
+###
+
+@overload  # Nd T
 def require[ArrayT: NDArray[Any]](
     a: ArrayT,
     dtype: None = None,
     requirements: _Requirements | Iterable[_Requirements] | None = None,
     *,
-    like: _SupportsArrayFunc | None = None
+    like: _SupportsArrayFunc | None = None,
 ) -> ArrayT: ...
-@overload
-def require(
-    a: object,
-    dtype: DTypeLike | None = None,
-    requirements: _E | Iterable[_RequirementsWithE] | None = None,
-    *,
-    like: _SupportsArrayFunc | None = None
-) -> NDArray[Any]: ...
-@overload
-def require(
-    a: object,
-    dtype: DTypeLike | None = None,
+@overload  # Nd T, dtype=<known>
+def require[ShapeT: _Shape, ScalarT: np.generic](
+    a: np.ndarray[ShapeT, Any],
+    dtype: _DTypeLike[ScalarT],
     requirements: _Requirements | Iterable[_Requirements] | None = None,
     *,
-    like: _SupportsArrayFunc | None = None
+    like: _SupportsArrayFunc | None = None,
+) -> np.ndarray[ShapeT, np.dtype[ScalarT]]: ...
+@overload  # Nd T, dtype=<unknown>
+def require[ShapeT: _Shape](
+    a: np.ndarray[ShapeT, Any],
+    dtype: DTypeLike,
+    requirements: _Requirements | Iterable[_Requirements] | None = None,
+    *,
+    like: _SupportsArrayFunc | None = None,
+) -> np.ndarray[ShapeT, np.dtype[Any]]: ...
+@overload  # Nd T, requirements=E
+def require[ShapeT: _Shape, DTypeT: np.dtype](
+    a: np.ndarray[ShapeT, DTypeT],
+    dtype: None = None,
+    requirements: _E | Iterable[_RequirementsWithE] | None = None,
+    *,
+    like: _SupportsArrayFunc | None = None,
+) -> np.ndarray[ShapeT, DTypeT]: ...
+@overload  # ?d, dtype=<known>
+def require[ScalarT: np.generic](
+    a: object,
+    dtype: _DTypeLike[ScalarT],
+    requirements: _Requirements | Iterable[_Requirements] | None = None,
+    *,
+    like: _SupportsArrayFunc | None = None,
+) -> NDArray[ScalarT]: ...
+@overload  # ?d  (fallback)
+def require(
+    a: object,
+    dtype: DTypeLike | None = None,
+    requirements: _RequirementsWithE | Iterable[_RequirementsWithE] | None = None,
+    *,
+    like: _SupportsArrayFunc | None = None,
 ) -> NDArray[Any]: ...
