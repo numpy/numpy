a174d0d30d323d24d7551aa35d0f893b1ae17775
Matt Haberland <matt.haberland@gmail.com>
2026-09-18 22:34:31 -0700

    BUG: avoid clog warning for NaN components

--- numpy/_core/tests/test_umath_complex.py
+++ numpy/_core/tests/test_umath_complex.py
@@ -151,6 +151,15 @@ class TestClog:
         assert np.isnan(y.real).all()
         assert np.isnan(y.imag).all()
 
+    @pytest.mark.parametrize("dtype", [np.complex64, np.complex128,
+                                       np.clongdouble])
+    def test_nan_real_nan_imag(self, dtype):
+        x = np.array([complex(np.nan, np.nan)], dtype=dtype)
+        with np.errstate(invalid='raise'):
+            y = np.log(x)
+        assert np.isnan(y.real).all()
+        assert np.isnan(y.imag).all()
+
     @platform_skip
     @pytest.mark.skipif(platform.machine() == "armv5tel", reason="See gh-413.")
     def test_special_values(self):
