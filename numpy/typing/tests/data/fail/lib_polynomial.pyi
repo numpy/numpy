c2f30fedf5b2b1900869d4d5ae2a0f847308a029
Joren Hammudoglu <jhammudoglu@gmail.com>
2026-09-21 17:50:16 +0200

    TYP: ``poly{add,sub}`` shape-typing and improved dtypes (#32684)

--- numpy/typing/tests/data/fail/lib_polynomial.pyi
+++ numpy/typing/tests/data/fail/lib_polynomial.pyi
@@ -27,5 +27,5 @@ np.polyfit(AR_f8, AR_f8, 1, w=AR_c16)  # type: ignore[arg-type]
 np.polyfit(AR_f8, AR_f8, 1, cov="bob")  # type: ignore[call-overload]
 
 np.polyval(AR_f8, AR_U)  # type: ignore[type-var]
-np.polyadd(AR_f8, AR_U)  # type: ignore[arg-type]
-np.polysub(AR_f8, AR_U)  # type: ignore[arg-type]
+np.polyadd(AR_f8, AR_U)  # type: ignore[type-var]
+np.polysub(AR_f8, AR_U)  # type: ignore[type-var]
