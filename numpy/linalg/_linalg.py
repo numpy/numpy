84cd9b6d16c4af99c0dff9af57c11acf2f6b3992
Yeonho Kim <168209377+danggeun@users.noreply.github.com>
2026-09-22 03:13:50 +0900

    BUG: linalg.pinv: use the computation dtype for rtol=None and empty input (#32679)
    Co-authored-by: Yeonho Kim <168209377+carrot-dev01@users.noreply.github.com>

--- numpy/linalg/_linalg.py
+++ numpy/linalg/_linalg.py
@@ -2229,7 +2229,8 @@ def pinv(a, rcond=None, hermitian=False, *, rtol=_NoValue):
         if rtol is _NoValue:
             rcond = 1e-15
         elif rtol is None:
-            rcond = max(a.shape[-2:]) * finfo(a.dtype).eps
+            result_t = _commonType(a)[1] if a.dtype.kind in "biu" else a.dtype
+            rcond = max(a.shape[-2:]) * finfo(result_t).eps
         else:
             rcond = rtol
     elif rtol is not _NoValue:
@@ -2241,7 +2242,8 @@ def pinv(a, rcond=None, hermitian=False, *, rtol=_NoValue):
     rcond = asarray(rcond)
     if _is_empty_2d(a):
         m, n = a.shape[-2:]
-        res = empty(a.shape[:-2] + (n, m), dtype=a.dtype)
+        result_t = _commonType(a)[1] if a.dtype.kind in "biu" else a.dtype
+        res = empty(a.shape[:-2] + (n, m), dtype=result_t)
         return wrap(res)
     a = a.conjugate()
     u, s, vt = svd(a, full_matrices=False, hermitian=hermitian)
