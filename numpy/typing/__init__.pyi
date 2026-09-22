0d2e3ab63aba9756ebf437c2d32e28316dda3922
Ganesh Kathiresan <ganesh3597@gmail.com>
2026-09-22 12:04:04 -0500

    MAINT: Undo linting changes

--- numpy/typing/__init__.pyi
+++ numpy/typing/__init__.pyi
@@ -1,12 +1,7 @@
 from typing import Final
 
 from numpy._pytesttester import PytestTester
-from numpy._typing import (  # type: ignore[deprecated]
-    ArrayLike,
-    DTypeLike,
-    NBitBase,
-    NDArray,
-)
+from numpy._typing import ArrayLike, DTypeLike, NBitBase, NDArray  # type: ignore[deprecated]
 
 __all__ = ["ArrayLike", "DTypeLike", "NBitBase", "NDArray"]
 
