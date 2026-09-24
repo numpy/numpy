2b20e2b90da86c0aa4f12c568ac044b20a1442b3
Joren Hammudoglu <jhammudoglu@gmail.com>
2026-09-24 20:13:25 +0200

    TYP: ``polynomial.*.{coef,linspace}`` gradual dtypes (#32779)

--- numpy/polynomial/_polybase.pyi
+++ numpy/polynomial/_polybase.pyi
@@ -56,7 +56,7 @@ class ABCPolyBase(Generic[_NameT_co], abc.ABC):  # noqa: UP046
     @abc.abstractmethod
     def basis_name(self) -> _NameT_co: ...
 
-    coef: _CoefSeries
+    coef: _Series[np.float64 | Any]
 
     def __init__(
         self,
@@ -168,7 +168,7 @@ class ABCPolyBase(Generic[_NameT_co], abc.ABC):  # noqa: UP046
         /,
         n: SupportsIndex = 100,
         domain: _SeriesLikeCoef_co | None = None,
-    ) -> _Tuple2[_Series[np.float64 | np.complex128]]: ...
+    ) -> _Tuple2[_Series[np.float64 | Any]]: ...
 
     #
     @overload
--- numpy/typing/tests/data/reveal/polynomial_polybase.pyi
+++ numpy/typing/tests/data/reveal/polynomial_polybase.pyi
@@ -98,7 +98,7 @@ assert_type(type(PS_herme).cast(PS_leg), npp.HermiteE)
 
 # attributes / properties
 
-assert_type(PS_all.coef, _Ar_x_n)
+assert_type(PS_all.coef, np.ndarray[tuple[int], np.dtype[np.float64 | Any]])
 assert_type(PS_all.domain, _Ar_x_2)
 assert_type(PS_all.window, _Ar_x_2)
 assert_type(PS_all.symbol, str)
@@ -142,12 +142,12 @@ assert_type(PS_poly.roots(), _Ar_x_n)
 
 assert_type(
     PS_poly.linspace(),
-    tuple[_Ar_1d[np.float64 | np.complex128], _Ar_1d[np.float64 | np.complex128]],
+    tuple[_Ar_1d[np.float64 | Any], _Ar_1d[np.float64 | Any]],
 )
 
 assert_type(
     PS_poly.linspace(9),
-    tuple[_Ar_1d[np.float64 | np.complex128], _Ar_1d[np.float64 | np.complex128]],
+    tuple[_Ar_1d[np.float64 | Any], _Ar_1d[np.float64 | Any]],
 )
 
 assert_type(PS_cheb.fit(AR_c_co, AR_c_co, SC_i_co), npp.Chebyshev)
